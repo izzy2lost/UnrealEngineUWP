48138666119f3698085fd0ade49fecae8c99ba60
James Yarrow <jamesya@microsoft.com>
2017-04-25 14:08:20 -0700

    Fixing include for UWP socket subsystem

--- Engine/Source/Runtime/Sockets/Private/UWP/SocketSubsystemUWP.cpp
+++ Engine/Source/Runtime/Sockets/Private/UWP/SocketSubsystemUWP.cpp
@@ -1,7 +1,7 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 
-#include "SocketsPrivatePCH.h"
 #include "SocketSubsystemUWP.h"
+#include "SocketSubsystemModule.h"
 #include "ModuleManager.h"
 
 #include "AllowWindowsPlatformTypes.h"
