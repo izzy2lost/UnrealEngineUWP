b89bc242e9012e5de3ec563404cfc25dc7f99953
James Yarrow <jamesya@microsoft.com>
2017-04-25 17:09:17 -0700

    Adjust build rules for d3d12 to use the Win10 SDK directly when possible

--- Engine/Source/ThirdParty/Windows/DX12/DX12.Build.cs
+++ Engine/Source/ThirdParty/Windows/DX12/DX12.Build.cs
@@ -7,25 +7,30 @@ public class DX12 : ModuleRules
 	{
 		Type = ModuleType.External;
 
-// @ATG_CHANGE : BEGIN UWP support
+		// @ATG_CHANGE : BEGIN UWP support
 		string DirectXSDKDir = WindowsPlatform.ShouldUseWindowsSDK10(Target.Platform) ?
 			UEBuildConfiguration.UEThirdPartySourceDirectory + "Windows/DX12" :
 			UEBuildConfiguration.UEThirdPartySourceDirectory + "Windows/DirectX";
-// @ATG_CHANGE : END
+		// @ATG_CHANGE : END
+
+		// For d3dx12, which is not part of the SDK distribution
 		PublicSystemIncludePaths.Add(DirectXSDKDir + "/include");
 
-		if (Target.Platform == UnrealTargetPlatform.Win64)
+
+// @ATG_CHANGE : BEGIN UWP support
+		if (WindowsPlatform.ShouldUseWindowsSDK10(Target.Platform))
+		{
+			PublicSystemIncludePaths.Add(UEBuildConfiguration.UEThirdPartySourceDirectory + "Windows/DirectXLegacy/Include");
+
+			// Don't add library folders.  Get these from the SDK now that it has caught up.
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
 			PublicLibraryPaths.Add(DirectXSDKDir + "/Lib/x64");
 		}
 		else if (Target.Platform == UnrealTargetPlatform.Win32)
 		{
 			PublicLibraryPaths.Add(DirectXSDKDir + "/Lib/x86");
-		}
-// @ATG_CHANGE : BEGIN UWP support
-		if (WindowsPlatform.ShouldUseWindowsSDK10(Target.Platform))
-		{
-			PublicSystemIncludePaths.Add(UEBuildConfiguration.UEThirdPartySourceDirectory + "Windows/DirectXLegacy/Include");
 		}
 // @ATG_CHANGE : END
 
