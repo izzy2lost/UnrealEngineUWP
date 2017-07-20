18fa04debe1a4ecd363dca4103af73fa45504e0c
James Yarrow <jamesya@microsoft.com>
2017-07-20 13:56:06 -0700

    Fix bad reference to old name of path variable, causing copy to fail.  #134

--- GetUWPDependencies.ps1
+++ GetUWPDependencies.ps1
@@ -37,7 +37,7 @@ function Install-Package($pathToNuget, $packageName, $installLocation, $subPaths
 	New-Item -ItemType Directory $aliasPath -ErrorAction Ignore
 
 	# Iterate over the sub-directories provided and copy them into our UE tree
-	$subPaths | %{[System.IO.Path]::Combine($actualPath, $_)} | Copy-Item -Destination $aliasPath -Recurse -Container -ErrorAction Ignore
+	$subPaths | %{[System.IO.Path]::Combine($unpackedToPath, $_)} | Copy-Item -Destination $aliasPath -Recurse -Container #-ErrorAction Ignore
 }
 
 # Package versions.  Should match OnlineSubsystemLive.build.cs
