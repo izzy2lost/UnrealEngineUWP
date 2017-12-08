b372d09fcb3a301abf78cdbb8a60a3150b0d4ea1
James Yarrow <jamesya@microsoft.com>
2017-12-08 10:43:09 -0800

    Fix problems with generated resw format

--- Engine/Source/Programs/DotNETCommon/DotNETUtilities/ResXResource/UEResXWriter.cs
+++ Engine/Source/Programs/DotNETCommon/DotNETUtilities/ResXResource/UEResXWriter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -39,7 +39,10 @@ namespace Tools.DotNETUtilities
 			XmlElement Value = Document.CreateElement("value");
 			Value.InnerText = InValue;
 
-			XmlElement Data = Document.CreateElement("data");
+			// @ATG_CHANGE : BEGIN
+			// Typo fix: Use parameter so that headers have resheader element name 
+			XmlElement Data = Document.CreateElement(InRootName);
+			// @ATG_CHANGE : END
 
 			{
 				XmlAttribute Attr = Document.CreateAttribute("name");
