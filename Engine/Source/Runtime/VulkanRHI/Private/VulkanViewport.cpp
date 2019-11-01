87a4a641d61d7ca538c3f798319bb74755f071ec
Jonas Meyer <Jonas.Meyer@epicgames.com>
2019-11-01 04:54:25 -0400

    Do intiial reset of VkQueryPools. fixes one validation issue. Because this needs a command buffer, it suddenly gets rather involved. #jira UE-82600 #rb rolando.caloca #lockdown nick.penwarden
    [CL 9953265 by Jonas Meyer in 4.23 branch]

--- Engine/Source/Runtime/VulkanRHI/Private/VulkanViewport.cpp
+++ Engine/Source/Runtime/VulkanRHI/Private/VulkanViewport.cpp
@@ -12,6 +12,8 @@
 #include "HAL/PlatformAtomics.h"
 #include "Engine/RendererSettings.h"
 
+extern FVulkanCommandBufferManager* GVulkanCommandBufferManager;
+
 struct FRHICommandProcessDeferredDeletionQueue final : public FRHICommand<FRHICommandProcessDeferredDeletionQueue>
 {
 	FVulkanDevice* Device;
@@ -832,7 +834,7 @@ bool FVulkanViewport::Present(FVulkanCommandListContext* Context, FVulkanCmdBuff
 	}
 
 	CmdBuffer->End();
-
+	GVulkanCommandBufferManager->FlushResetQueryPools();
 	if (FVulkanPlatform::SupportsStandardSwapchain())
 	{
 		if (LIKELY(!bFailedToDelayAcquireBackbuffer))
