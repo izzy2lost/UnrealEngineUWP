9c357066e55a05e7a365adfe91d7ed23f52ae032
Jason Bestimt <jason.bestimt@epicgames.com>
2019-11-13 09:52:21 -0500

    Fix for PhysX builds on Linux
    #JIRA: UE-83160
    #CodeReview: ryan.vance
    #RB: ryan.vance
    #lockdown cristina.riveron
    
    [CL 10123331 by Jason Bestimt in 4.23 branch]

--- Engine/Source/ThirdParty/PhysX3/PhysX_3.4/Source/GeomUtils/src/pcm/GuPersistentContactManifold.h
+++ Engine/Source/ThirdParty/PhysX3/PhysX_3.4/Source/GeomUtils/src/pcm/GuPersistentContactManifold.h
@@ -38,10 +38,12 @@
 
 #define PCM_LOW_LEVEL_DEBUG	0
 
+#ifndef PX_CLANG 
 // @MIXEDREALITY_CHANGE : BEGIN
 // 4324 - structure was padded due to declspec(align())
 #pragma warning( disable : 4324)
 // @MIXEDREALITY_CHANGE : END
+#endif
 
 namespace physx
 {
@@ -852,6 +854,8 @@ PX_INLINE void MultiplePersistentContactManifold::toBuffer(PxU8*  PX_RESTRICT bu
 
 #endif
 
+#ifndef PX_CLANG 
 // @MIXEDREALITY_CHANGE : BEGIN
 #pragma warning( default : 4324)
 // @MIXEDREALITY_CHANGE : END
+#endif
