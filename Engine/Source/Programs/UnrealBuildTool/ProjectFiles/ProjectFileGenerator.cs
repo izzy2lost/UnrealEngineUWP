03d13e02090779dffcf9292fdafb3268413c750b
Ben Wright <emmettjnr@outlook.com>
2020-05-24 09:20:43 +1200

    Revert "4.23 UWP - Hack to generate projects with VS2019 regardless of default VS2017 setting"
    This reverts commit ccc16a214c56ade61c5a8c68cca8085c511dacc7.

--- Engine/Source/Programs/UnrealBuildTool/ProjectFiles/ProjectFileGenerator.cs
+++ Engine/Source/Programs/UnrealBuildTool/ProjectFiles/ProjectFileGenerator.cs
@@ -550,9 +550,7 @@ namespace UnrealBuildTool
 				}
 				else if (PreferredAccessor == "visualstudio2017")
 				{
-					// @UWP_CHANGE : BEGIN UWP support - hack to generate projects with VS2019 regardless of default VS2017 setting
-					Format = ProjectFileFormat.VisualStudio2019;
-					// @UWP_CHANGE
+					Format = ProjectFileFormat.VisualStudio2017;
 					return true;
 				}
 				else if (PreferredAccessor == "visualstudio2019")
