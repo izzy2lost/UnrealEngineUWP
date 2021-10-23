78dd7afcd275dd795eaf91f11d359d32001a54d1
Chris Caulfield <Chris.Caulfield@epicgames.com>
2021-10-23 15:49:04 -0400

    Merge from UE5 Chaos - fix zero friction contacts (was not updating anchor positions in this case)
    #rb cedric.caillaud
    #jira none
    
    #ushell-cherrypick of 17900745 by chris.caulfield
    
    [CL 17900802 by Chris Caulfield in 4.27-Chaos branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/Collision/SolverCollisionContainer.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/Collision/SolverCollisionContainer.cpp
@@ -126,7 +126,7 @@ namespace Chaos
 				Solver.InitMaterial(
 					ManifoldPointIndex,
 					WorldContactVelocityTargetNormal,
-					ManifoldPoint.bPotentialRestingContact,
+					ManifoldPoint.bPotentialRestingContact && (Solver.StaticFriction() > 0),
 					ManifoldPoint.StaticFrictionMax);
 			}
 		}
