8e34475250bfcfb7ab09d31c7a8226642959a503
cedric caillaud <cedric.caillaud@epicgames.com>
2021-10-27 15:37:59 -0400

    Fix issue where actors with geometry collection component are not properly ignored in queries - fix the wrong order of filters passed to the constructor of the proxy - make sure the query filter Word0 has the right value
    #rb brice.criswell
    #jira none
    
    [CL 17950111 by cedric caillaud in 4.27-Chaos branch]

--- Engine/Source/Runtime/Experimental/GeometryCollectionEngine/Private/GeometryCollection/GeometryCollectionComponent.cpp
+++ Engine/Source/Runtime/Experimental/GeometryCollectionEngine/Private/GeometryCollection/GeometryCollectionComponent.cpp
@@ -1993,6 +1993,13 @@ void UGeometryCollectionComponent::OnCreatePhysicsState()
 			InitialSimFilter = FilterData.SimFilter;
 			InitialQueryFilter = FilterData.QuerySimpleFilter;
 
+			// since InitBody has not been called on the bodyInstance, OwnerComponent is nullptr
+			// we then need to set the owner on the query filters to allow for actor filtering 
+			if (const AActor* Owner = GetOwner())
+			{
+				InitialQueryFilter.Word0 = Owner->GetUniqueID();
+			}
+
 			// Enable for complex and simple (no dual representation currently like other meshes)
 			InitialQueryFilter.Word3 |= (EPDF_SimpleCollision | EPDF_ComplexCollision);
 			InitialSimFilter.Word3 |= (EPDF_SimpleCollision | EPDF_ComplexCollision);
@@ -2212,7 +2219,7 @@ void UGeometryCollectionComponent::RegisterAndInitializePhysicsProxy()
 	};
 
 
-	PhysicsProxy = new FGeometryCollectionPhysicsProxy(this, *DynamicCollection, SimulationParameters, InitialQueryFilter, InitialSimFilter, InitFunc, CacheSyncFunc, FinalSyncFunc);
+	PhysicsProxy = new FGeometryCollectionPhysicsProxy(this, *DynamicCollection, SimulationParameters, InitialSimFilter, InitialQueryFilter, InitFunc, CacheSyncFunc, FinalSyncFunc);
 	FPhysScene_Chaos* Scene = GetInnerChaosScene();
 	Scene->AddObject(this, PhysicsProxy);
 
