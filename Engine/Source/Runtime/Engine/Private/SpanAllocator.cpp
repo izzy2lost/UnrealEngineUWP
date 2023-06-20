378e6e8b037ed81f131e27690c40f3eecb2a7556
jamie hayes <jamie.hayes@epicgames.com>
2023-06-20 19:38:48 -0400

    Fix a bug where FSpanAllocator::GetSparselyAllocatedSize was not taking pending free spans into account. Keep track of current total allocated count so that calling GetSparselyAllocatedSize can be efficient.
    #rb ola.olsson
    
    [CL 26139003 by jamie hayes in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/SpanAllocator.cpp
+++ Engine/Source/Runtime/Engine/Private/SpanAllocator.cpp
@@ -4,7 +4,8 @@
 #include "CoreGlobals.h"
 
 FSpanAllocator::FSpanAllocator(bool bInGrowOnly) 
-	: CurrentMaxSize(0)
+	: CurrentSize(0)
+	, CurrentMaxSize(0)
 	, PeakMaxSize(0)
 	, FirstNonEmptySpan(0)
 	, bGrowOnly(bInGrowOnly)
@@ -13,6 +14,7 @@ FSpanAllocator::FSpanAllocator(bool bInGrowOnly)
 
 void FSpanAllocator::Reset()
 {
+	CurrentSize = 0;
 	CurrentMaxSize = 0;
 	PeakMaxSize = 0;
 	FirstNonEmptySpan = 0;
@@ -22,6 +24,7 @@ void FSpanAllocator::Reset()
 
 void FSpanAllocator::Empty()
 {
+	CurrentSize = 0;
 	CurrentMaxSize = 0;
 	PeakMaxSize = 0;
 	FirstNonEmptySpan = 0;
