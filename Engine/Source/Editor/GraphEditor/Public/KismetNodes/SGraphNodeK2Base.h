c5763e7ee024d8f471fe746f585e080f3dea0931
ben hoffman <ben.hoffman@epicgames.com>
2023-06-26 09:13:49 -0400

    Add custom styleset support for K2Nodes
    #rb JeanMichel.Dignard
    
    #ushell-cherrypick of 26229658 by kristof.morva1
    
    [CL 26232169 by ben hoffman in ue5-main branch]

--- Engine/Source/Editor/GraphEditor/Public/KismetNodes/SGraphNodeK2Base.h
+++ Engine/Source/Editor/GraphEditor/Public/KismetNodes/SGraphNodeK2Base.h
@@ -22,6 +22,11 @@ struct FSlateBrush;
 class GRAPHEDITOR_API SGraphNodeK2Base : public SGraphNode
 {
 public:
+	SLATE_BEGIN_ARGS(SGraphNodeK2Base) { }
+		SLATE_ARGUMENT(TSharedPtr<ISlateStyle>, Style)
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs);
 
 	//~ Begin SGraphNode Interface
 	virtual void UpdateGraphNode() override;
@@ -49,6 +54,12 @@ protected :
 
 	/** Retrieves text to tack on to the top of the tooltip (above the standard text) */
 	FText GetToolTipHeading() const;
+
+	/** Returns the slate style to use for this node */
+	const ISlateStyle& GetStyleSet() const;
+
+	/** The slate style to use. We'll fall back to App Style if it's unset. */
+	TSharedPtr<ISlateStyle> Style;
 
 protected:
 	static const FLinearColor BreakpointHitColor;
