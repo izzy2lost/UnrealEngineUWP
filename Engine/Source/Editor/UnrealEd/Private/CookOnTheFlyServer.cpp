dda38a1cff5a2fad57bba72e71070ceedb010eb7
matt peters <Matt.Peters@epicgames.com>
2023-06-27 19:30:27 -0400

    Turn off -SkipOnlyEditorOnly (so we now keep unsolicited packages that are otherwise unreferenced) to see if it is the cause of missing system-generated files. #rb Zousar.Shaker #rnx
    [CL 26275339 by matt peters in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Private/CookOnTheFlyServer.cpp
+++ Engine/Source/Editor/UnrealEd/Private/CookOnTheFlyServer.cpp
@@ -6611,6 +6611,7 @@ void UCookOnTheFlyServer::SetInitializeConfigSettings(UE::Cook::FInitializeConfi
 	// Debugging hidden dependencies
 	bOnlyEditorOnlyDebug = FParse::Param(FCommandLine::Get(), TEXT("OnlyEditorOnlyDebug"));
 	bSkipOnlyEditorOnly = false;
+#if 0
 	GConfig->GetBool(TEXT("CookSettings"), TEXT("SkipOnlyEditorOnly"), bSkipOnlyEditorOnly, GEditorIni);
 	FString ParamText;
 	if (FParse::Value(FCommandLine::Get(), TEXT("-SkipOnlyEditorOnly="), ParamText))
@@ -6621,6 +6622,7 @@ void UCookOnTheFlyServer::SetInitializeConfigSettings(UE::Cook::FInitializeConfi
 	{
 		bSkipOnlyEditorOnly = true;
 	}
+#endif
 	bSkipOnlyEditorOnly |= bOnlyEditorOnlyDebug;
 	if (bSkipOnlyEditorOnly)
 	{
