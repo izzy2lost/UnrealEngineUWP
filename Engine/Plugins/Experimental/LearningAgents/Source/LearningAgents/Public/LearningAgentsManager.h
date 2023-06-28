cb00df9a614b26d8c7c77189009b69368dc09752
brendan mulcahy <brendan.mulcahy@epicgames.com>
2023-06-28 10:10:00 -0400

    Learning Agents: Add ChildCanTick to manager
    #rb none
    #rnx
    
    [CL 26284388 by brendan mulcahy in ue5-main branch]

--- Engine/Plugins/Experimental/LearningAgents/Source/LearningAgents/Public/LearningAgentsManager.h
+++ Engine/Plugins/Experimental/LearningAgents/Source/LearningAgents/Public/LearningAgentsManager.h
@@ -28,7 +28,7 @@ class ULearningAgentsManagerComponent;
  * If you have multiple different types of objects you want controlled by Learning Agents, you should consider creating
  * one agent manager per object type, rather than trying to share an agent manager.
  */
-UCLASS(BlueprintType, Blueprintable)
+UCLASS(BlueprintType, Blueprintable, meta=(ChildCanTick))
 class LEARNINGAGENTS_API ALearningAgentsManager : public AActor
 {
 	GENERATED_BODY()
