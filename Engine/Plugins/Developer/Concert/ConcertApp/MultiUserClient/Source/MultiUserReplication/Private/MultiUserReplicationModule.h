811ee6b080fa2a422bcd4b53f0813c2140c0164c
dominik peacock <dominik.peacock@geotech.one>
2023-07-06 07:44:54 -0400

    Add missing header guards
    #rb none
    [FYI] Jason.Walter
    
    [CL 26371984 by dominik peacock in ue5-main branch]

--- Engine/Plugins/Developer/Concert/ConcertApp/MultiUserClient/Source/MultiUserReplication/Private/MultiUserReplicationModule.h
+++ Engine/Plugins/Developer/Concert/ConcertApp/MultiUserClient/Source/MultiUserReplication/Private/MultiUserReplicationModule.h
@@ -1,5 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+#pragma once
+
 #include "Modules/ModuleManager.h"
 
 namespace UE::MultiUserReplication::Private
--- Engine/Plugins/Developer/Concert/ConcertApp/MultiUserClient/Source/MultiUserReplicationEditor/Private/MultiUserReplicationEditorModule.h
+++ Engine/Plugins/Developer/Concert/ConcertApp/MultiUserClient/Source/MultiUserReplicationEditor/Private/MultiUserReplicationEditorModule.h
@@ -1,5 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+#pragma once
+
 #include "IMultiUserReplicationEditorModule.h"
 #include "Misc/AssetCategoryPath.h"
 
