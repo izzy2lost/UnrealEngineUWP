ee1781fa87a3013e52040b37a6c8aee88a62db20
rune stubbe <Rune.Stubbe@epicgames.com>
2023-07-18 06:32:28 -0400

    Fixed issue where ExplicitTangent property on Nanite meshes would not be saved properly because of missing UPROPERTY annotations [FYI] graham.wihlidal, brian.karis #rb trivial
    [CL 26429864 by rune stubbe in ue5-main branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/EngineTypes.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/EngineTypes.h
@@ -2814,6 +2814,7 @@ struct FMeshNaniteSettings
 	uint8 bPreserveArea : 1;
 
 	/** Whether to store explicit tangents instead of using the implicitly derived ones. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = NaniteSettings)
 	uint8 bExplicitTangents : 1;
 
 	/** Position Precision. Step size is 2^(-PositionPrecision) cm. MIN_int32 is auto. */
