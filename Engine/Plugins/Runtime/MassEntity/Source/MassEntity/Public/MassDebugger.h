40d23f973c29c9d7551639c2b8e288b209fb45ff
yoan stamant <Yoan.Stamant@epicgames.com>
2023-07-19 16:42:29 -0400

    Fix CIS Issue 332632 [FYI] mieszko.zielinski #jira UE-190509
    [CL 26470258 by yoan stamant in ue5-main branch]

--- Engine/Plugins/Runtime/MassEntity/Source/MassEntity/Public/MassDebugger.h
+++ Engine/Plugins/Runtime/MassEntity/Source/MassEntity/Public/MassDebugger.h
@@ -19,7 +19,26 @@ struct FMassFragmentRequirements;
 struct FMassFragmentRequirementDescription;
 enum class EMassFragmentAccess : uint8;
 enum class EMassFragmentPresence : uint8;
+#endif // WITH_MASSENTITY_DEBUG
 
+namespace UE::Mass::Debug
+{
+	struct FArchetypeStats
+	{
+		/** Number of active entities of the archetype. */
+		int32 EntitiesCount = 0;
+		/** Number of entities that fit per chunk. */
+		int32 EntitiesCountPerChunk = 0;
+		/** Number of allocated chunks. */
+		int32 ChunksCount = 0;
+		/** Total amount of memory taken by this archetype */
+		SIZE_T AllocatedSize = 0;
+		/** Total amount of memory needed by a single entity */
+		int32 BytesPerEntity = 0;
+	};
+} // namespace UE::Mass::Debug
+
+#if WITH_MASSENTITY_DEBUG
 namespace UE::Mass::Debug
 {
 	struct MASSENTITY_API FQueryRequirementsView
@@ -37,20 +56,6 @@ namespace UE::Mass::Debug
 
 	FString DebugGetFragmentAccessString(EMassFragmentAccess Access);
 	MASSENTITY_API extern void DebugOutputDescription(TConstArrayView<UMassProcessor*> Processors, FOutputDevice& Ar);
-
-	struct FArchetypeStats
-	{
-		/** Number of active entities of the archetype. */
-		int32 EntitiesCount = 0;
-		/** Number of entities that fit per chunk. */
-		int32 EntitiesCountPerChunk = 0;
-		/** Number of allocated chunks. */
-		int32 ChunksCount = 0;
-		/** Total amount of memory taken by this archetype */
-		SIZE_T AllocatedSize = 0;
-		/** Total amount of memory needed by a single entity */
-		int32 BytesPerEntity = 0;
-	};
 
 	MASSENTITY_API extern bool HasDebugEntities();
 	MASSENTITY_API extern bool IsDebuggingSingleEntity();
