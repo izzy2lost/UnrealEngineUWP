ea8344fcf4753cc96d9141e0b0800bd2df730ba3
Ben Marsh <Ben.Marsh@epicgames.com>
2023-07-21 08:14:48 -0400

    Horde: Missing file.
    [CL 26508369 by Ben Marsh in ue5-main branch]

--- Engine/Source/Programs/Shared/EpicGames.Horde/Compute/ComputeTransport.cs
+++ Engine/Source/Programs/Shared/EpicGames.Horde/Compute/ComputeTransport.cs
@@ -11,51 +11,39 @@ namespace EpicGames.Horde.Compute
 	/// <summary>
 	/// Low-level interface for transferring data
 	/// </summary>
-	public interface IComputeTransport
+	public abstract class ComputeTransport
 	{
-		/// <summary>
-		/// Position in the stream; used for debugging
-		/// </summary>
-		long Position { get; }
-
 		/// <summary>
 		/// Reads data from the underlying transport into an output buffer
 		/// </summary>
 		/// <param name="buffer">Buffer to read into</param>
 		/// <param name="cancellationToken">Cancellation token for the operation</param>
-		ValueTask<int> ReadPartialAsync(Memory<byte> buffer, CancellationToken cancellationToken);
+		public abstract ValueTask<int> ReadPartialAsync(Memory<byte> buffer, CancellationToken cancellationToken);
 
 		/// <summary>
 		/// Writes data to the underlying transport
 		/// </summary>
 		/// <param name="buffer">Buffer to be written</param>
 		/// <param name="cancellationToken">Cancellation token for the operation</param>
-		ValueTask WriteAsync(ReadOnlySequence<byte> buffer, CancellationToken cancellationToken);
+		public abstract ValueTask WriteAsync(ReadOnlySequence<byte> buffer, CancellationToken cancellationToken);
 
 		/// <summary>
 		/// Indicate that all data has been written to the transport layer, and that there will be no more calls to <see cref="WriteAsync(ReadOnlySequence{Byte}, CancellationToken)"/>
 		/// </summary>
 		/// <param name="cancellationToken">Cancellation token for the operation</param>
-		ValueTask MarkCompleteAsync(CancellationToken cancellationToken);
-	}
+		public abstract ValueTask MarkCompleteAsync(CancellationToken cancellationToken);
 
-	/// <summary>
-	/// Extension methods for <see cref="IComputeTransport"/>
-	/// </summary>
-	public static class ComputeTransportExtensions
-	{
 		/// <summary>
 		/// Fill the given buffer with data
 		/// </summary>
-		/// <param name="transport">Transport object</param>
 		/// <param name="buffer">Buffer to read into</param>
 		/// <param name="cancellationToken">Cancellation token for the operation</param>
-		public static async ValueTask ReadFullAsync(this IComputeTransport transport, Memory<byte> buffer, CancellationToken cancellationToken)
+		public async ValueTask ReadFullAsync(Memory<byte> buffer, CancellationToken cancellationToken)
 		{
 			int read = 0;
 			while (read < buffer.Length)
 			{
-				int partialRead = await transport.ReadPartialAsync(buffer.Slice(read, buffer.Length - read), cancellationToken);
+				int partialRead = await ReadPartialAsync(buffer.Slice(read, buffer.Length - read), cancellationToken);
 				if (partialRead == 0)
 				{
 					throw new EndOfStreamException();
@@ -67,19 +55,18 @@ namespace EpicGames.Horde.Compute
 		/// <summary>
 		/// Fill the given buffer with data
 		/// </summary>
-		/// <param name="transport">Transport object</param>
 		/// <param name="buffer">Buffer to read into</param>
 		/// <param name="cancellationToken">Cancellation token for the operation</param>
-		public static async ValueTask<bool> ReadOptionalAsync(this IComputeTransport transport, Memory<byte> buffer, CancellationToken cancellationToken)
+		public async ValueTask<bool> ReadOptionalAsync(Memory<byte> buffer, CancellationToken cancellationToken)
 		{
-			int read = await transport.ReadPartialAsync(buffer, cancellationToken);
+			int read = await ReadPartialAsync(buffer, cancellationToken);
 			if (read == 0)
 			{
 				return false;
 			}
 			if (read < buffer.Length)
 			{
-				await transport.ReadFullAsync(buffer.Slice(read), cancellationToken);
+				await ReadFullAsync(buffer.Slice(read), cancellationToken);
 			}
 			return true;
 		}
@@ -87,11 +74,10 @@ namespace EpicGames.Horde.Compute
 		/// <summary>
 		/// Writes data to the underlying transport
 		/// </summary>
-		/// <param name="transport">Transport instance</param>
 		/// <param name="buffer">Buffer to be written</param>
 		/// <param name="cancellationToken">Cancellation token for the operation</param>
-		public static ValueTask WriteAsync(this IComputeTransport transport, ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken)
-			=> transport.WriteAsync(new ReadOnlySequence<byte>(buffer), cancellationToken);
+		public ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken)
+			=> WriteAsync(new ReadOnlySequence<byte>(buffer), cancellationToken);
 	}
 }
 
