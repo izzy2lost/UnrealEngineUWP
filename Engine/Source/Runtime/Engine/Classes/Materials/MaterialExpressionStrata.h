eab670e3384f31607d1860df4b4edc807e1cbf2f
sebastien hillaire <Sebastien.Hillaire@epicgames.com>
2023-07-22 06:34:53 -0400

    Substrate - removed invalid comment
    #rb none
    
    [CL 26534995 by sebastien hillaire in ue5-main branch]

--- Engine/Source/Runtime/Engine/Classes/Materials/MaterialExpressionStrata.h
+++ Engine/Source/Runtime/Engine/Classes/Materials/MaterialExpressionStrata.h
@@ -1000,7 +1000,7 @@ class UMaterialExpressionStrataTransmittanceToMFP : public UMaterialExpressionSt
 	FExpressionInput TransmittanceColor;
 
 	/**
-	* The desired thickness in centimeter. This can be set lower than 0.1mm (= 0.01cm) to enable the Thin lighting model on the slab node for instance.
+	* The desired thickness in centimeter.
 	* Another use case example: this node output called thickness can be modulated before it is plugged in a slab node, this can be used to achieve simple scattering/transmittance variation of the same material.
 	*/
 	UPROPERTY()
