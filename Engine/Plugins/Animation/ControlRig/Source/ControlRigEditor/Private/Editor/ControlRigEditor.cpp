ee71109e8224e3ad1e45b60d2f30a1db1a0d431a
helge mathee <Helge.Mathee@epicgames.com>
2023-07-24 11:50:17 -0400

    ControlRig: Fix crash on close
    #rb jaime.cifuentes
    #jira UE-187997
    
    [CL 26544633 by helge mathee in ue5-main branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRigEditor/Private/Editor/ControlRigEditor.cpp
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRigEditor/Private/Editor/ControlRigEditor.cpp
@@ -145,12 +145,6 @@ FControlRigEditor::~FControlRigEditor()
 		}
 	}
 
-	if (UWorld* PreviewWorld = GetPersonaToolkit()->GetPreviewScene()->GetWorld())
-	{
-		PreviewWorld->MarkObjectsPendingKill();
-		PreviewWorld->MarkAsGarbage();
-	}
-
 	if (PersonaToolkit.IsValid())
 	{
 		constexpr bool bSetPreviewMeshInAsset = false;
