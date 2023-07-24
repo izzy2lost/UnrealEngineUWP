12f70e8c70c2b36331b4de4748b4318c2514e5fe
huw bowles <huw.bowles@epicgames.com>
2023-07-24 12:41:09 -0400

    [PCG] Get Bounds node to compute bounds of input spatial data
    #rb julien.lheureux
    [FYI] antoine.guilbaud, jeansebastian.guay
    
    [CL 26547405 by huw bowles in ue5-main branch]

---
+++ Engine/Plugins/Experimental/PCG/Source/PCG/Public/Elements/PCGGetBounds.h
@@ -0,0 +1,37 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "PCGElement.h"
+#include "PCGSettings.h"
+
+#include "PCGGetBounds.generated.h"
+
+/**
+ * Computes the bounds of the inputs as attributes.
+ */
+UCLASS(BlueprintType, ClassGroup = (Procedural))
+class PCG_API UPCGGetBoundsSettings : public UPCGSettings
+{
+	GENERATED_BODY()
+
+public:
+	//~Begin UPCGSettings interface
+#if WITH_EDITOR
+	virtual FName GetDefaultNodeName() const override { return TEXT("GetBounds"); }
+	virtual FText GetDefaultNodeTitle() const override;
+	virtual FText GetNodeTooltipText() const override;
+	virtual EPCGSettingsType GetType() const override { return EPCGSettingsType::Spatial; }
+#endif
+
+protected:
+	virtual TArray<FPCGPinProperties> InputPinProperties() const override;
+	virtual TArray<FPCGPinProperties> OutputPinProperties() const override;
+	virtual FPCGElementPtr CreateElement() const override;
+	//~End UPCGSettings interface
+};
+
+class FPCGGetBoundsElement : public FSimplePCGElement
+{
+protected:
+	virtual bool ExecuteInternal(FPCGContext* Context) const override;
+};
