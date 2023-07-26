9a14af365756c0fef9c36a1298f407a43f046f47
eric mcdaniel <Eric.Mcdaniel@epicgames.com>
2023-07-26 17:41:30 -0400

    Increase network replication priority on AGameStateBase to ensure game state transitions reliably occur on the clients
    - this will indirectly prioritize these actors for inclusion in replays which is where the current issue occurs
      - increasing the net priority vs just the replay priority to safeguard against potential similar issues under high load in non-replay scenarios
      - NetPriority of 10 chosen as this is utilized by another game object to workaround similar issues given existing comments
    
    - testing by Matt Breindel
      - generated project specific replay without change on console platform which fails to include AGameStateBase::OnRep_ReplicatedHasBegunPlay
      - with this change in place, replay was again generated and AGameStateBase::OnRep_ReplicatedHasBegunPlay replication was included in the replay
    
    #rb Matt.Breindel, Ryan.Gerleve
    
    [CL 26623188 by eric mcdaniel in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/GameStateBase.cpp
+++ Engine/Source/Runtime/Engine/Private/GameStateBase.cpp
@@ -37,6 +37,8 @@ AGameStateBase::AGameStateBase(const FObjectInitializer& ObjectInitializer)
 
 	SumServerWorldTimeSecondsDelta = 0.0;
 	NumServerWorldTimeSecondsDeltas = 0;
+
+	NetPriority = 10.f;	// We need to prioritize updates to ensure that game state transitions reliably occur on the clients.
 }
 
 const AGameModeBase* AGameStateBase::GetDefaultGameMode() const
