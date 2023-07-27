5baf1889393508c02e301c322316726dbd8ecb7d
richard malo <Richard.Malo@epicgames.com>
2023-07-27 07:07:59 -0400

    WorldPartition: Added IsListedInSceneOutliner in WorldPartitionActorDesc and use it to hide unloaded ActorDescItems in the World Outliner. It is also used to hide actor bounds in World Partition Editor. #rb patrick.enfedaque #jira UE-191295
    [CL 26636014 by richard malo in ue5-main branch]

--- Engine/Source/Editor/WorldPartitionEditor/Private/WorldPartition/SWorldPartitionEditorGrid2D.cpp
+++ Engine/Source/Editor/WorldPartitionEditor/Private/WorldPartition/SWorldPartitionEditorGrid2D.cpp
@@ -1067,7 +1067,7 @@ void SWorldPartitionEditorGrid2D::Tick(const FGeometry& AllottedGeometry, const 
 	{
 		if (bShowActors)
 		{
-			if (!GetWorldPartition()->IsStreamingEnabled() || ActorDesc->GetIsSpatiallyLoaded())
+			if (ActorDesc->IsListedInSceneOutliner() && (!GetWorldPartition()->IsStreamingEnabled() || ActorDesc->GetIsSpatiallyLoaded()))
 			{
 				ShownActorGuids.Add(ActorDesc->GetGuid());
 			}
