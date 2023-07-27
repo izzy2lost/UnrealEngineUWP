438358551c273230f4626b8a570511fb55831c35
peter tarasenko <p.tarasenko@pixelalabs.com>
2023-07-27 17:30:31 -0400

    nDisplay: Fixed conflict when merging CL 26622815.
    #rb none
    
    [CL 26657036 by peter tarasenko in ue5-main branch]

--- Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterProjection/Private/Policy/Camera/DisplayClusterProjectionCameraPolicy.cpp
+++ Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterProjection/Private/Policy/Camera/DisplayClusterProjectionCameraPolicy.cpp
@@ -118,10 +118,10 @@ bool FDisplayClusterProjectionCameraPolicy::GetProjectionMatrix(IDisplayClusterV
 	if (InViewport)
 	{
 		// Support inner camera custom frustum
-		const float HalfFOVH = 0.5 * ClampedCameraFOVDegrees;
-		const float HalfFOVV = HalfFOVH / CameraAspectRatio;
+		const float HalfFOVH = ZNear * FMath::Tan(FMath::DegreesToRadians(0.5 * ClampedCameraFOV));
+		const float HalfFOVV = HalfFOVH / InCameraAspectRatio;
 
-		InViewport->CalculateProjectionMatrix(InContextNum, -HalfFOVH, HalfFOVH, HalfFOVV, -HalfFOVV, ZNear, ZFar, true);
+		InViewport->CalculateProjectionMatrix(InContextNum, -HalfFOVH, HalfFOVH, HalfFOVV, -HalfFOVV, ZNear, ZFar, false);
 		OutPrjMatrix = InViewport->GetContexts()[InContextNum].ProjectionMatrix;
 	}
 	else
