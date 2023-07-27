3087d932158e6aafb2e864b0981777f8dd16644f
adrien logut <adrien.logut@epicgames.com>
2023-07-27 14:35:51 -0400

    [PCG] Fix outstanding issues in the latest tracking changes
    * Landscape changes now correctly dirty the component, fixing issues where the component was not refreshing after a landscape move or sculpting
    * Actor Dependencies are now ignoreing tracking on PCGComponent and PCGGraphInterface, as they are already tracked by the PCGComponent itself
    * Fix an issue where actors already tracked were not registering new components.
    * Add detection of StaticMesh/Texture compilation result to avoid dirtying, especially on load. Might need a revisit.
    * Add "PanicButton" command to disable object tracking if it becomes unstable.
    * Tracking is ignoring "by default" any changes to a PCG Component and PCG Graph, since they are already tracked by the PCG Component
    * Tracking will also register to generate/cleanup events from the component.
    
    #rnx
    #jira UE-190894, FORT-632465, UE-191348
    #rb julien.lheureux
    
    [CL 26648701 by adrien logut in ue5-main branch]

--- Engine/Plugins/Experimental/PCG/Source/PCG/Private/PCGComponent.cpp
+++ Engine/Plugins/Experimental/PCG/Source/PCG/Private/PCGComponent.cpp
@@ -2105,6 +2105,12 @@ bool UPCGComponent::DirtyTrackedActor(AActor* InActor, bool bIntersect, const TS
 		}
 	}
 
+	// Special case for landscape, we should dirty.
+	if (ShouldTrackLandscape() && InActor->IsA<ALandscapeProxy>())
+	{
+		bWasDirtied = true;
+	}
+
 	return bWasDirtied;
 }
 
