091879c83a598178f3306b8fe9d68983dd09ee03
ruslan idrisov <ruslan.idrisov@epicgames.com>
2023-07-27 11:15:48 -0400

    Color Correction Regions: Fixed bug: When CCR is part of sublevel it's state is not transfered and it could remain visible/hidden despite sublevel that contains CCR having an opposite state.
    #jira UE-191290
    #rb Eric.RenaudHoude
    
    [CL 26641335 by ruslan idrisov in ue5-main branch]

--- Engine/Plugins/Experimental/ColorCorrectRegions/Source/ColorCorrectRegions/Public/ColorCorrectRegion.h
+++ Engine/Plugins/Experimental/ColorCorrectRegions/Source/ColorCorrectRegions/Public/ColorCorrectRegion.h
@@ -226,6 +226,11 @@ public:
 		return ColorCorrectRenderProxy;
 	};
 
+	/**
+	* Copy state required for rendering to be consumed by Scene view extension.
+	*/
+	void TransferState();
+
 private:
 
 #if WITH_METADATA
@@ -239,11 +244,6 @@ private:
 	* ActorListChangeType represents EPropertyChangeType
 	*/
 	void HandleAffectedActorsPropertyChange(uint32 ActorListChangeType);
-
-	/**
-	* Copy state required for rendering to be consumed by Scene view extension.
-	*/
-	void TransferState();
 
 #if WITH_EDITOR
 	/** Called when a sequencer has its time changed. */
