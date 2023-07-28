c2ca810ecc682f9a9b1bb92ca048d905daa553c2
jordan hoffmann <jordan.hoffmann@epicgames.com>
2023-07-27 20:40:52 -0400

    [CISFIX] botched merge was backed out in Dev-FN-26 but the backout got merged into FN-Main when it shouldn't have returning files to their state in 26659901
    [CL 26662683 by jordan hoffmann in ue5-main branch]

--- Engine/Source/Editor/PropertyEditor/Private/SDetailsSplitter.cpp
+++ Engine/Source/Editor/PropertyEditor/Private/SDetailsSplitter.cpp
@@ -345,6 +345,16 @@ void SDetailsSplitter::AddSlot(const FSlot::FSlotArguments& SlotArgs, int32 Inde
 	}, Index);
 }
 
+SDetailsSplitter::FPanel& SDetailsSplitter::GetPanel(int32 Index)
+{
+	return Panels[Index];
+}
+
+void SDetailsSplitter::AddHighlights(const TMap<FString, TMap<FPropertySoftPath, FLinearColor>>& Highlights)
+{
+	CustomHighlights = Highlights;
+}
+
 SDetailsSplitter::FSlot::FSlotArguments SDetailsSplitter::Slot()
 {
 	return FSlot::FSlotArguments(MakeUnique<FSlot>());
@@ -495,7 +505,7 @@ FReply SDetailsSplitter::OnMouseMove(const FGeometry& MyGeometry, const FPointer
 		
 		const int32 RightIndex = LeftIndex + 1;
 		const FPanel& RightPanel = Panels[RightIndex];
-		if (LeftPanel.IsReadonly.Get(false) && RightPanel.IsReadonly.Get(false))
+		if (LeftPanel.IsReadonly.Get(true) && RightPanel.IsReadonly.Get(true))
 		{
 			continue;
 		}
@@ -529,7 +539,7 @@ FReply SDetailsSplitter::OnMouseMove(const FGeometry& MyGeometry, const FPointer
 					return ETreeTraverseControl::Continue;
 				}
 
-				if (LeftPropertyRect.IsValid() && !RightPanel.IsReadonly.Get(false))
+				if (LeftPropertyRect.IsValid() && !RightPanel.IsReadonly.Get(true))
 				{
 					const FSlateRect CopyButtonZoneLeftToRight = FSlateRect(
 						LeftPropertyRect.Right,
@@ -553,7 +563,7 @@ FReply SDetailsSplitter::OnMouseMove(const FGeometry& MyGeometry, const FPointer
 					}
 				}
 				
-				if (RightPropertyRect.IsValid() && !LeftPanel.IsReadonly.Get(false))
+				if (RightPropertyRect.IsValid() && !LeftPanel.IsReadonly.Get(true))
 				{
 					const FSlateRect CopyButtonZoneRightToLeft = FSlateRect(
 						RightPropertyRect.Left - 15.f,
