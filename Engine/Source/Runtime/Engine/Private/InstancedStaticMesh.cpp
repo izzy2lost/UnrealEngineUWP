4938b9e11577baa1a66435959e0a2decc2f2b34c
zach bethel <zach.bethel@epicgames.com>
2023-07-31 18:05:52 -0400

    Moved instanced static mesh initialization work to the constructor.  - CreateRenderThreadResources is meant for RHI resource creation and not scene proxy setup.  - This change is necessary to move CreateRenderThreadResources to happen during UpdateAllPrimitiveSceneInfos.
    [CL 26723832 by zach bethel in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/InstancedStaticMesh.cpp
+++ Engine/Source/Runtime/Engine/Private/InstancedStaticMesh.cpp
@@ -1902,29 +1902,7 @@ void FInstancedStaticMeshSceneProxy::SetupProxy(UInstancedStaticMeshComponent* I
 				);
 			}
 		}
-	}
-}
 
-
-void FInstancedStaticMeshSceneProxy::CreateRenderThreadResources(FRHICommandListBase& RHICmdList)
-{
-	FStaticMeshSceneProxy::CreateRenderThreadResources(RHICmdList);
-
-	const bool bCanUseGPUScene = UseGPUScene(GetScene().GetShaderPlatform(), GetScene().GetFeatureLevel());
-	
-	// Flush upload of GPU data for ISM/HISM
-	if (ensure(InstancedRenderData.PerInstanceRenderData.IsValid()))
-	{
-		FStaticMeshInstanceBuffer& InstanceBuffer = InstancedRenderData.PerInstanceRenderData->InstanceBuffer;
-		if (!bCanUseGPUScene)
-		{
-			InstanceBuffer.FlushGPUUpload(RHICmdList);
-		}
-	}
-
-	if (bCanUseGPUScene)
-	{
-		bSupportsInstanceDataBuffer = true;
 		// TODO: can the PerInstanceRenderData ever not be valid here?
 		if (ensure(InstancedRenderData.PerInstanceRenderData.IsValid()))
 		{
@@ -1986,6 +1964,23 @@ void FInstancedStaticMeshSceneProxy::CreateRenderThreadResources(FRHICommandList
 #endif
 				}
 			}
+		}
+	}
+}
+
+void FInstancedStaticMeshSceneProxy::CreateRenderThreadResources(FRHICommandListBase& RHICmdList)
+{
+	FStaticMeshSceneProxy::CreateRenderThreadResources(RHICmdList);
+
+	// Flush upload of GPU data for ISM/HISM
+	if (ensure(InstancedRenderData.PerInstanceRenderData.IsValid()))
+	{
+		const bool bCanUseGPUScene = UseGPUScene(GetScene().GetShaderPlatform(), GetScene().GetFeatureLevel());
+
+		if (!bCanUseGPUScene)
+		{
+			FStaticMeshInstanceBuffer& InstanceBuffer = InstancedRenderData.PerInstanceRenderData->InstanceBuffer;
+			InstanceBuffer.FlushGPUUpload(RHICmdList);
 		}
 	}
 
