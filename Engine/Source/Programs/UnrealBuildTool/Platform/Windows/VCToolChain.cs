da089d0875d4110ce04611d30e07cd5c7ec0ae24
joe kirchoff <joe.kirchoff@epicgames.com>
2023-07-31 18:02:16 -0400

    Support for .natstepfilter
    #rnx
    #jira UE-190863
    #jira UE-191341
    
    #ushell-cherrypick of 26715542 by Joe.Kirchoff
    
    [CL 26723680 by joe kirchoff in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/Windows/VCToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Windows/VCToolChain.cs
@@ -1389,7 +1389,7 @@ namespace UnrealBuildTool
 
 		public override FileItem? CopyDebuggerVisualizer(FileItem SourceFile, DirectoryReference IntermediateDirectory, IActionGraphBuilder Graph)
 		{
-			if (SourceFile.HasExtension(".natvis"))
+			if (SourceFile.HasExtension(".natvis") || SourceFile.HasExtension(".natstepfilter"))
 			{
 				FileItem IntermediateFile = FileItem.GetItemByFileReference(FileReference.Combine(IntermediateDirectory, SourceFile.Name));
 				Graph.CreateCopyAction(SourceFile, IntermediateFile);
@@ -1400,7 +1400,7 @@ namespace UnrealBuildTool
 
 		public override FileItem? LinkDebuggerVisualizer(FileItem SourceFile, DirectoryReference IntermediateDirectory)
 		{
-			if (SourceFile.HasExtension(".natvis"))
+			if (SourceFile.HasExtension(".natvis") || SourceFile.HasExtension(".natstepfilter"))
 			{
 				FileItem IntermediateFile = FileItem.GetItemByFileReference(FileReference.Combine(IntermediateDirectory, SourceFile.Name));
 				return IntermediateFile;
