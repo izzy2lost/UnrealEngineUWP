37b316b3007e005128ec4cf8b48ea9f9b4c68f1d
andrew scheidecker <andrew.scheidecker@epicgames.com>
2023-07-31 09:14:11 -0400

    Fix serialization of FP specials with the UE structured archive JSON formatter: Like JS, JSON does not directly support FP specials in its numeric literal syntax. However, JS has Infinity and NaN global variables, and JSON does not have anything like that. To work around the omission, this change makes the JSON formatter encode FP specials as a tagged string in the form:     "Number:+inf"     "Number:-inf"     "Number:-nan:0x123"     "Number:+nan:0xabc" This change also gets rid of the special case that tried to print floats as integers as it invoked UB to cast an out-of-range float to an int. #rb Francis.Hurteau
    [CL 26704226 by andrew scheidecker in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Private/Serialization/Formatters/JsonArchiveOutputFormatter.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/Serialization/Formatters/JsonArchiveOutputFormatter.cpp
@@ -10,6 +10,7 @@
 #include "UObject/SoftObjectPath.h"
 #include "UObject/LazyObjectPtr.h"
 #include "UObject/ObjectPtr.h"
+#include <cinttypes>
 
 #if WITH_TEXT_ARCHIVE_SUPPORT
 
@@ -248,11 +249,7 @@ void FJsonArchiveOutputFormatter::Serialize(int64& Value)
 
 void FJsonArchiveOutputFormatter::Serialize(float& Value)
 {
-	if((float)(int)Value == Value)
-	{
-		WriteValue(LexToString((int)Value));
-	}
-	else
+	if(FPlatformMath::IsFinite(Value))
 	{
 		FString String = FString::Printf(TEXT("%.17g"), Value);
 #if DO_GUARD_SLOW
@@ -262,15 +259,22 @@ void FJsonArchiveOutputFormatter::Serialize(float& Value)
 #endif
 		WriteValue(String);
 	}
+	else if(FPlatformMath::IsNaN(Value))
+	{
+		const uint32 ValueAsInt = BitCast<uint32>(Value);
+		const bool bIsNegative = !!(ValueAsInt & 0x80000000);
+		const uint32 Significand = ValueAsInt & 0x007fffff;
+		WriteValue(FString::Printf(TEXT("\"Number:%snan:0x%" PRIx32 "\""), bIsNegative ? TEXT("-") : TEXT("+"), Significand));
+	}
+	else
+	{
+		WriteValue(Value < 0.0f ? TEXT("\"Number:-inf\"") : TEXT("\"Number:+inf\""));
+	}
 }
 
 void FJsonArchiveOutputFormatter::Serialize(double& Value)
 {
-	if((double)(int)Value == Value)
-	{
-		WriteValue(LexToString((int)Value));
-	}
-	else
+	if(FPlatformMath::IsFinite(Value))
 	{
 		FString String = FString::Printf(TEXT("%.17g"), Value);
 #if DO_GUARD_SLOW
@@ -279,6 +283,17 @@ void FJsonArchiveOutputFormatter::Serialize(double& Value)
 		check(RoundTripped == Value);
 #endif
 		WriteValue(String);
+	}
+	else if(FPlatformMath::IsNaN(Value))
+	{
+		const uint64 ValueAsInt = BitCast<uint64>(Value);
+		const bool bIsNegative = !!(ValueAsInt & 0x8000000000000000);
+		const uint64 Significand = ValueAsInt & 0x000fffffffffffff;
+		WriteValue(FString::Printf(TEXT("\"Number:%snan:0x%" PRIx64 "\""), bIsNegative ? TEXT("-") : TEXT("+"), Significand));
+	}
+	else
+	{
+		WriteValue(Value < 0.0 ? TEXT("\"Number:-inf\"") : TEXT("\"Number:+inf\""));
 	}
 }
 
