b4bbc6d017d4aca002bf6fad63b998bb93d190a1
vitalii boiko <Vitalii.Boiko@xa.epicgames.com>
2023-08-01 16:34:28 -0400

    nDisplay: added missing cvar value to control SecondaryScreenPercentage
    #jira UE-189952
    #rb Alejandro.Arango
    
    [CL 26753603 by vitalii boiko in ue5-main branch]

--- Engine/Plugins/Runtime/nDisplay/Source/DisplayCluster/Private/Game/EngineClasses/Basics/DisplayClusterViewportClient.cpp
+++ Engine/Plugins/Runtime/nDisplay/Source/DisplayCluster/Private/Game/EngineClasses/Basics/DisplayClusterViewportClient.cpp
@@ -719,7 +719,7 @@ void UDisplayClusterViewportClient::Draw(FViewport* InViewport, FCanvas* SceneCa
 				}
 
 				// Set up secondary resolution fraction for the view family.
-				if (!bStereoRendering && ViewFamily.SupportsScreenPercentage())
+				if (ViewFamily.SupportsScreenPercentage())
 				{
 					float CustomSecondaryScreenPercentage = IConsoleManager::Get().FindConsoleVariable(TEXT("r.SecondaryScreenPercentage.GameViewport"), false)->GetFloat();
 					if (CustomSecondaryScreenPercentage > 0.0)
@@ -778,7 +778,7 @@ void UDisplayClusterViewportClient::Draw(FViewport* InViewport, FCanvas* SceneCa
 					float CustomBufferRatio = DCViewFamily.CustomBufferRatio;
 
 					float GlobalResolutionFraction = 1.0f;
-					float SecondaryScreenPercentage = 1.0f;
+					float SecondaryScreenPercentage = ViewFamily.SecondaryViewFraction;
 
 					if (ViewFamily.EngineShowFlags.ScreenPercentage)
 					{
