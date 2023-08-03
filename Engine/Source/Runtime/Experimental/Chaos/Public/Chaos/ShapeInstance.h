9352e8a6cbdfea0f8db82553b7e512c69f821e22
dmytro vovk <dmytro.vovk@epicgames.com>
2023-08-03 16:36:48 -0400

    Packed Chaos::FShapeInstanceProxy to fit into 192b MB3 bin #rb Benn.Gallagher
    [CL 26826110 by dmytro vovk in ue5-main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/ShapeInstance.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/ShapeInstance.h
@@ -35,7 +35,7 @@ namespace Chaos
 	class FPerShapeData
 	{
 	protected:
-		enum class EPerShapeDataType : uint8
+		enum class EPerShapeDataType : uint32
 		{
 			Proxy,
 			Sim,
@@ -201,9 +201,10 @@ namespace Chaos
 
 		virtual void SerializeMaterials(FChaosArchive& Ar) = 0;
 
-		EPerShapeDataType Type;
-		uint8 bIsSingleMaterial : 1;	// For use by FShapeInstance (here because the space is available for free)
-		int32 ShapeIdx;
+		EPerShapeDataType Type : 2;
+		uint32 bIsSingleMaterial : 1;	// For use by FShapeInstance (here because the space is available for free)
+		uint32 ShapeIdx : 29;
+		FShapeDirtyFlags DirtyFlags;	// For use by FShapeInstanceProxy as there's 4 bytes of padding here
 		FImplicitObjectPtr Geometry;
 		TAABB<FReal, 3> WorldSpaceInflatedShapeBounds;
 	};
@@ -458,7 +459,6 @@ namespace Chaos
 		explicit FShapeInstanceProxy(int32 InShapeIdx)
 			: FPerShapeData(EPerShapeDataType::Proxy, InShapeIdx)
 			, Proxy(nullptr)
-			, DirtyFlags()
 			, CollisionData()
 			, Materials()
 		{
@@ -468,7 +468,6 @@ namespace Chaos
 		FShapeInstanceProxy(int32 InShapeIdx, TSerializablePtr<FImplicitObject> InGeometry)
 			: FPerShapeData(EPerShapeDataType::Proxy, InShapeIdx)
 			, Proxy(nullptr)
-			, DirtyFlags()
 			, CollisionData()
 			, Materials()
 		{
@@ -478,7 +477,6 @@ namespace Chaos
 		FShapeInstanceProxy(int32 InShapeIdx, const FImplicitObjectPtr& InGeometry)
 			: FPerShapeData(EPerShapeDataType::Proxy, InShapeIdx, InGeometry)
 			, Proxy(nullptr)
-			, DirtyFlags()
 			, CollisionData()
 			, Materials()
 		{
@@ -488,7 +486,6 @@ namespace Chaos
 
 
 		IPhysicsProxyBase* Proxy;
-		FShapeDirtyFlags DirtyFlags;
 
 		TShapeProperty<FCollisionData, EShapeProperty::CollisionData> CollisionData;
 		TShapeProperty<FMaterialData, EShapeProperty::Materials> Materials;
