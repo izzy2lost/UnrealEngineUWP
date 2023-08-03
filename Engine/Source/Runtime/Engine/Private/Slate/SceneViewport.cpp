4e41307a47757885f5c7942261efd524788c61ad
aris theophanidis <aris.theophanidis@epicgames.com>
2023-08-03 15:02:21 -0400

    Reduce log spamming by making the 'scene viewport resized log' verbose. #rb vincent.gauthier
    [CL 26821808 by aris theophanidis in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Slate/SceneViewport.cpp
+++ Engine/Source/Runtime/Engine/Private/Slate/SceneViewport.cpp
@@ -1598,7 +1598,7 @@ void FSceneViewport::ResizeViewport(uint32 NewSizeX, uint32 NewSizeY, EWindowMod
 		default: WindowModeName = TEXT("INVALID"); break;
 		}
 
-		UE_LOG(LogViewport, Log, TEXT("Scene viewport resized to %dx%d, mode %s."), NewSizeX, NewSizeY, WindowModeName);
+		UE_LOG(LogViewport, Verbose, TEXT("Scene viewport resized to %dx%d, mode %s."), NewSizeX, NewSizeY, WindowModeName);
 
 		bIsResizing = true;
 
