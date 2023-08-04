1e3a13668b14d30d231f6185f94382a2917e3052
chris kulla <chris.kulla@epicgames.com>
2023-08-04 18:10:25 -0400

    Path Tracer: Add Path Tracing properties to Rendering quick category
    [FYI] Matt.Hoffman
    #rb trivial
    
    [CL 26863402 by chris kulla in ue5-main branch]

--- Engine/Source/Editor/DetailCustomizations/Private/DetailCustomizations.cpp
+++ Engine/Source/Editor/DetailCustomizations/Private/DetailCustomizations.cpp
@@ -575,6 +575,7 @@ void FDetailCustomizationsModule::RegisterSectionMappings()
 			Section->AddCategory("Materials");
 			Section->AddCategory("Mobile");
 			Section->AddCategory("Ray Tracing");
+			Section->AddCategory("Path Tracing");
 			Section->AddCategory("Rendering");
 			Section->AddCategory("Texture Streaming");
 			Section->AddCategory("Virtual Texture");
