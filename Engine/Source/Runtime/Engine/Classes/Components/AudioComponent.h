0b07907b3c86342bcc4fb60dba3906f31ac473fd
halfdan ingvarsson <halfdan.ingvarsson@epicgames.com>
2023-08-04 21:32:05 -0400

    AudioComponent: Add missing #includes
    #rnx
    [FYI] Aaron.McLeran
    
    [CL 26866399 by halfdan ingvarsson in ue5-main branch]

--- Engine/Source/Runtime/Engine/Classes/Components/AudioComponent.h
+++ Engine/Source/Runtime/Engine/Classes/Components/AudioComponent.h
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 #pragma once
 
+#include "Audio.h"
 #include "Audio/SoundParameterControllerInterface.h"
 #include "Components/SceneComponent.h"
 #include "CoreMinimal.h"
@@ -12,6 +13,8 @@
 #include "Sound/QuartzSubscription.h"
 #include "Sound/SoundAttenuation.h"
 #include "Sound/SoundModulationDestination.h"
+#include "Sound/SoundSubmixSend.h"
+#include "Sound/SoundSourceBusSend.h"
 #if UE_ENABLE_INCLUDE_ORDER_DEPRECATED_IN_5_2
 #include "Sound/SoundWave.h"
 #include "Quartz/AudioMixerClockHandle.h"
@@ -30,7 +33,6 @@ class USoundClass;
 class USoundConcurrency;
 class USoundEffectSourcePresetChain;
 class USoundWave;
-enum class EBusSendType : uint8;
 struct FAudioComponentParam;
 using FSharedISourceBufferListenerPtr = TSharedPtr<ISourceBufferListener, ESPMode::ThreadSafe>;
 
