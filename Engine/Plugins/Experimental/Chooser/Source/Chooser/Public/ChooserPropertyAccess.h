38efa9aae550aa5370ff2ac9b573082558accaa4
keith yerex <keith.yerex@epicgames.com>
2023-08-08 16:14:31 -0400

    Change default value for ChooserPropertyAccess Context Index back to 0. Fixes issues with values needing to be re-bound in editor #rb samuele.rigamonti
    [CL 26928789 by keith yerex in ue5-main branch]

--- Engine/Plugins/Experimental/Chooser/Source/Chooser/Public/ChooserPropertyAccess.h
+++ Engine/Plugins/Experimental/Chooser/Source/Chooser/Public/ChooserPropertyAccess.h
@@ -74,7 +74,7 @@ struct CHOOSER_API FChooserPropertyBinding
 	TArray<FName> PropertyBindingChain;
 	
 	UPROPERTY()
-	int ContextIndex = -1;
+	int ContextIndex = 0;
 	
 	UPROPERTY()
 	bool IsBoundToRoot = false;
