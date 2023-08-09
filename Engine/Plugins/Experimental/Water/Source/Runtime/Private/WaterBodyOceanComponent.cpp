4c4079032b337b264d2aad5bf4b430d112115a2e
roey borsteinas <roey.borsteinas@epicgames.com>
2023-08-09 12:43:43 -0400

    Water: Ensure that when oceans are spawned in the editor, they always fill the zone to which they belong by default.
    #rb jonathan.bard
    
    #p4v-cherrypick 26953207
    
    [CL 26960588 by roey borsteinas in ue5-main branch]

--- Engine/Plugins/Experimental/Water/Source/Runtime/Private/WaterBodyOceanComponent.cpp
+++ Engine/Plugins/Experimental/Water/Source/Runtime/Private/WaterBodyOceanComponent.cpp
@@ -511,6 +511,17 @@ void UWaterBodyOceanComponent::OnPostActorCreated()
 #if WITH_EDITOR
 	if (UWorld* World = GetWorld(); World && World->IsGameWorld() == false)
 	{
+	}
+	return FBoxSphereBounds(EForceInit::ForceInit).TransformBy(LocalToWorld);
+}
+
+void UWaterBodyOceanComponent::OnPostActorCreated()
+{
+	Super::OnPostActorCreated();
+
+#if WITH_EDITOR
+	if (UWorld* World = GetWorld(); World && World->IsGameWorld() == false)
+	{
 		UpdateWaterZones();
 		FillWaterZoneWithOcean();
 	}
