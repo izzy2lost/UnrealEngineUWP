6d835b5bdb2cb46a342a9707d10bede6eb4e4fba
ocea ellis <ocea.ellis@epicgames.com>
2023-08-09 14:16:15 -0400

    [PCG] Adds a node that applies a new random seed from point input using its position and user seed input.
    #rb huw.bowles, julien.lheureux
    #jira UE-184264
    
    [CL 26965287 by ocea ellis in ue5-main branch]

---
+++ Engine/Plugins/Experimental/PCG/Source/PCG/Private/Elements/PCGMutateSeed.cpp
@@ -0,0 +1,49 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Elements/PCGMutateSeed.h"
+
+#include "PCGContext.h"
+#include "PCGPin.h" 
+#include "PCGParamData.h"
+#include "Data/PCGPointData.h"
+#include "Helpers/PCGBlueprintHelpers.h"
+#include "Helpers/PCGHelpers.h"
+#include "Metadata/PCGMetadata.h"
+
+#define LOCTEXT_NAMESPACE "PCGMutateSeedSettings"
+
+UPCGMutateSeedSettings::UPCGMutateSeedSettings() 
+{
+	bUseSeed = true;
+}
+
+FPCGElementPtr UPCGMutateSeedSettings::CreateElement() const
+{
+	return MakeShared<FPCGMutateSeedElement>();
+}
+
+bool FPCGMutateSeedElement::ExecuteInternal(FPCGContext* Context) const
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(FPCGMutateSeedElement::Execute);
+	
+	check(Context);
+
+	const UPCGMutateSeedSettings* Settings = Context->GetInputSettings<UPCGMutateSeedSettings>();
+	check(Settings);
+
+	TArray<FPCGTaggedData> Inputs = Context->InputData.GetInputs();
+	TArray<FPCGTaggedData>& Outputs = Context->OutputData.TaggedData;
+
+	const int Seed = Context->GetSeed();
+
+	ProcessPoints(Context, Inputs, Outputs, [Seed](const FPCGPoint& InPoint, FPCGPoint& OutPoint)
+	{
+		OutPoint = InPoint;
+		OutPoint.Seed = PCGHelpers::ComputeSeed(UPCGBlueprintHelpers::ComputeSeedFromPosition(OutPoint.Transform.GetLocation()), Seed, OutPoint.Seed);
+		return true;
+	});
+	
+	return true;
+}
+
+#undef LOCTEXT_NAMESPACE
