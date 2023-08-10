4b9db8caeea32e2dbd31bf276a770426aaa82e5b
jerome delattre <Jerome.Delattre@epicgames.com>
2023-08-10 10:01:02 -0400

    Make UObject.Class AttemptToFindUninitializedScriptStructMembers test runnable in all application type
    #rnx
    #rb trivial
    
    [CL 26990953 by jerome delattre in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/Class.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/Class.cpp
@@ -3736,7 +3736,7 @@ public:
 	virtual bool ElevateLogWarningsToErrors() override { return false; }
 };
 
-IMPLEMENT_CUSTOM_SIMPLE_AUTOMATION_TEST(FAutomationTestAttemptToFindUninitializedScriptStructMembers, FAutomationTestUObjectClassBase, "UObject.Class AttemptToFindUninitializedScriptStructMembers", EAutomationTestFlags::EditorContext | EAutomationTestFlags::ClientContext | EAutomationTestFlags::ServerContext | EAutomationTestFlags::SmokeFilter)
+IMPLEMENT_CUSTOM_SIMPLE_AUTOMATION_TEST(FAutomationTestAttemptToFindUninitializedScriptStructMembers, FAutomationTestUObjectClassBase, "UObject.Class AttemptToFindUninitializedScriptStructMembers", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::SmokeFilter)
 bool FAutomationTestAttemptToFindUninitializedScriptStructMembers::RunTest(const FString& Parameters)
 {
 	// This test fails when running tests under UHT because there is no TestUninitializedScriptStructMembersTest, so just skip it in that config.
