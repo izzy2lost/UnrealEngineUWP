a2e67b84ac0b2ae9062950e7dd7af96ca7f00740
peter engstrom <Peter.Engstrom@epicgames.com>
2023-08-11 10:26:00 -0400

    Iris - Fixed priority calculation.
    #rnx
    #rb mattias.hornlund
    
    [CL 27027397 by peter engstrom in ue5-main branch]

--- Engine/Source/Runtime/Experimental/Iris/Core/Public/Iris/ReplicationSystem/Prioritization/SphereNetObjectPrioritizer.h
+++ Engine/Source/Runtime/Experimental/Iris/Core/Public/Iris/ReplicationSystem/Prioritization/SphereNetObjectPrioritizer.h
@@ -50,9 +50,14 @@ protected:
 	{
 		VectorRegister InnerRadius;
 		VectorRegister OuterRadius;
+		// OuterRadius - InnerRadius
+		VectorRegister RadiusDiff;
+		VectorRegister InvRadiusDiff;
+		VectorRegister InnerPriority;
 		VectorRegister OuterPriority;
 		VectorRegister OutsidePriority;
-		VectorRegister PriorityFactor;
+		// OuterPriority - InnerPriority 
+		VectorRegister PriorityDiff;
 	};
 
 	struct FBatchParams
