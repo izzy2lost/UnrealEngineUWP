5a87b69c38f3250d0d555e3f3fe3f3970291b7a9
adam kinge <adam.kinge@epicgames.com>
2023-08-11 00:46:34 -0400

    Only iOS and tvOS currently support device connection type specific icons
    #jira UE-190844
    #rb jack.porter
    #rnx
    #lockdown jack.porter
    
    [CL 27022315 by adam kinge in ue5-main branch]

--- Engine/Source/Developer/IOS/IOSTargetPlatform/Private/IOSTargetPlatform.cpp
+++ Engine/Source/Developer/IOS/IOSTargetPlatform/Private/IOSTargetPlatform.cpp
@@ -504,6 +504,9 @@ bool FIOSTargetPlatform::SupportsFeature( ETargetPlatformFeatures Feature ) cons
 		case ETargetPlatformFeatures::ShowAsPlatformGroup:
 			return false;
 
+		case ETargetPlatformFeatures::SupportsMultipleConnectionTypes:
+			return true;
+
 		default:
 			break;
 	}
