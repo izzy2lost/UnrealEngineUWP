c7ce26515e4c83139d6bcebcf73107d4733da458
joseph sozio <Joseph.Sozio@epicgames.com>
2023-08-12 11:00:49 -0400

    [Backout] - CL27046000 [FYI] bernard.lambert Original CL Desc ----------------------------------------------------------------- Extracted logic to reload objects affected by an ini file from UOnlineHotfixManager::HotfixIniFile into a seperate function.
    #rnx
    #RB [at]Josh.Adams [at]Mic.Rooney [at]Camille.Gendreau [at]Ghislain.Theriault [at]Fourat.Jellouli
    
    [CL 27059697 by joseph sozio in ue5-main branch]

--- Engine/Plugins/Online/OnlineFramework/Source/Hotfix/Public/OnlineHotfixManager.h
+++ Engine/Plugins/Online/OnlineFramework/Source/Hotfix/Public/OnlineHotfixManager.h
@@ -405,8 +405,6 @@ public:
 
 	/** Factory method that returns the configured hotfix manager */
 	static UOnlineHotfixManager* Get(UWorld* World);
-
-	static void ReloadObjectsAffectedByConfigFile(const FString& IniDataFileName, const FString& IniData, const FString& ConfigFilename, TArray<FString>& ReloadedClassesPathNames, bool bUseLoadConfig);
 };
 
 #if UE_ENABLE_INCLUDE_ORDER_DEPRECATED_IN_5_2
