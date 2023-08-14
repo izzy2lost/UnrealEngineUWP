cc0e85ba291abbf6c33ce0f08f880a5311ffaec0
david harvey <David.Harvey@epicgames.com>
2023-08-14 03:51:43 -0400

    Prevent future inclusion of MinWindows.h, PreWindowsApi.h and PostWindowsApi.h via header guard warning.
     - use WindowsHWrapper.h, AllowWindowsPlatformTypes.h and HideWindowsPlatformTypes.h instead.
    
    #jira UE-152863
    #rb Devin.Doucette
    
    [CL 27064458 by david harvey in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Microsoft/WindowsHWrapper.h
+++ Engine/Source/Runtime/Core/Public/Microsoft/WindowsHWrapper.h
@@ -8,10 +8,18 @@
 #else
 	#include "CoreTypes.h"
 	#include "HAL/PlatformMemory.h"
+
+	#if defined(WINDOWS_H_WRAPPER_GUARD)
+	#error WINDOWS_H_WRAPPER_GUARD already defined
+	#endif
+	#define WINDOWS_H_WRAPPER_GUARD
+	
 	#include "Microsoft/PreWindowsApiPrivate.h"
 	#ifndef STRICT
 	#define STRICT
 	#endif
 	#include "Microsoft/MinWindowsPrivate.h"
 	#include "Microsoft/PostWindowsApiPrivate.h"
+	
+	#undef WINDOWS_H_WRAPPER_GUARD
 #endif
