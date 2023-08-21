060009e26e5b48ef4a6f19ad6fff4811afb78c89
ocea ellis <ocea.ellis@epicgames.com>
2023-08-21 10:53:34 -0400

    [PCG] Adds a node that adds tags from the output pin to the next node. --Added unit tests that check no tags, one tag, and multiple tags
    #rb julien.lheureux, huw.bowles, adrien.logut
    #jira UE-193358
    
    [CL 27239333 by ocea ellis in ue5-main branch]

---
+++ Engine/Plugins/Experimental/PCG/Source/PCG/Private/Elements/PCGAddTag.cpp
@@ -0,0 +1,68 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Elements/PCGAddTag.h"
+
+#include "PCGContext.h"
+#include "PCGPin.h"
+#include "Helpers/PCGHelpers.h"
+
+#define LOCTEXT_NAMESPACE "PCGAddTagElement"
+
+EPCGDataType UPCGAddTagSettings::GetCurrentPinTypes(const UPCGPin* InPin) const
+{
+	check(InPin);
+	if (!InPin->IsOutputPin())
+	{
+		return Super::GetCurrentPinTypes(InPin);
+	}
+
+	// Output pin narrows to union of inputs on first pin
+	const EPCGDataType InputTypeUnion = GetTypeUnionOfIncidentEdges(PCGPinConstants::DefaultInputLabel);
+	return InputTypeUnion != EPCGDataType::None ? InputTypeUnion : EPCGDataType::Any;
+}
+
+TArray<FPCGPinProperties> UPCGAddTagSettings::InputPinProperties() const
+{
+	TArray<FPCGPinProperties> PinProperties;
+	PinProperties.Emplace(PCGPinConstants::DefaultInputLabel, EPCGDataType::Any);
+
+	return PinProperties;
+}
+
+TArray<FPCGPinProperties> UPCGAddTagSettings::OutputPinProperties() const
+{
+	TArray<FPCGPinProperties> PinProperties;
+	PinProperties.Emplace(PCGPinConstants::DefaultOutputLabel, EPCGDataType::Any);
+
+	return PinProperties;
+}
+
+FPCGElementPtr UPCGAddTagSettings::CreateElement() const
+{
+	return MakeShared<FPCGAddTagElement>();
+}
+
+bool FPCGAddTagElement::ExecuteInternal(FPCGContext* Context) const
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(FPCGAddTagElement::Execute);
+	
+	check(Context);
+
+	const UPCGAddTagSettings* Settings = Context->GetInputSettings<UPCGAddTagSettings>();
+	check(Settings);
+	
+	Context->OutputData = Context->InputData;
+	const TArray<FString> TagsArray = PCGHelpers::GetStringArrayFromCommaSeparatedString(Settings->TagsToAdd);
+	
+	for (const FString& Tag : TagsArray)
+	{
+		for (FPCGTaggedData& OutputTaggedData : Context->OutputData.TaggedData)
+		{
+				OutputTaggedData.Tags.Add(Tag);
+		}
+	}
+	
+	return true;
+}
+
+#undef LOCTEXT_NAMESPACE
