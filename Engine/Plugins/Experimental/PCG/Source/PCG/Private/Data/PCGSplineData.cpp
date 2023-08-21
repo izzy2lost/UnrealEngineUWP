93d900b9536860d9abacec0cebd98ec4d6754a5e
charles lefebvre <charles.lefebvre@epicgames.com>
2023-08-21 12:10:04 -0400

    Remove impossible cast in UPCGSplineData::CreatePointData UPCGSplineProjectionData is not a UPCGSplineData #rnx #rb huw.bowles [FYI] julien.lheureux
    [CL 27241748 by charles lefebvre in ue5-main branch]

--- Engine/Plugins/Experimental/PCG/Source/PCG/Private/Data/PCGSplineData.cpp
+++ Engine/Plugins/Experimental/PCG/Source/PCG/Private/Data/PCGSplineData.cpp
@@ -119,11 +119,12 @@ const UPCGPointData* UPCGSplineData::CreatePointData(FPCGContext* Context) const
 
 	const UPCGSpatialData* ProjectionTarget = nullptr;
 	FPCGProjectionParams ProjectionParams;
+	/* Impossible cast, UPCGSplineProjectionData is not a UPCGSplineData
 	if (const UPCGSplineProjectionData* SplineProjection = Cast<const UPCGSplineProjectionData>(this))
 	{
 		ProjectionTarget = SplineProjection->GetSurface();
 		ProjectionParams = SplineProjection->GetProjectionParams();
-	}
+	}*/
 
 	PCGSplineSampler::SampleLineData(this, nullptr, ProjectionTarget, ProjectionParams, SamplerParams, Data);
 	UE_LOG(LogPCG, Verbose, TEXT("Spline generated %d points"), Data->GetPoints().Num());
