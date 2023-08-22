17b885f0502cbb3d0f1bd1d30c19f9243f42d2d7
steven barnett <Steven.Barnett@epicgames.com>
2023-08-22 14:40:01 -0400

    Address CIS issue in UE non-unity
    [CL 27280971 by steven barnett in ue5-main branch]

--- Engine/Plugins/Experimental/Buoyancy/Source/Runtime/Public/BuoyancyEventInterface.h
+++ Engine/Plugins/Experimental/Buoyancy/Source/Runtime/Public/BuoyancyEventInterface.h
@@ -2,6 +2,9 @@
 
 #pragma once
 #include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+#include "UObject/Interface.h"
 #include "BuoyancyEventInterface.generated.h"
 
 UINTERFACE(Blueprintable)
