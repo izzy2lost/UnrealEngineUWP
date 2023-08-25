631192e7ad6ecdae31c5c201547128dfd6137b3b
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2023-08-25 00:44:11 -0400

    Remove incorrect mobile specific assert related to PSO pre-caching of skin VF
    [CL 27367762 by dmitriy dyomin in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/GPUSkinVertexFactory.cpp
+++ Engine/Source/Runtime/Engine/Private/GPUSkinVertexFactory.cpp
@@ -750,10 +750,9 @@ void TGPUSkinVertexFactory<BoneInfluenceType>::GetVertexElements(ERHIFeatureLeve
 	int32 MorphDeltaStreamIndex;
 	GetVertexElements(FeatureLevel, InputStreamType, GPUSkinData, OutElements, VertexStreams, MorphDeltaStreamIndex);
 
-	if (UseGPUScene(GMaxRHIShaderPlatform, GMaxRHIFeatureLevel))
+	if (UseGPUScene(GMaxRHIShaderPlatform, GMaxRHIFeatureLevel) && 
+		FeatureLevel > ERHIFeatureLevel::ES3_1) // Skin VF does not use GPUScene on mobile
 	{
-		// For ES3.1 attribute ID needs to be done differently
-		check(FeatureLevel > ERHIFeatureLevel::ES3_1);
 		OutElements.Add(FVertexElement(VertexStreams.Num(), 0, VET_UInt, 16, 0, true));
 	}
 }
