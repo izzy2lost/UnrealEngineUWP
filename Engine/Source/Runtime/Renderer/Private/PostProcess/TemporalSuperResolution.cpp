138adda4e1128527ebb0b91c664c86a6c6f43eb0
guillaume abadie <Guillaume.Abadie@epicgames.com>
2023-08-25 20:34:45 -0400

    Temporarily disables an ensure in FTSRDilateVelocityCS::ShouldCompilePermutation()
    #rb trivial
    
    [CL 27397923 by guillaume abadie in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/PostProcess/TemporalSuperResolution.cpp
+++ Engine/Source/Runtime/Renderer/Private/PostProcess/TemporalSuperResolution.cpp
@@ -542,7 +542,6 @@ class FTSRDilateVelocityCS : public FTSRShader
 		{
 			return false;
 		}
-		ensureMsgf(VelocityEncodeHasPixelAnimation(Parameters.Platform), TEXT("Platform did not return true from VelocityEncodeHasPixelAnimation().  TSR requires encoding UMaterial::bHasPixelAnimation in the velocity buffer."));
 		return true;
 	}
 }; // class FTSRDilateVelocityCS
