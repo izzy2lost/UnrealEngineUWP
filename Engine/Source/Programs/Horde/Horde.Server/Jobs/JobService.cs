42b71a5b1cfc571295818a3a2d0ade7e44366787
Josh Engebretson <Josh.Engebretson@epicgames.com>
2023-08-29 11:20:42 -0400

    Horde: Revert preflight change description update #jira none #rnx
    [CL 27448797 by Josh Engebretson in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Server/Jobs/JobService.cs
+++ Engine/Source/Programs/Horde/Horde.Server/Jobs/JobService.cs
@@ -1036,10 +1036,6 @@ namespace Horde.Server.Jobs
 							{
 								job = await AutoSubmitChangeAsync(streamConfig, job, graph);
 							}
-							else if (job.ClonedPreflightChange == 0 && outcome == JobStepOutcome.Success && job.AbortedByUserId == null)
-							{
-								await _perforceService.UpdateChangelistDescriptionAsync(streamConfig.ClusterName, job.PreflightChange, x => x.TrimEnd() + $"\n#preflight {job.Id}");
-							}
 							else if (job.ClonedPreflightChange != 0)
 							{
 								await DeleteShelvedChangeAsync(streamConfig.ClusterName, job.ClonedPreflightChange);
