add058d3450f8b5c2d929802bd38368268edfc6a
max chen <Max.Chen@epicgames.com>
2023-08-29 13:56:27 -0400

    Sequencer: Remove dependency on SceneOutliner private
    #jira UE-194142
    #rnx
    #rb matt.hoffman
    
    [CL 27455815 by max chen in ue5-main branch]

--- Engine/Source/Editor/MovieSceneTools/MovieSceneTools.Build.cs
+++ Engine/Source/Editor/MovieSceneTools/MovieSceneTools.Build.cs
@@ -9,7 +9,6 @@ public class MovieSceneTools : ModuleRules
 		PrivateIncludePaths.AddRange(
             new string[] {
 				System.IO.Path.Combine(GetModuleDirectory("UnrealEd"), "Private"), //compatibility for FBX importer
-				System.IO.Path.Combine(GetModuleDirectory("SceneOutliner"), "Private"),
 			}
         );
 
