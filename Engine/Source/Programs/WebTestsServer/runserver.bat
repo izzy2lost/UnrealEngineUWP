8d3f25ce73df5832d6fe2ba074b2d7e33872cdc4
lorry li <lorry.li@epicgames.com>
2023-08-30 10:31:43 -0400

    Upgrade pip package in python env to fix the pip outdated warning.
    #jira UE-182678
    [REVIEW] [at]stephen.ma [at]rafa.lecina
    #rb [at]stephen.ma [at]rafa.lecina
    
    [CL 27484161 by lorry li in ue5-main branch]

--- Engine/Source/Programs/WebTestsServer/runserver.bat
+++ Engine/Source/Programs/WebTestsServer/runserver.bat
@@ -5,6 +5,7 @@ if exist env (
 ) else (
 	..\..\..\Binaries\ThirdParty\Python3\Win64\python.exe -m venv env
 	call .\env\Scripts\activate.bat
+	python -m pip install --upgrade pip
 	pip install -r requirements.txt
 	if %ERRORLEVEL% NEQ 0 (
 		rmdir /s /q env
--- Engine/Source/Programs/WebTestsServer/runserver.sh
+++ Engine/Source/Programs/WebTestsServer/runserver.sh
@@ -3,6 +3,7 @@ if [ -d "env" ]; then
 else
 	run ../../../Binaries/ThirdParty/Python3/Mac/python3 -m venv env
 	source ./env/Scripts/activate.sh
+	run python -m  pip install --upgrade pip
 	run pip install -r requirements.txt
 	if [ $? -ne 0 ]; then
 		rm -rf env
