91e10a72ee48aab399aede965f851fdbc890aaee
zach rammell <zach.rammell@epicgames.com>
2023-08-31 14:33:58 -0400

    Add revision control columns initial functionality
    #rb ronald.koppers brooke.hubert
    #jira UE-192061
    
    [CL 27531555 by zach rammell in ue5-main branch]

---
+++ Engine/Plugins/Experimental/TypedElementsDataStorage/Source/TypedElementsDataStorage/Private/Compatibility/TypedElementSCCProcessors.cpp
@@ -0,0 +1,93 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TypedElementSCCProcessors.h"
+
+#include "Elements/Columns/TypedElementMiscColumns.h"
+#include "Elements/Columns/TypedElementRevisionControlColumns.h"
+#include "Elements/Columns/TypedElementSelectionColumns.h"
+#include "Elements/Columns/TypedElementViewportColumns.h"
+#include "Elements/Framework/TypedElementQueryBuilder.h"
+#include "Elements/Framework/TypedElementRegistry.h"
+
+FAutoConsoleCommandWithArgsAndOutputDevice SetSelectionSCCStateConsoleCommand(
+	TEXT("TEDS.Debug.SetSCCState"),
+	TEXT("Adds a source control state to selected objects."),
+	FConsoleCommandWithArgsAndOutputDeviceDelegate::CreateLambda([](const TArray<FString>& Args, FOutputDevice& Output)
+		{
+			using namespace TypedElementQueryBuilder;
+			using DSI = ITypedElementDataStorageInterface;
+
+			TRACE_CPUPROFILER_EVENT_SCOPE(TEDS.Debug.SetSCCState);
+
+			if (ITypedElementDataStorageInterface* DataStorage = UTypedElementRegistry::GetInstance()->GetMutableDataStorage())
+			{
+				static TypedElementQueryHandle AllSelectedQuery = TypedElementInvalidQueryHandle;
+				if (AllSelectedQuery == TypedElementInvalidQueryHandle)
+				{
+					AllSelectedQuery = DataStorage->RegisterQuery(
+						Select()
+						.Where()
+							.All<FTypedElementSelectionColumn>()
+						.Compile());
+				}
+				
+				if (AllSelectedQuery == TypedElementInvalidQueryHandle)
+				{
+					return;
+				}
+
+				ESCCModification Modification = ESCCModification::Modified;
+
+				if (Args.Num() > 0)
+				{
+					// Parse the index
+					int32 StateIndex;
+					LexFromString(StateIndex, *Args[0]);
+
+					if (!(StateIndex >= 0 && StateIndex <= int32(ESCCModification::Conflicted)))
+					{
+						Output.Log(TEXT("State index out of range"));
+						return;
+					}
+
+					Modification = static_cast<ESCCModification>(StateIndex);
+				}
+				
+				TArray<TypedElementRowHandle> RowHandles;
+				
+				DataStorage->RunQuery(AllSelectedQuery, [&RowHandles](const DSI::FQueryDescription&, DSI::IDirectQueryContext& Context)
+				{
+					RowHandles = Context.GetRowHandles();
+				});
+
+				for (TypedElementRowHandle Row : RowHandles)
+				{
+					DataStorage->AddOrGetColumn<FSCCStatusColumn>(Row)->Modification = Modification;
+					DataStorage->AddColumn<FTypedElementSyncFromWorldTag>(Row);
+				}
+			}
+		}
+	));
+
+void UTypedElementSCCFactory::RegisterQueries(ITypedElementDataStorageInterface& DataStorage) const
+{
+	using namespace TypedElementQueryBuilder;
+	using DSI = ITypedElementDataStorageInterface;
+
+	DataStorage.RegisterQuery(
+		Select(
+			TEXT("Change selection outline colors based on SCC status"),
+			// This is in PrePhysics because the outline->actor query is in DuringPhysics and contexts don't flush changes between tick groups
+			FProcessor(DSI::EQueryTickPhase::PrePhysics, DataStorage.GetQueryTickGroupName(DSI::EQueryTickGroups::SyncExternalToDataStorage)),
+			[](DSI::IQueryContext& Context, TypedElementRowHandle Row, const FSCCStatusColumn& SCCStatus)
+			{
+				constexpr int BasicSelectionColorCount = 2;
+				Context.AddColumn<FTypedElementViewportColorColumn>(Row, { .SelectionOutlineColorIndex = static_cast<uint8>(int(SCCStatus.Modification) + BasicSelectionColorCount) });
+				Context.AddColumns<FTypedElementSyncBackToWorldTag>(Row);
+			}
+		)
+		.Where()
+			.All<FTypedElementSyncFromWorldTag>()
+		.Compile()
+	);
+}
