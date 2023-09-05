9643f3c1fbb35bbd24c0eacefeefadc300937f34
alfaroh corneyiii <alfaroh.corneyiii@epicgames.com>
2023-09-05 16:22:22 -0400

    Fix modulation crash on PIE/Shutdown #rb rob.gay
    [CL 27610416 by alfaroh corneyiii in ue5-main branch]

--- Engine/Source/Runtime/Engine/Classes/Sound/SoundModulationDestination.h
+++ Engine/Source/Runtime/Engine/Classes/Sound/SoundModulationDestination.h
@@ -179,40 +179,50 @@ namespace Audio
 			ENGINE_API void UpdateModulators(const TSet<TObjectPtr<USoundModulatorBase>>& InModulators);
 			ENGINE_API void UpdateModulators(const TSet<USoundModulatorBase*>& InModulators);
 			ENGINE_API void UpdateModulators(const TSet<const USoundModulatorBase*>& InModulators);
-	
+
 	private:
 			ENGINE_API void UpdateModulatorsInternal(TArray<TUniquePtr<Audio::IModulatorSettings>>&& ProxySettings);
 
-			ENGINE_API void ResetHandles();
+			struct FModulationDestinationData
+			{
+				FDeviceId DeviceId = INDEX_NONE;
 
-			FDeviceId DeviceId = INDEX_NONE;
+				float ValueTarget = 1.0f;
 
-			float ValueTarget = 1.0f;
+				bool bIsBuffered = false;
+				bool bValueNormalized = false;
+				bool bHasProcessed = false;
 
-			bool bIsBuffered = false;
-			bool bValueNormalized = false;
-			bool bHasProcessed = false;
+				FAlignedFloatBuffer OutputBuffer;
 
-			FAlignedFloatBuffer OutputBuffer;
+				TSet<FModulatorHandle> Handles;
 
-			TSet<FModulatorHandle> Handles;
+				FModulationParameter Parameter;
 
-			FModulationParameter Parameter;
+				mutable FCriticalSection HandleCritSection;
 
-			mutable FCriticalSection HandleCritSection;
+				FModulationDestinationData& operator=(const FModulationDestinationData& InDestInfo);
+				FModulationDestinationData& operator=(FModulationDestinationData&& InDestInfo);
+				const FDeviceId& GetDeviceId() const;
+				const FModulationParameter& GetParameter() const;
+				void SetHandles(TSet<FModulatorHandle>&& Handles);
+				void ResetHandles();
+			};
+
+			TSharedRef<FModulationDestinationData> DestinationData{ MakeShared<FModulationDestinationData>() };
 
 		public:
 			/** Returns buffer of interpolated modulation values. If not set to "IsBuffered" when initialized, returns an empty array. */
 			FORCEINLINE const FAlignedFloatBuffer& GetBuffer() const
 			{
-				return OutputBuffer;
+				return DestinationData->OutputBuffer;
 			}
 
 			/** Returns whether or not the destination has requested to 
 			  * process the control or not. */
 			FORCEINLINE bool GetHasProcessed() const
 			{
-				return bHasProcessed;
+				return DestinationData->bHasProcessed;
 			}
 
 			/** Returns sample value last reported by modulator. Returns value in unit space, unless
@@ -220,7 +230,7 @@ namespace Audio
 			 */
 			FORCEINLINE float GetValue() const
 			{
-				return ValueTarget;
+				return DestinationData->ValueTarget;
 			}
 	};
 } // namespace Audio
