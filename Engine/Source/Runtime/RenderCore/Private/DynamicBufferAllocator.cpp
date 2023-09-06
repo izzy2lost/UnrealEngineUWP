2457350cc6b4d707f27d438b0e1e4d8bc12ce70f
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2023-09-06 10:53:12 -0400

    Change FGlobalDynamicReadBuffer to BUF_Dynamic instead of BUF_Volatile. Only VulkanRHI has a special path for BUF_Volatile and it does not align with FGlobalDynamicReadBuffer expectations about buffer lifetime #rb allan.bentham
    [CL 27635352 by dmitriy dyomin in ue5-main branch]

--- Engine/Source/Runtime/RenderCore/Private/DynamicBufferAllocator.cpp
+++ Engine/Source/Runtime/RenderCore/Private/DynamicBufferAllocator.cpp
@@ -143,7 +143,7 @@ FGlobalDynamicReadBuffer::FAllocation FGlobalDynamicReadBuffer_AllocateInternal(
 			const uint32 NewBufferSize = FMath::Max(AlignedNum, (uint32)GMinReadBufferRenderingBufferSize);
 			Buffer = new FDynamicAllocReadBuffer();
 			BufferPool->Buffers.Add(Buffer);
-			Buffer->Initialize(RHICmdList, TEXT("FGlobalDynamicReadBuffer_AllocateInternal"), sizeof(Type), NewBufferSize, Format, BUF_Volatile);
+			Buffer->Initialize(RHICmdList, TEXT("FGlobalDynamicReadBuffer_AllocateInternal"), sizeof(Type), NewBufferSize, Format, BUF_Dynamic);
 		}
 
 		// Lock the buffer if needed.
