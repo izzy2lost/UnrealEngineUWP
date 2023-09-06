519285d8b803caf1920072f4a52f87bd063ca694
mark winter <mark.winter@cubicmotion.com>
2023-09-06 07:44:38 -0400

    PythonScriptPlugin: Update GetInterpreterExecutablePath to use python3 on Macos/Linux.
    #rb jamie.dale
    
    [CL 27630515 by mark winter in ue5-main branch]

--- Engine/Plugins/Experimental/PythonScriptPlugin/Source/PythonScriptPlugin/Private/PyUtil.cpp
+++ Engine/Plugins/Experimental/PythonScriptPlugin/Source/PythonScriptPlugin/Private/PyUtil.cpp
@@ -1042,7 +1042,7 @@ FString GetInterpreterExecutablePath(bool* OutIsEnginePython)
 #if PLATFORM_WINDOWS
 	PythonPath /= TEXT("python.exe");
 #elif PLATFORM_MAC || PLATFORM_LINUX
-	PythonPath /= TEXT("bin/python");
+	PythonPath /= TEXT("bin/python3");
 #else
 	static_assert(false, "Python not supported on this platform!");
 #endif
