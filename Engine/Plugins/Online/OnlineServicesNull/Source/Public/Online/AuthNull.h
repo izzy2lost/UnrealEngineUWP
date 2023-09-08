236175e51ad0b10c17238e5776bbfa2682f0035c
chris varnsverry <Chris.Varnsverry@epicgames.com>
2023-09-07 21:39:58 -0400

    - Move OnlineServicesNullModule class to public header - Add "GetRegistryPriority" for ease of overriding.
    [REVIEW] [at]Rob.Cannaday [at]Michael.Kirzinger [at]Dave.Haslam
    
    [CL 27700122 by chris varnsverry in ue5-main branch]

--- Engine/Plugins/Online/OnlineServicesNull/Source/Public/Online/AuthNull.h
+++ Engine/Plugins/Online/OnlineServicesNull/Source/Public/Online/AuthNull.h
@@ -21,10 +21,10 @@ public:
 class FOnlineAccountIdRegistryNull : public IOnlineAccountIdRegistry
 {
 public:
-	static FOnlineAccountIdRegistryNull& Get();
+	ONLINESERVICESNULL_API static FOnlineAccountIdRegistryNull& Get();
 
-	FAccountId Find(const FString& AccountId) const;
-	FAccountId FindOrAddAccountId(const FString& AccountId);
+	ONLINESERVICESNULL_API FAccountId Find(const FString& AccountId) const;
+	ONLINESERVICESNULL_API FAccountId FindOrAddAccountId(const FString& AccountId);
 
 	// Begin IOnlineAccountIdRegistry
 	virtual FString ToString(const FAccountId& AccountId) const override;
