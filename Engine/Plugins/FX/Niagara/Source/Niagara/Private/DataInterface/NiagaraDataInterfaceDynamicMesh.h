9487c7b06c3595ff4abd4ee183f8df2734cc55ae
michael galetzka <michael.galetzka@epicgames.com>
2023-09-11 17:03:15 -0400

    Fixed dynamic mesh c++ class description
    #rb none
    [FYI] stu.mckenna
    
    [CL 27770809 by michael galetzka in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/DataInterface/NiagaraDataInterfaceDynamicMesh.h
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/DataInterface/NiagaraDataInterfaceDynamicMesh.h
@@ -48,7 +48,7 @@ struct FNiagaraDynamicMeshMaterial
 	}
 };
 
-/** Data Interface allowing sampling of a texture */
+/** Data Interface that can create mesh geometry at runtime from triangle data (e.g. to render a custom generated mesh per particle) */
 UCLASS(Experimental, EditInlineNew, Category = "Mesh", CollapseCategories, meta = (DisplayName = "Dynamic Mesh"), MinimalAPI)
 class UNiagaraDataInterfaceDynamicMesh : public UNiagaraDataInterface, public INiagaraRenderableMeshInterface
 {
