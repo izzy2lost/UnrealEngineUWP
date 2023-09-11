beb1c8b0d28227c2cd53f30c1230e77c70300a10
roey borsteinas <roey.borsteinas@epicgames.com>
2023-09-11 12:21:33 -0400

    Water: resubmit fix for physics material not being set on the water collision components
    #rb jonathan.bard
    
    [CL 27757427 by roey borsteinas in ue5-main branch]

--- Engine/Plugins/Experimental/Water/Source/Runtime/Private/WaterBodyComponent.cpp
+++ Engine/Plugins/Experimental/Water/Source/Runtime/Private/WaterBodyComponent.cpp
@@ -1847,6 +1847,7 @@ void UWaterBodyComponent::CopySharedCollisionSettingsToComponent(UPrimitiveCompo
 	InComponent->SetCollisionResponseToChannels(BodyInstance.GetResponseToChannels());
 	InComponent->SetCollisionProfileName(GetCollisionProfileName(), /*bUpdateOverlaps=*/ true);
 	InComponent->SetGenerateOverlapEvents(GetGenerateOverlapEvents());
+	InComponent->SetPhysMaterialOverride(PhysicalMaterial);
 }
 
 void UWaterBodyComponent::CopySharedNavigationSettingsToComponent(UPrimitiveComponent* InComponent)
