14d84c3bb0e8cb4fdb18a9bf892b2829083b7e78
zach bethel <zach.bethel@epicgames.com>
2023-09-13 11:51:59 -0400

    Re-enabled render command pipes by default.
    [CL 27834049 by zach bethel in ue5-main branch]

--- Engine/Source/Runtime/RenderCore/Private/RenderingThread.cpp
+++ Engine/Source/Runtime/RenderCore/Private/RenderingThread.cpp
@@ -1545,7 +1545,7 @@ inline ERenderCommandPipeMode GetValidatedRenderCommandPipeMode(int32 CVarValue)
 ERenderCommandPipeMode GRenderCommandPipeMode = ERenderCommandPipeMode::None;
 FAutoConsoleVariable CVarRenderCommandPipeMode(
 	TEXT("r.RenderCommandPipeMode"),
-	1,
+	2,
 	TEXT("Controls behavior of the main render thread command pipe.")
 	TEXT(" 0: Render commands are launched individually as tasks;\n")
 	TEXT(" 1: Render commands are enqueued into a render command pipe for the render thread only.;\n")
