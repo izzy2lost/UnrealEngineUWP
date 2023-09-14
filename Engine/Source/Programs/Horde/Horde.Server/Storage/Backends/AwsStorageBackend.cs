d69b3214c9288eee0686d9dd90132ea45319ac43
Ben Marsh <Ben.Marsh@epicgames.com>
2023-09-14 17:51:26 -0400

    Horde: Add missing .blob extension to files read from AWS.
    [CL 27897548 by Ben Marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Server/Storage/Backends/AwsStorageBackend.cs
+++ Engine/Source/Programs/Horde/Horde.Server/Storage/Backends/AwsStorageBackend.cs
@@ -259,7 +259,7 @@ namespace Horde.Server.Storage.Backends
 			}
 		}
 
-		string GetFullPath(string path) => _pathPrefix + path;
+		string GetFullPath(string path) => $"{_pathPrefix}{path}.blob";
 
 		/// <inheritdoc/>
 		public Task<Stream> OpenAsync(string path, int offset, int? length, CancellationToken cancellationToken)
