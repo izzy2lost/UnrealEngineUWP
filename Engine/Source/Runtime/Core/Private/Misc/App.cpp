cf982f098f95747435785a910577e62a36ac2564
lorry li <lorry.li@epicgames.com>
2023-09-13 22:30:45 -0400

    Log platform, device id and machine id when startup.
    [REVIEW] [at]michael.atchison [at]chris.applegarth
    #rb [at]michael.atchison
    
    [CL 27865351 by lorry li in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/Misc/App.cpp
+++ Engine/Source/Runtime/Core/Private/Misc/App.cpp
@@ -375,6 +375,11 @@ void FApp::PrintStartupLogMessages()
 {
 	UE_LOG(LogInit, Log, TEXT("ExecutableName: %s"), FPlatformProcess::ExecutableName(false));
 	UE_LOG(LogInit, Log, TEXT("Build: %s"), FApp::GetBuildVersion());
+
+	UE_LOG(LogInit, Log, TEXT("Platform=%s"), ANSI_TO_TCHAR(FPlatformProperties::PlatformName()));
+	UE_LOG(LogInit, Log, TEXT("MachineId=%s"), *FPlatformMisc::GetLoginId());
+	UE_LOG(LogInit, Log, TEXT("Deviceid=%s"), *FPlatformMisc::GetDeviceId());
+
 	UE_LOG(LogInit, Log, TEXT("Engine Version: %s"), *FEngineVersion::Current().ToString());
 	UE_LOG(LogInit, Log, TEXT("Compatible Engine Version: %s"), *FEngineVersion::CompatibleWith().ToString());
 	UE_LOG(LogInit, Log, TEXT("Net CL: %u"), FNetworkVersion::GetNetworkCompatibleChangelist());
