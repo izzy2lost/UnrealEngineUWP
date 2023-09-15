6aac4ee6c5fd643a0cab3f04dc55c28b08d01624
michael bao <michael.bao@epicgames.com>
2023-09-15 12:00:48 -0400

    accidental pragma optimize removal
    [CL 27918383 by michael bao in ue5-main branch]

--- Engine/Source/Runtime/Experimental/GeometryCollectionEngine/Private/GeometryCollection/GeometryCollectionComponent.cpp
+++ Engine/Source/Runtime/Experimental/GeometryCollectionEngine/Private/GeometryCollection/GeometryCollectionComponent.cpp
@@ -1,5 +1,4 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-#pragma optimize("", off)
 #include "GeometryCollection/GeometryCollectionComponent.h"
 
 #include "AI/Navigation/NavCollisionBase.h"
