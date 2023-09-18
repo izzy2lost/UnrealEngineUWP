9d4f8c2b15322bb49fe868308b058f083df03674
josh adams <josh.adams@epicgames.com>
2023-09-18 13:52:38 -0400

    - Quick fix for the missing NDAd preview platform issue on Mac/Linux #rb michael.wanderson [FYI] michael.wanderson #jira UE-195437
    [CL 27966407 by josh adams in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/Misc/DataDrivenPlatformInfoRegistry.cpp
+++ Engine/Source/Runtime/Core/Private/Misc/DataDrivenPlatformInfoRegistry.cpp
@@ -276,6 +276,11 @@ static void ParsePreviewPlatforms(const FConfigFile& IniFile)
 
 				FName PlatformName = *GetSectionString(Section.Value, FName("PlatformName"));
 				checkf(PlatformName != NAME_None, TEXT("DataDrivenPlatformInfo section [%s] must specify a PlatformName"), *SectionName);
+				
+				if (!FDataDrivenPlatformInfoRegistry::GetPlatformInfo(PlatformName).bEnabledForUse)
+				{
+					continue;
+				}
 
 				FPreviewPlatformMenuItem Item;
 				Item.PlatformName = PlatformName;
