a1c173294d47b1885626f42f44fde7442775bca0
jimmy andrews <Jimmy.Andrews@epicgames.com>
2023-09-19 14:55:17 -0400

    Fix source mesh being invisible when plane cut tool is started with show preview unchecked
    #jira UE-191076
    #rb rinat.abdrashitov
    
    [CL 28006067 by jimmy andrews in ue5-main branch]

--- Engine/Plugins/Experimental/MeshModelingToolsetExp/Source/MeshModelingToolsExp/Private/PlaneCutTool.cpp
+++ Engine/Plugins/Experimental/MeshModelingToolsetExp/Source/MeshModelingToolsExp/Private/PlaneCutTool.cpp
@@ -192,8 +192,8 @@ void UPlaneCutTool::SetupPreviews()
 		// set initial preview to un-processed mesh, so stuff doesn't just disappear if the first cut takes a while
 		Preview->PreviewMesh->UpdatePreview(MeshesToCut[PreviewIdx]->GetMesh().Get());
 		Preview->PreviewMesh->SetTransform((FTransform)UE::ToolTarget::GetLocalToWorldTransform(Targets[PreviewIdx]));
-		Preview->SetVisibility(BasicProperties->bShowPreview);
 	}
+	UpdateVisibility();
 }
 
 
@@ -345,18 +345,24 @@ void UPlaneCutTool::PostEditChangeProperty(FPropertyChangedEvent& PropertyChange
 #endif
 
 
+void UPlaneCutTool::UpdateVisibility()
+{
+	for (int Idx = 0; Idx < Targets.Num(); Idx++)
+	{
+		UE::ToolTarget::SetSourceObjectVisible(Targets[Idx], !BasicProperties->bShowPreview);
+	}
+	for (UMeshOpPreviewWithBackgroundCompute* Preview : Previews)
+	{
+		Preview->SetVisibility(BasicProperties->bShowPreview);
+	}
+}
+
+
 void UPlaneCutTool::OnPropertyModified(UObject* PropertySet, FProperty* Property)
 {
 	if (Property && (Property->GetFName() == GET_MEMBER_NAME_CHECKED(UPlaneCutToolProperties, bShowPreview)))
 	{
-		for (int Idx = 0; Idx < Targets.Num(); Idx++)
-		{
-			UE::ToolTarget::SetSourceObjectVisible(Targets[Idx], !BasicProperties->bShowPreview);
-		}
-		for (UMeshOpPreviewWithBackgroundCompute* Preview : Previews)
-		{
-			Preview->SetVisibility(BasicProperties->bShowPreview);
-		}
+		UpdateVisibility();
 	}
 
 	InvalidatePreviews();
