250649dedb1d10434562e0406bd031c909df120b
brian smith <brian.smith@epicgames.com>
2023-09-19 09:10:41 -0400

    Live Link VCAM v1.3.2 build 5
    [CL 27993057 by brian smith in ue5-main branch]

--- Engine/Extras/VirtualProduction/LiveLinkVCAM/vcam/WebRTC/Services/WebRTCClient.swift
+++ Engine/Extras/VirtualProduction/LiveLinkVCAM/vcam/WebRTC/Services/WebRTCClient.swift
@@ -300,8 +300,8 @@ extension WebRTCClient {
     private func configureAudioSession() {
         self.rtcAudioSession.lockForConfiguration()
         do {
-            try self.rtcAudioSession.setCategory(AVAudioSession.Category.ambient.rawValue) /* Playback only */
-            try self.rtcAudioSession.setMode(AVAudioSession.Mode.default.rawValue) /* Mod.rawValuee to default mode */
+            try self.rtcAudioSession.setCategory(AVAudioSession.Category.ambient) /* Playback only */
+            try self.rtcAudioSession.setMode(AVAudioSession.Mode.default) /* Mod.rawValuee to default mode */
         } catch let error {
             debugPrint("Error changeing AVAudioSession category: \(error)")
         }
