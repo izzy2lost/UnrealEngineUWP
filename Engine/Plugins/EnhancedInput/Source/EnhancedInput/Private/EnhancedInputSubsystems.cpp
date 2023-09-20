4aa0787d90521be347cbf441667e197413c3891a
ben hoffman <ben.hoffman@epicgames.com>
2023-09-20 15:33:58 -0400

    Add a flag on Default Mapping Contexts for Enhanced Input to register with the User Input settings by default.
    #rb jules.blok
    
    [CL 28051194 by ben hoffman in ue5-main branch]

--- Engine/Plugins/EnhancedInput/Source/EnhancedInput/Private/EnhancedInputSubsystems.cpp
+++ Engine/Plugins/EnhancedInput/Source/EnhancedInput/Private/EnhancedInputSubsystems.cpp
@@ -280,9 +280,21 @@ void UEnhancedInputWorldSubsystem::AddDefaultMappingContexts()
 		{
 			if (const UInputMappingContext* IMC = ContextSetting.InputMappingContext.LoadSynchronous())
 			{
-				if (!HasMappingContext(IMC))
+				if (ContextSetting.bAddImmediately)
 				{
-					AddMappingContext(IMC, ContextSetting.Priority);
+					if (!HasMappingContext(IMC))
+					{
+						FModifyContextOptions Opts = {};
+						Opts.bNotifyUserSettings = ContextSetting.bRegisterWithUserSettings;
+						AddMappingContext(IMC, ContextSetting.Priority, Opts);	
+					}					
+				}
+				else if (ContextSetting.bRegisterWithUserSettings)
+				{
+					if (UEnhancedInputUserSettings* Settings = GetUserSettings())
+					{
+						Settings->RegisterInputMappingContext(IMC);
+					}
 				}
 			}
 		}
@@ -297,7 +309,9 @@ void UEnhancedInputWorldSubsystem::RemoveDefaultMappingContexts()
 		{
 			if (const UInputMappingContext* IMC = ContextSetting.InputMappingContext.LoadSynchronous())
 			{
-				RemoveMappingContext(IMC);
+				FModifyContextOptions Opts = {};
+				Opts.bNotifyUserSettings = ContextSetting.bRegisterWithUserSettings;
+				RemoveMappingContext(IMC, Opts);
 			}
 		}
 	}
