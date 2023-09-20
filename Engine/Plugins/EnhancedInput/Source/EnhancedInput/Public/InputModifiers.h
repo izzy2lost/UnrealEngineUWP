72dfe2afa4836788a233a7af87fdb452b8a8edd7
ben hoffman <ben.hoffman@epicgames.com>
2023-09-20 14:24:36 -0400

    Remove TODO comment that doesnt need to be there and provide some more info about the actual property
    #rb trivial
    #rnx
    
    [CL 28047109 by ben hoffman in ue5-main branch]

--- Engine/Plugins/EnhancedInput/Source/EnhancedInput/Public/InputModifiers.h
+++ Engine/Plugins/EnhancedInput/Source/EnhancedInput/Public/InputModifiers.h
@@ -164,7 +164,13 @@ public:
 	virtual EDataValidationResult IsDataValid(class FDataValidationContext& Context) const override;
 #endif
 
-	// TODO: Detail customization to only show modifiable axes for the relevant binding? This thing has no idea what it's bound to...
+	/**
+	 * The scalar that will be applied to the input value.
+	 *
+	 * For example, if you have a scalar of (2.0, 2.0, 2.0), each input axis will be multiplied by 2.0.
+	 *  
+	 * Note: This will do nothing on boolean input action types, as they can only be true or false.
+	 */
 	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category=Settings)
 	FVector Scalar = FVector::OneVector;
 
