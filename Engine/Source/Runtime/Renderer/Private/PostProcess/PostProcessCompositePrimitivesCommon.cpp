54a1c8836632e6f3e5a9b5f44e891e0d876a0bb0
christopher fiala <christopher.fiala@epicgames.com>
2023-09-20 19:23:24 -0400

    Fixes debug traces failing to draw correctly in offset stereo views, corrects improper UV scaling in MainPopulateSceneDepthPS and reduces special cases during shader setup.
    #jira UE-194945
    #rb jon.cain
    
    [CL 28065475 by christopher fiala in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/PostProcess/PostProcessCompositePrimitivesCommon.cpp
+++ Engine/Source/Runtime/Renderer/Private/PostProcess/PostProcessCompositePrimitivesCommon.cpp
@@ -256,15 +256,11 @@ void PopulateDepthPass(FRDGBuilder& GraphBuilder,
 	{
 		PassParameters->ColorTexture = InSceneColor.Texture;
 		PassParameters->ColorSampler = PointClampSampler;
+	}
 
-		PassParameters->Color = GetScreenPassTextureViewportParameters(View.StereoPass == EStereoscopicPass::eSSP_FULL ? FScreenPassTextureViewport(InSceneColor.Texture->Desc.Extent, View.ViewRect) : FScreenPassTextureViewport(InSceneColor));
-		PassParameters->Depth = GetScreenPassTextureViewportParameters(FScreenPassTextureViewport(InSceneDepth.Texture->Desc.Extent, View.ViewRect));
-	}
-	else
-	{
-		PassParameters->Color = GetScreenPassTextureViewportParameters(FScreenPassTextureViewport(InSceneColor));
-		PassParameters->Depth = GetScreenPassTextureViewportParameters(FScreenPassTextureViewport(InSceneDepth));
-	}
+	PassParameters->Color = GetScreenPassTextureViewportParameters(FScreenPassTextureViewport(InSceneColor));
+	PassParameters->Depth = GetScreenPassTextureViewportParameters(FScreenPassTextureViewport(InSceneDepth));
+
 	PassParameters->DepthTexture = InSceneDepth.Texture;
 	PassParameters->DepthSampler = PointClampSampler;
 	PassParameters->DepthTextureJitter = SceneDepthJitter;
