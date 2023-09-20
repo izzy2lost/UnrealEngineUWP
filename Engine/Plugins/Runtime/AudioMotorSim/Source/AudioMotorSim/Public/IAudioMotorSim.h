e171fe6e9683f509068c27afb789e2de8850fc9f
miles flanagan <miles.flanagan@psyonix.com>
2023-09-20 12:30:57 -0400

    Add ability to disable/enable motor sim components at runtime.
    #rnx
    #rb ryan.mangin
    
    [CL 28040873 by miles flanagan in ue5-main branch]

--- Engine/Plugins/Runtime/AudioMotorSim/Source/AudioMotorSim/Public/IAudioMotorSim.h
+++ Engine/Plugins/Runtime/AudioMotorSim/Source/AudioMotorSim/Public/IAudioMotorSim.h
@@ -49,6 +49,10 @@ public:
 	// Called when something Resets this component
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category="AudioMotorSim", DisplayName = "OnReset")
 	void BP_Reset();
+	
+	// controls whether this will run its update function
+	UFUNCTION(BlueprintCallable, Category="AudioMotorSim")
+	void SetEnabled(bool bNewEnabled);
 
 #if WITH_EDITORONLY_DATA
 	// Input data after running this component
@@ -61,4 +65,9 @@ public:
 
 	virtual void GetCachedData(FAudioMotorSimInputContext& OutInput, FAudioMotorSimRuntimeContext& OutRuntimeInfo);
 #endif
+
+protected:
+	// will only update if enabled
+    UPROPERTY(BlueprintReadOnly, Category="AudioMotorSim")
+    bool bEnabled = true;
 };
