88262553a7ca6d70345d7a310a84447214a1fb5a
jeanmichel dignard <JeanMichel.Dignard@epicgames.com>
2023-09-21 18:25:15 -0400

    Add tooltip for Editor Utility actions when cooked
    #jira UE-195740
    #rb none
    [FYI] daren.cheng
    
    [CL 28111295 by jeanmichel dignard in ue5-main branch]

--- Engine/Source/Editor/Blutility/Private/BlutilityMenuExtensions.cpp
+++ Engine/Source/Editor/Blutility/Private/BlutilityMenuExtensions.cpp
@@ -784,7 +784,7 @@ void FBlutilityMenuExtensions::CreateBlutilityActionsMenu(FMenuBuilder& MenuBuil
 
 			if (FunctionAndUtil.Util->GetUtilityBlueprintAsset().AssetClassPath == UBlueprintGeneratedClass::StaticClass()->GetClassPathName())
 			{
-				TooltipText = LOCTEXT("AssetUtilTooltip", "Click to execute");
+				TooltipText = FText::Format(LOCTEXT("AssetUtilTooltip", "{0}\n\n(Click to execute)"), FunctionAndUtil.FunctionData.TooltipText);
 			}
 			else if (FilterFailureMessage.IsEmpty())
 			{
