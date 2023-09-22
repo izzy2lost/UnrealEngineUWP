2d15051bbf7eaef51e73c8e3db4ef8da2c7607d6
andrew rodham <Andrew.Rodham@epicgames.com>
2023-09-22 17:12:14 -0400

    Fixed up part of CL that does not exist on the branch that CL 28152800 was submitted on
    #rb none
    
    [CL 28157147 by andrew rodham in ue5-main branch]

--- Engine/Source/Runtime/MovieSceneTracks/Private/Systems/MovieSceneCustomPrimitiveDataSystem.cpp
+++ Engine/Source/Runtime/MovieSceneTracks/Private/Systems/MovieSceneCustomPrimitiveDataSystem.cpp
@@ -215,12 +215,17 @@ namespace UE::MovieScene
 				};
 				FBlendInfo IgnoredBlendInfo;
 				FBlendInfo BlendInfo;
+
+				FHierarchicalBlendTarget BlendTarget;
+
 				for (FMovieSceneEntityID Input : Inputs)
 				{
 					FBlendInfo& BlendInfoToUpdate = Linker->EntityManager.HasComponent(Input, BuiltInComponents->Tags.Ignored) ? IgnoredBlendInfo : BlendInfo;
 
 					TOptionalComponentReader<int16> HBiasComponent = Linker->EntityManager.ReadComponent(Input, BuiltInComponents->HierarchicalBias);
 					const int16 HBias = HBiasComponent ? *HBiasComponent : 0;
+
+					BlendTarget.Add(HBias);
 
 					if (HBias > BlendInfoToUpdate.HBias)
 					{
@@ -258,12 +263,16 @@ namespace UE::MovieScene
 					{
 						if (!Linker->EntityManager.HasComponent(Input, BuiltInComponents->HierarchicalBlendTarget))
 						{
-							Linker->EntityManager.AddComponent(Input, BuiltInComponents->HierarchicalBlendTarget, BlendInfo.HBias);
+							Linker->EntityManager.AddComponent(Input, BuiltInComponents->HierarchicalBlendTarget, BlendTarget);
 						}
 						else
 						{
-							Linker->EntityManager.WriteComponentChecked(Input, BuiltInComponents->HierarchicalBlendTarget, BlendInfo.HBias);
+							Linker->EntityManager.WriteComponentChecked(Input, BuiltInComponents->HierarchicalBlendTarget, BlendTarget);
 						}
+					}
+					else
+					{
+						Linker->EntityManager.RemoveComponent(Input, BuiltInComponents->HierarchicalBlendTarget);
 					}
 
 					// Ensure we have the blender type tag on the inputs.
