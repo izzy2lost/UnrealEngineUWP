8d32841d931d9e40932e16c251955245d23fe232
bob tellez <Bob.Tellez@epicgames.com>
2023-09-21 21:23:59 -0400

    [Backout] - CL28116232 [FYI] Justin.Marcus Original CL Desc ----------------------------------------------------------------- Enable GameFeaturePlugin.AsyncLoad by default. [REVIEW] [at]Bob.Tellez [at]Thomas.Ross [at]Dave.Belanger [at]Graeme.Thornton
    [CL 28119346 by bob tellez in ue5-main branch]

--- Engine/Plugins/Experimental/GameFeatures/Source/GameFeatures/Private/GameFeaturePluginStateMachine.cpp
+++ Engine/Plugins/Experimental/GameFeatures/Source/GameFeatures/Private/GameFeaturePluginStateMachine.cpp
@@ -60,7 +60,7 @@ namespace UE::GameFeatures
 		ECVF_Default);
 
 	static TAutoConsoleVariable<bool> CVarAsyncLoad(TEXT("GameFeaturePlugin.AsyncLoad"),
-		true,
+		false,
 		TEXT("Enable to use aysnc loading"));
 
 	static TAutoConsoleVariable<bool> CVarAllowForceMonolithicShaderLibrary(TEXT("GameFeaturePlugin.AllowForceMonolithicShaderLibrary"),
