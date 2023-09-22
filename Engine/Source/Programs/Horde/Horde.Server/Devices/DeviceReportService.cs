42d9fad644ece1728dceae84307731e7aec6c382
Ben Marsh <Ben.Marsh@epicgames.com>
2023-09-22 14:57:00 -0400

    Horde: Remove unused using directives.
    [CL 28149186 by Ben Marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Server/Devices/DeviceReportService.cs
+++ Engine/Source/Programs/Horde/Horde.Server/Devices/DeviceReportService.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using EpicGames.Horde.Api;
 using Horde.Server.Jobs;
 using Horde.Server.Notifications;
 using Horde.Server.Server;
