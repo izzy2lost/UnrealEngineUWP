288e577812f84f3062a664e7ea7c485dd94e41fa
josh adams <josh.adams@epicgames.com>
2023-09-25 09:45:57 -0400

    - Fixed an checkfSlow to handle being passed in EngineDir but missing a trailing slash #rb trivial #rnx
    [CL 28186305 by josh adams in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/Misc/Paths.cpp
+++ Engine/Source/Runtime/Core/Private/Misc/Paths.cpp
@@ -311,8 +311,8 @@ FString FPaths::ConvertPath(const FString& Path, EPathConversion Method, const T
 	}
 	else
 	{
-		checkfSlow(!(bIsProjectConversion && !Path.StartsWith(FPaths::ProjectDir())), TEXT("Called ConvertPath with a Platform_ method, but Path didn't start with FPaths::ProjectDir(). This is not supported"));
-		checkfSlow(!(bIsEngineConversion && !Path.StartsWith(FPaths::EngineDir())), TEXT("Called ConvertPath with a Engine_ method, but Path didn't start with FPaths::EngineDir(). This is not supported"));
+		checkfSlow(!(bIsProjectConversion && !Path.StartsWith(FPaths::ProjectDir().TrimChar('/'))), TEXT("Called ConvertPath with a Platform_ method, but Path [%s] didn't start with FPaths::ProjectDir() [%s]. This is not supported"), *Path, *FPaths::ProjectDir());
+		checkfSlow(!(bIsEngineConversion && !Path.StartsWith(FPaths::EngineDir().TrimChar('/'))), TEXT("Called ConvertPath with a Engine_ method, but Path [%s] didn't start with FPaths::EngineDir() [%s]. This is not supported"), *Path, *FPaths::EngineDir());
 
 		// skip over engine or project directory
 		Prefix = (int)Method < (int)EPathConversion::Project_First ? FPaths::EngineDir() : FPaths::ProjectDir();
