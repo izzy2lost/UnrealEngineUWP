db503bbe7dad6659db328d0b3ef85a1041f6da05
zousar shaker <zousar.shaker@epicgames.com>
2023-09-25 11:40:16 -0400

    Fix issue with use of macro in condition surfaced by static analysis.
    #rb none,Trivial
    
    [CL 28189716 by zousar shaker in ue5-main branch]

--- Engine/Source/Developer/Zen/Private/ZenServerInterface.cpp
+++ Engine/Source/Developer/Zen/Private/ZenServerInterface.cpp
@@ -1804,7 +1804,9 @@ FZenServiceInstance::Initialize()
 static void
 PromptUserToStopRunningServerInstanceForUpdate(const FString& ServerFilePath)
 {
-	if (IS_PROGRAM || FApp::IsUnattended() || IsRunningCommandlet() || GIsRunningUnattendedScript)
+#if !IS_PROGRAM
+	if (FApp::IsUnattended() || IsRunningCommandlet() || GIsRunningUnattendedScript)
+#endif
 	{
 		// Just log as there is no one to show a message
 		UE_LOG(LogZenServiceInstance, Display, TEXT("ZenServer needs to be updated to a new version. Please shut down any tools that are using the ZenServer at '%s'"), *ServerFilePath);
@@ -1819,7 +1821,9 @@ PromptUserToStopRunningServerInstanceForUpdate(const FString& ServerFilePath)
 static void
 PromptUserOfLockedDataFolder(const FString& DataPath)
 {
-	if (IS_PROGRAM || FApp::IsUnattended() || IsRunningCommandlet() || GIsRunningUnattendedScript)
+#if !IS_PROGRAM
+	if (FApp::IsUnattended() || IsRunningCommandlet() || GIsRunningUnattendedScript)
+#endif
 	{
 		// Just log as there is no one to show a message
 		UE_LOG(LogZenServiceInstance, Warning, TEXT("ZenServer Failed to auto launch, an unknown process is locking the data folder '%s'"), *DataPath);
@@ -1834,7 +1838,9 @@ PromptUserOfLockedDataFolder(const FString& DataPath)
 static void
 PromptUserOfFailedShutDownOfExistingProcess(uint16 Port)
 {
-	if (IS_PROGRAM || FApp::IsUnattended() || IsRunningCommandlet() || GIsRunningUnattendedScript)
+#if !IS_PROGRAM
+	if (FApp::IsUnattended() || IsRunningCommandlet() || GIsRunningUnattendedScript)
+#endif
 	{
 		// Just log as there is no one to show a message
 		UE_LOG(LogZenServiceInstance, Warning, TEXT("ZenServer Failed to auto launch, failed to shut down currently running service using port %u"), Port);
