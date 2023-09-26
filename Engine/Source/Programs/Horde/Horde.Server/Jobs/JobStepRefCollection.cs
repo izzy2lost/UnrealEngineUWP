ac9d5a50d98be0bf496a4d7d08ed53783befc8d8
Josh Engebretson <Josh.Engebretson@epicgames.com>
2023-09-26 10:11:27 -0400

    Horde: Make bisection indexes sparse #jira none #rnx
    [CL 28226880 by Josh Engebretson in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Server/Jobs/JobStepRefCollection.cs
+++ Engine/Source/Programs/Horde/Horde.Server/Jobs/JobStepRefCollection.cs
@@ -116,7 +116,7 @@ namespace Horde.Server.Jobs
 		{
 			List<MongoIndex<JobStepRef>> indexes = new List<MongoIndex<JobStepRef>>();
 			indexes.Add(keys => keys.Ascending(x => x.StreamId).Ascending(x => x.TemplateId).Ascending(x => x.Name).Descending(x => x.Change));
-			indexes.Add(_bisectTaskIdIndex = MongoIndex.Create<JobStepRef>(keys => keys.Descending(x => x.BisectTaskId)));			
+			indexes.Add(_bisectTaskIdIndex = MongoIndex.Create<JobStepRef>(keys => keys.Descending(x => x.BisectTaskId), sparse: true));
 
 			_jobStepRefs = mongoService.GetCollection<JobStepRef>("JobStepRefs", indexes);
 			_telemetrySink = telemetrySink;
