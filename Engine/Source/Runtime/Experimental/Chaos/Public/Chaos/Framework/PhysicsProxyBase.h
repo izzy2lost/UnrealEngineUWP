3cbb994795ae3f5a62aec8109755a509f6fce77a
markus boberg <markus.boberg@epicgames.com>
2023-09-27 07:12:16 -0400

    [NetPhysics] Temp disable RenderInterpolation CVars due to compile errors
    [CL 28269342 by markus boberg in ue5-main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/Framework/PhysicsProxyBase.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/Framework/PhysicsProxyBase.h
@@ -125,19 +125,19 @@ public:
 	static float GetRenderInterpErrorCorrectionDuration()
 	{
 		static float RenderInterpErrorCorrectionDuration = 0.5f;
-		static FAutoConsoleVariableRef CVarRenderInterpErrorCorrectionDuration(TEXT("p.RenderInterp.ErrorCorrectionDuration"), RenderInterpErrorCorrectionDuration, TEXT("How long in seconds to apply error correction over."));
+	//	temp disable, static FAutoConsoleVariableRef CVarRenderInterpErrorCorrectionDuration(TEXT("p.RenderInterp.ErrorCorrectionDuration"), RenderInterpErrorCorrectionDuration, TEXT("How long in seconds to apply error correction over."));
 		return RenderInterpErrorCorrectionDuration;
 	}
 	static float GetRenderInterpErrorVelocitySmoothingDuration()
 	{
 		static float RenderInterpErrorVelocitySmoothingDuration = 0.5f;
-		static FAutoConsoleVariableRef CVarRenderInterpErrorVelocitySmoothingDuration(TEXT("p.RenderInterp.ErrorVelocitySmoothingDuration"), RenderInterpErrorVelocitySmoothingDuration, TEXT("How long in seconds to apply error velocity smoothing correction over, should be smaller than or equal to p.RenderInterp.ErrorCorrectionDuration. RENDERINTERPOLATION_VELOCITYSMOOTHING needs to be defined."));
+	//	temp disable, static FAutoConsoleVariableRef CVarRenderInterpErrorVelocitySmoothingDuration(TEXT("p.RenderInterp.ErrorVelocitySmoothingDuration"), RenderInterpErrorVelocitySmoothingDuration, TEXT("How long in seconds to apply error velocity smoothing correction over, should be smaller than or equal to p.RenderInterp.ErrorCorrectionDuration. RENDERINTERPOLATION_VELOCITYSMOOTHING needs to be defined."));
 		return RenderInterpErrorVelocitySmoothingDuration;
 	}
 	static bool GetRenderInterpDebugDraw()
 	{
 		static bool RenderInterpDebugDraw = false;
-		static FAutoConsoleVariableRef CVarRenderInterpDebugDraw(TEXT("p.RenderInterp.DebugDraw"), RenderInterpDebugDraw, TEXT("Draw debug lines for physics render interpolation, also needs p.Chaos.DebugDraw.Enabled set"));
+	//	temp disable, static FAutoConsoleVariableRef CVarRenderInterpDebugDraw(TEXT("p.RenderInterp.DebugDraw"), RenderInterpDebugDraw, TEXT("Draw debug lines for physics render interpolation, also needs p.Chaos.DebugDraw.Enabled set"));
 		return RenderInterpDebugDraw;
 	}
 
