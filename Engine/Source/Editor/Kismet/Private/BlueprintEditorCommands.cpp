9ec8bd2e3007e3141c2a37c53fdb58de0e595eae
jordan hoffmann <jordan.hoffmann@epicgames.com>
2023-09-28 18:34:40 -0400

    [CIS FIX] Remapped "Add Selected Actor Reference(s)" to ctrl-R since there was a collision between it and "add reroute node" and it's the less versatile of the two #rb mike.beach #jira UE-193592
    [CL 28331590 by jordan hoffmann in ue5-main branch]

--- Engine/Source/Editor/Kismet/Private/BlueprintEditorCommands.cpp
+++ Engine/Source/Editor/Kismet/Private/BlueprintEditorCommands.cpp
@@ -417,7 +417,7 @@ void FBlueprintSpawnNodeCommands::RegisterCommands()
 	}
 
 	TSharedPtr<FNodeSpawnInfo> AddActorRefAction = MakeShareable(new FActorRefSpawnInfo);
-	UI_COMMAND(AddActorRefAction->CommandInfo, "Add Selected Actor Reference(s)", "Spawns node(s) which reference the currently selected actor(s) in the level.", EUserInterfaceActionType::Button, FInputChord(EKeys::R));
+	UI_COMMAND(AddActorRefAction->CommandInfo, "Add Selected Actor Reference(s)", "Spawns node(s) which reference the currently selected actor(s) in the level.", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control, EKeys::R));
 	NodeCommands.Add(AddActorRefAction);
 }
 
