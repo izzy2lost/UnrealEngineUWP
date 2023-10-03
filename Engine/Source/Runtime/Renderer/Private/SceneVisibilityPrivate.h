ce61a4a17db4261d4fab97b67a2906208824ed2a
christopher fiala <christopher.fiala@epicgames.com>
2023-10-02 20:34:18 -0400

    Update to CL 27111705 to fix frustum culled primitives not being properly merged. Changes approach to merging primitive lists as well as visibility maps between occlusion and relevance, rather than passing through all primitives for primary views even when culled and filtering later.
    #jira UE-193551
    #rb arciel.rekman
    
    [CL 28402809 by christopher fiala in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/SceneVisibilityPrivate.h
+++ Engine/Source/Runtime/Renderer/Private/SceneVisibilityPrivate.h
@@ -333,6 +333,7 @@ private:
 	{
 		FComputeAndMarkRelevance* Context = nullptr;
 		TCommandPipe<FPrimitiveIndexList> CommandPipe{ TEXT("RelevancePipe") };
+		TCommandPipe<FPrimitiveIndexList>* PrimaryViewCommandPipe; // When using instanced stereo, secondary views will also send their commands to the primary view's command pipe to merge data
 
 	} Relevance;
 
@@ -702,6 +703,7 @@ private:
 	const bool bLaunchOnAddPrimitive;
 	bool bFinished = false;
 	bool bFinalized = false;
+	FSceneBitArray InstancedPrimitiveAddedMap;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
