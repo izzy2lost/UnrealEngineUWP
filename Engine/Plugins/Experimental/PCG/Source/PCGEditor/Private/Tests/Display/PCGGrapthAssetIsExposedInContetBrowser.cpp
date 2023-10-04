ab4e1663b7265e5a10a578c32aaff45064174b5f
julien lheureux <julien.lheureux@epicgames.com>
2023-10-04 15:59:02 -0400

    Fixed editor automation test around PCG
    #rb trivial
    #jira UE-197039
    
    [CL 28471710 by julien lheureux in ue5-main branch]

--- Engine/Plugins/Experimental/PCG/Source/PCGEditor/Private/Tests/Display/PCGGrapthAssetIsExposedInContetBrowser.cpp
+++ Engine/Plugins/Experimental/PCG/Source/PCGEditor/Private/Tests/Display/PCGGrapthAssetIsExposedInContetBrowser.cpp
@@ -12,32 +12,18 @@ IMPLEMENT_CUSTOM_SIMPLE_AUTOMATION_TEST(FPCGGraphExposedInContentMenuTest, FPCGT
 bool FPCGGraphExposedInContentMenuTest::RunTest(const FString& Parameters)
 {
 	const FName NAME_AssetTools = "AssetTools";
-	const FString PluginName = "PCG";
-	uint32 FactoryCategories = 0;
 	bool pcgGraphFound = false;
 
 	FAssetToolsModule& AssetToolsModule = FModuleManager::LoadModuleChecked<FAssetToolsModule>(NAME_AssetTools);
-
-	TArray<FAdvancedAssetCategory> AdvancedAssetCategories;
-	AssetToolsModule.Get().GetAllAdvancedAssetCategories(/*out*/ AdvancedAssetCategories);
-
-	FAdvancedAssetCategory* PCGAssetCategory = AdvancedAssetCategories.FindByPredicate([&PluginName](const FAdvancedAssetCategory& AdvancedAssetCategory) {
-		return AdvancedAssetCategory.CategoryName.ToString().Contains(PluginName);
-		});
-
-	UTEST_NOT_NULL(TEXT("PCG category not found"), PCGAssetCategory)
 
 	// We check if the PCG category factory's display name contains "PCG Graph"
 	TArray<UFactory*> Factories = AssetToolsModule.Get().GetNewAssetFactories();
 	for (UFactory* Factory : Factories)
 	{
-		FactoryCategories = Factory->GetMenuCategories();
-		if (FactoryCategories & PCGAssetCategory->CategoryType)
+		if (Factory->GetDisplayName().ToString().Contains("PCG Graph"))
 		{
-			if (Factory->GetDisplayName().ToString().Contains("PCG Graph")) {
-				pcgGraphFound = true;
-				break;
-			}
+			pcgGraphFound = true;
+			break;
 		}
 	}
 
