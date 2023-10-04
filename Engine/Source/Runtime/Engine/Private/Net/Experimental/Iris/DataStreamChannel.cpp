3d0969b566a209a6be379d88bec567f0d230c6f5
mattias hornlund <Mattias.Hornlund@epicgames.com>
2023-10-04 08:08:21 -0400

    Iris - Do not warn if DataStreamChannel is not open
    #rb Peter.engstrom
    
    [CL 28450982 by mattias hornlund in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Net/Experimental/Iris/DataStreamChannel.cpp
+++ Engine/Source/Runtime/Engine/Private/Net/Experimental/Iris/DataStreamChannel.cpp
@@ -206,7 +206,7 @@ void UDataStreamChannel::Tick()
 	// We probably want separate bandwidth management for iris as we are not pre-filling sendbuffer before call to NetReady.
 	if (!IsNetReady(UE::Net::Private::bIrisSaturateBandwidth))
 	{
-		UE_LOG(LogIris, Warning, TEXT("Disallowed to write first object in batch, with Iris this is not good!"))
+		UE_CLOG(bHandshakeComplete, LogIris, Warning, TEXT("Disallowed to write first packet in batch, with Iris this is not good!"))
 		return;
 	}
 
