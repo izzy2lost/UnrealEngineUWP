11ac3c94f1741cd1f092e7ed754bc6aca49c0c19
ludovic chabant <Ludovic.Chabant@epicgames.com>
2023-10-04 18:29:43 -0400

    Sequencer: upgrade visibility track to ECS evaluation
    #rb andrew.rodham
    #jira UE-126074
    
    [CL 28479929 by ludovic chabant in ue5-main branch]

--- Engine/Source/Runtime/MovieSceneTracks/Public/Tracks/MovieSceneVisibilityTrack.h
+++ Engine/Source/Runtime/MovieSceneTracks/Public/Tracks/MovieSceneVisibilityTrack.h
@@ -4,7 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "UObject/ObjectMacros.h"
-#include "Tracks/MovieSceneBoolTrack.h"
+#include "Tracks/MovieScenePropertyTrack.h"
+
 #include "MovieSceneVisibilityTrack.generated.h"
 
 /**
@@ -12,14 +13,13 @@
  */
 UCLASS(MinimalAPI)
 class UMovieSceneVisibilityTrack
-	: public UMovieSceneBoolTrack
+	: public UMovieScenePropertyTrack
 {
 	GENERATED_UCLASS_BODY()
 
 public:
 
 	// UMovieSceneTrack interface
-	virtual FMovieSceneEvalTemplatePtr CreateTemplateForSection(const UMovieSceneSection& InSection) const override;
 	virtual void PostLoad() override;
 	virtual bool SupportsType(TSubclassOf<UMovieSceneSection> SectionClass) const override;
 	virtual UMovieSceneSection* CreateNewSection() override;
