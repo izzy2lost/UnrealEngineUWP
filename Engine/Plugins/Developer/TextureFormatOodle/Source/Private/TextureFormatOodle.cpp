e6737fe03c26e4f7d7893b95528a21c6cdf2ea2f
fabian giesen <fabian.giesen@epicgames.com>
2023-10-05 14:43:38 -0400

    Oodle 2.9.11
    Also add official VisionOS, Win ARM64 libs.
    
    #rb charles.bloom
    
    [CL 28509461 by fabian giesen in ue5-main branch]

--- Engine/Plugins/Developer/TextureFormatOodle/Source/Private/TextureFormatOodle.cpp
+++ Engine/Plugins/Developer/TextureFormatOodle/Source/Private/TextureFormatOodle.cpp
@@ -813,7 +813,8 @@ public:
 			TEXT("2.9.7"),
 			TEXT("2.9.8"),
 			TEXT("2.9.9"),
-			TEXT("2.9.10")
+			TEXT("2.9.10"),
+			TEXT("2.9.11")
 		};
 		const int32 OodleTextureVersionsCount = (int32)( sizeof(OodleTextureVersions)/sizeof(OodleTextureVersions[0]) );
 
