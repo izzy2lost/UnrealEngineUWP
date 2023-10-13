90c67d3d53636a53060d2fb72db7be293bc19ec2
shawn mcgrath <shawn.mcgrath@epicgames.com>
2023-10-12 21:03:13 -0400

    - EVVM ARM half float to match X64 #rb none #trivial
    [CL 28738538 by shawn mcgrath in ue5-main branch]

--- Engine/Source/Runtime/VectorVM/Private/VectorVMExperimental.cpp
+++ Engine/Source/Runtime/VectorVM/Private/VectorVMExperimental.cpp
@@ -386,9 +386,7 @@ VM_FORCEINLINE VectorRegister4i VVMIntLShift(VectorRegister4i v0, VectorRegister
 static FORCEINLINE void VVM_floatToHalf(void *output, float const *input)
 {
 	float16x4_t out0 = vcvt_f16_f32(vld1q_f32(input + 0));
-	float16x4_t out1 = vcvt_f16_f32(vld1q_f32(input + 4));
 	vst1_f16((__fp16 *)((char *)output + 0), out0);
-	vst1_f16((__fp16 *)((char *)output + 4), out1);
 }
 
 
