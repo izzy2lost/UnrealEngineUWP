f3c6c1dd58e7452d5eeaac41f2732e99246bebae
tomasz obrebski <tomasz.obrebski@epicgames.com>
2023-10-16 11:26:29 -0400

    Gauntlet device configuration profiles
    #rb wojciech.krywult, David.Harvey
    
    [CL 28799239 by tomasz obrebski in ue5-main branch]

--- Engine/Source/Programs/AutomationTool/Gauntlet/Unreal/RunUnreal.cs
+++ Engine/Source/Programs/AutomationTool/Gauntlet/Unreal/RunUnreal.cs
@@ -180,6 +180,8 @@ namespace Gauntlet
 
 				UnrealTargetPlatform PlatformType = UnrealTargetPlatform.Parse(PlatformString);
 
+				SetupPlatformConfigurationProfiles(PlatformType, ContextOptions);
+
 				if (!InitializedDevices)
 				{
 					// Setup the devices and assign them to the executor
@@ -450,12 +452,14 @@ namespace Gauntlet
 
 		protected void SetupDevices(UnrealTargetPlatform DefaultPlatform, UnrealTestOptions Options)
 		{
-
 			Reservation.ReservationDetails = Options.JobDetails;
 
 			DevicePool.Instance.SetLocalOptions(Options.TempDir, Options.Parallel > 1, Options.DeviceURL);
 			DevicePool.Instance.AddLocalDevices(Options.MaxLocalDevices);
 			DevicePool.Instance.AddVirtualDevices(Options.MaxVirtualDevices);
+
+			string EngineDeviceConfigDir = Path.Combine(Globals.UnrealRootDir, "Engine", "Build", "DeviceConfigProfiles");
+			string ProjectDeviceConfigDir = Path.Combine(Options.ProjectPath.Directory.FullName, "Build", "DeviceConfigProfiles");
 
 			foreach (var DeviceWithParams in Options.DeviceList)
 			{
@@ -472,6 +476,15 @@ namespace Gauntlet
 
 				DevicePool.Instance.AddDevices(Platform, DeviceWithParams.Argument);
 			}
+		}
+
+		protected void SetupPlatformConfigurationProfiles(UnrealTargetPlatform PlatformType, UnrealTestOptions Options)
+		{
+			string EngineDeviceConfigDir = Path.Combine(Globals.UnrealRootDir, "Engine", "Build", "DeviceConfigProfiles");
+			string ProjectDeviceConfigDir = Path.Combine(Options.ProjectPath.Directory.FullName, "Build", "DeviceConfigProfiles");
+
+			DeviceConfigurationCache.Instance.DiscoverConfigurationProfiles(PlatformType, "Engine", EngineDeviceConfigDir);
+			DeviceConfigurationCache.Instance.DiscoverConfigurationProfiles(PlatformType, Options.Project, ProjectDeviceConfigDir);
 		}
 	}
 }
