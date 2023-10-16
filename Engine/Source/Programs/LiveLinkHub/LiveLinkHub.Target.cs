504e888f5eedbcce354a09175dd3dea55e01478f
jeremie roy <jeremie.roy@epicgames.com>
2023-10-16 12:18:42 -0400

    Initial submission of livelink hub program launcher
    #jira UE-195057
    #rb jason.walter, geoffrey.douglas
    
    [CL 28801840 by jeremie roy in ue5-main branch]

---
+++ Engine/Source/Programs/LiveLinkHub/LiveLinkHub.Target.cs
@@ -0,0 +1,45 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+[SupportedPlatforms(UnrealPlatformClass.Desktop)]
+public class LiveLinkHubTarget : TargetRules
+{
+	public LiveLinkHubTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Program;
+		LinkType = TargetLinkType.Modular;
+		LaunchModuleName = "LiveLinkHubLauncher";
+
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+
+		SolutionDirectory = "Programs/LiveLink";
+
+		AdditionalPlugins.Add("LiveLink");
+		AdditionalPlugins.Add("ContentBrowserAssetDataSource");
+		AdditionalPlugins.Add("StructUtils");
+		AdditionalPlugins.Add("UdpMessaging");
+		AdditionalPlugins.Add("QuicMessaging");
+
+		bCompileAgainstCoreUObject = true;
+		bCompileAgainstEngine = true;
+		bCompileAgainstEditor = true;
+		bBuildWithEditorOnlyData = true;
+		bIncludePluginsForTargetPlatforms = false;
+		bUsesSlate = true;
+
+		bCompileWithPluginSupport = true;
+		bBuildDeveloperTools = true;
+		bIsBuildingConsoleApplication = false;
+
+		GlobalDefinitions.Add("WITH_LIVELINK_HUB=1");
+
+		bEnableTrace = false;
+
+		OptedInModulePlatforms = new UnrealTargetPlatform[] { UnrealTargetPlatform.Win64, UnrealTargetPlatform.Mac,
+															  UnrealTargetPlatform.Linux, UnrealTargetPlatform.LinuxArm64 };
+
+		// todo: Look into using ExeBinariesSubFolder
+	}
+}
---
+++ Engine/Source/Programs/LiveLinkHub/Private/LiveLinkHubRun.cpp
@@ -0,0 +1,25 @@
+#include "LiveLinkHubRun.h"
+
+#include "LaunchEngineLoop.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogLiveLinkHub, Log, All);
+
+#define LOCTEXT_NAMESPACE "LiveLinkHub"
+
+int32 RunLiveLinkHub(const TCHAR* CommandLine)
+{
+	FTaskTagScope Scope(ETaskTag::EGameThread);
+	
+	// Start up the main loop, adding some extra command line arguments:
+	const int32 Result = GEngineLoop.PreInit(*FString::Printf(TEXT("%s %s"), CommandLine, TEXT("LiveLinkHubCommandlet -Messaging")));
+
+	if (Result != 0)
+	{
+		UE_LOG(LogLiveLinkHub, Error, TEXT("EngineLoop PreInit failed!"));
+		return Result;
+	}
+
+	GEngineLoop.Exit();
+
+	return Result;
+}
