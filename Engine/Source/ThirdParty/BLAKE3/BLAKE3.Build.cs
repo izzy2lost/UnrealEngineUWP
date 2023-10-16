39fe1e489a329088b45fe79d96005ed497897d0d
joe kirchoff <joe.kirchoff@epicgames.com>
2023-10-16 16:37:06 -0400

    BLAKE3: Win arm64 lib
    #rnx
    
    [CL 28816866 by joe kirchoff in ue5-main branch]

--- Engine/Source/ThirdParty/BLAKE3/BLAKE3.Build.cs
+++ Engine/Source/ThirdParty/BLAKE3/BLAKE3.Build.cs
@@ -18,7 +18,7 @@ public class BLAKE3 : ModuleRules
 
 		if (Target.IsInPlatformGroup(UnrealPlatformGroup.Windows))
 		{
-			PublicAdditionalLibraries.Add(Path.Combine(LibraryPath, "Win64", "Release", "BLAKE3.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(LibraryPath, "Win64", Target.Architecture.bIsX64 ? string.Empty : "arm64", "Release", "BLAKE3.lib"));
 		}
 		else if (Target.Platform == UnrealTargetPlatform.Mac)
 		{
