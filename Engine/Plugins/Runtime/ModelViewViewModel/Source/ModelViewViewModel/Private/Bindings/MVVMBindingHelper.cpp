1cde09da8395497cb15d3b83721ad8d4823cd159
patrick boutot <Patrick.Boutot@epicgames.com>
2023-10-16 13:02:18 -0400

    MVVM: Make sure the different object ptr type matches before copying them. Evaluated path use the correct container address when evaluating a function. #jira UE-194034 #rb editor-ui-systems, zousar.shaker
    [CL 28804088 by patrick boutot in ue5-main branch]

--- Engine/Plugins/Runtime/ModelViewViewModel/Source/ModelViewViewModel/Private/Bindings/MVVMBindingHelper.cpp
+++ Engine/Plugins/Runtime/ModelViewViewModel/Source/ModelViewViewModel/Private/Bindings/MVVMBindingHelper.cpp
@@ -461,7 +461,11 @@ namespace UE::MVVM::BindingHelper
 				&& DestinationObjectProperty
 				&& SourceObjectProperty->PropertyClass
 				&& DestinationObjectProperty->PropertyClass
-				&& SourceObjectProperty->PropertyClass->IsChildOf(DestinationObjectProperty->PropertyClass);
+				&& SourceObjectProperty->PropertyClass->IsChildOf(DestinationObjectProperty->PropertyClass)
+				&& (
+					SourceObjectProperty->GetClass() == DestinationObjectProperty->GetClass()
+					|| SourceObjectProperty->AllowObjectTypeReinterpretationTo(DestinationObjectProperty) // prevents TWeakObjectPtr to UObject*, allow TObjectPtr to UObject*
+				);
 		}
 
 		bool IsObjectArrayPropertyCompatible(const FProperty* Source, const FProperty* Destination)
