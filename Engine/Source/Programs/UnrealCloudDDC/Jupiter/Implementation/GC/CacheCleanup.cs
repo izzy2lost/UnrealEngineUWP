54b473b81c9e9bea91da54bdc0be6b83fa738198
Joakim Lindqvist <Joakim.Lindqvist@epicgames.com>
2023-10-17 09:51:44 -0400

    Unreal Cloud DDC - Bypass cache when reading back ref value to check if its finalized - this avoids thrasing the scylla cache while doing the full table scan that is GC.
    #rb none
    
    [CL 28837726 by Joakim Lindqvist in ue5-main branch]

--- Engine/Source/Programs/UnrealCloudDDC/Jupiter/Implementation/GC/CacheCleanup.cs
+++ Engine/Source/Programs/UnrealCloudDDC/Jupiter/Implementation/GC/CacheCleanup.cs
@@ -132,7 +132,7 @@ namespace Jupiter.Implementation
 					{
 						try
 						{
-							RefRecord refRecord = await _referencesStore.GetAsync(ns, bucket, name, IReferencesStore.FieldFlags.None, IReferencesStore.OperationFlags.None);
+							RefRecord refRecord = await _referencesStore.GetAsync(ns, bucket, name, IReferencesStore.FieldFlags.None, IReferencesStore.OperationFlags.BypassCache);
 							if (!refRecord.IsFinalized)
 							{
 								_logger.LogInformation("Deleting object {Namespace} {Bucket} {Name} as it is not finalized", ns, bucket, name);
