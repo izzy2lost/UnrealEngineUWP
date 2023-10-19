c9257739c23dff64e82dfba19c9b2b54f7f9653e
sameer mirza <sameer.mirza@epicgames.com>
2023-10-19 15:16:14 -0400

    Tooltip on Links for Material Expressions #jira UE-190993 #rb Jason.Nadro
    [CL 28929367 by sameer mirza in ue5-main branch]

--- Engine/Source/Editor/GraphEditor/Private/MaterialGraphConnectionDrawingPolicy.cpp
+++ Engine/Source/Editor/GraphEditor/Private/MaterialGraphConnectionDrawingPolicy.cpp
@@ -238,8 +238,8 @@ TSharedPtr<IToolTip> FMaterialGraphConnectionDrawingPolicy::GetConnectionToolTip
 	TSharedPtr<SGraphPin> Pin2Widget;
 	OverlapData.GetPinWidgets(GraphPanel, Pin1Widget, Pin2Widget);
 
-	const FText LeftText = FText::Format(NSLOCTEXT("Unreal", "PinConnectionTooltipLeft", "<< {0}"), GetNodePinInfo(Pin1Widget));
-	const FText RightText = FText::Format(NSLOCTEXT("Unreal", "PinConnectionTooltipRight", "{0} >>"), GetNodePinInfo(Pin2Widget));
+	const FText LeftText = Pin1Widget != nullptr ? FText::Format(NSLOCTEXT("Unreal", "PinConnectionTooltipLeft", "<< {0}"), GetNodePinInfo(Pin1Widget)) : FText();
+	const FText RightText = Pin2Widget != nullptr ? FText::Format(NSLOCTEXT("Unreal", "PinConnectionTooltipRight", "{0} >>"), GetNodePinInfo(Pin2Widget)) : FText();
 
 	return SNew(SToolTip)
 		[
