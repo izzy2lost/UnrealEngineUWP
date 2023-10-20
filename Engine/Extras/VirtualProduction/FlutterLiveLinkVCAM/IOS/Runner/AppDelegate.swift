c1fd35f9cdd5225c76a4f4f347c65662717b1354
elliot colp <elliot.colp@xa.epicgames.com>
2023-10-20 18:42:03 -0400

    [LiveLinkVCAM] iOS implementation of pixel streaming #jira UE-197363 #rb Jason.Walter
    [CL 28987248 by elliot colp in ue5-main branch]

--- Engine/Extras/VirtualProduction/FlutterLiveLinkVCAM/IOS/Runner/AppDelegate.swift
+++ Engine/Extras/VirtualProduction/FlutterLiveLinkVCAM/IOS/Runner/AppDelegate.swift
@@ -1,13 +1,45 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
 import UIKit
 import Flutter
+import WebRTC
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  var peerConnectionApi: FlutterRtcPeerConnectionApi?
+  var videoViewControllerApi: FlutterRtcVideoViewControllerApi?
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    RTCInitializeSSL()
+    
     GeneratedPluginRegistrant.register(with: self)
+    
+    // Register RtcVideoView platform view
+    var videoViewRegistrar: FlutterPluginRegistrar? = self.registrar(forPlugin: "RtcVideoView")
+    if (videoViewRegistrar == nil) {
+      fatalError("Failed to register RtcVideoView plugin")
+    }
+    
+    videoViewRegistrar?.register(
+      RtcVideoViewFactory(
+        messenger: videoViewRegistrar!.messenger()
+      ),
+      withId: "com.epicgames.live_link_vcam.view.RtcVideoView"
+    )
+    
+    // Register Pigeon APIs
+    let controller = window?.rootViewController as! FlutterViewController
+    
+    peerConnectionApi = FlutterRtcPeerConnectionApi(binaryMessenger: controller.binaryMessenger)
+    videoViewControllerApi = FlutterRtcVideoViewControllerApi(binaryMessenger: controller.binaryMessenger)
+  
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  override func applicationWillTerminate(_ application: UIApplication) {
+    peerConnectionApi?.disposeAll()
   }
 }
---
+++ Engine/Extras/VirtualProduction/FlutterLiveLinkVCAM/IOS/Runner/WebRTC/API/FlutterRtcPeerConnectionApi.swift
@@ -0,0 +1,125 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+import Foundation
+import WebRTC
+
+/// Handles messages about FlutterRtcPeerConnections to and from the Flutter API.
+class FlutterRtcPeerConnectionApi: FlutterPluginApi<RtcPeerConnectionFlutterApi> {
+  init(binaryMessenger: FlutterBinaryMessenger) {
+    super.init(flutter: RtcPeerConnectionFlutterApi(binaryMessenger: binaryMessenger))
+    
+    RtcPeerConnectionHostApiSetup.setUp(binaryMessenger: binaryMessenger, api: self)
+    FlutterRtcPeerConnectionApi.instance = self
+  }
+  
+  deinit {
+    if (FlutterRtcPeerConnectionApi.instance === self) {
+      FlutterRtcPeerConnectionApi.instance = nil
+    }
+  }
+  
+  /// The active singleton instance of this class.
+  private(set) static var instance: FlutterRtcPeerConnectionApi?
+  
+  /// Manager for indexed peer connections shared with Flutter.
+  private let peerConnectionManager = IdObjectManager<FlutterRtcPeerConnection>(debugTypeName: "PeerConnection")
+  
+  /// Manager for indexed media stream tracks shared with Flutter.
+  let mediaStreamTrackManager = IdObjectManager<FlutterRtcMediaStreamTrack>(debugTypeName: "MediaStreamTrack")
+  
+  /// Video decoder used for incoming video streams. Not actually used, but necessary to create an RTCPeerConnectionFactory.
+  private let videoEncoderFactory = RTCDefaultVideoEncoderFactory()
+  
+  /// Video decoder used for incoming video streams.
+  private let videoDecoderFactory = RTCDefaultVideoDecoderFactory()
+  
+  /// Factory used to create PeerConnections as requested by the client.
+  private var _peerConnectionFactory: RTCPeerConnectionFactory?
+  private var peerConnectionFactory: RTCPeerConnectionFactory {
+    get {
+      if (_peerConnectionFactory == nil) {
+        _peerConnectionFactory = makePeerConnectionFactory()
+      }
+      
+      return _peerConnectionFactory!
+    }
+    
+    set {
+      _peerConnectionFactory = newValue
+    }
+  }
+  
+  /// Field trials passed from Flutter to be used when creating connections.
+  private var fieldTrials: Dictionary<String, String>? = [:]
+  
+  /// Clean up any allocated resources.
+  func disposeAll() {
+    peerConnectionManager.disposeAll()
+    mediaStreamTrackManager.disposeAll()
+    _peerConnectionFactory = nil
+  }
+  
+  /// Make a new factory for producing peer connections with the current configuration.
+  private func makePeerConnectionFactory() -> RTCPeerConnectionFactory {
+    RTCInitFieldTrialDictionary(fieldTrials)
+    
+    return RTCPeerConnectionFactory(encoderFactory: videoEncoderFactory, decoderFactory: videoDecoderFactory)
+  }
+}
+
+// MARK: Host API
+extension FlutterRtcPeerConnectionApi: RtcPeerConnectionHostApi {
+  func setFieldTrials(fieldTrials: [String: String]?) throws {
+    self.fieldTrials = fieldTrials
+    
+    // Dispose of existing factory since the settings were changed.
+    // This will be lazily recreated.
+    _peerConnectionFactory = nil
+  }
+  
+  func create() throws -> Int64 {
+    let config = RTCConfiguration()
+    config.sdpSemantics = .unifiedPlan
+    config.continualGatheringPolicy = .gatherContinually
+    
+    return peerConnectionManager.register(newObject: FlutterRtcPeerConnection(
+      api: self,
+      peerConnectionFactory: peerConnectionFactory,
+      config: config
+    ))
+  }
+  
+  func dispose(connectionId: Int64) throws {
+    peerConnectionManager.unregister(id: connectionId)?.onUnregistered()
+  }
+  
+  func setRemoteDescription(connectionId: Int64, description: RtcSessionDescription, completion: @escaping (Result<Void, Error>) -> Void) {
+    peerConnectionManager
+      .getChecked(id: connectionId)
+      .setRemoteDescription(
+        description: description,
+        completion: completion
+      )
+  }
+  
+  func setLocalDescription(connectionId: Int64, description: RtcSessionDescription, completion: @escaping (Result<Void, Error>) -> Void) {
+    peerConnectionManager
+      .getChecked(id: connectionId)
+      .setLocalDescription(
+        description: description,
+        completion: completion
+      )
+  }
+  
+  func addRemoteCandidate(connectionId: Int64, candidate: RtcIceCandidate, completion: @escaping (Result<Void, Error>) -> Void) {
+    peerConnectionManager
+      .getChecked(id: connectionId)
+      .addRemoteCandidate(candidate: candidate, completion: completion)
+  }
+
+  func createAnswer(connectionId: Int64, completion: @escaping (Result<RtcSessionDescription, Error>) -> Void) {
+    peerConnectionManager
+      .getChecked(id: connectionId)
+      .createAnswer(completion: completion)
+  }
+}
