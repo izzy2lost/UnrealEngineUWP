fb2cb63ea3b6e844182b66bb57bf8f21bc4a1e61
ola olsson <Ola.Olsson@epicgames.com>
2023-10-20 05:00:58 -0400

    Add note to explain that setting ShadowResolutionScale to zero disables shadow maps but not contact shadows.
    #rb wouter.dek
    
    [CL 28957316 by ola olsson in ue5-main branch]

--- Engine/Source/Runtime/Engine/Classes/Components/LightComponent.h
+++ Engine/Source/Runtime/Engine/Classes/Components/LightComponent.h
@@ -81,6 +81,7 @@ class ULightComponent : public ULightComponentBase
 
 	/** 
 	 * Scales the resolution of shadowmaps used to shadow this light.  By default shadowmap resolution is chosen based on screen size of the caster. 
+	 * Setting the scale to zero disables shadow maps, but does not disable, e.g., contact shadows.
 	 * Note: shadowmap resolution is still clamped by 'r.Shadow.MaxResolution'
 	 */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Light, AdvancedDisplay, meta=(UIMin = ".125", UIMax = "8"))
