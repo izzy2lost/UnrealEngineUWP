b54eb0607b9e1af050e42116f547892e1d99b80a
marc audy <Marc.Audy@epicgames.com>
2023-10-20 01:28:14 -0400

    Fix warning V591: Non-void function should return a value. [CODEREVIEW] Michael.Lentine
    [CL 28955613 by marc audy in ue5-main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Public/PhysicsProxy/JointConstraintProxy.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/PhysicsProxy/JointConstraintProxy.h
@@ -114,7 +114,7 @@ class TThreadedJointConstraintPhysicsProxyBase : protected FJointConstraintPhysi
 public:
 
 #define CHAOS_INNER_JOINT_PROPERTY(OuterProp, FuncName, Inner, InnerType)\
-	const InnerType& Get##FuncName() const { ReadRef([](const auto& Data){ return Data.Inner;}); }\
+	const InnerType& Get##FuncName() const { return ReadRef([](const auto& Data){ return Data.Inner;}); }\
 	void Set##FuncName(const InnerType& Val) { Write([&Val](auto& Data){ Data.Inner = Val;}); }\
 
 #include "Chaos/JointProperties.inl"
