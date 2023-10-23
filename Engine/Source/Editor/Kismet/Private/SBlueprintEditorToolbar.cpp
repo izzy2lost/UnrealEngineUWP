76ba9126918770a3759cd5b8f09793d3749bbd7c
dave jones2 <dave.jones2@epicgames.com>
2023-10-23 16:49:59 -0400

    Removed really old tooltip. "Blueprint Props" hasn't been used in about eight years at this point.
    #rb ben.hoffman
    
    [CL 29022987 by dave jones2 in ue5-main branch]

--- Engine/Source/Editor/Kismet/Private/SBlueprintEditorToolbar.cpp
+++ Engine/Source/Editor/Kismet/Private/SBlueprintEditorToolbar.cpp
@@ -274,7 +274,7 @@ void FFullBlueprintEditorCommands::RegisterCommands()
 	UI_COMMAND(SwitchToBlueprintDefaultsMode, "Defaults", "Switches to Class Defaults Mode", EUserInterfaceActionType::ToggleButton, FInputChord());
 	UI_COMMAND(SwitchToComponentsMode, "Components", "Switches to Components Mode", EUserInterfaceActionType::ToggleButton, FInputChord());
 
-	UI_COMMAND(EditGlobalOptions, "Class Settings", "Edit Class Settings (Previously known as Blueprint Props)", EUserInterfaceActionType::ToggleButton, FInputChord());
+	UI_COMMAND(EditGlobalOptions, "Class Settings", "Edit Class Settings", EUserInterfaceActionType::ToggleButton, FInputChord());
 	UI_COMMAND(EditClassDefaults, "Class Defaults", "Edit the initial values of your class.", EUserInterfaceActionType::ToggleButton, FInputChord());
 
 	UI_COMMAND(JumpToErrorNode, "Jump to Error Node", "When enabled, then the Blueprint will snap focus to nodes producing an error during compilation", EUserInterfaceActionType::ToggleButton, FInputChord());
