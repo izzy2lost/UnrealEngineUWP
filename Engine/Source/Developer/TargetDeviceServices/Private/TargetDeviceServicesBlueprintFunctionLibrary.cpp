95794253801a0ee58005cdef7415cdb9d2a4744a
oleg krasnov <oleg.krasnov@xa.epicgames.com>
2023-10-25 12:43:25 -0400

    TargetDeviceSessions - Fix build issue for Mac Editor
    #rb David.Harvey
    
    [CL 29092887 by oleg krasnov in ue5-main branch]

--- Engine/Source/Developer/TargetDeviceServices/Private/TargetDeviceServicesBlueprintFunctionLibrary.cpp
+++ Engine/Source/Developer/TargetDeviceServices/Private/TargetDeviceServicesBlueprintFunctionLibrary.cpp
@@ -59,9 +59,7 @@ TMap<FString, FDeviceSnapshots> UTargetDeviceServicesBlueprintFunctionLibrary::G
 				FDeviceSnapshots* DeviceSnapshotsOfCurrentDeviceTypePtr = Snapshots.Find(DefaultDeviceType);
 				if (nullptr == DeviceSnapshotsOfCurrentDeviceTypePtr)
 				{
-					// Initializer list constructor call
-					TArray<FDeviceSnapshot> Entries{MoveTemp(DeviceSnapshot)};
-					Snapshots.Emplace(DefaultDeviceType, MoveTemp(Entries));
+					Snapshots.Emplace(DefaultDeviceType, FDeviceSnapshots{TArray<FDeviceSnapshot>{MoveTemp(DeviceSnapshot)}});
 				}
 				else
 				{
