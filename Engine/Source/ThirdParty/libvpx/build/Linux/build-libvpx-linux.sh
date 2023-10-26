8f660195389353b3dab15f2069c9b1d406d2b306
sergey ksinishin <sergey.ksinishin@epicgames.com>
2023-10-26 08:51:33 -0400

    Update scripts to build the Linux version of a library in the docker using Epic's toolchain
    #rnx
    [FYI] Achim.Moller, ivan.sidarau, Jens.Petersam
    
    [CL 29130858 by sergey ksinishin in ue5-main branch]

--- Engine/Source/ThirdParty/libvpx/build/Linux/build-libvpx-linux.sh
+++ Engine/Source/ThirdParty/libvpx/build/Linux/build-libvpx-linux.sh
@@ -59,13 +59,25 @@ tar xzf $TARBALL -C $SCRATCH_DIR
 BUILD_CFLAGS="-fvisibility=hidden"
 CONFIG_OPTIONS="--enable-postproc --enable-multi-res-encoding --enable-temporal-denoising --enable-vp9-temporal-denoising --enable-vp9-postproc --size-limit=16384x16384 --enable-realtime-only --disable-examples --disable-tools --disable-docs --disable-unit-tests --disable-libyuv --enable-vp9-highbitdepth --disable-avx512 --disable-shared --enable-static --as=yasm"
 SLICE="x86_64-linux-gcc"
+# SLICE="generic-gnu"
+
+# setup for using custom toolchain
+if [[ ! -z "$UE_SYSROOT" ]]; then
+   TOOLCHAIN="x86_64-unknown-linux-gnu"
+   export CFLAGS="--sysroot=$UE_SYSROOT/$TOOLCHAIN"
+   export CXXFLAGS="--sysroot=$UE_SYSROOT/$TOOLCHAIN -I$UE_SYSROOT/include/c++/v1"
+   export LDFLAGS="--sysroot=$UE_SYSROOT/$TOOLCHAIN"
+   export CROSS="$TOOLCHAIN-"
+   export PATH=$UE_SYSROOT/$TOOLCHAIN/bin:$PATH
+fi
 
 cd $DIR
 echo "#######################################"
 echo "# Configuring $LIBVPX_DIR"
 ./configure --target=${SLICE} ${CONFIG_OPTIONS} --disable-pic --extra-cflags="${BUILD_CFLAGS}" > $DEST_DIR/build.log
 echo "# Building $LIBVPX_DIR"
-make -j >> $DEST_DIR/build.log
+# use make -j SHELL="sh -x" to see command line of every call
+make -j8 >> $DEST_DIR/build.log 2>&1
 if [ $? -ne 0 ]; then
 	echo ""
 	echo "#######################################"
@@ -75,16 +87,19 @@ if [ $? -ne 0 ]; then
 fi
 # use some hardcoded knowledge and get static library out
 cp $DIR/libvpx.a $DEST_DIR/libvpx.a
+cp $DIR/libvpx_g.a $DEST_DIR/libvpx_g.a
 
 #####################
 # build PIC version
 
 cd $DIR
+make clean > /dev/null
 echo "#######################################"
 echo "# Configuring $LIBVPX_DIR with PIC"
 ./configure --target=${SLICE} ${CONFIG_OPTIONS} --enable-pic --extra-cflags="${BUILD_CFLAGS}" > $DEST_DIR/build-pic.log
 echo "# Building $LIBVPX_DIR with PIC"
-make -j >> $DEST_DIR/build-pic.log
+# use make -j SHELL="sh -x" to see command line of every call
+make -j8 >> $DEST_DIR/build-pic.log 2>&1
 if [ $? -ne 0 ]; then
 	echo ""
 	echo "#######################################"
@@ -94,6 +109,7 @@ if [ $? -ne 0 ]; then
 fi
 # use some hardcoded knowledge and get static library out
 cp $DIR/libvpx.a $DEST_DIR/libvpx_fPIC.a
+cp $DIR/libvpx_g.a $DEST_DIR/libvpx_g_fPIC.a
 
 if [ $? -eq 0 ]; then
 	echo ""
