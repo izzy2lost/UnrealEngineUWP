07190ceb7cc007873a8578407e84162c6e25f43d
richard malo <Richard.Malo@epicgames.com>
2023-10-26 09:45:19 -0400

    Exposed UDataLayerLoadingPolicy and FWorldPartitionActorViewProxy to plugins via ENGINE_API #rb jeanfrancois.dube #jira UE-199058
    [CL 29133058 by richard malo in ue5-main branch]

--- Engine/Source/Runtime/Engine/Public/WorldPartition/WorldPartitionActorDescViewProxy.h
+++ Engine/Source/Runtime/Engine/Public/WorldPartition/WorldPartitionActorDescViewProxy.h
@@ -7,10 +7,10 @@
 /**
  * A view on top of an actor desc, which reverts to the actor if its loaded and dirty.
  */
-class FWorldPartitionActorViewProxy : public FWorldPartitionActorDescView
+class ENGINE_API FWorldPartitionActorViewProxy : public FWorldPartitionActorDescView
 {
 public:
-	ENGINE_API FWorldPartitionActorViewProxy(const FWorldPartitionActorDesc* InActorDesc);
+	FWorldPartitionActorViewProxy(const FWorldPartitionActorDesc* InActorDesc);
 
 private:
 	TUniquePtr<FWorldPartitionActorDesc> CachedActorDesc;
