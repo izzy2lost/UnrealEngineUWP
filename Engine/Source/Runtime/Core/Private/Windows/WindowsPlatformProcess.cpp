0278512e3e869a60b76ea02b6c6ae1c780bc9e63
josh adams <josh.adams@epicgames.com>
2023-10-26 20:08:02 -0400

    [Backout] - CL29148565 [FYI] Josh.Adams Original CL Desc ----------------------------------------------------------------- - Changes needed to run staged/standalone programs that are in Restricted location #rb j.baumgartner
    [CL 29157307 by josh adams in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/Windows/WindowsPlatformProcess.cpp
+++ Engine/Source/Runtime/Core/Private/Windows/WindowsPlatformProcess.cpp
@@ -1119,12 +1119,7 @@ const TCHAR* FWindowsPlatformProcess::BaseDir()
 
 			FString CollapseResult(Result);
 #ifdef UE_RELATIVE_BASE_DIR
-			// this may have been defined at compile time because we are in Restricted, but then we have been staged as a program, and then remapped out of Restricted
-			// so if we are already in a Binaries/Win64 directory
-			if (!CollapseResult.EndsWith(TEXT("/Binaries/Win64/")))
-			{
-				CollapseResult /= UE_RELATIVE_BASE_DIR;
-			}
+			CollapseResult /= UE_RELATIVE_BASE_DIR;
 #endif
 			FPaths::CollapseRelativeDirectories(CollapseResult);
 			FCString::Strcpy(Result, *CollapseResult);
