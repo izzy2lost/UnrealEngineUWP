89e61b9c3377004e1daa288c7c244ed4bd7cf89a
joe kirchoff <joe.kirchoff@epicgames.com>
2023-10-30 19:40:57 -0400

    UnrealBuildTool: Rename some shared classes
    #rnx
    #jira UE-198294
    
    [CL 29258027 by joe kirchoff in ue5-main branch]

--- Engine/Source/Programs/Shared/EpicGames.Box/Utils.cs
+++ Engine/Source/Programs/Shared/EpicGames.Box/Utils.cs
@@ -5,14 +5,14 @@ using System;
 namespace EpicGames.Box
 {
 	/// <summary>
-	/// Box utils
+	/// Utils
 	/// </summary>
 	public static partial class Utils
 	{
 		static Lazy<bool> Available { get; set; } = new Lazy<bool>(() => false);
 
 		/// <summary>
-		/// Is box available?
+		/// Is available?
 		/// </summary>
 		public static bool IsAvailable() => Available.Value;
 	}
