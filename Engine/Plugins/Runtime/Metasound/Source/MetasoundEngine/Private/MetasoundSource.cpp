499d1ce0966eabb4980a3569448240d94f8fd633
phil popp <Phil.Popp@epicgames.com>
2023-11-01 21:18:10 -0400

    [Backout] - CL29329991 [FYI] phil.popp Original CL Desc ----------------------------------------------------------------- Default graph preset inflation to "true" #rb Maxwell.Hayes, Aaron.Mcleran
    [CL 29346598 by phil popp in ue5-main branch]

--- Engine/Plugins/Runtime/Metasound/Source/MetasoundEngine/Private/MetasoundSource.cpp
+++ Engine/Plugins/Runtime/Metasound/Source/MetasoundEngine/Private/MetasoundSource.cpp
@@ -53,7 +53,7 @@ namespace Metasound
 {
 	namespace ConsoleVariables
 	{
-		bool bEnableExperimentalRuntimePresetGraphInflation = true;
+		bool bEnableExperimentalRuntimePresetGraphInflation = false;
 	}
 
 	namespace SourcePrivate
@@ -168,7 +168,7 @@ FAutoConsoleVariableRef CVarMetaSoundEnableExperimentalRUntimePresetGraphInflati
 	TEXT("au.MetaSound.Experimental.EnableRuntimePresetGraphInflation"),
 	Metasound::ConsoleVariables::bEnableExperimentalRuntimePresetGraphInflation,
 	TEXT("Enables experimental feature of MetaSounds which reduces overhead of preset graphs\n")
-	TEXT("Default: true"),
+	TEXT("Default: false"),
 	ECVF_Default);
 
 
