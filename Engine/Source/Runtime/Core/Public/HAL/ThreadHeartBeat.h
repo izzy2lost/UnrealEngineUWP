befbd611ff1c2f35638fc38fef9e406b6c548f60
anton dunchev <anton.dunchev@epicgames.com>
2023-11-02 22:40:11 -0400

    [UE] Expose FGameThreadHitchHeartBeatThreaded::bStartSuspended to correctly track state of the heartbeat thread.
    #rb simon.orr
    #tests replay
    #rnx
    [FYI] ben.woodhouse
    
    [CL 29395465 by anton dunchev in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/HAL/ThreadHeartBeat.h
+++ Engine/Source/Runtime/Core/Public/HAL/ThreadHeartBeat.h
@@ -340,6 +340,7 @@ class FGameThreadHitchHeartBeatThreaded : public FRunnable
 	double FirstStartTime;
 	double FrameStartTime;
 	int32 SuspendedCount;
+	bool bStartSuspended = false;
 
 #if WALK_STACK_ON_HITCH_DETECTED
 #if LOOKUP_SYMBOLS_IN_HITCH_STACK_WALK
@@ -388,6 +389,11 @@ public:
 	* Resume heartbeat hitch detection. Call only after first calling SuspendHeartBeat.
 	*/
 	CORE_API void ResumeHeartBeat();
+	
+	/**
+	* Check if started suspended.
+	*/
+	CORE_API bool IsStartedSuspended();
 
 	// No-op, used in FUnixSignalGameHitchHeartBeat
 	void Restart() {}
