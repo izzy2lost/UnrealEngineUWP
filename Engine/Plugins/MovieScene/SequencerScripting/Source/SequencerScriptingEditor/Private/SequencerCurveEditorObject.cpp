c675a033715daf586aeaf28489755cd23c8f90fc
bob tellez <Bob.Tellez@epicgames.com>
2023-11-07 04:45:00 -0500

    #UE Fix copyright
    [FYI] Mike.Zyracki
    
    [CL 29515610 by bob tellez in ue5-main branch]

--- Engine/Plugins/MovieScene/SequencerScripting/Source/SequencerScriptingEditor/Private/SequencerCurveEditorObject.cpp
+++ Engine/Plugins/MovieScene/SequencerScripting/Source/SequencerScriptingEditor/Private/SequencerCurveEditorObject.cpp
@@ -1,5 +1,5 @@
-
 // Copyright Epic Games, Inc. All Rights Reserved.
+
 #include "SequencerCurveEditorObject.h"
 #include "MVVM/CurveEditorExtension.h"
 #include "MVVM/ViewModels/SequencerEditorViewModel.h"
