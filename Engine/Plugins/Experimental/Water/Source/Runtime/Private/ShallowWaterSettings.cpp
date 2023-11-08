ae12cd66c228c0475bcdc80fdcadf57fff9e5d1d
Asher Zhu <Asher.Zhu@epicgames.com>
2023-11-08 04:02:28 -0500

    WaterBody: Add ShallowWater intergration WIP
    - WaterDataFunctions.ush: Add #if VM_SIMULATION==0 wrapper around WaterDataFunctions.ush so that Niagara VM using this file can compile
    
    #jira UE-199945
    
    
    #virtualized
    
    [CL 29554507 by Asher Zhu in ue5-main branch]

---
+++ Engine/Plugins/Experimental/Water/Source/Runtime/Private/ShallowWaterSettings.cpp
@@ -0,0 +1,16 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShallowWaterSettings.h"
+
+UShallowWaterSettings::UShallowWaterSettings()
+{
+
+	
+	DefaultShallowWaterNiagaraSimulation = TSoftObjectPtr<UNiagaraSystem>(FSoftObjectPath(TEXT("/NiagaraFluids/Templates/Liquid/2D/Systems/ShallowWater/Grid2D_SW_FollowPlayer.Grid2D_SW_FollowPlayer")));
+
+}
+
+FName UShallowWaterSettings::GetCategoryName() const
+{
+	return FName("Plugins");
+}
