d5084c708321647b9d5f7c8bf1dabae06aa4d280
joe kirchoff <joe.kirchoff@epicgames.com>
2023-11-09 18:46:17 -0500

    UnrealBuildTool: Analysis warning
    #rnx
    
    [CL 29615474 by joe kirchoff in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildTool/System/ActionGraph.cs
+++ Engine/Source/Programs/UnrealBuildTool/System/ActionGraph.cs
@@ -364,7 +364,7 @@ namespace UnrealBuildTool
 			if (ActionCount > ParallelExecutor.GetDefaultNumParallelProcesses(BuildConfiguration.MaxParallelActions, BuildConfiguration.bAllCores, Logger))
 			{
 #if __UBAEXECUTOR_AVAILABLE__
-				if (BuildConfiguration.bAllowUBAExecutor && UBAExecutor.IsAvailable(Logger))
+				if (BuildConfiguration.bAllowUBAExecutor && UBAExecutor.IsAvailable())
 				{
 					return new UBAExecutor(BuildConfiguration.MaxParallelActions, BuildConfiguration.bAllCores, BuildConfiguration.bCompactOutput, Logger, TargetDescriptors.FirstOrDefault()?.AdditionalArguments);
 				}
@@ -385,7 +385,7 @@ namespace UnrealBuildTool
 			}
 
 #if __UBAEXECUTOR_AVAILABLE__
-			if (BuildConfiguration.bAllowUBALocalExecutor && UBALocalExecutor.IsAvailable(Logger))
+			if (BuildConfiguration.bAllowUBALocalExecutor && UBALocalExecutor.IsAvailable())
 			{
 				return new UBALocalExecutor(BuildConfiguration.MaxParallelActions, BuildConfiguration.bAllCores, BuildConfiguration.bCompactOutput, Logger, TargetDescriptors.FirstOrDefault()?.AdditionalArguments);
 			}
