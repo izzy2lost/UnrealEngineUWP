93324e0ceda1599d297439a37bcf74452bf29c5f
jimmy andrews <Jimmy.Andrews@epicgames.com>
2023-11-10 20:03:23 -0500

    [Backout] - CL29660243 [FYI] jimmy.andrews Original CL Desc ----------------------------------------------------------------- [Backout] - CL29647273 [FYI] Jimmy.Andrews Original CL Desc ----------------------------------------------------------------- Change LOD manager's "Show Seams" option to "Show Borders" and disable it when the default LOD is showing
    #jira UE-196668
    #rb matija.kecman
    
    [CL 29665185 by jimmy andrews in ue5-main branch]

--- Engine/Plugins/Experimental/MeshLODToolset/Source/MeshLODToolset/Private/Tools/LODManagerTool.cpp
+++ Engine/Plugins/Experimental/MeshLODToolset/Source/MeshLODToolset/Private/Tools/LODManagerTool.cpp
@@ -89,6 +89,7 @@ void ULODManagerTool::Setup()
 	LODPreviewProperties = NewObject<ULODManagerPreviewLODProperties>(this);
 	AddToolPropertySource(LODPreviewProperties);
 	LODPreviewProperties->VisibleLOD = this->DefaultLODName;
+	LODPreviewProperties->bShowingDefaultLOD = true;
 	LODPreviewProperties->WatchProperty(LODPreviewProperties->VisibleLOD, [this](FString NewLOD) { bPreviewLODValid = false; });
 	LODPreviewProperties->WatchProperty(LODPreviewProperties->bShowSeams, [this](bool bNewValue) { bPreviewLODValid = false; });
 
@@ -386,6 +387,15 @@ void ULODManagerTool::UpdatePreviewLOD()
 	}
 	bPreviewLODValid = true;
 
+	auto SetShowingDefaultLOD = [this](bool bNewValue)
+	{
+		if (bNewValue != LODPreviewProperties->bShowingDefaultLOD)
+		{
+			LODPreviewProperties->bShowingDefaultLOD = bNewValue;
+			NotifyOfPropertyChangeByTool(LODPreviewProperties);
+		}
+	};
+
 	FString SelectedLOD = LODPreviewProperties->VisibleLOD;
 	const FLODName* FoundName = ActiveLODNames.Find(SelectedLOD);
 	if (SelectedLOD.IsEmpty() || FoundName == nullptr || FoundName->IsDefault() )
@@ -395,6 +405,7 @@ void ULODManagerTool::UpdatePreviewLOD()
 		ClearPreviewLines();
 		LODPreviewLines->SetAllVisible(false);
 		UE::ToolTarget::ShowSourceObject(Targets[0]);
+		SetShowingDefaultLOD(true);
 		return;
 	}
 
@@ -404,6 +415,8 @@ void ULODManagerTool::UpdatePreviewLOD()
 		CacheLODMesh(SelectedLOD, *FoundName);
 		Found = LODMeshCache.Find(SelectedLOD);
 	}
+
+	SetShowingDefaultLOD(Found == nullptr);
 	
 	if (Found)
 	{
