be79809210476dccdfc9d4f69ae4b39c11a34959
ryan buehler <ryan.buehler@epicgames.com>
2023-11-14 12:41:20 -0500

    [PCG] Fix failing test: Editor.Plugins.Tools.PCG.PCGVolumeExposedInCreationMenu
    #rnx
    #jira UE-200370
    #rb julien.lheureux
    
    [CL 29717397 by ryan buehler in ue5-main branch]

--- Engine/Plugins/Experimental/PCG/Source/PCGEditor/Private/Tests/Display/PCGVolumeExposedInCreationMenu.cpp
+++ Engine/Plugins/Experimental/PCG/Source/PCGEditor/Private/Tests/Display/PCGVolumeExposedInCreationMenu.cpp
@@ -9,12 +9,12 @@ IMPLEMENT_CUSTOM_SIMPLE_AUTOMATION_TEST(FPCGVolumeExposedInCreationMenu, FPCGTes
 
 bool FPCGVolumeExposedInCreationMenu::RunTest(const FString& Parameters)
 {
-	// create menu queries place mode categories to dislplay 
+	// create menu queries place mode categories to display
 	const IPlacementModeModule& PlacementModeModule = IPlacementModeModule::Get();
 	// PCG volume is located in Volumes category
 	const FName PlacementModeCategoryHandle = TEXT("Volumes");
-	FString PCGDisplayVolumeName = TEXT("PCGVolume");
-	TArray<TSharedPtr<FPlaceableItem>>OutItems;
+	FString PCGDisplayVolumeName = TEXT("PCG Volume");
+	TArray<TSharedPtr<FPlaceableItem>> OutItems;
 
 
 	PlacementModeModule.GetItemsForCategory(PlacementModeCategoryHandle, OutItems);
