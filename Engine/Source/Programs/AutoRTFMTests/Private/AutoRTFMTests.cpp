692c32fdeee57d3ddcf9e7697c83a4135e4a6842
brandon schaefer <Brandon.Schaefer@epicgames.com>
2023-11-14 15:52:36 -0500

    Disable AutoRTFM runtime by default Enable Tests to default AutoRTFM on
    #rb Brandon.Schaefer
    [FYI] Michael.Nicolella, Neil.Henning
    
    [CL 29724121 by brandon schaefer in ue5-main branch]

--- Engine/Source/Programs/AutoRTFMTests/Private/AutoRTFMTests.cpp
+++ Engine/Source/Programs/AutoRTFMTests/Private/AutoRTFMTests.cpp
@@ -6,3 +6,15 @@
 IMPLEMENT_APPLICATION(AutoRTFMTests, "AutoRTFMTests");
 
 #include "catch_amalgamated.cpp"
+
+class SetupListener : public Catch::EventListenerBase {
+public:
+	using Catch::EventListenerBase::EventListenerBase;
+
+	void testRunStarting(Catch::TestRunInfo const&) override
+	{
+		AutoRTFM::SetAutoRTFMRuntime(true);
+	}
+};
+
+CATCH_REGISTER_LISTENER(SetupListener)
