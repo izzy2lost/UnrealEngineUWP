faaf880e30226b535c2709a5c751cb2401dce755
carl bystrom <carl.bystrom@epicgames.com>
2023-11-15 08:13:05 -0500

    Horde: Fix warnings in ServerTestsApp
    [CL 29743884 by carl bystrom in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Server.Tests/ServerTestsApp.cs
+++ Engine/Source/Programs/Horde/Horde.Server.Tests/ServerTestsApp.cs
@@ -1,10 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Horde.Server.Tests
