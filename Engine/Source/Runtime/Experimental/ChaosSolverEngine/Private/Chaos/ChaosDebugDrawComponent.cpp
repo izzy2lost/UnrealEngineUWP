00b13855035d5f34332b6a03226381c8427c17da
bob tellez <Bob.Tellez@epicgames.com>
2023-11-15 12:34:18 -0500

    [Backout] - CL29745360 [FYI] Yoan.StAmant Original CL Desc ----------------------------------------------------------------- Updated ChaosDebugDrawComponent to render in the Editor world #rb Mieszko.Zielinski [FYI] aris.theophanidis
    [CL 29750701 by bob tellez in ue5-main branch]

--- Engine/Source/Runtime/Experimental/ChaosSolverEngine/Private/Chaos/ChaosDebugDrawComponent.cpp
+++ Engine/Source/Runtime/Experimental/ChaosSolverEngine/Private/Chaos/ChaosDebugDrawComponent.cpp
@@ -85,7 +85,7 @@ void DebugDrawChaos(AActor* DebugDrawActor, const TArray<Chaos::FLatentDrawComma
 		return;
 	}
 
-	if (World->IsPreviewWorld())
+	if (!World->IsGameWorld())
 	{
 		return;
 	}
@@ -205,8 +205,6 @@ UChaosDebugDrawComponent::UChaosDebugDrawComponent()
 	PrimaryComponentTick.bCanEverTick = true;
 	PrimaryComponentTick.SetTickFunctionEnable(true);
 	PrimaryComponentTick.TickGroup = TG_PostUpdateWork;
-
-	bTickInEditor = true;
 }
 
 void UChaosDebugDrawComponent::BeginDestroy()
@@ -246,36 +244,6 @@ void UChaosDebugDrawComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
 	Chaos::FDebugDrawQueue::GetInstance().SetConsumerActive(this, bInPlay);
 #endif
 }
-
-#if WITH_EDITOR && CHAOS_DEBUG_DRAW
-void UChaosDebugDrawComponent::OnRegister()
-{
-	Super::OnRegister();
-
-	const UWorld* World = GetWorld();
-	check(World);
-
-	// GameWorld is handled by BeginPlay/EndPlay
-	if (!World->IsGameWorld() && !World->IsPreviewWorld())
-	{
-		Chaos::FDebugDrawQueue::GetInstance().SetConsumerActive(this, /*bConsumerActive*/true);
-	}
-}
-
-void UChaosDebugDrawComponent::OnUnregister()
-{
-	const UWorld* World = GetWorld();
-	check(World);
-
-	// GameWorld is handled by BeginPlay/EndPlay
-	if (!World->IsGameWorld() && !World->IsPreviewWorld())
-	{
-		Chaos::FDebugDrawQueue::GetInstance().SetConsumerActive(this, /*bConsumerActive*/false);
-	}
-
-	Super::OnUnregister();
-}
-#endif // WITH_EDITOR && CHAOS_DEBUG_DRAW
 
 void UChaosDebugDrawComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
@@ -349,7 +317,7 @@ void UChaosDebugDrawComponent::BindWorldDelegates()
 void UChaosDebugDrawComponent::HandlePostWorldInitialization(UWorld* World, const UWorld::InitializationValues IVS)
 {
 #if CHAOS_DEBUG_DRAW
-	if ((World != nullptr) && !World->IsPreviewWorld())
+	if ((World != nullptr) && World->IsGameWorld())
 	{
 		CreateDebugDrawActor(World);
 	}
