2c37f2f6f757064b7b532a73e6f3d116ffdb6fc8
halfdan ingvarsson <halfdan.ingvarsson@epicgames.com>
2023-11-15 19:26:13 -0500

    Curve Expressions: Handle multi-threaded switch-outs of data assets.
    #rnx
    #rb kiaran.ritchie
    
    [CL 29765734 by halfdan ingvarsson in ue5-main branch]

--- Engine/Plugins/Experimental/Animation/CurveExpression/Source/Runtime/Private/AnimNode_RemapCurvesBase.cpp
+++ Engine/Plugins/Experimental/Animation/CurveExpression/Source/Runtime/Private/AnimNode_RemapCurvesBase.cpp
@@ -104,7 +104,12 @@ const TMap<FName, CurveExpression::Evaluator::FExpressionObject>& FAnimNode_Rema
 	case ERemapCurvesExpressionSource::DataAsset:
 		if (CurveExpressionsDataAsset)
 		{
-			return CurveExpressionsDataAsset->GetCompiledExpressionMap();
+			CachedAssetExpressionData = CurveExpressionsDataAsset->GetCompiledExpressionData();
+			
+			if (CachedAssetExpressionData)
+			{
+				return CachedAssetExpressionData->ExpressionMap;
+			}
 		}
 	}
 
@@ -126,7 +131,12 @@ const TArray<FName>& FAnimNode_RemapCurvesBase::GetCompiledExpressionConstants()
 	case ERemapCurvesExpressionSource::DataAsset:
 		if (CurveExpressionsDataAsset)
 		{
-			return CurveExpressionsDataAsset->GetCompiledExpressionConstants();
+			CachedAssetExpressionData = CurveExpressionsDataAsset->GetCompiledExpressionData();
+			
+			if (CachedAssetExpressionData)
+			{
+				return CachedAssetExpressionData->NamedConstants;
+			}
 		}
 	}
 
--- Engine/Plugins/Experimental/Animation/CurveExpression/Source/Runtime/Private/CurveExpressionsDataAsset.cpp
+++ Engine/Plugins/Experimental/Animation/CurveExpression/Source/Runtime/Private/CurveExpressionsDataAsset.cpp
@@ -6,6 +6,7 @@
 #include "ExpressionEvaluator.h"
 #endif
 
+#include "CurveExpressionCustomVersion.h"
 #include "String/ParseLines.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(CurveExpressionsDataAsset)
@@ -60,52 +61,10 @@ TArray<FCurveExpressionParsedAssignment> FCurveExpressionList::GetParsedAssignme
 	return ParsedAssignments;
 }
 
-void UCurveExpressionsDataAsset::SynchronizeThreadCopy() const
-{
-	const uint32 Serial = SerialNumber.load();
-	if (ThreadCopy.SerialNumber != Serial)
-	{
-		FScopeLock ScopeLock(&ExpressionMapWriteLock);
-		
-		FExpressionDataShadowCopy& MutableThreadCopy = const_cast<FExpressionDataShadowCopy&>(ThreadCopy);
-
-		MutableThreadCopy.SerialNumber = Serial;
-		MutableThreadCopy.NamedConstants = NamedConstants;
-		MutableThreadCopy.ExpressionMap = ExpressionMap;
-	}
-}
-
-
-const TArray<FName>& UCurveExpressionsDataAsset::GetCompiledExpressionConstants() const
-{
-	if (IsInGameThread())
-	{
-		return NamedConstants;
-	}
-	else
-	{
-		SynchronizeThreadCopy();
-		return ThreadCopy.NamedConstants;
-	}
-}
-
-
-const TMap<FName, CurveExpression::Evaluator::FExpressionObject>& UCurveExpressionsDataAsset::GetCompiledExpressionMap() const
-{
-	if (IsInGameThread())
-	{
-		return ExpressionMap;
-	}
-	else
-	{
-		SynchronizeThreadCopy();
-		return ThreadCopy.ExpressionMap;
-	}
-}
-
-
 void UCurveExpressionsDataAsset::Serialize(FArchive& Ar)
 {
+	Ar.UsingCustomVersion(FCurveExpressionCustomVersion::GUID);
+	
 	UObject::Serialize(Ar);
 
 #if WITH_EDITOR
@@ -116,8 +75,22 @@ void UCurveExpressionsDataAsset::Serialize(FArchive& Ar)
 	}
 #endif
 
+	if (Ar.IsLoading() || !ExpressionData.IsValid())
+	{
+		ExpressionData = MakeShared<FExpressionData>();
+	}
+
+	if (Ar.IsLoading() && Ar.CustomVer(FCurveExpressionCustomVersion::GUID) < FCurveExpressionCustomVersion::ExpressionDataInSharedObject)
+	{
+		ExpressionData->NamedConstants = NamedConstants_DEPRECATED;
+	}
+	else
+	{
+		Ar << ExpressionData->NamedConstants;
+	}
+	
 	// Serialize the compiled map so we can read it in cooked builds.
-	Ar << ExpressionMap;
+	Ar << ExpressionData->ExpressionMap;
 }
 
 
@@ -137,22 +110,24 @@ void UCurveExpressionsDataAsset::CompileExpressions()
 {
 	using namespace CurveExpression::Evaluator;
 	
-	FScopeLock ScopeLock(&ExpressionMapWriteLock);
-
+	TSharedPtr<FExpressionData> NewExpressionData(new FExpressionData);
+	
 	TSet<FName> ConstantNames;
 	
-	ExpressionMap.Reset();
 	for (const FCurveExpressionParsedAssignment& Assignment: Expressions.GetParsedAssignments())
 	{
 		if (const FExpressionObject* Expression = Assignment.Result.TryGet<FExpressionObject>())
 		{
 			ConstantNames.Append(Expression->GetUsedConstants());
-			ExpressionMap.Add(Assignment.TargetName, *Expression);
+			NewExpressionData->ExpressionMap.Add(Assignment.TargetName, *Expression);
 		}
 	}
 	
-	NamedConstants = ConstantNames.Array();
-	SerialNumber += 1;
+	NewExpressionData->NamedConstants = ConstantNames.Array();
+
+	// Swap the new set of expressions in. Any client referring to the old expression data will still be
+	// able to do so.
+	ExpressionData = NewExpressionData;
 }
 
 #endif
