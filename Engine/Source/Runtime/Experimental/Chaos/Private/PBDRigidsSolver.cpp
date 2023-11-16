f6eb8a6b73d82dd1fdbbc99ec8986c8393415714
vincent robert <vincent.robert@epicgames.com>
2023-11-16 13:02:03 -0500

    ASAN crash Remove GC proxy from stall events
    [REVIEW] [at]benn.gallagher
    #rb Benn.Gallagher
    
    
    #localization none
    #tests test in PIE
    
    [CL 29783179 by vincent robert in ue5-main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/PBDRigidsSolver.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/PBDRigidsSolver.cpp
@@ -793,11 +793,27 @@ namespace Chaos
 
 
 		EnqueueCommandImmediate([InProxy, this]()
-			{
-				GeometryCollectionPhysicsProxies_Internal.RemoveSingle(InProxy);
-				InProxy->SyncBeforeDestroy();
-				PendingDestroyGeometryCollectionPhysicsProxy.Add(InProxy);
-			});
+		{
+			// Generally need to remove stale events for particles that no longer exist
+			GetEventManager()->template ClearEvents<FCollisionEventData>(EEventType::Collision, [InProxy]
+			(FCollisionEventData& EventDataInOut)
+				{
+					const FCollisionDataArray& CollisionData = EventDataInOut.CollisionData.AllCollisionsArray;
+					if (CollisionData.Num() > 0)
+					{
+						check(InProxy);
+						const TArray<int32>* const CollisionIndices = EventDataInOut.PhysicsProxyToCollisionIndices.PhysicsProxyToIndicesMap.Find(InProxy);
+						if (CollisionIndices)
+						{
+							EventDataInOut.PhysicsProxyToCollisionIndices.PhysicsProxyToIndicesMap.Remove(InProxy);
+						}
+					}
+				});
+
+			GeometryCollectionPhysicsProxies_Internal.RemoveSingle(InProxy);
+			InProxy->SyncBeforeDestroy();
+			PendingDestroyGeometryCollectionPhysicsProxy.Add(InProxy);
+		});
 	}
 
 	void FPBDRigidsSolver::RegisterObject(FClusterUnionPhysicsProxy* Proxy)
