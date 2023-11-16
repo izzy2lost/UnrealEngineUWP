ff42e9e464b92a5d1beeaa778bc8aa920e3ef9d6
zack neyland <zack.neyland@epicgames.com>
2023-11-15 21:04:26 -0500

    UBT: Fix for MakeStubItems with Circular Deps.
    [CL 29769278 by zack neyland in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/Mac/MacToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Mac/MacToolChain.cs
@@ -666,7 +666,10 @@ namespace UnrealBuildTool
 
 		private FileItem MakeStubItem(LinkEnvironment LinkEnvironment, string DylibPath)
 		{
-			string IntermediateDirectory = (ProjectFile == null || LinkEnvironment.OutputFilePaths.All(x => x.IsUnderDirectory(Unreal.EngineDirectory)) ? Unreal.EngineDirectory : ProjectFile.Directory) + "/Intermediate/Mac";
+			FileReference DylibPathRef = new FileReference(DylibPath);
+			bool bIsEngineDylib = DylibPathRef.IsUnderDirectory(Unreal.EngineDirectory);
+
+			string IntermediateDirectory = (ProjectFile == null || LinkEnvironment.OutputFilePaths.All(x => x.IsUnderDirectory(Unreal.EngineDirectory)) || bIsEngineDylib ? Unreal.EngineDirectory : ProjectFile.Directory) + "/Intermediate/Mac";
 			return FileItem.GetItemByPath(Path.Combine(IntermediateDirectory, "Stubs", LinkEnvironment.Architecture.ToString(), Path.GetFileName(DylibPath)));
 		}
 
