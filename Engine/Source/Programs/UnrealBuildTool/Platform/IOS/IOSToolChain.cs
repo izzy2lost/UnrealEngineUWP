70538bb54fa0362d11c8c7b7b503a6d10f0b9af3
calvin zheng <calvin.zheng@epicgames.com>
2023-11-20 13:57:09 -0500

    Undo changelist 29782249 Linker crash still happening on EngineTestEditor, revert to previous solution.
    #rb trivial
    [FYI] zack.neyland
    
    [CL 29845998 by calvin zheng in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/IOS/IOSToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/IOS/IOSToolChain.cs
@@ -307,8 +307,9 @@ namespace UnrealBuildTool
 			}
 		}
 
-		protected void GetLinkArguments_Global(LinkEnvironment LinkEnvironment, List<string> Arguments)
+		protected override void GetLinkArguments_Global(LinkEnvironment LinkEnvironment, List<string> Arguments)
 		{
+			base.GetLinkArguments_Global(LinkEnvironment, Arguments);
 			Arguments.Add(FormatArchitectureArg(LinkEnvironment.Architectures));
 
 			DirectoryReference SDKPath = Settings.GetSDKPath(LinkEnvironment.Architecture);
--- Engine/Source/Programs/UnrealBuildTool/Platform/Mac/MacToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Mac/MacToolChain.cs
@@ -225,8 +225,9 @@ namespace UnrealBuildTool
 			return Result;
 		}
 
-		protected void GetLinkArguments_Global(LinkEnvironment LinkEnvironment, List<string> Arguments)
+		protected override void GetLinkArguments_Global(LinkEnvironment LinkEnvironment, List<string> Arguments)
 		{
+			base.GetLinkArguments_Global(LinkEnvironment, Arguments);
 			// Pass through architecture and OS info		
 			Arguments.Add(FormatArchitectureArg(LinkEnvironment.Architectures));
 			Arguments.Add(String.Format("-isysroot \"{0}\"", ToolChainSettings.Value.GetSDKPath(LinkEnvironment.Architecture)));
