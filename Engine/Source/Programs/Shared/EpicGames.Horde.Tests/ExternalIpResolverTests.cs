69d4bc09316a913c758ae15d532f663060acf856
carl bystrom <carl.bystrom@epicgames.com>
2023-11-24 13:03:21 -0500

    Horde: Add external IP resolver for use by compute resource allocation
    [CL 29922137 by carl bystrom in ue5-main branch]

---
+++ Engine/Source/Programs/Shared/EpicGames.Horde.Tests/ExternalIpResolverTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using EpicGames.Horde.Compute;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EpicGames.Horde.Tests;
+
+[TestClass]
+public class ExternalIpResolverTests
+{
+	private class StubMessageHandler : HttpMessageHandler
+	{
+		private readonly HttpStatusCode _statusCode;
+		private readonly string _content;
+
+		public StubMessageHandler(HttpStatusCode statusCode, string content)
+		{
+			_statusCode = statusCode;
+			_content = content;
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			return Task.FromResult(new HttpResponseMessage(_statusCode) { Content = new StringContent(_content) });
+		}
+	}
+	
+	[TestMethod]
+	public async Task BasicAsync()
+	{
+		Assert.AreEqual(IPAddress.Parse("192.168.2.3"), await GetIpAsync("192.168.2.3"));
+		await Assert.ThrowsExceptionAsync<ExternalIpResolverException>(() => GetIpAsync("bad-ip"));
+		await Assert.ThrowsExceptionAsync<ExternalIpResolverException>(() => GetIpAsync("192.168.2.3", HttpStatusCode.NotFound));
+	}
+	
+	[TestMethod]
+	[Ignore]
+	public async Task IntegrationAsync()
+	{
+		using HttpClient httpClient = new ();
+		ExternalIpResolver resolver = new (httpClient);
+		IPAddress externalIp = await resolver.GetExternalIpAddressAsync();
+		Console.WriteLine("External IP: " + externalIp);
+	}
+
+	private static async Task<IPAddress> GetIpAsync(string content, HttpStatusCode statusCode = HttpStatusCode.OK, CancellationToken cancellationToken = default)
+	{
+		using StubMessageHandler handler = new (statusCode, content);
+		using HttpClient httpClient = new (handler);
+		ExternalIpResolver resolver = new (httpClient);
+		return await resolver.GetExternalIpAddressAsync(cancellationToken);
+	}
+}
