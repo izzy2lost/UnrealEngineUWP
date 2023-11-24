734fa112a6dbef147dd9ad10e2d541c20b4060d6
ben marsh <Ben.Marsh@epicgames.com>
2023-11-24 09:40:22 -0500

    Build fix on Mac/Linux editor.
    #jira
    
    [CL 29919397 by ben marsh in ue5-main branch]

--- Engine/Source/Developer/Horde/Private/HordePlatform.cpp
+++ Engine/Source/Developer/Horde/Private/HordePlatform.cpp
@@ -76,7 +76,7 @@ void FHordePlatform::CreateUniqueIdentifier(char* NameBuffer, size_t NameBufferL
 #else
 	struct timespec ts;
 	clock_gettime(CLOCK_MONOTONIC, &ts);
-	snprintf(NameBuffer, NameBufferLen, "%u%zu%zu_%lu", getpid(), (size_t)ts.tv_sec, (size_t)ts.tv_nsec, AtomicIncrement(&Counter));
+	snprintf(NameBuffer, NameBufferLen, "%u%zu%zu_%d", getpid(), (size_t)ts.tv_sec, (size_t)ts.tv_nsec, FPlatformAtomics::InterlockedIncrement(&Counter));
 #endif
 }
 
