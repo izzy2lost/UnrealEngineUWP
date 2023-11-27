1b892fe03b8a0b68b737564a6262d76a599f8540
ben marsh <Ben.Marsh@epicgames.com>
2023-11-27 11:09:03 -0500

    Horde: Fix static analysis warning.
    [CL 29935191 by ben marsh in ue5-main branch]

--- Engine/Source/Programs/Shared/EpicGames.Horde.Tests/BundlePacketTests.cs
+++ Engine/Source/Programs/Shared/EpicGames.Horde.Tests/BundlePacketTests.cs
@@ -29,13 +29,8 @@ namespace EpicGames.Horde.Tests
 				_fragment = fragment;
 			}
 
-			public ValueTask<BlobData> ReadBlobDataAsync(CancellationToken cancellationToken = default) => throw new NotImplementedException();
+			public ValueTask<BlobData> ReadAsync(CancellationToken cancellationToken = default) => throw new NotImplementedException();
 			public ValueTask FlushAsync(CancellationToken cancellationToken = default) => throw new NotImplementedException();
-
-			public ValueTask<BlobData> ReadAsync(CancellationToken cancellationToken = default)
-			{
-				throw new NotImplementedException();
-			}
 
 			public bool TryAppendIdentifier(Utf8StringBuilder builder)
 			{
