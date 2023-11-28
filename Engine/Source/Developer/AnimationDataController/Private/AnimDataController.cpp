a1ed5af77450b60debaa1d53265f7ac4a3fb078f
jurre debaare <jurre.debaare@epicgames.com>
2023-11-28 09:58:58 -0500

    Duplicate fix from CL 26459834 over to AnimDataController as well #rb Thomas.Sarkanen
    [CL 29959518 by jurre debaare in ue5-main branch]

--- Engine/Source/Developer/AnimationDataController/Private/AnimDataController.cpp
+++ Engine/Source/Developer/AnimationDataController/Private/AnimDataController.cpp
@@ -1978,9 +1978,10 @@ bool UAnimDataController::DuplicateAttribute(const FAnimationAttributeIdentifier
 				{
 					FTransaction Transaction = ConditionalTransaction(LOCTEXT("DuplicateAttribute", "Duplicating Animation Attribute"), bShouldTransact);
 
-					FAnimatedBoneAttribute& DuplicateAttribute = Model->AnimatedBoneAttributes.AddDefaulted_GetRef();
+					FAnimatedBoneAttribute DuplicateAttribute;
 					DuplicateAttribute.Identifier = NewAttributeIdentifier;
 					DuplicateAttribute.Curve = AttributePtr->Curve;
+					Model->AnimatedBoneAttributes.Add(DuplicateAttribute);
 
 					FAttributeAddedPayload Payload;
 					Payload.Identifier = NewAttributeIdentifier;
