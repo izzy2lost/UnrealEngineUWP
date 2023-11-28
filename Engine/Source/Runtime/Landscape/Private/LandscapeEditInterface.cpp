921d07e18778b0b589f818c79091cdebb0751368
jonathan bard <jonathan.bard@epicgames.com>
2023-11-28 09:50:47 -0500

    Fixed ensure when painting/sculpting outside a landscape #rb chris.tchou #jira UE-201067 #tests editor
    [CL 29959327 by jonathan bard in ue5-main branch]

--- Engine/Source/Runtime/Landscape/Private/LandscapeEditInterface.cpp
+++ Engine/Source/Runtime/Landscape/Private/LandscapeEditInterface.cpp
@@ -4283,10 +4283,10 @@ void FLandscapeEditDataInterface::GetWeightDataTempl(ULandscapeLayerInfoObject* 
 			ComponentSizeX, ComponentSizeY, CornerValues,
 			NoBorderY1, NoBorderY2, ComponentDataExist, StoreData );
 		// Update valid region
-		ValidX1 = FMath::Max<int32>(X1, ValidX1);
-		ValidX2 = FMath::Min<int32>(X2, ValidX2);
-		ValidY1 = FMath::Max<int32>(Y1, ValidY1);
-		ValidY2 = FMath::Min<int32>(Y2, ValidY2);
+		ValidX1 = FMath::Min<int32>(X1, ValidX1);
+		ValidX2 = FMath::Max<int32>(X2, ValidX2);
+		ValidY1 = FMath::Min<int32>(Y1, ValidY1);
+		ValidY2 = FMath::Max<int32>(Y2, ValidY2);
 	}
 	else
 	{
