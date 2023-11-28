66ed4fd5e2462a51384e7903281dc174127e5550
jamie dale <Jamie.Dale@epicgames.com>
2023-11-28 09:40:19 -0500

    Removed some FText->FString->FText churn
    #rnx
    
    [CL 29959153 by jamie dale in ue5-main branch]

--- Engine/Source/Editor/StatsViewer/Private/SStatsViewer.cpp
+++ Engine/Source/Editor/StatsViewer/Private/SStatsViewer.cpp
@@ -718,24 +718,24 @@ TSharedRef<SWidget> SStatsViewer::OnGetFilterMenuContent() const
 			TWeakFieldPtr< FProperty > Property = *PropertyIter;
 			if( Property->HasAnyPropertyFlags(CPF_AssetRegistrySearchable) )
 			{
-				FString FilterName = Property->GetDisplayNameText().ToString();
-				if( FilterName.Len() == 0 )
+				FText FilterName = Property->GetDisplayNameText();
+				if( FilterName.IsEmpty() )
 				{
-					FilterName = UEditorEngine::GetFriendlyName(Property.Get());
+					FilterName = FText::AsCultureInvariant(UEditorEngine::GetFriendlyName(Property.Get()));
 				}
 
-				FString FilterDesc = Property->GetToolTipText().ToString();
-				if( FilterDesc.Len() == 0 )
+				FText FilterDesc = Property->GetToolTipText();
+				if( FilterDesc.IsEmpty() )
 				{
-					FilterDesc = UEditorEngine::GetFriendlyName(Property.Get());
+					FilterDesc = FText::AsCultureInvariant(UEditorEngine::GetFriendlyName(Property.Get()));
 				}
 
 				FFormatNamedArguments Arguments;
-				Arguments.Add(TEXT("FilterName"), FText::FromString(FilterName));
-				Arguments.Add(TEXT("FilterDesc"), FText::FromString(FilterDesc));
+				Arguments.Add(TEXT("FilterName"), FilterName);
+				Arguments.Add(TEXT("FilterDesc"), FilterDesc);
 
 				MenuBuilder.AddMenuEntry( 
-					FText::FromString( FilterName ), 
+					FilterName, 
 					FText::Format( LOCTEXT( "FilterMenuEntry_Tooltip", "Search statistics by {FilterName}.\n{FilterDesc}" ), Arguments ), 
 					FSlateIcon(), 
 					FUIAction( 
