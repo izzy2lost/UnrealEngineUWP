c48857e0bf89af1ac44282fee139454ba7b25e38
henrik karlsson <henrik.karlsson@epicgames.com>
2023-11-29 19:14:53 -0500

    [Uba] * Fixed GenerateSolution.bat
    [CL 30003577 by henrik karlsson in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildAccelerator/Project/GenerateSolution.bat
+++ Engine/Source/Programs/UnrealBuildAccelerator/Project/GenerateSolution.bat
@@ -3,8 +3,8 @@ setlocal
 
 cd "%~dp0"
 
-call ..\..\..\..\..\..\Build\BatchFiles\GenerateProjectFiles.bat -Project="%~dp0\Uba.uproject" -Game -Platforms=Win64+Linux+LinuxArm64 -DEBUGCONFIGS -SHIPPINGCONFIGS
+call ..\..\..\..\Build\BatchFiles\GenerateProjectFiles.bat -Project="%~dp0\Uba.uproject" -Game -Platforms=Win64+Linux+LinuxArm64 -DEBUGCONFIGS -SHIPPINGCONFIGS
 move /Y Uba_LinuxLinuxArm64Win64.sln Uba.sln
 
-call ..\..\..\..\..\..\Build\BatchFiles\GenerateProjectFiles.bat -Project="%~dp0\Uba.uproject" -Game -Platforms=Linux -VisualStudioLinux
+call ..\..\..\..\Build\BatchFiles\GenerateProjectFiles.bat -Project="%~dp0\Uba.uproject" -Game -Platforms=Linux -VisualStudioLinux
 
