5ed85a8c97c9403621c48f1cc48f45ad4bb11296
ben marsh <Ben.Marsh@epicgames.com>
2023-12-01 16:33:52 -0500

    Horde: Move RemoteWorkerCpp into the new HordeTest app built by UBT.
    [CL 30054172 by ben marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Samples/HordeTest/Main.cpp
+++ Engine/Source/Programs/Horde/Samples/HordeTest/Main.cpp
@@ -14,37 +14,58 @@
 #include <assert.h>
 #include "Memory/SharedBuffer.h"
 #include "Storage/ChunkedBufferWriter.h"
+#include "Compute/ComputeBuffer.h"
+#include "Compute/ComputeChannel.h"
+#include "Compute/ComputeTransport.h"
+#include "Compute/ComputeSocket.h"
 
 extern TCHAR GInternalProjectName[64] = { 0, };
 extern const TCHAR* GForeignEngineDir = nullptr;
-
-int CreateBundle(const std::filesystem::path& InputDir, const std::filesystem::path& OutputFile);
-int ExtractBundle(const std::filesystem::path& InputFile, const std::filesystem::path& OutputDir);
 
 int main(int ArgC, const char* ArgV[])
 {
 	if (ArgC == 5 && FCStringAnsi::Stricmp(ArgV[1], "bundle") == 0 && FCStringAnsi::Stricmp(ArgV[2], "create") == 0)
 	{
-		std::filesystem::path InputDir = ArgV[3];
-		std::filesystem::path OutputFile = ArgV[4];
-		return CreateBundle(InputDir, OutputFile);
+		extern int CreateBundle(const std::filesystem::path & InputDir, const std::filesystem::path & OutputFile);
+		return CreateBundle(ArgV[3], ArgV[4]);
 	}
 
 	if (ArgC == 5 && FCStringAnsi::Stricmp(ArgV[1], "bundle") == 0 && FCStringAnsi::Stricmp(ArgV[2], "extract") == 0)
 	{
-		std::filesystem::path InputFile = ArgV[3];
-		std::filesystem::path OutputDir = ArgV[4];
-		return ExtractBundle(InputFile, OutputDir);
+		extern int ExtractBundle(const std::filesystem::path & InputFile, const std::filesystem::path & OutputDir);
+		return ExtractBundle(ArgV[3], ArgV[4]);
+	}
+
+	if (ArgC == 3 && FCStringAnsi::Stricmp(ArgV[1], "compute") == 0 && FCStringAnsi::Stricmp(ArgV[2], "test") == 0)
+	{
+		extern int RunComputeTests();
+		return RunComputeTests();
+	}
+
+	if (ArgC == 3 && FCStringAnsi::Stricmp(ArgV[1], "compute") == 0 && FCStringAnsi::Stricmp(ArgV[2], "worker") == 0)
+	{
+		extern int RunComputeWorker();
+		return RunComputeWorker();
 	}
 
 	printf("Arguments:\n");
 	printf("\n");
 	printf("  bundle create <InputDir> <OutputFile>\n");
+	printf("     - Archives a folder to a set of bundles\n");
+	printf("\n");
 	printf("  bundle extract <InputFile> <OutputDir>\n");
+	printf("     - Extracts a set of bundles to a folder\n");
+	printf("\n");
+	printf("  compute test\n");
+	printf("     - Runs some basic tests on primitives in the compute system\n");
+	printf("\n");
+	printf("  compute worker\n");
+	printf("     - Runs a worker app compatible with the RemoteClient sample in the Horde sln\n");
 	return 1;
 }
 
 // --------------------------------------------------------------------------------------------------------
+// 'bundle create' command
 
 FBlobHandleWithHash CreateFromStream(std::ifstream& Stream, FBlobWriter& Writer, int64& OutLength, FIoHash& OutStreamHash)
 {
@@ -125,6 +146,7 @@ int CreateBundle(const std::filesystem::path& InputDir, const std::filesystem::p
 }
 
 // --------------------------------------------------------------------------------------------------------
+// 'bundle extract' command
 
 void ExtractFile(const std::filesystem::path& Path, const FBlobHandle& Handle)
 {
@@ -169,4 +191,135 @@ int ExtractBundle(const std::filesystem::path& InputFile, const std::filesystem:
 	ExtractDirectory(OutputDir, Handle);
 
 	return 0;
+}
+
+// --------------------------------------------------------------------------------------------------------
+// 'compute test' command
+
+int RunComputeTests()
+{
+	void ComputeBufferTest();
+	ComputeBufferTest();
+	std::cout << "ComputeBufferTest ok" << std::endl;
+
+	void ComputeSocketTest();
+	ComputeSocketTest();
+	std::cout << "ComputeSocketTest ok" << std::endl;
+
+	return 0;
+}
+
+void ComputeBufferTest()
+{
+	char TestData[] = "hello world!";
+
+	FComputeBuffer Buffer;
+	verify(Buffer.CreateNew(FComputeBuffer::FParams()));
+
+	FComputeBufferWriter Writer = Buffer.CreateWriter();
+	unsigned char* WriteBuffer = Writer.WaitToWrite(sizeof(TestData));
+	memcpy(WriteBuffer, TestData, sizeof(TestData));
+	Writer.AdvanceWritePosition(sizeof(TestData));
+
+	FComputeBufferReader Reader = Buffer.CreateReader();
+	const unsigned char* ReadBuffer = Reader.WaitToRead(sizeof(TestData));
+	check(memcmp(ReadBuffer, TestData, sizeof(TestData)) == 0);
+	check(!Reader.IsComplete());
+
+	Reader.AdvanceReadPosition(sizeof(TestData));
+	check(!Reader.IsComplete());
+
+	Writer.MarkComplete();
+	check(Reader.IsComplete());
+}
+
+template<size_t TestDataSize> void CheckChannelSendRecv(TSharedPtr<FComputeChannel>& SendChannel, TSharedPtr<FComputeChannel>& RecvChannel, const char(&TestData)[TestDataSize])
+{
+	SendChannel->Send(TestData, TestDataSize);
+
+	char RecvTestData[TestDataSize];
+	verify(RecvChannel->Recv(RecvTestData, TestDataSize) == TestDataSize);
+
+	verify(memcmp(TestData, RecvTestData, TestDataSize) == 0);
+}
+
+void ComputeSocketTest()
+{
+	// Buffers for transferring between client and server
+	FComputeBuffer ClientToServerBuffer;
+	verify(ClientToServerBuffer.CreateNew(FComputeBuffer::FParams()));
+
+	FComputeBuffer ServerToClientBuffer;
+	verify(ServerToClientBuffer.CreateNew(FComputeBuffer::FParams()));
+
+	// Client transport
+	TUniquePtr<FComputeSocket> ClientSocket = CreateComputeSocket(TUniquePtr<FComputeTransport>(new FBufferTransport(ClientToServerBuffer.CreateWriter(), ServerToClientBuffer.CreateReader())), EComputeSocketEndpoint::Local);
+	TSharedPtr<FComputeChannel> ClientChannel1 = ClientSocket->CreateChannel(1);
+	TSharedPtr<FComputeChannel> ClientChannel2 = ClientSocket->CreateChannel(2);
+
+	// Server socket
+	TUniquePtr<FComputeSocket> ServerSocket = CreateComputeSocket(TUniquePtr<FComputeTransport>(new FBufferTransport(ServerToClientBuffer.CreateWriter(), ClientToServerBuffer.CreateReader())), EComputeSocketEndpoint::Remote);
+	TSharedPtr<FComputeChannel> ServerChannel1 = ServerSocket->CreateChannel(1);
+	TSharedPtr<FComputeChannel> ServerChannel2 = ServerSocket->CreateChannel(2);
+
+	// Close the original buffers now that the sockets are set up
+	ClientToServerBuffer.Close();
+	ServerToClientBuffer.Close();
+
+	// Send data over channel 1
+	CheckChannelSendRecv(ClientChannel1, ServerChannel1, "Channel 1: Client -> Server");
+
+	CheckChannelSendRecv(ServerChannel2, ClientChannel2, "Channel 2: Server -> Client");
+	CheckChannelSendRecv(ClientChannel2, ServerChannel2, "Channel 2: Client -> Server");
+
+	// Send data over channel 1 again
+	CheckChannelSendRecv(ClientChannel1, ServerChannel1, "Channel 1: Client -> Server (2)");
+	CheckChannelSendRecv(ServerChannel1, ClientChannel1, "Channel 1: Server -> Client (2)");
+}
+
+// --------------------------------------------------------------------------------------------------------
+// 'compute worker' command
+
+int RunComputeWorker()
+{
+	const int ChannelId = 100;
+
+	FWorkerComputeSocket Socket;
+	if (!Socket.Open())
+	{
+		std::cout << "Environment variable not set correctly" << std::endl;
+		return 1;
+	}
+
+	TSharedPtr<FComputeChannel> Channel = Socket.CreateChannel(ChannelId);
+	if (!Channel->IsValid())
+	{
+		std::cout << "Unable to create channel to initiator" << std::endl;
+		return 1;
+	}
+
+	std::cout << "Connected to initiator" << std::endl;
+
+	size_t Length = 0;
+	char Buffer[4];
+
+	Buffer[0] = 0;
+	Channel->Send(Buffer, 1); // Send a dummy one-byte message to let the remote know we're listening
+
+	for (;;)
+	{
+		size_t RecvLength = Channel->Recv(Buffer + Length, sizeof(Buffer) - Length);
+		if (RecvLength == 0)
+		{
+			return 0;
+		}
+
+		Length += RecvLength;
+
+		if (Length >= 4)
+		{
+			std::cout << "Read value " << *(int*)Buffer << std::endl;
+			Length = 0;
+		}
+	}
 }
