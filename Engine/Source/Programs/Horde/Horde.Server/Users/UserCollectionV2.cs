0ef5bd6b565bbd1930df23c7b81f7078a8cef05f
ben marsh <Ben.Marsh@epicgames.com>
2023-12-01 21:03:48 -0500

    Horde: Revert change to use FindMatchingElement() in MongoDB query; not supported by Linq2 driver.
    #jira
    
    [CL 30065668 by ben marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Server/Users/UserCollectionV2.cs
+++ Engine/Source/Programs/Horde/Horde.Server/Users/UserCollectionV2.cs
@@ -399,7 +399,7 @@ namespace Horde.Server.Users
 			{
 				JobTemplateSettingsDocument doc = new JobTemplateSettingsDocument(templateOptions.StreamId, templateOptions.TemplateId, templateOptions.TemplateHash, templateOptions.Arguments.ToList());
 				FilterDefinition<UserSettingsDocument> filter = Builders<UserSettingsDocument>.Filter.Eq(x => x.Id, userId) & Builders<UserSettingsDocument>.Filter.ElemMatch(x => x.JobTemplateSettings, t => t.StreamId == templateOptions.StreamId && t.TemplateId == templateOptions.TemplateId);
-				UpdateResult result = await _userSettings.UpdateOneAsync(filter, Builders<UserSettingsDocument>.Update.Set(x => x.JobTemplateSettings.FirstMatchingElement(), doc));
+				UpdateResult result = await _userSettings.UpdateOneAsync(filter, Builders<UserSettingsDocument>.Update.Set(x => x.JobTemplateSettings[-1], doc));
 				if (result.ModifiedCount == 0)
 				{
 					updates.Add(Builders<UserSettingsDocument>.Update.PushEach(x => x.JobTemplateSettings, new[] { doc }, -100));
