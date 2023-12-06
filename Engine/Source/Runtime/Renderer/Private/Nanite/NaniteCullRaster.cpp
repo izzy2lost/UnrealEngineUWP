a1c6f2dcafd14431262a88ce3e67884bd5ab0b01
rune stubbe <Rune.Stubbe@epicgames.com>
2023-12-06 09:50:56 -0500

    Changed default culling implementation to non-persistent after latest round of optimizations #rb ola.olsson [FYI] brian.karis, jamie.hayes, graham.wihlidal
    [CL 30154331 by rune stubbe in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/Nanite/NaniteCullRaster.cpp
+++ Engine/Source/Runtime/Renderer/Private/Nanite/NaniteCullRaster.cpp
@@ -243,8 +243,9 @@ static TAutoConsoleVariable<int32> CVarLargePageRectThreshold(
 
 static TAutoConsoleVariable<int32> CVarNanitePersistentThreadsCulling(
 	TEXT("r.Nanite.PersistentThreadsCulling"),
-	1,
-	TEXT("Perform node and cluster culling in one combined kernel using persistent threads."),
+	0,
+	TEXT("Perform node and cluster culling in one combined kernel using persistent threads.")
+	TEXT("It doesn't scale threads with GPU size and relies on scheduler behavior, so it is not recommended for non-fixed hardware platforms."),
 	ECVF_RenderThreadSafe
 );
 
