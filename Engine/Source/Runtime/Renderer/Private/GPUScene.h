d20717a8cc542da9c5405c4b202ee2f1a4fe7a90
tiago costa <tiago.costa@epicgames.com>
2023-12-07 07:53:13 -0500

    Workaround crash resizing LightDataBuffer due to MemcpyResource only supporting buffers with BytesPerElement = 4,8,16,32. - switched back to TRefCountPtr<FRDGPooledBuffer>
    #rb Ola.Olsson
    
    [CL 30177275 by tiago costa in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/GPUScene.h
+++ Engine/Source/Runtime/Renderer/Private/GPUScene.h
@@ -13,7 +13,6 @@
 #include "LightSceneData.h"
 #include "SceneUniformBuffer.h"
 #include "UnifiedBuffer.h"
-#include "RendererPrivateUtils.h"
 
 class FRDGExternalAccessQueue;
 class FRHICommandList;
@@ -334,7 +333,7 @@ public:
 private:
 	static constexpr int32 InitialBufferSize = 256;
 
-	TPersistentStructuredBuffer<FLightSceneData> LightDataBuffer;
+	TRefCountPtr<FRDGPooledBuffer> LightDataBuffer;
 
 	// Buffers used by GPU-Scene, since they can be resized during updates AND the render passes must retain the 
 	// right copy (this is chiefly because the init of shadow views after pre-pass means we need to be able to set 
