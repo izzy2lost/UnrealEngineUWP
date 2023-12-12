aefbd80a6c7b8de658597b0bf993ee3277feb628
ben marsh <Ben.Marsh@epicgames.com>
2023-12-12 09:52:18 -0500

    Horde: Fix static analysis warnings.
    #jira
    
    [CL 30262994 by ben marsh in ue5-main branch]

--- Engine/Source/Programs/Shared/EpicGames.Core.Tests/LogValueTests.cs
+++ Engine/Source/Programs/Shared/EpicGames.Core.Tests/LogValueTests.cs
@@ -2,12 +2,10 @@
 
 using System;
 using System.Buffers;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EpicGames.Core.Tests
--- Engine/Source/Programs/Shared/EpicGames.Horde/Agents/Leases/LeaseId.cs
+++ Engine/Source/Programs/Shared/EpicGames.Horde/Agents/Leases/LeaseId.cs
@@ -1,7 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using System.ComponentModel;
-using System.Text.Json;
 using EpicGames.Core;
 
 namespace EpicGames.Horde.Agents.Leases
