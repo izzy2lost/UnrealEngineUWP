5ebd6fcb40e6357be4891215f931ecb7589b7d68
brandon schaefer <Brandon.Schaefer@epicgames.com>
2023-12-13 14:52:55 -0500

    Remove generated file
    #rb Brandon.Schaefer
    
    [CL 30300730 by brandon schaefer in ue5-main branch]

--- Engine/Programs/AutoRTFMTests/Source/AutoRTFMTestsClient.Target.cs
+++
@@ -1,7 +0,0 @@
-// Created by ushell to make blueprint projects work end to end.
-// Placed in Source/ because UAT cleans Intermediate/Source/
-using UnrealBuildTool;
-public class AutoRTFMTestsClientTarget : TargetRules {
-public AutoRTFMTestsClientTarget(TargetInfo Target) : base(Target) {
-Type = TargetType.Client;
-ExtraModuleNames.AddRange(new string[] {"UnrealGame"} ); }}
