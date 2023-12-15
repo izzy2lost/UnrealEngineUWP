bd7c6ac588f80acca06d930ec445038170228aab
logan buchy <logan.buchy@epicgames.com>
2023-12-15 12:37:44 -0500

    Add TEDS PivotOffset Column definition
    #jira UE-202793
    #rb ronald.koppers
    
    [CL 30352136 by logan buchy in ue5-main branch]

---
+++ Engine/Source/Runtime/TypedElementFramework/Public/Elements/Columns/TypedElementPivotOffsetColumns.h
@@ -0,0 +1,16 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Elements/Interfaces/TypedElementDataStorageInterface.h"
+
+#include "TypedElementPivotOffsetColumns.generated.h"
+
+USTRUCT()
+struct FTypedElementPivotOffset final : public FTypedElementDataStorageColumn
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FVector Offset;
+};
