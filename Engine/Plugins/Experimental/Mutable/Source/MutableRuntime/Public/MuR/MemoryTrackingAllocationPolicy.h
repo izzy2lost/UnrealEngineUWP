83fd93e7406cf222d40ffb60a9cdda1555015645
jason hoerner <jason.hoerner@epicgames.com>
2023-12-15 06:58:37 -0500

    [Backout Resubmit with missing include build fix] SlackReport debug command for TArray slack tracking. * Requires ENABLE_ARRAY_SLACK_TRACKING define to be enabled at compile time (enabling the tracking all the time would be too significant a performance hit, as it requires a function call per array count change). * Generates TSV (tab separated value) reports in Shared/Logs/SlackReport, which can be loaded into a spreadsheet program.  A filename can be specified to the command, or a default one with an incrementing index will be used. * Switch -Stack=N controls the number of stack frames used for sorting.  Setting a value less than the maximum (9 levels) can coalesce instances of the same allocation from slightly different call stacks.  Switch -Verbose=0 enables a more condensed report that only shows the largest unique Num / Max bucket per call stack.
    #rb Steve.Robb
    
    [CL 30346075 by jason hoerner in ue5-main branch]

--- Engine/Plugins/Experimental/Mutable/Source/MutableRuntime/Public/MuR/MemoryTrackingAllocationPolicy.h
+++ Engine/Plugins/Experimental/Mutable/Source/MutableRuntime/Public/MuR/MemoryTrackingAllocationPolicy.h
@@ -186,6 +186,17 @@ namespace mu
 			{
 				return Base.GetInitialCapacity();
 			}
+
+#if UE_ENABLE_ARRAY_SLACK_TRACKING
+			FORCEINLINE void SlackTrackerLogNum(SizeType NewNumUsed)
+			{
+				if constexpr (TAllocatorTraits<BaseAlloc>::SupportsSlackTracking)
+				{
+					Base.SlackTrackerLogNum(NewNumUsed);
+				}
+			}
+#endif
+
 		private:
 			typename BaseAlloc::ForAnyElementType Base;
 			SSIZE_T AllocSize = 0;
@@ -237,6 +248,7 @@ template<typename BaseAlloc, typename Counter>
 struct TAllocatorTraits<mu::FMemoryTrackingAllocatorWrapper<BaseAlloc, Counter>> : public TAllocatorTraitsBase<mu::FMemoryTrackingAllocatorWrapper<BaseAlloc, Counter>>
 {
 	enum { SupportsElementAlignment = TAllocatorTraits<BaseAlloc>::SupportsElementAlignment };
+	enum { SupportsSlackTracking = TAllocatorTraits<BaseAlloc>::SupportsSlackTracking };
 	enum { SupportsMoveFromOtherAllocator = false };
 };
 
