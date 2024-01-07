954b609c7abb30ae27a722e56ff9b7dbb043b69a
richard graham <richard.graham@epicgames.com>
2024-01-07 15:50:57 -0500

    Virtual Scouting - rename Camera aperture array to Viewfinder to avoid ambiguity with the VPSettings plugin [FYI] gregg.bond1
    [CL 30475841 by richard graham in ue5-main branch]

--- Engine/Plugins/Experimental/VirtualScouting/Source/VirtualScouting/Public/VirtualScoutingSettings.h
+++ Engine/Plugins/Experimental/VirtualScouting/Source/VirtualScouting/Public/VirtualScoutingSettings.h
@@ -19,17 +19,17 @@ public:
 	UPROPERTY(Config, EditAnywhere, BlueprintReadWrite, Category="Virtual Scouting", meta=(DisplayName="Show Measurements in Imperial Units"))
 	bool bUseImperial = false;
 
-	UPROPERTY(Config, EditAnywhere, BlueprintReadWrite, Category="Virtual Scouting", meta=(DisplayName="Camera Apertures"))
-	TArray<float> CameraApertureArray = {1.0,1.4,2.0,2.8,4.0,5.6,8.0,11.0,16.0,22.0};
+	UPROPERTY(Config, EditAnywhere, BlueprintReadWrite, Category="Virtual Scouting", meta=(DisplayName="Viewfinder Apertures"))
+	TArray<float> ViewfinderApertureArray = {1.0,1.4,2.0,2.8,4.0,5.6,8.0,11.0,16.0,22.0};
 
-	UFUNCTION(BlueprintPure, Category="Virtual Scouting")
+	UFUNCTION(BlueprintPure, Category="Virtual Scouting", DisplayName="Virtual Scouting Settings")
 	static UVirtualScoutingSettings* GetVirtualScoutingSettings();
 };
 
 /**
  * Per user settings for Virtual Scouting Editor.
  */
-UCLASS(Config=EditorPerProjectUserSettings, DisplayName="Virtual Scouting Editor")
+UCLASS(Config=EditorPerProjectUserSettings, DisplayName="Virtual Scouting Editor Settings")
 class VIRTUALSCOUTING_API UVirtualScoutingEditorSettings : public UObject
 {
 	GENERATED_BODY()
