86e935281a83e3eb651c3e4f24f093c4830f821d
henrik karlsson <henrik.karlsson@epicgames.com>
2024-01-10 00:23:22 -0500

    [ShaderCompileWorker] * Fixed linux compile error
    [CL 30525276 by henrik karlsson in ue5-main branch]

--- Engine/Source/Programs/ShaderCompileWorker/Private/ShaderCompileWorker.cpp
+++ Engine/Source/Programs/ShaderCompileWorker/Private/ShaderCompileWorker.cpp
@@ -58,6 +58,15 @@ HMODULE GetUbaModule()
 }
 #endif
 
+inline bool IsUsingUBA()
+{
+#if PLATFORM_WINDOWS // Currently only implemented for windows
+	return GetUbaModule() != nullptr;
+#else
+	return false;
+#endif
+}
+
 #if USING_CODE_ANALYSIS
 	UE_NORETURN static inline void ExitWithoutCrash(FSCWErrorCode::ECode ErrorCode, const FString& Message);
 #endif
@@ -727,7 +736,7 @@ private:
 		// In xml mode, we signal completion by creating a zero byte "Success" file after the output file has been fully written.
 		// In intercept mode, completion is signaled by this process terminating.
 		// For UBA we can't delete the file when running remotely because there might be a crash or disconnect happening before result is sent back and then we can't retry
-		if (!IsUsingXGE() && !KeepInput && !GetUbaModule())
+		if (!IsUsingXGE() && !KeepInput && !IsUsingUBA())
 		{
 			do 
 			{
