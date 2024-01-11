a1d667dc53c6460c7735303f8c83d7e704821f98
charles bloom <charles.bloom@epicgames.com>
2024-01-11 14:56:22 -0500

    TextureLODSettings make TMGS_Angular fall back to SimpleAverage rather than NoMipMaps when not a cube map
    #rnx
    
    [CL 30575087 by charles bloom in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/TextureLODSettings.cpp
+++ Engine/Source/Runtime/Engine/Private/TextureLODSettings.cpp
@@ -143,6 +143,7 @@ int32 UTextureLODSettings::CalculateLODBias(const UTexture* Texture, bool bIncCi
 	TextureMaxSize = Texture->MaxTextureSize;
 #endif // #if WITH_EDITORONLY_DATA
 
+	// note: these are the Platform texture size and can return 0 during build!
 	float Width = Texture->GetSurfaceWidth();
 	float Height = Texture->GetSurfaceHeight();
 
@@ -298,9 +299,9 @@ void UTextureLODSettings::GetMipGenSettings(const UTexture& Texture, TextureMipG
 
 	// angular filtering only applies to cubemaps
 	// note: currently intentionally NOT allowed on cubearrays
-	if (Setting == TMGS_Angular && !Texture.IsA(UTextureCube::StaticClass()))
+	if (Setting == TMGS_Angular && Texture.GetTextureClass() != ETextureClass::Cube )
 	{
-		Setting = TMGS_NoMipmaps;
+		Setting = TMGS_SimpleAverage;
 	}
 
 	OutMipGenSettings = Setting;
