ae0b221327706d8ddfc31600d39092af5a803ba7
igor stefanovic <igor.stefanovic@3lateral.com>
2024-01-12 09:42:30 -0500

    Deleting duplication code from the stream
    #rb peter.wigg
    
    [CL 30592176 by igor stefanovic in ue5-main branch]

--- Engine/Plugins/VirtualProduction/CaptureManager/Source/CaptureUtils/Private/CaptureUtilsModule.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Modules/ModuleManager.h"
-
-class FCaptureUtilsModule : public IModuleInterface
-{
-};
-
-IMPLEMENT_MODULE(FCaptureUtilsModule, CaptureUtils)
--- Engine/Plugins/VirtualProduction/CaptureManager/Source/LiveLinkHubCaptureSource/Private/LiveLinkHubCaptureSourceLog.h
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#pragma once
-
-#include "Logging/LogMacros.h"
-
-DECLARE_LOG_CATEGORY_EXTERN(LogLiveLinkHubCaptureSource, Log, All);
-DECLARE_LOG_CATEGORY_EXTERN(LogMessagingCapability, Log, All);
