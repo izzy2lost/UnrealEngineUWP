3ec67cb51a08320e2928c5b0ed8837672b0d4486
frederick lupien <frederick.lupien@epicgames.com>
2024-01-12 13:46:24 -0500

    Add a console command variable to disable hand ik retargeting anim node  (a.AnimNode.HandIKRetargeting.Enable 1/0)
    #rb Jose.Villarroel
    [FYI] Paul.McLaurin
    
    [CL 30599297 by frederick lupien in ue5-main branch]

--- Engine/Source/Runtime/AnimGraphRuntime/Private/BoneControllers/AnimNode_HandIKRetargeting.cpp
+++ Engine/Source/Runtime/AnimGraphRuntime/Private/BoneControllers/AnimNode_HandIKRetargeting.cpp
@@ -6,6 +6,10 @@
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(AnimNode_HandIKRetargeting)
 
+#if ENABLE_ANIM_DEBUG
+static TAutoConsoleVariable<bool> CVarAnimHandIKRetargetingEnable(TEXT("a.AnimNode.HandIKRetargeting.Enable"), true, TEXT("Enable / Disable Hand IK retargeting"));
+#endif
+
 /////////////////////////////////////////////////////
 // FAnimNode_HandIKRetargeting
 
@@ -37,6 +41,12 @@ void FAnimNode_HandIKRetargeting::EvaluateSkeletalControl_AnyThread(FComponentSp
 	DECLARE_SCOPE_HIERARCHICAL_COUNTER_ANIMNODE(EvaluateSkeletalControl_AnyThread)
 	ANIM_MT_SCOPE_CYCLE_COUNTER_VERBOSE(HandIKRetargeting, !IsInGameThread());
 
+#if ENABLE_ANIM_DEBUG
+	if (!CVarAnimHandIKRetargetingEnable.GetValueOnAnyThread())
+	{
+		return;
+	}
+#endif 
 	checkSlow(OutBoneTransforms.Num() == 0);
 
 	const FBoneContainer& BoneContainer = Output.Pose.GetPose().GetBoneContainer();
