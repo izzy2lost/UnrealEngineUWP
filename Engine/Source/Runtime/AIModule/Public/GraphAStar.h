f721f3cc5729971c8d910dd2ce99b60c29445b64
yoan stamant <Yoan.Stamant@epicgames.com>
2024-01-19 14:27:15 -0500

    [GraphAStar] Removed shrinking option parameter from PopIndex in FOpenList since we never want to shrink #rb Aris.Theophanidis
    [CL 30724015 by yoan stamant in ue5-main branch]

--- Engine/Source/Runtime/AIModule/Public/GraphAStar.h
+++ Engine/Source/Runtime/AIModule/Public/GraphAStar.h
@@ -299,17 +299,20 @@ struct FGraphAStar
 			check(false); // We should never reach here.
 		}
 
-		int32 PopIndex(EAllowShrinking AllowShrinking = EAllowShrinking::Yes)
+		int32 PopIndex()
 		{
 			int32 SearchNodeIndex = INDEX_NONE;
+
+			// During A* we grow the array as needed and it does not make sense to shrink in the process.
 			Super::HeapPop(SearchNodeIndex, NodeSorter, EAllowShrinking::No);
 			NodePool[SearchNodeIndex].MarkNotOpened();
 			return SearchNodeIndex;
 		}
-		UE_ALLOWSHRINKING_BOOL_DEPRECATED("PopIndex")
+
+		UE_DEPRECATED(5.4, "PopIndex with a boolean bAllowShrinking has been deprecated - please use the version without parameter")
 		FORCEINLINE int32 PopIndex(bool bAllowShrinking)
 		{
-			return PopIndex(bAllowShrinking ? EAllowShrinking::Yes : EAllowShrinking::No);
+			return PopIndex();
 		}
 	};
 
