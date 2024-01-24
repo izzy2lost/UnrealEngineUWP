2a6f0ca55a5641a01cc402cf5ae5ea7987b6aa4f
joe kirchoff <joe.kirchoff@epicgames.com>
2024-01-24 15:14:30 -0500

    UnrealBuildTool: Move module and plugin validation out of BuildAsync and cleanup warnings
    #rnx
    
    [CL 30858471 by joe kirchoff in 5.4 branch]

--- Engine/Source/Programs/UnrealBuildTool/Configuration/UEBuildModule.cs
+++ Engine/Source/Programs/UnrealBuildTool/Configuration/UEBuildModule.cs
@@ -341,16 +341,16 @@ namespace UnrealBuildTool
 		public HashSet<UEBuildModule> GetDependencies(bool bWithIncludePathModules, bool bWithDynamicallyLoadedModules)
 		{
 			HashSet<UEBuildModule> Modules = new HashSet<UEBuildModule>();
-			Modules.UnionWith(PublicDependencyModules!);
-			Modules.UnionWith(PrivateDependencyModules!);
+			Modules.UnionWith(PublicDependencyModules ?? new());
+			Modules.UnionWith(PrivateDependencyModules ?? new());
 			if (bWithIncludePathModules)
 			{
-				Modules.UnionWith(PublicIncludePathModules!);
-				Modules.UnionWith(PrivateIncludePathModules!);
+				Modules.UnionWith(PublicIncludePathModules ?? new());
+				Modules.UnionWith(PrivateIncludePathModules ?? new());
 			}
 			if (bWithDynamicallyLoadedModules)
 			{
-				Modules.UnionWith(DynamicallyLoadedModules!);
+				Modules.UnionWith(DynamicallyLoadedModules ?? new());
 			}
 			return Modules;
 		}
