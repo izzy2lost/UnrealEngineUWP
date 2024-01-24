84fe4ade6f15840f80b6847f356ae0edd72030b6
helge mathee <Helge.Mathee@epicgames.com>
2024-01-24 09:59:08 -0500

    SchematicGraph: Render label with backdrop
    #rb sara.schvartzman
    #jira UE-173816
    #rnx
    
    [CL 30842689 by helge mathee in ue5-main branch]

--- Engine/Source/Editor/AnimationEditorWidgets/Public/SchematicGraphPanel/SchematicGraphStyle.h
+++ Engine/Source/Editor/AnimationEditorWidgets/Public/SchematicGraphPanel/SchematicGraphStyle.h
@@ -37,6 +37,8 @@ public:
 		Set("Schematic.Dot.Large", new IMAGE_BRUSH_SVG("SchematicGraph/Schematic_Dot_Large", Icon128x128));
 		Set("Schematic.Dot.Group", new IMAGE_BRUSH_SVG("SchematicGraph/Schematic_Dot_Group", Icon128x128));
 
+		Set("Schematic.Label.Background", new FSlateRoundedBoxBrush(FStyleColors::White, 4.0f, FStyleColors::Transparent, 0.0f));
+
 		FSlateStyleRegistry::RegisterSlateStyle(*this);
 	}
 
