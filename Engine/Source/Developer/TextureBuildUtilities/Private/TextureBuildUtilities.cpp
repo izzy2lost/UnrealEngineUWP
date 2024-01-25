471244fa0861ce509a1673e6925ad8ae99b47b2c
dan thompson <dan.thompson@epicgames.com>
2024-01-25 14:28:31 -0500

    Undoing backout - this was depending on the original issue, not an issue in and of itself. This does also correct a separate issue with the texture editor using the removed metadata information.
    Removing texture build metadata due to ddc complexities. It will be added to the metadata system in the new build flow SoonTM. Additionally, the alpha information for the source mips is no longer necessary as that is computed elsewhere now.
    #rb fabian.giesen
    #jira UE-183750
    
    [CL 30895383 by dan thompson in 5.4 branch]

--- Engine/Source/Developer/TextureBuildUtilities/Private/TextureBuildUtilities.cpp
+++ Engine/Source/Developer/TextureBuildUtilities/Private/TextureBuildUtilities.cpp
@@ -255,10 +255,6 @@ FCbObject FTextureBuildMetadata::ToCompactBinaryWithDefaults() const
 
 	FCbWriter Writer;
 	Writer.BeginObject();
-	if (bSourceMipsAlphaDetected != Defaults.bSourceMipsAlphaDetected)
-	{
-		Writer.AddBool("bSourceMipsAlphaDetected", bSourceMipsAlphaDetected);
-	}
 	if (PreEncodeMipsHash != Defaults.PreEncodeMipsHash)
 	{
 		Writer << UTF8TEXTVIEW("PreEncodeMipsHash") << PreEncodeMipsHash;
@@ -269,7 +265,6 @@ FCbObject FTextureBuildMetadata::ToCompactBinaryWithDefaults() const
 
 FTextureBuildMetadata::FTextureBuildMetadata(FCbObject InCbObject)
 {
-	bSourceMipsAlphaDetected = InCbObject["bSourceMipsAlphaDetected"].AsBool(bSourceMipsAlphaDetected);
 	PreEncodeMipsHash = InCbObject["PreEncodeMipsHash"].AsUInt64(PreEncodeMipsHash);
 }
 
