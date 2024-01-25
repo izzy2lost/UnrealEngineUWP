af7e120675dc1fa589b4484e03da95ff4ded6a39
markus breyer <Markus.Breyer@epicgames.com>
2024-01-25 18:44:40 -0500

    New UnrealEditorVM Target and regression tests
    #rb saam.barati
    #rnx
    
    [CL 30907153 by markus breyer in 5.4 branch]

---
+++ Engine/Source/UnrealEditorVM.Target.cs
@@ -0,0 +1,48 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class UnrealEditorVMTarget : UnrealEditorTarget
+{
+	public UnrealEditorVMTarget( TargetInfo Target ) : base(Target)
+	{
+		// Use the new VM (= don't use the BPVM)
+		bUseVerseBPVM = false;
+		bUseAutoRTFMCompiler = true;
+		BuildEnvironment = TargetBuildEnvironment.Unique;
+		bLegalToDistributeBinary = true;
+
+		DisablePlugins.AddRange(
+			new string[] {
+				"ConcertSyncEntity",
+				"EntityActor",
+				"EntityCore",
+				"EntityFramework",
+				"EpicGamesEngine",
+				"EpicGamesEngineEditor",
+				"EpicGamesEngineRestricted",
+				"EpicGamesTemporary",
+				"UnrealEngineExperimental",
+				"VerseColors",
+				"VerseEngine",
+				"VerseEngineRestricted",
+				"VerseExperimental",
+				"VerseGameplay",
+				"VerseGameplayDebug",
+				"VerseGameplayTags",
+				"VerseGeoScript",
+				"VerseInput",
+				"VerseMovement",
+				"VersePrint",
+				"VerseRestricted",
+				"VerseSimulation",
+				"VerseSimulationMetadata",
+				"VerseUEStandalone",
+				"VerseUI",
+				"VerseWorldPartition",
+				"VGameplayRst",
+			}
+		);
+	}
+}
