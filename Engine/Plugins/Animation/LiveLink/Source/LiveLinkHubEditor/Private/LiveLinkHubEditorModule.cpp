e2bbf3710d51499b1d13d069b5748dcf40bbe96d
jeremie roy <jeremie.roy@epicgames.com>
2024-01-26 17:49:50 -0500

    [Backout] - CL30904477 (Re-add with fixes) [FYI] edwin.maynard Original CL Desc ----------------------------------------------------------------- [Backout] - CL30895823 (backed out due to errors) [FYI] jeremie.roy Original CL Desc ----------------------------------------------------------------- Add livelink hub status bar to the editor (Only appears when we have an active connection to a hub instance)
    #jira UE-203796 UE-202636
    #rb Geoffrey.Douglas, jason.walter
    
    [CL 30945322 by jeremie roy in 5.4 branch]

---
+++ Engine/Plugins/Animation/LiveLink/Source/LiveLinkHubEditor/Private/LiveLinkHubEditorModule.cpp
@@ -0,0 +1,38 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "LiveLinkHubEditorModule.h"
+
+#include "SLiveLinkHubEditorStatusBar.h"
+#include "ToolMenus.h"
+
+void FLiveLinkHubEditorModule::OnPostEngineInit()
+{
+	if (GEditor)
+	{
+		RegisterLiveLinkHubStatusBar();
+	}
+}
+
+void FLiveLinkHubEditorModule::RegisterLiveLinkHubStatusBar()
+{
+	UToolMenu* Menu = UToolMenus::Get()->ExtendMenu(TEXT("LevelEditor.StatusBar.ToolBar"));
+
+	FToolMenuSection& LiveLinkHubSection = Menu->AddSection(TEXT("LiveLinkHub"), FText::GetEmpty(), FToolMenuInsert(NAME_None, EToolMenuInsertType::First));
+
+	LiveLinkHubSection.AddEntry(
+		FToolMenuEntry::InitWidget(TEXT("LiveLinkHubStatusBar"), CreateLiveLinkHubWidget(), FText::GetEmpty(), true, false)
+	);
+}
+
+void FLiveLinkHubEditorModule::UnregisterLiveLinkHubStatusBar()
+{
+	UToolMenus::UnregisterOwner(this);
+}
+
+TSharedRef<SWidget> FLiveLinkHubEditorModule::CreateLiveLinkHubWidget()
+{
+	return SNew(SLiveLinkHubEditorStatusBar);
+}
+
+IMPLEMENT_MODULE(FLiveLinkHubEditorModule, LiveLinkHubEditor);
