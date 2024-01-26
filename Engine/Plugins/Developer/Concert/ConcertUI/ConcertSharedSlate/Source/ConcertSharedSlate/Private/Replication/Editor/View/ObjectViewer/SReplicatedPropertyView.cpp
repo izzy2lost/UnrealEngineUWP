a834e64e3c9bedf55bcc7e987960ef0f5042b7ce
dominik peacock <dominik.peacock@geotech.one>
2024-01-26 12:56:42 -0500

    Refactor property tree UI in preparation for extracting property filtering to ConcertSharedClientSlate
    [CL 30930366 by dominik peacock in 5.4 branch]

--- Engine/Plugins/Developer/Concert/ConcertUI/ConcertSharedSlate/Source/ConcertSharedSlate/Private/Replication/Editor/View/ObjectViewer/SReplicatedPropertyView.cpp
+++ Engine/Plugins/Developer/Concert/ConcertUI/ConcertSharedSlate/Source/ConcertSharedSlate/Private/Replication/Editor/View/ObjectViewer/SReplicatedPropertyView.cpp
@@ -4,12 +4,10 @@
 
 #include "Replication/Editor/Model/IReplicationStreamModel.h"
 #include "Replication/Editor/Model/ReplicatedObjectData.h"
-#include "Replication/Editor/Model/ReplicatedPropertyData.h"
-#include "Replication/Editor/View/ObjectViewer/Property/SPropertyTreeView.h"
 #include "Replication/Editor/View/SelectionViewerColumns.h"
 
 #include "Algo/AllOf.h"
-#include "Algo/ForEach.h"
+#include "Property/SFilteredPropertyTreeView.h"
 #include "Widgets/Layout/SWidgetSwitcher.h"
 #include "Widgets/Text/STextBlock.h"
 
@@ -20,6 +18,7 @@ namespace UE::ConcertSharedSlate
 	void SReplicatedPropertyView::Construct(const FArguments& InArgs, TSharedRef<IReplicationStreamModel> InPropertiesModel)
 	{
 		PropertiesModel = MoveTemp(InPropertiesModel);
+		
 		GetSelectedRootObjectsDelegate = InArgs._GetSelectedRootObjects;
 		check(GetSelectedRootObjectsDelegate.IsBound());
 		
@@ -45,23 +44,13 @@ namespace UE::ConcertSharedSlate
 			SetPropertyContent(EReplicatedPropertyContent::SelectionTooBig);
 			return;
 		}
+		
 		const FSoftClassPath Class = *SharedClass;
-		if (!ensureMsgf(Class.IsValid(), TEXT("This should only trigger if IReplicationSubobjectView returned a object not contained the model & that the UI is view-only. In that case fix your IReplicationSubobjectView or use it in an UI editor.")))
+		if (!ensure(Class.IsValid()))
 		{
 			return;
 		}
-
-		// If the objects have changed, the classes may share properties.
-		// In that case, below we'd reuse the item pointer, which would cause the tree view to re-use the old row widgets.
-		// However, we must regenerate all column widgets since they may be referencing the object the row was originally built for. So they'd display the state of the previous object still!
-		// Example: Assign property combo-box in Multi-User All Clients view displays who has the property assigned.
-		// Note: If the objects did not change, we definitely want to reuse item pointers since otherwise the user row selection is reset.
-		const bool bCanReusePropertyData = PreviousSelectedObjects == SelectedObjects; // This SHOULD be an order independent compare but usually Num == 1, so whatever
-		if (!bCanReusePropertyData)
-		{
-			ChainToPropertyDataCache.Reset();
-		}
-
+		
 		// Build the set of properties that are shared by all of the selected objects
 		TSet<FConcertPropertyChain> SharedProperties = PropertiesModel->GetAllProperties(SelectedObjects[0]);
 		for (int32 i = 1; i < SelectedObjects.Num(); ++i)
@@ -69,32 +58,17 @@ namespace UE::ConcertSharedSlate
 			SharedProperties = PropertiesModel->GetAllProperties(SelectedObjects[i]).Union(SharedProperties);
 		}
 		
-		// Try to re-use old instances by using the old ChainToPropertyDataCache. This is also done so the expansion states restore correctly in the tree view.
-		TMap<FConcertPropertyChain, TSharedPtr<FReplicatedPropertyData>> NewChainToPropertyDataCache;
+		// If the objects have changed, the classes may share properties.
+		// In that case, below we'd reuse the item pointer, which would cause the tree view to re-use the old row widgets.
+		// However, we must regenerate all column widgets since they may be referencing the object the row was originally built for. So they'd display the state of the previous object still!
+		// Example: Assign property combo-box in Multi-User All Clients view displays who has the property assigned.
+		// Note: If the objects did not change, we definitely want to reuse item pointers since otherwise the user row selection is reset.
+		const bool bCanReusePropertyData = PreviousSelectedObjects == SelectedObjects; // This SHOULD be an order independent compare but usually Num == 1, so whatever
 		
-		PropertyRowData.Empty();
-		for (const FConcertPropertyChain& PropertyChain : SharedProperties)
-		{
-			const TSharedPtr<FReplicatedPropertyData>* ExistingItem = ChainToPropertyDataCache.Find(PropertyChain);
-			const TSharedRef<FReplicatedPropertyData> Item = ExistingItem ? ExistingItem->ToSharedRef() : AllocatePropertyData(Class, PropertyChain);
-			PropertyRowData.Emplace(Item);
-			NewChainToPropertyDataCache.Emplace(PropertyChain, Item);
-		}
-
-		// If an item was removed, then NewPathToPropertyDataCache does not contain it. 
-		ChainToPropertyDataCache = MoveTemp(NewChainToPropertyDataCache);
+		ReplicatedProperties->RefreshPropertyData(SharedProperties, Class, bCanReusePropertyData);
 		
-		// The tree view requires the item source to only contain the root items.
-		BuildRootPropertyRowData();
 		SetPropertyContent(EReplicatedPropertyContent::Properties);
-
 		PreviousSelectedObjects = MoveTemp(SelectedObjects);
-		ReplicatedProperties->OnItemsChanged();
-	}
-
-	void SReplicatedPropertyView::RequestResortForColumn(const FName& ColumnId)
-	{
-		ReplicatedProperties->RequestResortForColumn(ColumnId);
 	}
 
 	TArray<FSoftObjectPath> SReplicatedPropertyView::GetObjectsSelectedForPropertyEditing() const
@@ -131,18 +105,14 @@ namespace UE::ConcertSharedSlate
 			// EReplicatedPropertyContent::Properties
 			+SWidgetSwitcher::Slot()
 			[
-				SAssignNew(ReplicatedProperties, SPropertyTreeView)
-				.RootItemsSource(&RootPropertyRowData)
-				.OnGetChildren(this, &SReplicatedPropertyView::GetPropertyRowChildren)
+				SAssignNew(ReplicatedProperties, SFilteredPropertyTreeView)
 				.Columns(Columns)
 				.ExpandableColumnLabel(ReplicationColumns::Property::LabelColumnId)
 				.PrimarySort(PrimarySort)
 				.SecondarySort(SecondarySort)
 				.SelectionMode(ESelectionMode::Multi)
-				.NameModel(InArgs._NameModel)
 				.LeftOfSearchBar() [ InArgs._LeftOfPropertySearchBar.Widget ]
 				.RightOfSearchBar() [ InArgs._RightOfPropertySearchBar.Widget ]
-				.SelectedObjects(this, &SReplicatedPropertyView::GetObjectsSelectedForPropertyEditing)
 			]
 			
 			// EReplicatedPropertyContent::NoSelection
@@ -164,23 +134,6 @@ namespace UE::ConcertSharedSlate
 			];
 	}
 	
-	TSharedRef<FReplicatedPropertyData> SReplicatedPropertyView::AllocatePropertyData(FSoftClassPath OwningClass, FConcertPropertyChain PropertyChain)
-	{
-		return MakeShared<FReplicatedPropertyData>(MoveTemp(OwningClass), MoveTemp(PropertyChain));
-	}
-
-	void SReplicatedPropertyView::BuildRootPropertyRowData()
-	{
-		RootPropertyRowData.Empty(PropertyRowData.Num());
-		for (const TSharedPtr<FReplicatedPropertyData>& PropertyData : PropertyRowData)
-		{
-			if (PropertyData->GetProperty().IsRootProperty())
-			{
-				RootPropertyRowData.Emplace(PropertyData);
-			}
-		}
-	}
-	
 	TOptional<FSoftClassPath> SReplicatedPropertyView::GetClassForPropertiesFromSelection(const TArray<FSoftObjectPath>& Objects) const
 	{
 		const FSoftClassPath Class = PropertiesModel->GetObjectClass(Objects[0]);
@@ -189,22 +142,6 @@ namespace UE::ConcertSharedSlate
 			return PropertiesModel->GetObjectClass(Object) == Class;
 		});
 		return bAllHaveSameClass ? Class : TOptional<FSoftClassPath>{};
-	}
-
-	void SReplicatedPropertyView::GetPropertyRowChildren(TSharedPtr<FReplicatedPropertyData> ReplicatedPropertyData, TFunctionRef<void(TSharedPtr<FReplicatedPropertyData>)> ProcessChild)
-	{
-		TArray<TSharedPtr<FReplicatedPropertyData>> Children;
-		
-		// Not the most efficient but it should be fine.
-		for (const TSharedPtr<FReplicatedPropertyData>& Data : PropertyRowData)
-		{
-			if (Data->GetProperty().IsDirectChildOf(ReplicatedPropertyData->GetProperty()))
-			{
-				Children.Add(Data);
-			}
-		}
-
-		Algo::ForEach(Children, [&ProcessChild](const TSharedPtr<FReplicatedPropertyData>& Data){ ProcessChild(Data); });
 	}
 
 	void SReplicatedPropertyView::SetPropertyContent(EReplicatedPropertyContent Content) const
