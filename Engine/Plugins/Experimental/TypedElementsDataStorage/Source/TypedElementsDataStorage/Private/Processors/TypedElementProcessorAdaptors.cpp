c75b4d68dbf66efed93865949d6c1f082a859bfb
ronald koppers <ronald.koppers@epicgames.com>
2024-01-26 17:20:04 -0500

    Fixed a failing automated test.
    A "is valid"-check for a previous bug was only applied in half the cases. A recent change merged both implementations but was based on the version that didn't have the checks. The checks have been reintroduced.
    
    #jira UE-204981
    #rb logan.buchy
    
    [CL 30943698 by ronald koppers in ue5-main branch]

--- Engine/Plugins/Experimental/TypedElementsDataStorage/Source/TypedElementsDataStorage/Private/Processors/TypedElementProcessorAdaptors.cpp
+++ Engine/Plugins/Experimental/TypedElementsDataStorage/Source/TypedElementsDataStorage/Private/Processors/TypedElementProcessorAdaptors.cpp
@@ -359,20 +359,28 @@ public:
 		Context.Defer().PushCommand<FMassDeferredAddCommand>(
 			[AddedColumn](FMassEntityManager& System)
 			{
-				// Check before adding.  Mass's AddFragmentToEntity is not idempotent and will assert if adding
-				// column to a row that already has one
-				FStructView Fragment = System.GetFragmentDataStruct(AddedColumn->Entity, AddedColumn->FragmentType);
-				if (!Fragment.IsValid())
+				// Check entity before proceeding. It's possible it may have been invalidated before this deferred call fired.
+				if (System.IsEntityActive(AddedColumn->Entity))
 				{
-					System.AddFragmentToEntity(AddedColumn->Entity, AddedColumn->FragmentType);
+					// Check before adding.  Mass's AddFragmentToEntity is not idempotent and will assert if adding
+					// column to a row that already has one
+					FStructView Fragment = System.GetFragmentDataStruct(AddedColumn->Entity, AddedColumn->FragmentType);
+					if (!Fragment.IsValid())
+					{
+						System.AddFragmentToEntity(AddedColumn->Entity, AddedColumn->FragmentType);
+					}
 				}
 			});
 
 		Context.Defer().PushCommand<FMassDeferredSetCommand>(
 			[AddedColumn](FMassEntityManager& System)
 			{
-				FStructView Fragment = System.GetFragmentDataStruct(AddedColumn->Entity, AddedColumn->FragmentType);
-				AddedColumn->Relocator(AddedColumn->FragmentType, Fragment.GetMemory(), AddedColumn->Object);
+				// Check entity before proceeding. It's possible it may have been invalidated before this deferred call fired.
+				if (System.IsEntityActive(AddedColumn->Entity))
+				{
+					FStructView Fragment = System.GetFragmentDataStruct(AddedColumn->Entity, AddedColumn->FragmentType);
+					AddedColumn->Relocator(AddedColumn->FragmentType, Fragment.GetMemory(), AddedColumn->Object);
+				}
 			});
 		
 		return ColumnData;
