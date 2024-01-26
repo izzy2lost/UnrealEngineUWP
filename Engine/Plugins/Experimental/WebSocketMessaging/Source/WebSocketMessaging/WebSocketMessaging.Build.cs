4d8fe98a1d59482f652ee1b133cd9a676dfa9bb5
george rolfe <george.rolfe@xa.epicgames.com>
2024-01-26 01:09:07 -0500

    [WebSocketMessaging] Moved plugin
    [CL 30915128 by george rolfe in 5.4 branch]

---
+++ Engine/Plugins/Experimental/WebSocketMessaging/Source/WebSocketMessaging/WebSocketMessaging.Build.cs
@@ -0,0 +1,35 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class WebSocketMessaging : ModuleRules
+{
+	public WebSocketMessaging(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+			}
+			);
+
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Messaging",
+				"WebSockets",
+				"WebSocketNetworking",
+				"Json",
+				"JsonUtilities",
+				"Cbor",
+				"Serialization"
+			}
+			);
+	}
+}
