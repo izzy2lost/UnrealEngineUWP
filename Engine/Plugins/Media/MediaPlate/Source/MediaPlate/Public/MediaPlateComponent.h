d972d90d1abd5c001e2866ba3050e4e2e9ea3df7
dario mazzanti <dario.mazzanti@geotech.one>
2024-01-28 16:08:38 -0500

    [Media IO] Media Plate UX Improvements
    - Categories rearrangement
    - Play/Pause buttons are now mutually exclusive
    - Aspect Ratio and Letterbox Ratio widgets are now of type SSpinBox
    - Sphere Horizontal and Vertical Arc properties have not been switched to SSpinBox for performance reasons (scrubbing would just freeze the editor momentarily)
    
    Note related to UE 5.4 Feature Complete Schedule:
    This CL originally included a scrubber widget placed above the playback control buttons. That widget has been removed in order to submit this CL in time for UE 5.4 feature complete.
    The scrubber widget is in CL 30926672 which still needs to go under review.
    It should be fairly easy to add once that is reviewed.
    
    #jira UE-200385
    #rb eric.renaudhoude, Ruslan.Idrisov
    
    [CL 30960550 by dario mazzanti in ue5-main branch]

--- Engine/Plugins/Media/MediaPlate/Source/MediaPlate/Public/MediaPlateComponent.h
+++ Engine/Plugins/Media/MediaPlate/Source/MediaPlate/Public/MediaPlateComponent.h
@@ -3,8 +3,6 @@
 #pragma once
 
 #include "Components/ActorComponent.h"
-#include "CoreMinimal.h"
-#include "MediaPlayerOptions.h"
 #include "MediaPlayerProxyInterface.h"
 #include "MediaSource.h"
 #include "MediaTextureTracker.h"
@@ -48,11 +46,11 @@ struct FMediaTextureResourceSettings
 	GENERATED_USTRUCT_BODY()
 
 	/** Enable mips generation */
-	UPROPERTY(EditAnywhere, Category = "Media Texture Resource Settings")
+	UPROPERTY(EditAnywhere, Category = "MediaTexture", meta = (DisplayName = "Enable RealTime Mips"))
 	bool bEnableGenMips = false;
 
 	/** Current number of mips to be generated as output */
-	UPROPERTY(EditAnywhere, Category = "Media Texture Resource Settings")
+	UPROPERTY(EditAnywhere, Category = "MediaTexture", meta = (DisplayName = "Mips Quantity"))
 	uint8 CurrentNumMips = 1;
 };
 
@@ -187,14 +185,15 @@ public:
 	float StartTime = 0.0f;
 
 	/** Holds the component to play sound. */
-	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = MediaPlate)
+	UPROPERTY(EditAnywhere, Category = "Advanced", meta = (DisplayName = "Audio Component"))
 	TObjectPtr<UMediaSoundComponent> SoundComponent;
 
 	/** Holds the component for the mesh. */
-	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = MediaPlate)
+	UPROPERTY(EditAnywhere, Category = "Advanced")
 	TObjectPtr<UStaticMeshComponent> StaticMeshComponent;
+
 	/** Holds the component for the mesh. */
-	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = MediaPlate)
+	UPROPERTY(EditAnywhere, Category = "Advanced|Other")
 	TArray<TObjectPtr<UStaticMeshComponent>> Letterboxes;
 
 	/** What media playlist to play. */
@@ -206,7 +205,7 @@ public:
 	int32 PlaylistIndex = 0;
 
 	/** Override the default cache settings. */
-	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = "MediaPlate")
+	UPROPERTY(EditAnywhere, Category = "Cache", meta = (DisplayName = "Cache", ShowOnlyInnerProperties))
 	FMediaSourceCacheSettings CacheSettings;
 
 	/** Set the arc size in degrees used for visible mips and tiles calculations, specific to the sphere. */
@@ -347,11 +346,11 @@ private:
 	bool bLoop = true;
 
 	/** Visible mips and tiles calculation mode for the supported mesh types in MediaPlate. (Player restart on change.) */
-	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = "MediaPlate", meta = (AllowPrivateAccess = true))
+	UPROPERTY(EditAnywhere, Category = "EXR Tiles & Mips", meta = (DisplayName = "Visible Tiles & Mips Logic", AllowPrivateAccess = true))
 	EMediaTextureVisibleMipsTiles VisibleMipsTilesCalculations;
 
 	/** Media texture mip map bias shared between the (image sequence) loader and the media texture sampler. */
-	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = "MediaPlate", meta = (AllowPrivateAccess = true, UIMin = "-16.0", UIMax = "15.99"))
+	UPROPERTY(EditAnywhere, Category = "EXR Tiles & Mips", meta = (DisplayName = "Mips Bias", AllowPrivateAccess = true, UIMin = "-16.0", UIMax = "15.99"))
 	float MipMapBias = 0.0f;
 
 	/** If true then set the aspect ratio automatically based on the media. */
@@ -359,15 +358,15 @@ private:
 	bool bIsAspectRatioAuto = true;
 
 	/** If true then enable the use of MipLevelToUpscale as defined below. */
-	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = "MediaPlate", meta = (AllowPrivateAccess = true))
+	UPROPERTY(EditAnywhere, Category = "EXR Tiles & Mips", meta = (DisplayName = "Enable Mip Upscaling", AllowPrivateAccess = true))
 	bool bEnableMipMapUpscaling = false;
 
 	/* With exr playback, upscale into lower quality mips from this specified level. All levels including and above the specified value will be fully read. */
-	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = "MediaPlate", meta = (EditCondition = "bEnableMipMapUpscaling", AllowPrivateAccess = true, UIMin = "0", UIMax = "16"))
+	UPROPERTY(EditAnywhere, Category = "EXR Tiles & Mips", meta = (DisplayName = "Upscale Mip Level", EditCondition = "bEnableMipMapUpscaling", EditConditionHides, AllowPrivateAccess = true, UIMin = "0", UIMax = "16"))
 	int32 MipLevelToUpscale = 16;
 
 	/** If true then Media Plate will attempt to load and upscale lower quality mips and display those at the poles (Sphere object only). */
-	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = "MediaPlate", meta = (EditCondition = "VisibleMipsTilesCalculations == EMediaTextureVisibleMipsTiles::Sphere", AllowPrivateAccess = true))
+	UPROPERTY(EditAnywhere, Category = "EXR Tiles & Mips", meta = (DisplayName = "Adaptive Pole Mip Upscale", EditCondition = "VisibleMipsTilesCalculations == EMediaTextureVisibleMipsTiles::Sphere", EditConditionHides, AllowPrivateAccess = true))
 	bool bAdaptivePoleMipUpscaling = true;
 
 	/** If > 0, then this is the aspect ratio of our screen and 
@@ -397,7 +396,7 @@ private:
 	TArray<TObjectPtr<UMediaTexture>> MediaTextures;
 
 	/** Exposes Media Texture settings via Media Plate component. */
-	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = "MediaPlate")
+	UPROPERTY(EditAnywhere, Category = "MediaTexture", meta = (ShowOnlyInnerProperties))
 	FMediaTextureResourceSettings MediaTextureSettings;
 
 	/** This component's media player */
