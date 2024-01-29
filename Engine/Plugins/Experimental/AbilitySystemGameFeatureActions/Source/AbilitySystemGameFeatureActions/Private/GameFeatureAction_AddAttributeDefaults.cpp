05021bf1ef5aa3ab391aad35fbc61e9e2a582625
rex hill <Rex.Hill@epicgames.com>
2024-01-29 14:51:53 -0500

    Fix GameFeatureAction_AddAttributeDefaults not cleaning up references to objects when unregistered #rb brian.zaugg
    #tests unloaded a plugin with debugger attached in editor
    
    [CL 30975686 by rex hill in 5.4 branch]

--- Engine/Plugins/Experimental/AbilitySystemGameFeatureActions/Source/AbilitySystemGameFeatureActions/Private/GameFeatureAction_AddAttributeDefaults.cpp
+++ Engine/Plugins/Experimental/AbilitySystemGameFeatureActions/Source/AbilitySystemGameFeatureActions/Private/GameFeatureAction_AddAttributeDefaults.cpp
@@ -2,11 +2,17 @@
 
 #include "GameFeatureAction_AddAttributeDefaults.h"
 #include "AbilitySystemGlobals.h"
+#include "HAL/IConsoleManager.h"
 #include "Misc/PackageName.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(GameFeatureAction_AddAttributeDefaults)
 
 #define LOCTEXT_NAMESPACE "GameFeatures"
+
+namespace GameFeatureAction_AddAttributeDefaults
+{
+	static TAutoConsoleVariable<bool> CVarAllowRemoveAttributeDefaultTables(TEXT("GameFeatureAction_AddAttributeDefaults.AllowRemoveAttributeDefaultTables"), true, TEXT("Removes hard references when unregistering"));
+}
 
 //////////////////////////////////////////////////////////////////////
 // UGameFeatureAction_AddAttributeDefaults
@@ -28,8 +34,24 @@ void UGameFeatureAction_AddAttributeDefaults::OnGameFeatureRegistering()
 	}
 #endif // WITH_EDITOR
 
-	UAbilitySystemGlobals& AbilitySystemGlobals = UAbilitySystemGlobals::Get();
-	AbilitySystemGlobals.AddAttributeDefaultTables(*AttribDefaultTableNamesToAdd);
+	if (!AttribDefaultTableNamesToAdd->IsEmpty())
+	{
+		FNameBuilder OwnerNameBuilder;
+		GetPathName(nullptr, OwnerNameBuilder);
+		AttributeDefaultTablesOwnerName = FName(OwnerNameBuilder.ToView());
+
+		UAbilitySystemGlobals& AbilitySystemGlobals = UAbilitySystemGlobals::Get();
+		AbilitySystemGlobals.AddAttributeDefaultTables(AttributeDefaultTablesOwnerName, *AttribDefaultTableNamesToAdd);
+	}
+}
+
+void UGameFeatureAction_AddAttributeDefaults::OnGameFeatureUnregistering()
+{
+	if (!AttribDefaultTableNames.IsEmpty() && GameFeatureAction_AddAttributeDefaults::CVarAllowRemoveAttributeDefaultTables.GetValueOnAnyThread())
+	{
+		UAbilitySystemGlobals& AbilitySystemGlobals = UAbilitySystemGlobals::Get();
+		AbilitySystemGlobals.RemoveAttributeDefaultTables(AttributeDefaultTablesOwnerName, AttribDefaultTableNames);
+	}
 }
 
 //////////////////////////////////////////////////////////////////////
