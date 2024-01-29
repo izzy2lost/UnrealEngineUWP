26bc1ddce55c11aef1a57b80443aa63da3c38c5b
luke bermingham <Luke.Bermingham@xa.epicgames.com>
2024-01-28 19:09:08 -0500

    Pixel Streaming MediaIO capture improvements and bug fixes
    - Fix error where Pixel Streaming media capture seems to be running too early before a frame is rendered.
    - Add option to specify custom media capture in PixelStreamingVideoInputMediaCapture
    - Fix default case of capturing active scene viewport to be more explicitly named.
    - Specify the overrun action to be "skip" for lowest latency, unspecified default was flush, which will block the rendering thread on overrun to ensure the capture is done.
    
    #jira UE-200077
    #rb Matthew.Cotton
    
    [CL 30961437 by luke bermingham in ue5-main branch]

--- Engine/Plugins/Media/PixelStreaming/Source/PixelStreamingBlueprint/Private/PixelStreamingStreamerVideoInputMediaCapture.cpp
+++ Engine/Plugins/Media/PixelStreaming/Source/PixelStreamingBlueprint/Private/PixelStreamingStreamerVideoInputMediaCapture.cpp
@@ -16,6 +16,6 @@ UPixelStreamingStreamerVideoInputMediaCapture::UPixelStreamingStreamerVideoInput
 	}
 	else
 	{
-		VideoInput = FPixelStreamingVideoInputMediaCapture::Create();
+		VideoInput = FPixelStreamingVideoInputMediaCapture::CreateActiveViewportCapture();
 	}
 }
