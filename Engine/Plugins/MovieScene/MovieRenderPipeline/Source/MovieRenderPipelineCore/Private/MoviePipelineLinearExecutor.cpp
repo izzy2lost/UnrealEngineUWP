bf5742aed9bfbe7f014756886ee4e8f60b1d6977
matt hoffman <Matt.Hoffman@epicgames.com>
2024-01-30 17:25:31 -0500

    MovieGraph: Fixed completion percentages being [0-100] when passed to the UMoviePpipelineExecutorJob when they should have been [0-1] range. This caused the Status column in the Queue UI to always show full.
    #jira UE-205380
    #rb chad.williams, Jared.Therriault
    
    [CL 31023622 by matt hoffman in 5.4 branch]

--- Engine/Plugins/MovieScene/MovieRenderPipeline/Source/MovieRenderPipelineCore/Private/MoviePipelineLinearExecutor.cpp
+++ Engine/Plugins/MovieScene/MovieRenderPipeline/Source/MovieRenderPipelineCore/Private/MoviePipelineLinearExecutor.cpp
@@ -112,7 +112,7 @@ FText UMoviePipelineLinearExecutorBase::GetWindowTitle()
 	PercentFormatOptions.MaximumFractionalDigits = 0;
 	PercentFormatOptions.RoundingMode = ERoundingMode::HalfFromZero;
 
-	const float CompletionPercentage = GetCompletionPercentageFromActivePipeline();
+	const float CompletionPercentage = GetCompletionPercentageFromActivePipeline() * 100.f;
 
 	const FText TitleFormatString = LOCTEXT("MoviePreviewWindowTitleFormat", "Movie Pipeline Render (Preview) [Job {CurrentCount}/{TotalCount} Total] Current Job: {PercentComplete}% Completed.");
 	return FText::FormatNamed(TitleFormatString, TEXT("CurrentCount"), FText::AsNumber(CurrentPipelineIndex + 1), TEXT("TotalCount"), FText::AsNumber(Queue->GetJobs().Num()), TEXT("PercentComplete"), FText::AsNumber(CompletionPercentage, &PercentFormatOptions));
@@ -123,11 +123,11 @@ float UMoviePipelineLinearExecutorBase::GetCompletionPercentageFromActivePipelin
 	float CompletionPercentage = 0.f;
 	if (const UMoviePipeline* AsLegacyPipeline = Cast<UMoviePipeline>(ActiveMoviePipeline))
 	{
-		CompletionPercentage = UMoviePipelineBlueprintLibrary::GetCompletionPercentage(AsLegacyPipeline) * 100.f;
+		CompletionPercentage = UMoviePipelineBlueprintLibrary::GetCompletionPercentage(AsLegacyPipeline);
 	}
 	else if (const UMovieGraphPipeline* AsGraphPipeline = Cast<UMovieGraphPipeline>(ActiveMoviePipeline))
 	{
-		CompletionPercentage = UMovieGraphBlueprintLibrary::GetCompletionPercentage(AsGraphPipeline) * 100.f;
+		CompletionPercentage = UMovieGraphBlueprintLibrary::GetCompletionPercentage(AsGraphPipeline);
 	}
 
 	return CompletionPercentage;
