acbff8c46828bf329553e8704a02f31f39f2d04d
andrew rodham <Andrew.Rodham@epicgames.com>
2024-01-31 15:54:17 -0500

    Niagara: Fixed layout fallout from recent Sequencer column improvements
    [CL 31062564 by andrew rodham in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/NiagaraEditor.Build.cs
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/NiagaraEditor.Build.cs
@@ -24,6 +24,7 @@ public class NiagaraEditor : ModuleRules
                 "EditorStyle",
 				"VectorVM",
                 "MovieScene",
+				"SequencerCore",
 				"Sequencer",
 				"TimeManagement",
 				"PropertyEditor",
