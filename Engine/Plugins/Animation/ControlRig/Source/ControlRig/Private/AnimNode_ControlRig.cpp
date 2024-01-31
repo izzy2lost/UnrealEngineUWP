39b71352b1146f8aab84b67d859fea0883c77763
bob tellez <Bob.Tellez@epicgames.com>
2024-01-31 03:44:01 -0500

    [Backout] - CL30999475 [FYI] sara.schvartzman Original CL Desc ----------------------------------------------------------------- Control Rig: Fx FKControlRig AnimNode not showing controls as inputs #jira UE-159124 #rb Helge.Mathee #rnx
    [CL 31039992 by bob tellez in ue5-main branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/AnimNode_ControlRig.cpp
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/AnimNode_ControlRig.cpp
@@ -8,7 +8,6 @@
 #include "GameFramework/Actor.h"
 #include "Animation/NodeMappingContainer.h"
 #include "AnimationRuntime.h"
-#include "ControlRigObjectBinding.h"
 #include "Animation/AnimCurveUtils.h"
 #include "Animation/AnimStats.h"
 #include "Units/Execution/RigUnit_PrepareForExecution.h"
@@ -376,8 +375,6 @@ bool FAnimNode_ControlRig::UpdateControlRigIfNeeded(const UAnimInstance* InAnimI
 						
 						// create a new control rig using the new class
 						NewControlRig = NewObject<UControlRig>(InAnimInstance->GetOwningComponent(), ExpectedClass);
-						ControlRig->SetObjectBinding(MakeShared<FControlRigObjectBinding>());
-						ControlRig->GetObjectBinding()->BindToObject(InAnimInstance->GetOwningComponent());
 						NewControlRig->Initialize(true);
 						NewControlRig->RequestInit();
 
@@ -465,8 +462,6 @@ bool FAnimNode_ControlRig::UpdateControlRigIfNeeded(const UAnimInstance* InAnimI
 		if(ControlRig == nullptr)
 		{
 			ControlRig = NewObject<UControlRig>(InAnimInstance->GetOwningComponent(), ExpectedClass);
-			ControlRig->SetObjectBinding(MakeShared<FControlRigObjectBinding>());
-			ControlRig->GetObjectBinding()->BindToObject(InAnimInstance->GetOwningComponent());
 			ControlRig->Initialize(true);
 			ControlRig->RequestInit();
 		}
