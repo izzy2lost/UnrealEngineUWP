d2ab89473e2611964db6cea728265cebe7fde55f
daniel holden <daniel.holden@epicgames.com>
2024-02-01 12:25:19 -0500

    LearningAgents: Allowed users to specify per-agent callbacks in blueprints. #rb Brendan.Mulcahy
    #virtualized
    
    [CL 31091605 by daniel holden in ue5-main branch]

--- Engine/Plugins/Experimental/LearningAgents/Source/LearningAgents/Private/LearningAgentsPolicy.cpp
+++ Engine/Plugins/Experimental/LearningAgents/Source/LearningAgents/Private/LearningAgentsPolicy.cpp
@@ -183,7 +183,7 @@ void ULearningAgentsPolicy::SetupPolicy(
 			EncoderOutputSize,
 			Interactor->GetObservationSchema(),
 			Interactor->GetObservationSchemaElement(),
-			Seed);
+			UE::Learning::Random::Int(Seed ^ 0x658868dd));
 
 		UE_LEARNING_CHECK(EncoderInputSize == ObservationVectorSize);
 		UE_LEARNING_CHECK(EncoderOutputSize == ObservationEncodedVectorSize);
@@ -232,7 +232,7 @@ void ULearningAgentsPolicy::SetupPolicy(
 			PolicyNetwork->NeuralNetworkData = NewObject<ULearningNeuralNetworkData>(PolicyNetwork);
 		}
 
-		UE::NNE::RuntimeBasic::FModelBuilder Builder;
+		UE::NNE::RuntimeBasic::FModelBuilder Builder(UE::Learning::Random::Int(Seed ^ 0x69315bf9));
 
 		const int32 PolicyHiddenLayerSize = PolicySettings.HiddenLayerSize;
 		const int32 PolicyLayerNum = PolicySettings.LayerNum;
@@ -325,7 +325,7 @@ void ULearningAgentsPolicy::SetupPolicy(
 			DecoderOutputSize,
 			Interactor->GetActionSchema(),
 			Interactor->GetActionSchemaElement(),
-			Seed);
+			UE::Learning::Random::Int(Seed ^ 0xfa88bb7f));
 
 		UE_LEARNING_CHECK(DecoderInputSize == ActionEncodedVectorSize);
 		UE_LEARNING_CHECK(DecoderOutputSize == ActionDistributionVectorSize);
@@ -656,7 +656,7 @@ void ULearningAgentsPolicy::RunInference(const float ActionNoiseScale)
 	EncodeObservations();
 	EvaluatePolicy();
 	DecodeAndSampleActions(ActionNoiseScale);
-	Interactor->ScatterActions();
+	Interactor->PerformActions();
 }
 
 void ULearningAgentsPolicy::GetMemoryState(TArray<float>& OutMemoryState, const int32 AgentId) const
