8962797a3b16fd4c5774c9f60f6278a3518a5824
rob krajcarski <Rob.Krajcarski@epicgames.com>
2024-02-01 09:21:17 -0500

    Provides a fallback path to the parameter map traversal to ensure that we get accurate aliased variables in the history.  Without the aliases in place we can miss resetting the chunk cache in the translator which leads to variables leaking between emitters and between the emitter and the system scripts.  This was typically seen using a dynamic input in both system and update scripts (Multiply Float). #jira UE-188892 #rb stu.mckenna #lockdown marc.audy
    [CL 31085965 by rob krajcarski in 5.4 branch]

--- Engine/Source/Runtime/Core/Private/UObject/FortniteReleaseBranchCustomObjectVersion.cpp
+++ Engine/Source/Runtime/Core/Private/UObject/FortniteReleaseBranchCustomObjectVersion.cpp
@@ -6,7 +6,7 @@ TMap<FGuid, FGuid> FFortniteReleaseBranchCustomObjectVersion::GetSystemGuids()
 	TMap<FGuid, FGuid> SystemGuids;
 	const FDevSystemGuids& DevGuids = FDevSystemGuids::Get();
 
-	SystemGuids.Add(DevGuids.Niagara_LatestScriptCompileVersion, FGuid("E65477F2755541CEB2C366179FB735CA"));
+	SystemGuids.Add(DevGuids.Niagara_LatestScriptCompileVersion, FGuid("7B09FC98626D42E1993952FD3BC3ECE1"));
 	SystemGuids.Add(DevGuids.SkeletalMeshDerivedDataVersion, FGuid("5705956EC7134274A5A1BD3FC74DB3A9"));
 	return SystemGuids;
 }
