a902034fe18dfca8d180c4ec7b50f1ce43f68eb7
wouter burgers <wouter.burgers@epicgames.com>
2024-02-01 04:25:05 -0500

    Skein / ViewportStatus: Added Viewport Tooltips.
    #rb Marco.Anastasi
    #rnx
    
    [CL 31081122 by wouter burgers in ue5-main branch]

--- Engine/Source/Developer/SourceControlViewport/Private/SourceControlViewportModule.cpp
+++ Engine/Source/Developer/SourceControlViewport/Private/SourceControlViewportModule.cpp
@@ -2,15 +2,19 @@
 
 #include "SourceControlViewportModule.h"
 #include "SourceControlViewportOutlineMenu.h"
+#include "SourceControlViewportToolTips.h"
 
 void FSourceControlViewportModule::StartupModule()
 {
 	ViewportOutlineMenu = MakeShared<FSourceControlViewportOutlineMenu>();
 	ViewportOutlineMenu->Init();
+	ViewportToolTips = MakeShared<FSourceControlViewportToolTips>();
+	ViewportToolTips->Init();
 }
 
 void FSourceControlViewportModule::ShutdownModule()
 {
+	ViewportToolTips.Reset();
 	ViewportOutlineMenu.Reset();
 }
 
