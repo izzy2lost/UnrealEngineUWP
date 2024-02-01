c59f946d027f21aa8fc3a0fdde071039d57f7bc0
klukule <klukule@moowdesign.eu>
2024-01-31 21:17:33 -0500

    Horde: Use perforce login ticket instead of password if provided (#11399)
    [CL 31078404 by klukule in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Server/Perforce/PerforceService.cs
+++ Engine/Source/Programs/Horde/Horde.Server/Perforce/PerforceService.cs
@@ -288,7 +288,7 @@ namespace Horde.Server.Perforce
 
 			PerforceSettings settings = new PerforceSettings(serverAndPort, credentials.UserName);
 			settings.AppName = "Horde.Server";
-			settings.Password = credentials.Password;
+			settings.Password = String.IsNullOrEmpty(credentials.Ticket) ? credentials.Password : credentials.Ticket; 
 			settings.HostName = clientRecord?.Host;
 			settings.ClientName = clientRecord?.Name ?? "__DOES_NOT_EXIST__";
 			settings.PreferNativeClient = true;
