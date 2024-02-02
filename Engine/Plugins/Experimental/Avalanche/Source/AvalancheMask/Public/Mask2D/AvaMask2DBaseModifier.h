9343c4c86e1ae51c3c004ad4c1c7f23b8c75bdde
george rolfe <george.rolfe@xa.epicgames.com>
2024-02-01 20:25:07 -0500

    Mask modifier disable/enable support
    #jira UE-203107
    #rb Loris.Rossi
    
    [CL 31114859 by george rolfe in ue5-main branch]

--- Engine/Plugins/Experimental/Avalanche/Source/AvalancheMask/Public/Mask2D/AvaMask2DBaseModifier.h
+++ Engine/Plugins/Experimental/Avalanche/Source/AvalancheMask/Public/Mask2D/AvaMask2DBaseModifier.h
@@ -105,7 +105,6 @@ protected:
 	//~ Begin UActorModifierCoreBase
 	virtual void OnModifierCDOSetup(FActorModifierCoreMetadata& InMetadata) override;
 	virtual void OnModifierAdded(EActorModifierCoreEnableReason InReason) override;
-	virtual void OnModifierRemoved(EActorModifierCoreDisableReason InReason) override;
 	virtual void OnModifiedActorTransformed() override;
 	virtual void SavePreState() override;
 	virtual void RestorePreState() override;
