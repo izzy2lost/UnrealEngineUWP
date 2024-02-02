604c084cc5276076705c7cf7a12fbf584f55ec32
ben zeigler <Ben.Zeigler@epicgames.com>
2024-02-02 13:16:15 -0500

    Partial backout of CL 31114688 to remove temporary logging as the problem was actually with a different subsystem #rb mickael.gilabert
    [CL 31132124 by ben zeigler in 5.4 branch]

--- Engine/Plugins/Experimental/CommonConversation/Source/CommonConversationRuntime/Private/ConversationRegistry.cpp
+++ Engine/Plugins/Experimental/CommonConversation/Source/CommonConversationRuntime/Private/ConversationRegistry.cpp
@@ -160,20 +160,9 @@ UConversationRegistry* UConversationRegistry::GetFromWorld(const UWorld* World)
 
 void UConversationRegistry::Initialize(FSubsystemCollectionBase& Collection)
 {
-	UE_LOG(LogCommonConversationRuntime, Log, TEXT("Initializing UConversationRegistry %s"), *GetPathNameSafe(this));
-
 	Super::Initialize(Collection);
 
-	if (UGameFeaturesSubsystem* GameFeaturesSubsystem = GEngine ? GEngine->GetEngineSubsystem<UGameFeaturesSubsystem>() : nullptr)
-	{
-		GameFeaturesSubsystem->AddObserver(this);
-	}
-	else
-	{
-		// Logging to track down a crash
-		UClass* ParentClass = GetClass() ? GetClass()->GetSuperClass() : nullptr;
-		UE_LOG(LogCommonConversationRuntime, Error, TEXT("GameFeaturesSubsystem missing during UConversationRegistry::Initialize! Collection class is %s and parent is %s"), *GetPathNameSafe(Collection.GetBaseType()), *GetPathNameSafe(ParentClass));
-	}
+	UGameFeaturesSubsystem::Get().AddObserver(this);
 }
 
 void UConversationRegistry::Deinitialize()
