e120e36426cfb1b5dcdc1659640882026d3a478e
ahmed siddique <ahmed.siddique@epicgames.com>
2024-02-01 22:34:12 -0500

    Adding the HMI template for project creation. #android #rb ahmed.siddique
    #virtualized
    
    [CL 31117140 by ahmed siddique in ue5-main branch]

---
+++ Templates/TP_HMI_Blank/Source/TP_HMI_Blank/MyClass.h
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class TP_HMI_BLANK_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
---
+++ Templates/TP_HMI_Blank/Source/TP_HMI_Blank/TP_HMI_Blank.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TP_HMI_Blank.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TP_HMI_Blank, "TP_HMI_Blank" );
