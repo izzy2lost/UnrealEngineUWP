fe24a10153ae61e0cc71d7e4cb48571b25fb87f0
rob krajcarski <Rob.Krajcarski@epicgames.com>
2024-02-04 14:20:58 -0500

    Change serialization of EVVM runtime data to reduce fixed overhead of the script payload by about 200 bytes/script.  Also moves some costly functions into editor only and enables (or disable the disable) of the EVVM #rb Stu.McKenna #jira UE-154088 #lockdown marc.audy
    [CL 31163653 by rob krajcarski in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/UObject/FortniteReleaseBranchCustomObjectVersion.cpp
+++ Engine/Source/Runtime/Core/Private/UObject/FortniteReleaseBranchCustomObjectVersion.cpp
@@ -6,7 +6,7 @@ TMap<FGuid, FGuid> FFortniteReleaseBranchCustomObjectVersion::GetSystemGuids()
 	TMap<FGuid, FGuid> SystemGuids;
 	const FDevSystemGuids& DevGuids = FDevSystemGuids::Get();
 
-	SystemGuids.Add(DevGuids.Niagara_LatestScriptCompileVersion, FGuid("7B09FC98626D42E1993952FD3BC3ECE1"));
+	SystemGuids.Add(DevGuids.Niagara_LatestScriptCompileVersion, FGuid("495EDD7CBCED44B5B456F6CB2C76074C"));
 	SystemGuids.Add(DevGuids.SkeletalMeshDerivedDataVersion, FGuid("5705956EC7134274A5A1BD3FC74DB3A9"));
 	return SystemGuids;
 }
