c6ad349115bc90e14315904f715a22b0dd796c62
daniel moreno <daniel.moreno@epicgames.com>
2024-02-05 02:57:18 -0500

    [mutable] Bulk instance updater implementation - Test commandlet that updates all instances found in a given path. - Creted supporting classes/files to handle compilation and update of COs and COIs - Refactored other testing commandlets to make use of the shared methods and objects in the "ValidationUtils" file
    [REVIEW] [at]pere.rifa
    #rb pere.rifa
    
    [CL 31167040 by daniel moreno in 5.4 branch]

--- Engine/Plugins/Experimental/Mutable/Source/MutableValidation/Public/MuV/CustomizableObjectValidationCommandlet.h
+++ Engine/Plugins/Experimental/Mutable/Source/MutableValidation/Public/MuV/CustomizableObjectValidationCommandlet.h
@@ -8,7 +8,9 @@
 
 #include "CustomizableObjectValidationCommandlet.generated.h"
 
+// Forward declarations
 class UCustomizableObject;
+class UCOIUpdater;
 
 UCLASS()
 class UCustomizableObjectValidationCommandlet : public UCommandlet 
@@ -22,28 +24,11 @@ private:
 	/** Customizable Object to be tested */
 	UPROPERTY()
 	TObjectPtr<UCustomizableObject> ToTestCustomizableObject = nullptr;
-	
-	/** Customizable Object Instance currently being updated */
-	UPROPERTY()
-	TObjectPtr<UCustomizableObjectInstance> InstanceBeingUpdated = nullptr;
-
-	/** Customizable Skeletal Component currently being updated */
-	UPROPERTY()
-	TArray<TObjectPtr<USkeletalMeshComponent>> ComponentsBeingUpdated;
 
 	/** Array of COI to be generated with randomized parameter values */
 	UPROPERTY()
 	TArray<TObjectPtr<UCustomizableObjectInstance>> InstancesToProcess;
 	
-	/** Handle to be able to unbind OnInstanceUpdated(...) from instance end of update delegate. */
-	FDelegateHandle OnInstanceUpdateHandle;
-
-	/** Did any of the instances fail the UpdateSkeletalMesh process? */
-	bool bInstanceFailedUpdate = false;
-
-	/** Callback invoked once the currently updating instance has done the update.
-	 * @paragm Result is a container that provides us with data related with the instance updating process.
-	 */
-	UFUNCTION()
-	void OnInstanceUpdate(const FUpdateContext& Result);
+	UPROPERTY()
+	TObjectPtr<UCOIUpdater> InstanceUpdater;
 };
