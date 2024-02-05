28fdc4f611ffe7026819f189879da38809e76638
mieszko zielinski <mieszko.zielinski@epicgames.com>
2024-02-05 06:15:14 -0500

    Moved more types over from JunoProceduralWorld over to engine-level InstancedActors plugin.
    Types moved and renamed:
    * EJunoMassBulkLOD -> EInstancedActorsBulkLOD
    * EJunoInstancedActorFragmentFlags -> EInstancedActorsFragmentFlags
    * FJunoInstancedActorTagSet -> FInstancedActorsTagSet
    * FJunoInstancedActorVisualizationDesc -> FInstancedActorsVisualizationDesc
    * FJunoInstancedActorSoftVisualizationDesc -> FInstancedActorsSoftVisualizationDesc
    * FJunoInstancedActorVisualizationInfo -> FInstancedActorsVisualizationInfo
    * FJunoInstancedActorMeshSwitchFragment -> FInstancedActorsMeshSwitchFragment
    * FJunoInstancedActorManagerHandle -> FInstancedActorsManagerHandle
    * FJunoInstancedActorModifierVolumeHandle -> FInstancedActorsModifierVolumeHandle
    
    The change includes redirects in newly added BaseInstancedActors.ini
    
    #jira UE-177246
    
    [CL 31168940 by mieszko zielinski in ue5-main branch]

--- Engine/Plugins/Runtime/InstancedActors/Source/InstancedActors/InstancedActors.Build.cs
+++ Engine/Plugins/Runtime/InstancedActors/Source/InstancedActors/InstancedActors.Build.cs
@@ -23,7 +23,10 @@ namespace UnrealBuildTool.Rules
 				new string[] {
 					"Core",
 					"CoreUObject",
-					"Engine"
+					"Engine",
+					"GameplayTags",
+					"MassEntity",
+					"MassRepresentation",
 				}
 			);
 
