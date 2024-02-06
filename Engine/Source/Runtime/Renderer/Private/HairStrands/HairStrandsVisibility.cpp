0a2738246abff65d9f39047af27dd04ca9862b89
arciel rekman <arciel.rekman@epicgames.com>
2024-02-06 11:18:43 -0500

    Simplify Hair second/offset view fix.
    #rb Charles.deRousiers, Christopher.Fiala
    [REVIEW] [at]Charles.deRousiers, [at]Christopher.Fiala
    #jira UE-205629
    
    
    #changelist validated
    #virtualized
    
    [CL 31216392 by arciel rekman in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/HairStrands/HairStrandsVisibility.cpp
+++ Engine/Source/Runtime/Renderer/Private/HairStrands/HairStrandsVisibility.cpp
@@ -1242,11 +1242,10 @@ class FHairVelocityCS: public FGlobalShader
 		SHADER_PARAMETER_RDG_BUFFER_SRV(StructuredBuffer<FPackedHairVis>, NodeVis)
 		SHADER_PARAMETER_RDG_TEXTURE_UAV(RWTexture2D, OutVelocityTexture)
 		SHADER_PARAMETER_RDG_TEXTURE_UAV(RWTexture2D, OutResolveMaskTexture)
-		SHADER_PARAMETER_STRUCT_INCLUDE(FViewShaderParameters, View)
+		SHADER_PARAMETER_STRUCT_REF(FViewUniformShaderParameters, ViewUniformBuffer)
 
 		SHADER_PARAMETER(FIntPoint, TileCountXY)
 		SHADER_PARAMETER(uint32, TileSize)
-		SHADER_PARAMETER(FIntPoint, ViewRectMin)
 		SHADER_PARAMETER_RDG_BUFFER_SRV(Buffer<uint2>, TileDataBuffer)
 		SHADER_PARAMETER_RDG_BUFFER_SRV(Buffer<uint>, TileCountBuffer)
 		SHADER_PARAMETER_RDG_BUFFER_SRV(Buffer<uint>, HairTileCount)
@@ -1303,7 +1302,7 @@ static void AddHairVelocityPass(
 
 	FHairVelocityCS::FParameters* PassParameters = GraphBuilder.AllocParameters<FHairVelocityCS::FParameters>();
 	PassParameters->bNeedClear = bNeedClear ? 1u : 0u;
-	PassParameters->View = View.GetShaderParameters();
+	PassParameters->ViewUniformBuffer = View.ViewUniformBuffer;
 	PassParameters->VelocityThreshold = GetHairFastResolveVelocityThreshold(Resolution);
 	PassParameters->CoverageThreshold = GetHairWriteVelocityCoverageThreshold();
 	PassParameters->NodeIndex = NodeIndex;
@@ -1319,7 +1318,6 @@ static void AddHairVelocityPass(
 	PassParameters->Resolution			= Resolution;
 	PassParameters->TileCountXY			= TileData.TileCountXY;
 	PassParameters->TileSize			= TileData.TileSize;
-	PassParameters->ViewRectMin			= View.ViewRect.Min;
 	PassParameters->TileCountBuffer		= TileData.TileCountSRV;
 	PassParameters->TileDataBuffer		= TileData.GetTileBufferSRV(TileType);
 	PassParameters->TileIndirectArgs	= TileData.TileIndirectDispatchBuffer;
