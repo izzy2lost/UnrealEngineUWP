e7565ef140c7ebf991a3ed49ad03000bbce1c8a5
steve robb <Steve.Robb@epicgames.com>
2024-02-06 16:10:19 -0500

    Updated bAllowShrinking deprecation version number.
    [CL 31229679 by steve robb in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Containers/AllowShrinking.h
+++ Engine/Source/Runtime/Core/Public/Containers/AllowShrinking.h
@@ -11,4 +11,4 @@ enum class EAllowShrinking : uint8
 	Yes
 };
 
-#define UE_ALLOWSHRINKING_BOOL_DEPRECATED(FunctionName) UE_DEPRECATED_FORENGINE(5.4, FunctionName " with a boolean bAllowShrinking has been deprecated - please use the EAllowShrinking enum instead")
+#define UE_ALLOWSHRINKING_BOOL_DEPRECATED(FunctionName) UE_DEPRECATED_FORENGINE(5.5, FunctionName " with a boolean bAllowShrinking has been deprecated - please use the EAllowShrinking enum instead")
