5287df25a6442c71ae077b5715fb989d287c444a
ben marsh <Ben.Marsh@epicgames.com>
2024-02-06 17:02:12 -0500

    Horde: Sign tools bundled with installer.
    #jira UE-205621
    
    [CL 31232137 by ben marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Server/Server/DebugController.cs
+++ Engine/Source/Programs/Horde/Horde.Server/Server/DebugController.cs
@@ -9,7 +9,6 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
-using System.Security.Claims;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
