2adf36022ee24fe659786db6b761d87ccfba8209
charles derousiers <Charles.deRousiers@epicgames.com>
2024-02-07 01:33:42 -0500

    Add project settings for hair tristrip geometry as not all project are compatible with this.
    #rb charles.derousiers
    
    
    #changelist validated
    #virtualized
    
    [CL 31249381 by charles derousiers in 5.4 branch]

--- Engine/Source/Runtime/RenderCore/Private/Shader.cpp
+++ Engine/Source/Runtime/RenderCore/Private/Shader.cpp
@@ -2021,6 +2021,11 @@ void ShaderMapAppendKeyString(EShaderPlatform Platform, FString& KeyString)
 		}
 	}
 
+	if (GetHairStrandsUsesTriangleStrips())
+	{
+		KeyString += FString::Printf(TEXT("_STRDSTRIP"));
+	}
+
 	if (Substrate::IsSubstrateEnabled())
 	{
 		{
