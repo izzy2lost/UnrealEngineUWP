d9f205d7714ec847c13bfa0b2e29255f9c20b19f
ben marsh <Ben.Marsh@epicgames.com>
2024-02-07 11:50:16 -0500

    Horde: Fix static analysis warnings.
    [CL 31260178 by ben marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Server/Utilities/TimeOfDayJsonConverter.cs
+++ Engine/Source/Programs/Horde/Horde.Server/Utilities/TimeOfDayJsonConverter.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
-using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 
