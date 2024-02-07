eba788bc3d9d6c345a421804f230a0e1d33b1b66
logan buchy <logan.buchy@epicgames.com>
2024-02-07 14:26:30 -0500

    Export PropertyUpdatedWidgetBuilder public functions
    #rb brooke.hubert
    
    [CL 31266729 by logan buchy in ue5-main branch]

--- Engine/Source/Editor/PropertyEditor/Public/UserInterface/Widgets/PropertyUpdatedWidgetBuilder.h
+++ Engine/Source/Editor/PropertyEditor/Public/UserInterface/Widgets/PropertyUpdatedWidgetBuilder.h
@@ -15,31 +15,31 @@ public:
 	/**
 	 * default constructor
 	 */
-	FPropertyUpdatedWidgetBuilder();
+	PROPERTYEDITOR_API FPropertyUpdatedWidgetBuilder();
 
 	/**
 	 * Set the IsVisible delegate for the menu that this button is responsible for
 	 */
-	FPropertyUpdatedWidgetBuilder& Bind_IsVisible(TAttribute<EVisibility> InIsVisible);
+	PROPERTYEDITOR_API FPropertyUpdatedWidgetBuilder& Bind_IsVisible(TAttribute<EVisibility> InIsVisible);
 
 	/**
 	* Sets the ResetToDefault delegate
 	*
 	* @param InResetToDefault the FResetToDefault delegate to set for this widget builder
 	*/
-	FPropertyUpdatedWidgetBuilder& Set_ResetToDefault(FResetToDefault InResetToDefault);
+	PROPERTYEDITOR_API FPropertyUpdatedWidgetBuilder& Set_ResetToDefault(FResetToDefault InResetToDefault);
 
 	/**
 	 * returns the ResetToDefault delegate
 	 */
-	FResetToDefault Get_ResetToDefault();
+	PROPERTYEDITOR_API FResetToDefault Get_ResetToDefault();
 
 	/**
 	 *  binds the IsRowHovered Attribute
 	 *
 	 * @param bInIsRowHoveredAttr the attribute which will determine if the row for this property is hovered
 	 */
-	void Bind_IsRowHovered(TAttribute<bool> InIsRowHoveredAttr);
+	PROPERTYEDITOR_API void Bind_IsRowHovered(TAttribute<bool> InIsRowHoveredAttr);
 
 	/**
 	 * If true, the row that this is associated with is currently hovered over
