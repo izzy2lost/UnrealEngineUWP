27297a206b8cfec7896a6a4faeb0a15bef756d65
sebastien lussier <Sebastien.Lussier@epicgames.com>
2024-02-07 10:05:11 -0500

    Cleaning up deprecated methods #rb jeanfrancois.dube
    [CL 31257031 by sebastien lussier in 5.4 branch]

--- Engine/Source/Developer/HierarchicalLODUtilities/Private/HierarchicalLODUtilities.cpp
+++ Engine/Source/Developer/HierarchicalLODUtilities/Private/HierarchicalLODUtilities.cpp
@@ -58,11 +58,6 @@ DEFINE_LOG_CATEGORY_STATIC(LogHierarchicalLODUtilities, Verbose, All);
 
 #define LOCTEXT_NAMESPACE "HierarchicalLODUtils"
 
-void FHierarchicalLODUtilities::ExtractStaticMeshComponentsFromLODActor(AActor* Actor, TArray<UStaticMeshComponent*>& InOutComponents)
-{
-	UHLODProxy::ExtractStaticMeshComponentsFromLODActor(Cast<ALODActor>(Actor), InOutComponents);
-}
-
 void FHierarchicalLODUtilities::ExtractSubActorsFromLODActor(AActor* Actor, TArray<AActor*>& InOutActors)
 {
 	ALODActor* LODActor = CastChecked<ALODActor>(Actor);
@@ -224,39 +219,11 @@ UPackage* FHierarchicalLODUtilities::RetrieveLevelHLODPackage(const ULevel* InLe
 	return nullptr;
 }
 
-UPackage* FHierarchicalLODUtilities::CreateOrRetrieveLevelHLODPackage(const ULevel* InLevel)
-{
-	checkf(InLevel != nullptr, TEXT("Invalid Level supplied"));
-
-	UPackage* HLODPackage = nullptr;
-	UPackage* LevelOuterMost = InLevel->GetOutermost();
-
-	const FString PathName = FPackageName::GetLongPackagePath(LevelOuterMost->GetPathName());
-	const FString BaseName = FPackageName::GetShortName(LevelOuterMost->GetPathName());
-	const FString HLODLevelPackageName = FString::Printf(TEXT("%s/HLOD/%s_HLOD"), *PathName, *BaseName);
-
-	HLODPackage = CreatePackage( *HLODLevelPackageName);
-	HLODPackage->FullyLoad();
-	HLODPackage->Modify();
-	HLODPackage->SetPackageFlags(PKG_ContainsMapData);		// PKG_ContainsMapData required so FEditorFileUtils::GetDirtyContentPackages can treat this as a map package
-
-	// Target PackagePath; this is a hack to avoid save file dialog when we will be saving HLOD map package
-	HLODPackage->SetLoadedPath(FPackagePath::FromPackageNameChecked(HLODLevelPackageName));
-
-	return HLODPackage;
-}
-
 FString FHierarchicalLODUtilities::GetLevelHLODProxyName(const FString& InLevelPackageName, const uint32 InHLODLevelIndex)
 {
 	FString HLODProxyName;
 	FString HLODPackageName = GetHLODPackageName(InLevelPackageName, InHLODLevelIndex, HLODProxyName);
 	return HLODPackageName + TEXT(".") + HLODProxyName;
-}
-
-bool FHierarchicalLODUtilities::BuildStaticMeshForLODActor(ALODActor* LODActor, UPackage* AssetsOuter, const FHierarchicalSimplification& LODSetup, UMaterialInterface* InBaseMaterial)
-{
-	UHLODProxy* Proxy = FindObject<UHLODProxy>(AssetsOuter, *GetHLODProxyName(CastChecked<ULevel>(LODActor->GetOuter()), LODActor->LODLevel - 1));
-	return BuildStaticMeshForLODActor(LODActor, Proxy, LODSetup, InBaseMaterial);
 }
 
 static FString GetImposterMeshName(const UStaticMesh* InImposterMesh)
@@ -881,13 +848,6 @@ bool FHierarchicalLODUtilities::BuildStaticMeshForLODActor(ALODActor* LODActor, 
 	return true;
 }
 
-bool FHierarchicalLODUtilities::BuildStaticMeshForLODActor(ALODActor* LODActor, UPackage* AssetsOuter, const FHierarchicalSimplification& LODSetup)
-{
-PRAGMA_DISABLE_DEPRECATION_WARNINGS
-	return BuildStaticMeshForLODActor(LODActor, AssetsOuter, LODSetup, GEngine->DefaultHLODFlattenMaterial);
-PRAGMA_ENABLE_DEPRECATION_WARNINGS
-}
-
 EClusterGenerationError FHierarchicalLODUtilities::ShouldGenerateCluster(AActor* Actor, const int32 HLODLevelIndex)
 {
 	if (!Actor)
@@ -1014,11 +974,6 @@ void FHierarchicalLODUtilities::DestroyCluster(ALODActor* InActor)
 	{
 		DestroyCluster(ParentLOD);
 	}
-}
-
-void FHierarchicalLODUtilities::DestroyClusterData(ALODActor* InActor)
-{
-
 }
 
 ALODActor* FHierarchicalLODUtilities::CreateNewClusterActor(UWorld* InWorld, const int32 InLODLevel, AWorldSettings* WorldSettings)
