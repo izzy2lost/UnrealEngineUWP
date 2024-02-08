250050d4ba143f0096e847711188db7c528a9375
roey borsteinas <roey.borsteinas@epicgames.com>
2024-02-08 13:38:43 -0500

    Water: added some additional checks in the WaterViewExtension to track down an issue where water zones may not have registered water zone info.
    #rb jonathan.bard
    
    [CL 31296533 by roey borsteinas in ue5-main branch]

--- Engine/Plugins/Experimental/Water/Source/Runtime/Private/WaterViewExtension.cpp
+++ Engine/Plugins/Experimental/Water/Source/Runtime/Private/WaterViewExtension.cpp
@@ -391,14 +391,17 @@ void FWaterViewExtension::SetupView(FSceneViewFamily& InViewFamily, FSceneView& 
 		// Check if the view location is no longer within the current update bounds of a water zone and if so, queue an update for it.
 		for (AWaterZone* WaterZone : TActorRange<AWaterZone>(WorldPtr.Get()))
 		{
+			checkf(IsValid(WaterZone), TEXT("TActorRange<AWaterZone> gave us an invalid Water Zone actor!"));
 			FWaterZoneInfo* WaterZoneInfo = WaterZoneInfos.Find(WaterZone);
-			check(WaterZoneInfo != nullptr);
-			if (WaterZone->IsLocalOnlyTessellationEnabled())
+			if (ensureMsgf(WaterZoneInfo != nullptr, TEXT("Water Zone (%s) has no corresponding water zone info!"), *GetNameSafe(WaterZone)))
 			{
-				if (!WaterZoneInfo->UpdateBounds.IsSet() || !WaterZoneInfo->UpdateBounds->IsInside(FVector2D(ViewLocation)))
-				{
-					WaterZone->MarkForRebuild(EWaterZoneRebuildFlags::UpdateWaterInfoTexture);
-				}
+				 if (WaterZone->IsLocalOnlyTessellationEnabled())
+				 {
+					 if (!WaterZoneInfo->UpdateBounds.IsSet() || !WaterZoneInfo->UpdateBounds->IsInside(FVector2D(ViewLocation)))
+					 {
+						 WaterZone->MarkForRebuild(EWaterZoneRebuildFlags::UpdateWaterInfoTexture);
+					 }
+				 }
 			}
 		}
 	}
