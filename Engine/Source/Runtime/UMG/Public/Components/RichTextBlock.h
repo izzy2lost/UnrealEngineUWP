ce3bbfefa7aa31c66498e3c16eb351dbb2af694c
DoubleDeez <DoubleDeez@users.noreply.github.com>
2024-02-08 14:13:21 -0500

    PR #11434: UMG: In Blueprint, allow setting InText inline for URichTextBlock::SetText, this aligns with UTextBlock::SetText #jira UE-206178 #rb vincent.gauthier
    [CL 31297915 by DoubleDeez in ue5-main branch]

--- Engine/Source/Runtime/UMG/Public/Components/RichTextBlock.h
+++ Engine/Source/Runtime/UMG/Public/Components/RichTextBlock.h
@@ -204,7 +204,7 @@ public:
 	 * Warning: This will wipe any binding created for the Text property!
 	 * @param InText The text to assign to the widget
 	 */
-	UFUNCTION(BlueprintCallable, Category = "Widget")
+	UFUNCTION(BlueprintCallable, Category = "Widget", meta = (AutoCreateRefTerm = "InText"))
 	UMG_API virtual void SetText(const FText& InText);
 
 	UFUNCTION(BlueprintCallable, Category = "Widget")
