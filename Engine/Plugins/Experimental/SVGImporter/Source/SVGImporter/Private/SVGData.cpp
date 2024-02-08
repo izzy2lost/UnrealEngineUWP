935cf11fd949eb20abb869d3131cfb82fc6d5d15
dario mazzanti <dario.mazzanti@geotech.one>
2024-02-08 16:17:13 -0500

    [SVG Importer] Some imported SVGs are not smooth and show polyline segments
    - Add quality option for SVG Data import
    
    #jira UE-205972
    #rb Juan.Portillo, Loris.Rossi
    
    [CL 31304290 by dario mazzanti in 5.4 branch]

--- Engine/Plugins/Experimental/SVGImporter/Source/SVGImporter/Private/SVGData.cpp
+++ Engine/Plugins/Experimental/SVGImporter/Source/SVGImporter/Private/SVGData.cpp
@@ -1,35 +1,79 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+#if WITH_EDITOR
 #include "SVGData.h"
+#include "EditorFramework/AssetImportData.h"
+#include "EditorReimportHandler.h"
+#include "SVGImporterSettings.h"
 #include "SVGImporterUtils.h"
 #include "SVGTypes.h"
 #include "UObject/Package.h"
-
-#if WITH_EDITOR
-#include "EditorFramework/AssetImportData.h"
 #endif
 
+#if WITH_EDITOR
 void USVGData::Initialize(const FSVGDataInitializer& InInitializer)
 {
 	SVGFileContent = InInitializer.SVGTextBuffer;
 	CreateShapes(InInitializer.Elements);
-
-#if WITH_EDITOR
 	SourceFilePath = UAssetImportData::SanitizeImportFilename(InInitializer.SourceFilename, GetOutermost());
 	GenerateSVGTexture();
-#endif
 }
 
-#if WITH_EDITOR
+void USVGData::Reset()
+{
+	SVGFileContent = TEXT("");
+	Shapes.Empty();
+}
+
 void USVGData::GenerateSVGTexture()
 {
 	SVGTexture = FSVGImporterUtils::CreateSVGTexture(SVGFileContent, this);
 }
-#endif
+
+void USVGData::Reimport()
+{
+	constexpr bool bAskForNewFileIfMissing = true;
+	FReimportManager::Instance()->Reimport(this, bAskForNewFileIfMissing);
+
+	OnSVGDataReimport().Broadcast();
+}
+
+void USVGData::PostEditChangeProperty(FPropertyChangedEvent& InPropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(InPropertyChangedEvent);
+
+	const FName MemberName = InPropertyChangedEvent.GetMemberPropertyName();
+
+	if (MemberName == GET_MEMBER_NAME_CHECKED(USVGData, OverrideQuality))
+	{
+		bool bShouldReimport = false;
+		if (InPropertyChangedEvent.ChangeType == EPropertyChangeType::ValueSet)
+		{
+			bShouldReimport = true;
+		}
+		else if (InPropertyChangedEvent.ChangeType == EPropertyChangeType::ToggleEditable)
+		{
+			ESVGSplineConversionQuality ProjectQuality = ESVGSplineConversionQuality::None;
+			if (const USVGImporterSettings* SVGImporterSettings = GetDefault<USVGImporterSettings>())
+			{
+				ProjectQuality = SVGImporterSettings->GetSplineConversionQuality();
+			}
+
+			if (ProjectQuality != OverrideQuality)
+			{
+				bShouldReimport = true;
+			}
+		}
+
+		if (bShouldReimport)
+		{
+			Reimport();
+		}
+	}
+}
 
 void USVGData::CreateShapes(const TArray<TSharedRef<FSVGBaseElement>>& InSVGElements)
 {
-	constexpr float MaxDistanceFromSvgSpline = 1.e-3f;
 	for (TSharedPtr<FSVGBaseElement> Element : InSVGElements)
 	{
 		if (Element->Type != ESVGElementType::Group && Element->Type != ESVGElementType::Other)
@@ -64,10 +108,10 @@ void USVGData::CreateShapes(const TArray<TSharedRef<FSVGBaseElement>>& InSVGElem
 							FString CurrName = Element->Name + FString::FromInt(Count++);
 
 							USplineComponent* SplineComponent = NewObject<USplineComponent>(GetTransientPackage(), *CurrName);
-#if WITH_EDITOR
+
 							// We need to be able to set different arrive and leave tangents per point
 							SplineComponent->bAllowDiscontinuousSpline = true;
-#endif
+
 							int Key = 0;
 							SplineComponent->ClearSplinePoints(true);
 
@@ -99,7 +143,7 @@ void USVGData::CreateShapes(const TArray<TSharedRef<FSVGBaseElement>>& InSVGElem
 							SplineComponent->UpdateSpline();
 
 							TArray<FVector> Points;
-							FSVGImporterUtils::ConvertSVGSplineToPolyLine(SplineComponent, ESplineCoordinateSpace::Local, MaxDistanceFromSvgSpline, Points);
+							FSVGImporterUtils::ConvertSVGSplineToPolyLine(SplineComponent, ESplineCoordinateSpace::Local, GetConversionQualityFactor(), Points);
 
 							SplineComponent->DestroyComponent();
 
@@ -114,3 +158,42 @@ void USVGData::CreateShapes(const TArray<TSharedRef<FSVGBaseElement>>& InSVGElem
 		}
 	}
 }
+
+float USVGData::GetConversionQualityFactor() const
+{
+	ESVGSplineConversionQuality Quality = ESVGSplineConversionQuality::None;
+
+	if (bEnableOverrideQuality)
+	{
+		Quality = OverrideQuality;
+	}
+	else if (const USVGImporterSettings* SVGImporterSettings = GetDefault<USVGImporterSettings>())
+	{
+		Quality = SVGImporterSettings->GetSplineConversionQuality();
+	}
+
+	switch (Quality)
+	{
+		case ESVGSplineConversionQuality::VeryLow:
+			return 1.e-1f;
+
+		case ESVGSplineConversionQuality::Low:
+			return 1.e-2f;
+
+		case ESVGSplineConversionQuality::Normal:
+			return 1.e-3f;
+
+		case ESVGSplineConversionQuality::Increased:
+			return 1.e-4f;
+
+		case ESVGSplineConversionQuality::High:
+			return 1.e-5f;
+
+		case ESVGSplineConversionQuality::VeryHigh:
+			return 1.e-6f;
+
+		default:
+			return 1.e-3f;
+	}
+}
+#endif
