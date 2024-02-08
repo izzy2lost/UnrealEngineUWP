3a969deda8e39db1895421ac60c9555d89374949
christopher waters <christopher.waters@epicgames.com>
2024-02-08 17:02:01 -0500

    Adding missing RayTracing ShaderPlatform checks.
    #rb aleksander.netzel
    
    [CL 31306775 by christopher waters in 5.4 branch]

--- Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.cpp
+++ Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.cpp
@@ -3407,7 +3407,7 @@ void FDeferredShadingSceneRenderer::Render(FRDGBuilder& GraphBuilder)
 
 bool AnyRayTracingPassEnabled(const FScene* Scene, const FViewInfo& View)
 {
-	if (!IsRayTracingEnabled() || Scene == nullptr)
+	if (!IsRayTracingEnabled(View.GetShaderPlatform()) || Scene == nullptr)
 	{
 		return false;
 	}
@@ -3425,7 +3425,7 @@ bool AnyRayTracingPassEnabled(const FScene* Scene, const FViewInfo& View)
 
 bool ShouldRenderRayTracingEffect(bool bEffectEnabled, ERayTracingPipelineCompatibilityFlags CompatibilityFlags, const FSceneView* View)
 {
-	if (!IsRayTracingEnabled() || (View && !View->bAllowRayTracing))
+	if ((View && !IsRayTracingEnabled(View->GetShaderPlatform())) || (!View && !IsRayTracingEnabled()) || (View && !View->bAllowRayTracing))
 	{
 		return false;
 	}
