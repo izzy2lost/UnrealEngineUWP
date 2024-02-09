73deae7f19308ab1ad8048c6b80c11db4b3ac44c
tiago costa <tiago.costa@epicgames.com>
2024-02-09 07:27:55 -0500

    Fix bug when FStaticMeshRenderData::ReleaseResources() is called when FStaticMeshRenderData is not initialized. - caused crash when changing materials inside the Static Mesh Editor. - Fix:   - clear RayTracingGeometryGroupHandle after release.   - check whether FStaticMeshRenderData was initialized.
    [CL 31334986 by tiago costa in 5.4 branch]

--- Engine/Source/Runtime/Engine/Private/StaticMesh.cpp
+++ Engine/Source/Runtime/Engine/Private/StaticMesh.cpp
@@ -1954,6 +1954,8 @@ void FStaticMeshRenderData::InitResources(ERHIFeatureLevel::Type InFeatureLevel,
 
 void FStaticMeshRenderData::ReleaseResources()
 {
+	const bool bWasInitialized = bIsInitialized;
+
 	bIsInitialized = false;
 
 	for (int32 LODIndex = 0; LODIndex < LODResources.Num(); ++LODIndex)
@@ -1966,12 +1968,13 @@ void FStaticMeshRenderData::ReleaseResources()
 	}
 
 #if RHI_RAYTRACING
-	if (IsRayTracingAllowed())
+	if (bWasInitialized && IsRayTracingAllowed())
 	{
 		ENQUEUE_RENDER_COMMAND(CmdReleaseRayTracingGeometryGroup)(
 			[this](FRHICommandListImmediate&)
 			{
 				GRayTracingGeometryManager->ReleaseRayTracingGeometryGroup(RayTracingGeometryGroupHandle);
+				RayTracingGeometryGroupHandle = INDEX_NONE;
 			});
 	}
 #endif
