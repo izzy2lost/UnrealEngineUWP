4cf4b69897e5179bfd109917905e9f30ad7830bb
ben hoffman <ben.hoffman@epicgames.com>
2024-02-09 15:55:17 -0500

    [Backout] Part of - CL31224705 ----------------------------------------- Ensure that we set bShouldBeIgnored AFTER we retain the state of old triggers and modifiers. Otherwise we would never actually update the state of the mapping's bShouldBeIgnored flag and it would always be false. I believe that this issue was introduced with 24566913. Once we got the right behavior for that comparison operator, then the mapping would always be converted instead of the old behavior where it would not. This resulted in it overriding the bShouldBeIgnored flag to whatever the previous state was.
    #jira UE-196939
    #jira UE-206413
    #rnx
    [FYI] Richard.Graham
    
    [CL 31354440 by ben hoffman in ue5-main branch]

--- Engine/Plugins/EnhancedInput/Source/EnhancedInput/Private/EnhancedInputSubsystemInterface.cpp
+++ Engine/Plugins/EnhancedInput/Source/EnhancedInput/Private/EnhancedInputSubsystemInterface.cpp
@@ -1060,24 +1060,6 @@ void IEnhancedInputSubsystemInterface::RebuildControlMappings()
 		{
 			RemovedActions.Remove(Mapping.Action);
 
-			// Retain old mapping trigger/modifier state for identical key -> action mappings.
-			TArray<FEnhancedActionKeyMapping>::SizeType Idx = OldMappings.IndexOfByPredicate(
-				[&Mapping](const FEnhancedActionKeyMapping& Other)
-				{
-					// Use Equals() to ignore Triggers' values. We want to keep their values from before remapping to
-					// prevent resets. Otherwise, triggers like UInputTriggerPressed re-trigger when their value is
-					// reset to 0; and time counting triggers, like UInputTriggerHold, restart their time.
-					// But don't ignore Modifier and Trigger types and their order in the comparison. If we did, we'd
-					// replace new mappings for old ones with different Trigger and Modifier settings.
-					return Mapping.Equals(Other);
-				});
-
-			if (Idx != INDEX_NONE)
-			{
-				Mapping = MoveTemp(OldMappings[Idx]);
-				OldMappings.RemoveAtSwap(Idx);
-			}
-
 			// Was this key pressed last frame? If so, then we need to mark it to be ignored by PlayerInput
 			// until it is released to avoid re-processing a triggered event.
 			// This also prevents actions from triggering if the key is being held whilst the IMC is added and bIgnoreAllPressedKeysUntilReleaseOnRebuild
@@ -1089,6 +1071,23 @@ void IEnhancedInputSubsystemInterface::RebuildControlMappings()
 				{
 					Mapping.bShouldBeIgnored = true;
 				}
+			}
+
+			// Retain old mapping trigger/modifier state for identical key -> action mappings.
+			TArray<FEnhancedActionKeyMapping>::SizeType Idx = OldMappings.IndexOfByPredicate(
+				[&Mapping](const FEnhancedActionKeyMapping& Other)
+				{
+					// Use Equals() to ignore Triggers' values. We want to keep their values from before remapping to
+					// prevent resets. Otherwise, triggers like UInputTriggerPressed re-trigger when their value is
+					// reset to 0; and time counting triggers, like UInputTriggerHold, restart their time.
+					// But don't ignore Modifier and Trigger types and their order in the comparison. If we did, we'd
+					// replace new mappings for old ones with different Trigger and Modifier settings.
+					return Mapping.Equals(Other);
+				});
+			if (Idx != INDEX_NONE)
+			{
+				Mapping = MoveTemp(OldMappings[Idx]);
+				OldMappings.RemoveAtSwap(Idx);
 			}
 		}
 		for (const UInputAction* Action : RemovedActions)
