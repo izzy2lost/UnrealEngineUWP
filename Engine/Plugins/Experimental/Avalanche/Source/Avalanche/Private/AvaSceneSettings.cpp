fdb76c2f77e126b1cd76eb9dfdc6597aca8e58e9
juan portillo <juan.portillo@geotech.one>
2024-02-10 13:14:37 -0500

    Motion Design: Fix issue with Scene Tags not being able to be added/removed dynamically in RC. RC, by design, will set a value and there's no current way to add/remove scene attributes without disturbing the entire array of handles. To circumvent this, the scene attribute object implementation is being introduced. This change removes the existing Scene Tags property given its limitations in favor of Scene Tag Objects.
    #rb Dario.Mazzanti
    #jira UE-206459
    
    #changelist validated
    #virtualized
    
    [CL 31369214 by juan portillo in ue5-main branch]

--- Engine/Plugins/Experimental/Avalanche/Source/Avalanche/Private/AvaSceneSettings.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AvaSceneSettings.h"
-
-void UAvaSceneSettings::SetTagAttributes(const FAvaTagHandleContainer& InTagAttributes)
-{
-	TagAttributes = InTagAttributes;
-}
