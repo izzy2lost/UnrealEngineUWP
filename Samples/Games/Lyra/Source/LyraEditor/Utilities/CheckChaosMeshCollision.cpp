b9e6d7d89f89582a24e0f1cc5d27ec5ee722e8b5
vincent robert <vincent.robert@epicgames.com>
2024-02-12 13:19:39 -0500

    Fix compilation in Lyra #jira UE-202397
    [CL 31390546 by vincent robert in 5.4 branch]

--- Samples/Games/Lyra/Source/LyraEditor/Utilities/CheckChaosMeshCollision.cpp
+++ Samples/Games/Lyra/Source/LyraEditor/Utilities/CheckChaosMeshCollision.cpp
@@ -23,9 +23,9 @@ bool CheckMeshDataForProblem(const Chaos::FTriangleMeshImplicitObject::Particles
 
 		for (int32 FaceIdx = 0; FaceIdx < NumTriangles; ++FaceIdx)
 		{
-			const VecType& A = Particles.X(Elements[FaceIdx][0]);
-			const VecType& B = Particles.X(Elements[FaceIdx][1]);
-			const VecType& C = Particles.X(Elements[FaceIdx][2]);
+			const VecType& A = Particles.GetX(Elements[FaceIdx][0]);
+			const VecType& B = Particles.GetX(Elements[FaceIdx][1]);
+			const VecType& C = Particles.GetX(Elements[FaceIdx][2]);
 
 			const VecType AB = B - A;
 			const VecType AC = C - A;
