cfbb711a119acea42a7b54bbd7b8cc0231685742
alex kahn <alex.kahn@epicgames.com>
2024-02-12 18:57:50 -0500

    [Backout] - CL31399131 - CIS Compile Error [FYI] Zousar.Shaker Original CL Desc ----------------------------------------------------------------- Fix intermittent hang at end of cook caused by shader compiler's completion callback destroying its own RequestOwner leading later to a hang in DDC's WaitForQuiescence method.
    #rb devin.doucette
    
    [CL 31406870 by alex kahn in 5.4 branch]

--- Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
+++ Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
@@ -1886,11 +1886,7 @@ void FShaderJobCache::AddToCacheAndProcessPending(FShaderCommonCompileJob* Finis
 		}
 
 		// remove ourselves from the jobs in flight
-		if (JobData.JobInFlight)
-		{
-			JobData.JobInFlight->RequestOwner->KeepAlive();
-			JobData.JobInFlight = nullptr;
-		}
+		JobData.JobInFlight = nullptr;
 		FinishedJob->JobCacheRef.Clear();
 	}
 
@@ -10813,11 +10809,7 @@ FShaderJobCacheRef FShaderJobCache::FindOrAdd(const FJobInputHash& Hash, EShader
 								UE_LOG(LogShaderCompilers, Display, TEXT("Cancelled job 0x%p (data 0x%p) with pending DDC hit."), JobDataPtr->JobInFlight.GetReference(), JobDataPtr);
 
 								delete NewStoredOutput;
-								if (JobDataPtr->JobInFlight)
-								{
-									JobDataPtr->JobInFlight->RequestOwner->KeepAlive();
-									JobDataPtr->JobInFlight = nullptr;
-								}
+								JobDataPtr->JobInFlight = nullptr;
 								JobLock.WriteUnlock();
 								return;
 							}
@@ -10864,11 +10856,7 @@ FShaderJobCacheRef FShaderJobCache::FindOrAdd(const FJobInputHash& Hash, EShader
 								CurHead = CurHead->NextLink;
 							}
 							JobDataPtr->DuplicateJobsWaitList = nullptr;
-							if (JobDataPtr->JobInFlight)
-							{
-								JobDataPtr->JobInFlight->RequestOwner->KeepAlive();
-								JobDataPtr->JobInFlight = nullptr;
-							}
+							JobDataPtr->JobInFlight = nullptr;
 							Job->JobCacheRef.Clear();
 
 							// Need to release the lock before calling ProcessFinishedJobs
@@ -10902,11 +10890,7 @@ FShaderJobCacheRef FShaderJobCache::FindOrAdd(const FJobInputHash& Hash, EShader
 							{
 								UE_LOG(LogShaderCompilers, Display, TEXT("Cancelled job 0x%p (data 0x%p) with pending DDC miss."), Job, JobDataPtr);
 
-								if (JobDataPtr->JobInFlight)
-								{
-									JobDataPtr->JobInFlight->RequestOwner->KeepAlive();
-									JobDataPtr->JobInFlight = nullptr;
-								}
+								JobDataPtr->JobInFlight = nullptr;
 								return;
 							}
 							else
