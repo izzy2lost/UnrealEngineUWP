d79095c7d332d81bafa9bea306b7566c211c7a62
sebastien hillaire <Sebastien.Hillaire@epicgames.com>
2024-02-12 07:17:12 -0500

    Substrate - Fixed support for dithered lod transitions. [FYI] charles.derousiers
    [CL 31378671 by sebastien hillaire in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialShared.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialShared.cpp
@@ -2541,17 +2541,12 @@ void FMaterial::SetupMaterialEnvironment(
 		switch (GetBlendMode())
 		{
 		case BLEND_Opaque:
-		{
-			SET_SHADER_DEFINE(OutEnvironment, MATERIALBLENDING_SOLID, 1);
-			SET_SHADER_DEFINE(OutEnvironment, SUBSTRATE_BLENDING_OPAQUE, 1);
-			break;
-		}
 		case BLEND_Masked:
 		{
 			// Only set MATERIALBLENDING_MASKED if the material is truly masked
 			//@todo - this may cause mismatches with what the shader compiles and what the renderer thinks the shader needs
 			// For example IsTranslucentBlendMode doesn't check IsMasked
-			if(!WritesEveryPixel())
+			if (!WritesEveryPixel())
 			{
 				SET_SHADER_DEFINE(OutEnvironment, MATERIALBLENDING_MASKED, 1);
 			}
@@ -2559,7 +2554,6 @@ void FMaterial::SetupMaterialEnvironment(
 			{
 				SET_SHADER_DEFINE(OutEnvironment, MATERIALBLENDING_SOLID, 1);
 			}
-			SET_SHADER_DEFINE(OutEnvironment, SUBSTRATE_BLENDING_MASKED, 1);
 			break;
 		}
 		case BLEND_Additive:
