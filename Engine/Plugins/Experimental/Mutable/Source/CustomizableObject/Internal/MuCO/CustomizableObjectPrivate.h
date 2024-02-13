13e6b5cd84098c9e58d9cf31080592dd2834afd3
pere rifa <pere.rifa@epicgames.com>
2024-02-13 04:43:04 -0500

    [Mutable] Add bDisableTextureStreaming to the derived data.
    #rnx
    #rb alexei.lebedev
    
    [CL 31417370 by pere rifa in ue5-main branch]

--- Engine/Plugins/Experimental/Mutable/Source/CustomizableObject/Internal/MuCO/CustomizableObjectPrivate.h
+++ Engine/Plugins/Experimental/Mutable/Source/CustomizableObject/Internal/MuCO/CustomizableObjectPrivate.h
@@ -691,6 +691,9 @@ public:
 	/** If the object is compiled, this flag is true if it was compiled with maximum optimizations. If the object is not compiled, its value is meaningless. */
 	bool bIsCompiledWithOptimization = true;
 
+	/** This is a non-user-controlled flag to disable streaming (set at object compilation time, depending on optimization). */
+	bool bDisableTextureStreaming = false;
+
 	ECustomizableObjectCompilationState CompilationState = ECustomizableObjectCompilationState::None;
 	
 #if WITH_EDITOR
@@ -722,9 +725,6 @@ public:
 	// This is a manual version number for the binary blobs in this asset.
 	// Increasing it invalidates all the previously compiled models.
 	// Warning: If while merging code both versions have changed, take the highest+1.
-	static constexpr int32 CurrentSupportedVersion = 424;
-
-	/** This is a non-user-controlled flag to disable streaming (set at object compilation time, depending on optimization). */
-	bool bDisableTextureStreaming = false;
+	static constexpr int32 CurrentSupportedVersion = 425;
 };
 
