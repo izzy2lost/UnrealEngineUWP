5ce42aaeb81ab62aaf63a98d74258d44ad06f829
george rolfe <george.rolfe@xa.epicgames.com>
2024-02-12 19:43:16 -0500

    [MotionDesign] moved impl from header in AvaCineCameraActor, AvaPostProcessVolume #rb Juan.Portillo
    [CL 31408068 by george rolfe in ue5-main branch]

--- Engine/Plugins/Experimental/Avalanche/Source/Avalanche/Public/Viewport/AvaCineCameraActor.h
+++ Engine/Plugins/Experimental/Avalanche/Source/Avalanche/Public/Viewport/AvaCineCameraActor.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "CineCameraActor.h"
-#include "CineCameraComponent.h"
 #include "AvaCineCameraActor.generated.h"
 
 /** 
@@ -13,69 +12,23 @@
  * In particular, Motion Design Editor configuration property "Camera Distance" is used to initialize camera position and manual focus.
  * See: Editor Preferences --> Motion Design --> Editor Settings --> Camera Distance
  */
-UCLASS(DisplayName = "Motion Design Cine Camera Actor")
-class AVALANCHE_API AAvaCineCameraActor : public ACineCameraActor
+UCLASS(MinimalAPI, DisplayName = "Motion Design Cine Camera Actor")
+class AAvaCineCameraActor : public ACineCameraActor
 {
 	friend class UAvaEditorSettings;
 	
 	GENERATED_BODY()
 
 public:
-	AAvaCineCameraActor(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-	{
-		/* todo: when the reset to defaults issue will be solved, Configure(GetDefaultCameraDistance()) could be called here to to initialize defaults */
-	}
+	AAvaCineCameraActor(const FObjectInitializer& ObjectInitializer);
 
 	/**
 	 * Initialize the camera with Avalanche scene default values: field of view, camera position, focus distance.
 	 * @param InCameraDistance: camera distance value, used to initialize camera position and manual focus distance
 	 */
-	void Configure(float InCameraDistance)
-	{
-		// initialize default camera distance - todo: we might use this value later for a custom reset?
-		DefaultCameraDistance = InCameraDistance;
-		
-		if (UCineCameraComponent* const CineCameraComp = GetCineCameraComponent())
-		{
-			CineCameraComp->SetFieldOfView(90.0f);
-
-			// smaller camera mesh, todo: this will be substituted with a custom visualizer, or other solution, to visualize frustum + direction
-			CineCameraComp->SetWorldScale3D(FVector(0.5f));
-
-			// using default ManualFocusDistance value from Default CineCameraComponent, since that is handled by UAvaEditorSettings
-			CineCameraComp->FocusSettings.ManualFocusDistance = DefaultCameraDistance;
-
-			FVector CameraPosition = FVector::ZeroVector;
-			CameraPosition.X = -DefaultCameraDistance;
-			SetActorLocation(CameraPosition);
-		}
-	}
+	AVALANCHE_API void Configure(float InCameraDistance);
 
 private:
-	/**
-	 * Updates some defaults of the Mutable CDO's CineCameraComponent, based on the provided CameraDistance value
-	 */
-	static void SetDefaultCameraDistance(float InCameraDistance)
-	{
-		AAvaCineCameraActor* const DefaultObject = GetMutableDefault<AAvaCineCameraActor>();
-
-		FVector CameraPosition = FVector::ZeroVector;
-		CameraPosition.X = -InCameraDistance;
-		DefaultObject->SetActorLocation(CameraPosition);
-		
-		if (UCineCameraComponent* const DefaultCineCameraComponent = DefaultObject->GetCineCameraComponent())
-		{
-			DefaultCineCameraComponent->FocusSettings.ManualFocusDistance = InCameraDistance;
-		}
-
-		DefaultObject->DefaultCameraDistance = InCameraDistance;
-	}
-
-	static float GetDefaultCameraDistance()
-	{
-		return GetMutableDefault<AAvaCineCameraActor>()->DefaultCameraDistance;
-	}
-
 	/** The CameraDistance value used when configuring this AvaCineCamera*/
 	UPROPERTY()
 	float DefaultCameraDistance;
