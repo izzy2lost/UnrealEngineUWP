a9c5a128b283bf92ec21f90a58e204dcb524e837
alex kahn <alex.kahn@epicgames.com>
2024-02-13 14:17:34 -0500

    [Backout] - CL31433483 - CIS UE Error Backout 1/4 [FYI] Yoan.StAmant Original CL Desc ----------------------------------------------------------------- Lyra and CitySample fixes for Issue 429340 #rb yoan.stamant
    [CL 31439353 by alex kahn in ue5-main branch]

--- Samples/Games/Lyra/Plugins/GameFeatures/ShooterCore/Source/ShooterCoreRuntime/Private/Input/AimAssistTargetManagerComponent.cpp
+++ Samples/Games/Lyra/Plugins/GameFeatures/ShooterCore/Source/ShooterCoreRuntime/Private/Input/AimAssistTargetManagerComponent.cpp
@@ -2,7 +2,6 @@
 
 #include "Input/AimAssistTargetManagerComponent.h"
 #include "CommonInputTypeEnum.h"
-#include "Engine/OverlapResult.h"
 #include "Engine/World.h"
 #include "GameFramework/InputSettings.h"
 #include "GameFramework/Character.h"
