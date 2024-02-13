42f6845ba8095b95713437af9df254c35116d9b4
dario mazzanti <dario.mazzanti@geotech.one>
2024-02-13 10:08:35 -0500

    [Motion Design] Hide UAvaText3DCharacterTransform Transform Category
    [CL 31426549 by dario mazzanti in 5.4 branch]

--- Engine/Plugins/Experimental/Avalanche/Source/AvalancheText/Public/AvaTextCharacterTransform.h
+++ Engine/Plugins/Experimental/Avalanche/Source/AvalancheText/Public/AvaTextCharacterTransform.h
@@ -9,7 +9,7 @@
 /**
  * This Component is kept for compatibility with old AAvaTextActor assets
  */
-UCLASS(ClassGroup=(Text3D), HideCategories = (Collision, Tags, Activation, Cooking, Rendering, Physics, Mobility, LOD, AssetUserData, Navigation), meta = (BlueprintSpawnableComponent))
+UCLASS(ClassGroup=(Text3D), HideCategories = (Collision, Tags, Activation, Cooking, Rendering, Physics, Mobility, LOD, AssetUserData, Navigation, Transform), meta = (BlueprintSpawnableComponent))
 class AVALANCHETEXT_API UAvaTextCharacterTransform : public UText3DCharacterTransform
 {
 	GENERATED_BODY()
