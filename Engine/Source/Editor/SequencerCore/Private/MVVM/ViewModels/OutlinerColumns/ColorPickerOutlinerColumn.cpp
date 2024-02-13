14162039340387b01331c2b83345f8bdbbd3c780
max chen <Max.Chen@epicgames.com>
2024-02-13 11:19:12 -0500

    Sequencer: ColorPicker -> Color Picker
    #rb andrew.rodham
    #rnx
    
    [CL 31430066 by max chen in 5.4 branch]

--- Engine/Source/Editor/SequencerCore/Private/MVVM/ViewModels/OutlinerColumns/ColorPickerOutlinerColumn.cpp
+++ Engine/Source/Editor/SequencerCore/Private/MVVM/ViewModels/OutlinerColumns/ColorPickerOutlinerColumn.cpp
@@ -8,7 +8,7 @@ namespace UE::Sequencer
 FColorPickerOutlinerColumn::FColorPickerOutlinerColumn()
 {
 	Name     = FCommonOutlinerNames::ColorPicker;
-	Label    = NSLOCTEXT("FColorPickerOutlinerColumn", "ColorPickerColumnColorPicker", "ColorPicker");
+	Label    = NSLOCTEXT("FColorPickerOutlinerColumn", "ColorPickerColumnColorPicker", "Color Picker");
 	Position = FOutlinerColumnPosition{ 30, EOutlinerColumnGroup::RightGutter };
 	Layout   = FOutlinerColumnLayout{ 6.f, FMargin(0.f), HAlign_Fill, VAlign_Fill, EOutlinerColumnSizeMode::Fixed, EOutlinerColumnFlags::None };
 }
