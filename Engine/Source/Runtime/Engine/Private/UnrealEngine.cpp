9eef5f13c2eeb150763cec7bb346f28336c900ec
chris varnsverry <Chris.Varnsverry@epicgames.com>
2024-02-13 10:52:42 -0500

    - Improve HandleGetIniCommand to handle section names surrounded with quotes, useful for section names with spaces in them.
    [REVIEW] [at]Josh.Adams [at]Rob.Cannaday
    #rb Rob.Cannaday
    
    [CL 31428843 by chris varnsverry in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
+++ Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
@@ -9847,6 +9847,7 @@ bool UEngine::HandleGetIniCommand(const TCHAR* Cmd, FOutputDevice& Ar)
 		int32 IniDelim = IniPlusSection.Find(TEXT(":"));
 		FString IniName;
 		FString SectionName = (IniDelim != INDEX_NONE ? IniPlusSection.Mid(IniDelim+1) : IniPlusSection);
+		SectionName.TrimQuotesInline();
 
 		if (IniDelim != INDEX_NONE)
 		{
