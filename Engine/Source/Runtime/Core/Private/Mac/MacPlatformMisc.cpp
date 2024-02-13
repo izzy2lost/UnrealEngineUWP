8bb0a82d6490a65f8588b3ab640e06fd57600d12
johan berg <Johan.Berg@epicgames.com>
2024-02-13 14:21:43 -0500

    Make Linux/Mac use editor version of crash reporter when compiled with WITH_EDITOR
    In order to properly separate crashes that happens during development versus crashes that occurr in shipped titles we need to use the editor version of the crash reporter when appropriate.
    
    #rb calvin.zheng
    #jira UE-206087, UE-205533
    
    [CL 31439621 by johan berg in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/Mac/MacPlatformMisc.cpp
+++ Engine/Source/Runtime/Core/Private/Mac/MacPlatformMisc.cpp
@@ -337,7 +337,11 @@ struct FMacApplicationInfo
 		// Cache & create the crash report folder.
 		FString ReportPath = FPaths::ConvertRelativePathToFull(FString::Printf(TEXT("%s"), *(FPaths::GameAgnosticSavedDir() / TEXT("Crashes"))));
 		FCStringAnsi::Strcpy(CrashReportPath, PATH_MAX+1, TCHAR_TO_UTF8(*ReportPath));
+#if WITH_EDITOR
+		FString ReportClient = FPaths::ConvertRelativePathToFull(FPlatformProcess::GenerateApplicationPath(TEXT("CrashReportClientEditor"), EBuildConfiguration::Development));
+#else
 		FString ReportClient = FPaths::ConvertRelativePathToFull(FPlatformProcess::GenerateApplicationPath(TEXT("CrashReportClient"), EBuildConfiguration::Development));
+#endif
 		FCStringAnsi::Strcpy(CrashReportClient, PATH_MAX+1, TCHAR_TO_UTF8(*ReportClient));
 		IFileManager::Get().MakeDirectory(*ReportPath, true);
 		
