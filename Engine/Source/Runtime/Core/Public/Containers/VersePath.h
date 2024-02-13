1aa8226fe5e831ef10ea969ee4dd11604b322c51
dave belanger <Dave.Belanger@epicgames.com>
2024-02-13 07:53:52 -0500

    Fix FVersePath equality and hash func: verse paths are case sensitive Fix inversed logic in FVersePath::operator!= #rb Steve.Robb
    [CL 31420635 by dave belanger in 5.4 branch]

--- Engine/Source/Runtime/Core/Public/Containers/VersePath.h
+++ Engine/Source/Runtime/Core/Public/Containers/VersePath.h
@@ -72,12 +72,12 @@ private:
 
 FORCEINLINE bool UE::Core::operator==(const FVersePath& Lhs, const FVersePath& Rhs)
 {
-	return Lhs.PathString == Rhs.PathString;
+	return Lhs.PathString.Equals(Rhs.PathString, ESearchCase::CaseSensitive);
 }
 
 FORCEINLINE bool UE::Core::operator!=(const FVersePath& Lhs, const FVersePath& Rhs)
 {
-	return Lhs.PathString == Rhs.PathString;
+	return !(Lhs == Rhs);
 }
 
 FORCEINLINE FArchive& UE::Core::operator<<(FArchive& Ar, FVersePath& VersePath)
@@ -87,5 +87,5 @@ FORCEINLINE FArchive& UE::Core::operator<<(FArchive& Ar, FVersePath& VersePath)
 
 FORCEINLINE uint32 GetTypeHash(const UE::Core::FVersePath& VersePath) // Must be outside namespace to not break Tuples. Note that it needs to match VersePathFwd.h so can't be hidden friend
 {
-	return GetTypeHash(VersePath.AsStringView());
+	return FCrc::StrCrc32<TCHAR>(*VersePath);
 }
