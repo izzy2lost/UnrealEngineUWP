699e5eda6ebb05b6a8a6f30c3bab51260d5f712b
ronald koppers <ronald.koppers@epicgames.com>
2024-02-14 11:30:11 -0500

    Re-enabled SCC integration with TEDS.
    #jira UE-206170
    #rb brooke.hubert
    [FYI] zach.rammell
    
    [CL 31476626 by ronald koppers in ue5-main branch]

--- Engine/Plugins/Experimental/TypedElementsDataStorage/Source/TypedElementsDataStorage/Private/Compatibility/TypedElementUObjectPackagePathToColumnQueries.cpp
+++ Engine/Plugins/Experimental/TypedElementsDataStorage/Source/TypedElementsDataStorage/Private/Compatibility/TypedElementUObjectPackagePathToColumnQueries.cpp
@@ -11,7 +11,7 @@
 #include "Misc/Paths.h"
 #include "UObject/Package.h"
 
-static bool bAutoPopulateRevisionControlState = false;
+static bool bAutoPopulateRevisionControlState = true;
 TYPEDELEMENTSDATASTORAGE_API FAutoConsoleVariableRef CVarAutoPopulateState(
 	TEXT("TEDS.RevisionControl.AutoPopulateState"),
 	bAutoPopulateRevisionControlState,
