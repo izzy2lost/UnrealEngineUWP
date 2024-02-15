9df8f87d3116bbfa47335f964d0bed289b69a5cb
zousar shaker <zousar.shaker@epicgames.com>
2024-02-15 17:57:34 -0500

    [Backout] - CL31524461 [FYI] Zousar.Shaker Original CL Desc ----------------------------------------------------------------- Allow printing of "Any" content type responses.
    [FYI] joakim.lindqvist
    
    [CL 31541046 by zousar shaker in 5.4 branch]

--- Engine/Source/Developer/DerivedDataCache/Private/HttpCacheStore.cpp
+++ Engine/Source/Developer/DerivedDataCache/Private/HttpCacheStore.cpp
@@ -779,7 +779,6 @@ FString FHttpCacheStore::FHttpOperation::GetBodyAsString() const
 	EHttpMediaType ContentType = GetContentType();
 	switch (ContentType)
 	{
-	case EHttpMediaType::Any:
 	case EHttpMediaType::Text:
 	case EHttpMediaType::Json:
 	case EHttpMediaType::Yaml:
