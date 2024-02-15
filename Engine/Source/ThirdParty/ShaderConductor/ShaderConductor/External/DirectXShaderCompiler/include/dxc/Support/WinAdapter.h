cf01f9160ef242164e57f1e4fa1d7c16a3183ea1
carl lloyd <carl.lloyd@epicgames.com>
2024-02-15 17:10:30 -0500

    Fix multiple definitions of BOOL on Apple platforms
    #rb trivial
    #jira UE-204112
    
    [CL 31538389 by carl lloyd in ue5-main branch]

--- Engine/Source/ThirdParty/ShaderConductor/ShaderConductor/External/DirectXShaderCompiler/include/dxc/Support/WinAdapter.h
+++ Engine/Source/ThirdParty/ShaderConductor/ShaderConductor/External/DirectXShaderCompiler/include/dxc/Support/WinAdapter.h
@@ -364,7 +364,7 @@ typedef BYTE BOOLEAN;
 typedef BOOLEAN *PBOOLEAN;
 
 // UE Change Begin: Bool is already defined on Apple platforms
-#ifndef BOOL
+#if !defined(BOOL) && !defined(__APPLE__)
 typedef bool BOOL;
 #endif
 // UE Change End: Bool is already defined on Apple platforms
