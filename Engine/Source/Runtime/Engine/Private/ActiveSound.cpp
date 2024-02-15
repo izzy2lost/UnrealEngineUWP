2d8fd94c21566e7fa38b5ed0ff173c9595906afa
Aaron McLeran <Aaron.McLeran@epicgames.com>
2024-02-14 22:09:36 -0500

    Fixing live attenuation updates.
    Made the max distance value use the attenuation setting asset and not use the cached value.
    
    #jira UE-197840
    #rb Jimmy.Smith
    #fyi sondra.moyls
    
    [CL 31509438 by Aaron McLeran in 5.4 branch]

--- Engine/Source/Runtime/Engine/Private/ActiveSound.cpp
+++ Engine/Source/Runtime/Engine/Private/ActiveSound.cpp
@@ -1398,6 +1398,28 @@ FWaveInstance& FActiveSound::AddWaveInstance(const UPTRINT WaveInstanceHash)
 	return *WaveInstance;
 }
 
+float FActiveSound::GetMaxDistance() const
+{
+	float OutMaxDistance = 0.0f;
+	float OutFocusFactor = 1.0f;
+	GetMaxDistanceAndFocusFactor(OutMaxDistance, OutFocusFactor);
+	return OutMaxDistance;
+}
+
+void FActiveSound::GetMaxDistanceAndFocusFactor(float& OutMaxDistance, float& OutFocusFactor) const
+{
+	OutMaxDistance = 0.0f;
+	OutFocusFactor = 1.0f;
+
+	if (!Sound)
+	{
+		return;
+	}
+
+	check(AudioDevice);
+	AudioDevice->GetMaxDistanceAndFocusFactor(Sound, GetWorld(), LastLocation, GetAttenuationSettings(), OutMaxDistance, OutFocusFactor);
+}
+
 void FActiveSound::ApplyRadioFilter(const FSoundParseParameters& ParseParams)
 {
 	check(AudioDevice);
@@ -1673,7 +1695,7 @@ void FActiveSound::UpdateAttenuation(float DeltaTime, FSoundParseParameters& Par
 	UpdateAttenuation(DeltaTime, ParseParams, Listener.ListenerIndex, SettingsAttenuationNode);
 }
 
-void FActiveSound::UpdateAttenuation(float DeltaTime, FSoundParseParameters& ParseParams, int32 ListenerIndex, const FSoundAttenuationSettings* SettingsAttenuationNode)
+const FSoundAttenuationSettings* FActiveSound::GetAttenuationSettings(const FSoundAttenuationSettings* SettingsAttenuationNode) const
 {
 	// We default to using the copied off "overridden" settings (or default constructed settings)
 	const FSoundAttenuationSettings* Settings = &AttenuationSettings;
@@ -1692,6 +1714,13 @@ void FActiveSound::UpdateAttenuation(float DeltaTime, FSoundParseParameters& Par
 			Settings = &SoundAttenuation->Attenuation;
 		}
 	}
+	return Settings;
+}
+
+void FActiveSound::UpdateAttenuation(float DeltaTime, FSoundParseParameters& ParseParams, int32 ListenerIndex, const FSoundAttenuationSettings* SettingsAttenuationNode)
+{
+	// We default to using the copied off "overridden" settings (or default constructed settings)
+	const FSoundAttenuationSettings* Settings = GetAttenuationSettings(SettingsAttenuationNode);
 
 	// Reset Focus data and recompute if necessary
 	FAttenuationFocusData FocusDataToApply;
--- Engine/Source/Runtime/Engine/Public/ActiveSound.h
+++ Engine/Source/Runtime/Engine/Public/ActiveSound.h
@@ -371,6 +371,12 @@ public:
 	/** Whether or not the active sound is currently playing audible sound. */
 	bool IsPlayingAudio() const { return bIsPlayingAudio; }
 
+	// Returns the max distance of the active sound
+	float GetMaxDistance() const;
+	void GetMaxDistanceAndFocusFactor(float& OutMaxDistance, float& OutFocusFactor) const;
+
+	const FSoundAttenuationSettings* GetAttenuationSettings(const FSoundAttenuationSettings* SettingsAttenuationNode = nullptr) const;
+
 	/** Whether or not sound reference is valid and set to play when silent. */
 	ENGINE_API bool IsPlayWhenSilent() const;
 
@@ -584,7 +590,7 @@ public:
 	/** Last time we checked for occlusion */
 	float LastOcclusionCheckTime;
 
-	/** The max distance this sound will be audible. */
+	UE_DEPRECATED(5.4, "Please do not use MaxDistance. Use GetMaxDistance() instead.")
 	float MaxDistance;
 
 	FTransform Transform;
