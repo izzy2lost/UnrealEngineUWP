fd8bfa171e503fe12766112127962b9d1aa49469
julien lheureux <julien.lheureux@epicgames.com>
2024-02-15 17:02:38 -0500

    Clarified usage of preview/load-as-preview modes on the pcg component
    #rnx
    #jira UE-206202
    #rb adrien.logut
    
    [CL 31537915 by julien lheureux in 5.4 branch]

--- Engine/Plugins/PCG/Source/PCG/Public/PCGCommon.h
+++ Engine/Plugins/PCG/Source/PCG/Public/PCGCommon.h
@@ -247,7 +247,7 @@ UENUM()
 enum class EPCGEditorDirtyMode : uint8
 {
 	Normal UMETA(Tooltip="Normal editing mode where generation changes (generation, cleanup) dirty the component and its resources."),
-	Preview UMETA(Tooltip="Editing mode where generation changes (generation, cleanup, resources) on the component will not trigger any dirty state, but will also not save any of the generated resources."),
+	Preview UMETA(Tooltip="Editing mode where generation changes (generation, cleanup, resources) on the component will not trigger any dirty state, but will also not save any of the generated resources. Also represents the state after loading from the Load as Preview edit mode, where this will hold the last saved generation until a new generation is triggered."),
 	LoadAsPreview UMETA(Tooltip="Acts as the normal editing mode until the next load of the component, at which state it acts as-if-transient, namely that any further generation changes will not dirty the component.")
 };
 
