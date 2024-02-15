ddccd88fd34e8ad561d9aeb817a33c9d9cc02798
james singer <james.singer@epicgames.com>
2024-02-15 11:52:00 -0500

    Remove references to mono from some Linux platform files (misleading comments)
    #jira UE-206564
    #rb ryan.hummer, josh.adams, brandon.schaefer
    
    [CL 31522858 by james singer in 5.4 branch]

--- Engine/Build/BatchFiles/RunUAT.sh
+++ Engine/Build/BatchFiles/RunUAT.sh
@@ -114,7 +114,7 @@ else
 fi
 
 # if we are running under UE, we need to run this with the term handler (otherwise canceling a UAT job from the editor
-# can leave mono, etc running in the background, which means we need the PID so we 
+# can leave dotnet, etc running in the background, which means we need the PID so we 
 # run it in the background
 if [ "$UE_DesktopUnrealProcess" = "1" ]; then
 	# you can't set a dotted env var nicely in sh, but env will run a command with
