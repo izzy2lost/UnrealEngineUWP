fd21af6e7a7d76edadc783b2c9209ef86954c192
ionut matasaru <ionut.matasaru@epicgames.com>
2024-02-15 10:15:49 -0500

    [Insights] Further improved drawing of graph track series. Disabled antialias for lines and maintained 1px line thickness no matter of screen scale. Also increased the minimum width of the context menu (to lower the resize effect for longer series names).
    #rb Catalin.Dragoiu
    
    [CL 31519494 by ionut matasaru in ue5-main branch]

--- Engine/Source/Developer/TraceInsights/Public/Insights/ViewModels/GraphTrack.h
+++ Engine/Source/Developer/TraceInsights/Public/Insights/ViewModels/GraphTrack.h
@@ -51,7 +51,7 @@ class TRACEINSIGHTS_API FGraphTrack : public FBaseTimingTrack
 
 private:
 	// Visual size of points (in pixels).
-	static constexpr float PointVisualSize = 5.0f;
+	static constexpr float PointVisualSize = 5.5f;
 
 	// Size of points (in pixels) used in reduction algorithm.
 	static constexpr double PointSizeX = 3.0f;
