bdef3d81ada83552791a67db9a4f11476726b345
Dmitriy Dyomin <Dmitriy.Dyomin@epicgames.com>
2024-02-16 04:42:02 -0500

    Attempt to fix vulkan RHI assert CmdBuffer->IsSubmitted() on running engine rendering tests #jira UE-206895
    [CL 31558214 by Dmitriy Dyomin in 5.4 branch]

--- Engine/Source/Runtime/VulkanRHI/Private/VulkanCommandBuffer.cpp
+++ Engine/Source/Runtime/VulkanRHI/Private/VulkanCommandBuffer.cpp
@@ -850,6 +850,13 @@ void FVulkanCommandBufferPool::FreeUnusedCmdBuffers(FVulkanQueue* InQueue, bool 
 {
 #if VULKAN_DELETE_STALE_CMDBUFFERS
 	FScopeLock ScopeLock(&CS);
+	
+	if (bTrimMemory)
+	{
+		VulkanRHI::vkTrimCommandPool(Device->GetInstanceHandle(), Handle, 0);
+		return;
+	}
+
 	const double CurrentTime = FPlatformTime::Seconds();
 
 	// In case Queue stores pointer to a cmdbuffer, do not delete it
@@ -873,18 +880,6 @@ void FVulkanCommandBufferPool::FreeUnusedCmdBuffers(FVulkanQueue* InQueue, bool 
 			CmdBuffers.RemoveAtSwap(Index, 1, EAllowShrinking::No);
 			FreeCmdBuffers.Add(CmdBuffer);
 		}
-	}
-
-	if (bTrimMemory)
-	{
-		VulkanRHI::vkTrimCommandPool(Device->GetInstanceHandle(), Handle, 0);
-		
-		for (int32 Index = 0; Index < FreeCmdBuffers.Num(); ++Index)
-		{
-			FVulkanCmdBuffer* CmdBuffer = FreeCmdBuffers[Index];
-			delete CmdBuffer;
-		}
-		FreeCmdBuffers.Empty(8);
 	}
 #endif
 }
