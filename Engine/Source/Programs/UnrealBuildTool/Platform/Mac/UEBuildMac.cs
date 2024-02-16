36764d25c83ebe83cf1bdd428dba705702ded77c
florin pascu <Florin.Pascu@epicgames.com>
2024-02-15 20:51:47 -0500

    Fix Editor Crash when Mac SDK is not installed Temporary put MacTargetPlatformSettings back to requiring sdk Also add SetPlatformHiddenFromUI to remove Mac from Platform dropdown #jira UE-207083, UE-207084
    [CL 31550742 by florin pascu in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/Mac/UEBuildMac.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Mac/UEBuildMac.cs
@@ -471,10 +471,9 @@ namespace UnrealBuildTool
 				// because remote IOS building needs the new XcodeProject Settings to show up in the editor, we bring in the Mac bits that expose it
 				if (ModuleName == "Engine")
 				{
-					Rules.DynamicallyLoadedModuleNames.Add("MacTargetPlatformSettings");
 					if (bIsPlatformAvailableForTargetWithSDK)
 					{
-						Rules.DynamicallyLoadedModuleNames.AddAll("MacTargetPlatform", "MacTargetPlatformControls", "MacPlatformEditor");
+						Rules.DynamicallyLoadedModuleNames.AddAll("MacTargetPlatform", "MacTargetPlatformSettings", "MacTargetPlatformControls", "MacPlatformEditor");
 					}
 				}
 			}
