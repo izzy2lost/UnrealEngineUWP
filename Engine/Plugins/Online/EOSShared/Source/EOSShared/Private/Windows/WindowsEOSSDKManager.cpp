64d8a0bec7cf73d5cc02b0126a46939017af4e42
jake niman <jake.niman@epicgames.com>
2024-02-16 15:22:58 -0500

    Add EOS Integrated Platform support for Steam using the -CustomConfig=SteamEOS command line option.
    This also includes the SteamEOS option to enable this in Lyra, but full Steam integrated platform support in Lyra is still a WIP
    
    #jira UE-164071
    [REVIEW] [at]michael.kirzinger [at]alejandro.aguilar
    #rb chris.varnsverry
    
    [CL 31577266 by jake niman in 5.4 branch]

--- Engine/Plugins/Online/EOSShared/Source/EOSShared/Private/Windows/WindowsEOSSDKManager.cpp
+++ Engine/Plugins/Online/EOSShared/Source/EOSShared/Private/Windows/WindowsEOSSDKManager.cpp
@@ -7,8 +7,25 @@
 #include "HAL/FileManager.h"
 #include "Misc/Paths.h"
 #include "EOSShared.h"
-
 #include "Windows/eos_Windows.h"
+
+FWindowsEOSSDKManager::FWindowsEOSSDKManager()
+{
+	PlatformSteamOptions =
+	{
+		2, //EOS_INTEGRATEDPLATFORM_STEAM_OPTIONS_API_LATEST
+		nullptr,
+		1,
+		57
+	};
+
+	UE_EOS_CHECK_API_MISMATCH(EOS_INTEGRATEDPLATFORM_STEAM_OPTIONS_API_LATEST, 2);
+}
+
+FWindowsEOSSDKManager::~FWindowsEOSSDKManager()
+{
+
+}
 
 IEOSPlatformHandlePtr FWindowsEOSSDKManager::CreatePlatform(const FEOSSDKPlatformConfig& PlatformConfig, EOS_Platform_Options& PlatformOptions)
 {
@@ -31,5 +48,16 @@ IEOSPlatformHandlePtr FWindowsEOSSDKManager::CreatePlatform(const FEOSSDKPlatfor
 
 	return FEOSSDKManager::CreatePlatform(PlatformConfig, PlatformOptions);
 }
+
+const void* FWindowsEOSSDKManager::GetIntegratedPlatformOptions()
+{
+	return &PlatformSteamOptions;
+}
+
+EOS_IntegratedPlatformType FWindowsEOSSDKManager::GetIntegratedPlatformType()
+{
+	return EOS_IPT_Steam;
+}
+
 
 #endif // WITH_EOS_SDK
