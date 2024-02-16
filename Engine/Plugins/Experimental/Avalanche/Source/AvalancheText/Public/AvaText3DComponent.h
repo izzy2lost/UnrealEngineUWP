653a091334b6c189909e10246aaa9c2c47b289ae
dario mazzanti <dario.mazzanti@geotech.one>
2024-02-16 12:58:15 -0500

    [Motion Design] Text 3D Component ordering and grouping cleanup
    #rb Juan.Portillo
    
    [CL 31571016 by dario mazzanti in 5.4 branch]

--- Engine/Plugins/Experimental/Avalanche/Source/AvalancheText/Public/AvaText3DComponent.h
+++ Engine/Plugins/Experimental/Avalanche/Source/AvalancheText/Public/AvaText3DComponent.h
@@ -119,109 +119,6 @@ public:
 
 	AVALANCHETEXT_API void RefreshMaterialInstances();
 
-private:
-#if WITH_EDITOR
-	static void RegisterOnPropertyChangeFunctions();
-#endif
-
-	void RefreshOpacity() const;
-	
-	void RefreshColor() const;
-	void RefreshExtrudeColor() const;
-	void RefreshBevelColor() const;
-
-	void RefreshAlignment();
-	void RefreshMotionDesignFont();
-	void RefreshHorizontalAlignment();
-	void RefreshVerticalAlignment();
-	void RefreshUnlit();
-	void RefreshMasked();
-	void RefreshGeometry();
-	void RefreshLayout();
-	void RefreshGradientValues();
-	AVALANCHETEXT_API void RefreshGradient();
-	void RefreshMaskValues();
-	void RefreshMask();
-	void RefreshMaterialGeometryParameterValues();
-
-	void RefreshFormatting();
-
-	void SetMaterialGeometryParameterValues(UMaterialInstanceDynamic* InMaterial, const FVector& TextScaleFactor) const;
-	void SetMaskedMaterialValues(UMaterialInstanceDynamic* InMaterial);
-
-	//~ Begin UObject
-#if WITH_EDITOR
-	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
-	//~ End UObject
-#endif
-
-	//~ Begin USceneComponent
-#if WITH_EDITOR
-	virtual void PostEditComponentMove(bool bFinished) override;
-#endif
-	virtual void OnUpdateTransform(EUpdateTransformFlags UpdateTransformFlags, ETeleportType Teleport) override;
-	virtual void OnAttachmentChanged() override;
-	virtual void OnRegister() override;
-	//~ End USceneComponent
-
-	//~ Begin UText3DComponent
-	virtual void FormatText(FText& InOutText) const override;
-	//~ End UText3DComponent
-
-	UPROPERTY(EditAnywhere, Getter="IsEnforcingUpperCase", Setter="SetEnforceUpperCase", Category="Text", meta=(DisplayAfter="Text", AllowPrivateAccess="true"))
-	bool bEnforceUpperCase = false;
-	
-	UPROPERTY(EditAnywhere, Getter="GetMotionDesignFont", Setter="SetMotionDesignFont", Category="Text", meta=(DisplayAfter="Text", AllowPrivateAccess="true"))
-	FAvaFont MotionDesignFont;
-
-	UPROPERTY(EditAnywhere, Getter="GetAlignment", Setter="SetAlignment", Category="Layout", meta=(DisplayAfter="VerticalAlignment", AllowPrivateAccess="true"))
-	FAvaTextAlignment Alignment;
-
-	UPROPERTY(EditAnywhere, Getter="GetColoringStyle", Setter="SetColoringStyle", Category="Style", meta=(DisplayAfter="Text", AllowPrivateAccess="true"))
-	EAvaTextColoringStyle ColoringStyle = EAvaTextColoringStyle::Solid;
-
-	UPROPERTY(EditAnywhere, Getter="GetColor", Setter="SetColor", Category="Style", meta=(EditCondition="ColoringStyle == EAvaTextColoringStyle::Solid", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	FLinearColor Color = FLinearColor::White;
-
-	UPROPERTY(EditAnywhere, Getter="GetExtrudeColor", Setter="SetExtrudeColor", Category="Style", meta=(EditCondition="ColoringStyle == EAvaTextColoringStyle::Solid && Extrude > 0.0", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	FLinearColor ExtrudeColor = FLinearColor::Gray;
-
-	UPROPERTY(EditAnywhere, Getter="GetBevelColor", Setter="SetBevelColor", Category="Style", meta=(EditCondition="ColoringStyle == EAvaTextColoringStyle::Solid && Bevel > 0.0", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	FLinearColor BevelColor = FLinearColor::Gray;
-
-	UPROPERTY(EditAnywhere, Getter="GetGradientSettings", Setter="SetGradientSettings", Category="Style", meta=(EditCondition="ColoringStyle == EAvaTextColoringStyle::Gradient", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	FAvaLinearGradientSettings GradientSettings;
-
-	UPROPERTY(EditAnywhere, Getter="GetMainTexture", Setter="SetMainTexture", Category="Style", meta=(EditCondition="ColoringStyle == EAvaTextColoringStyle::FromTexture", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	TObjectPtr<UTexture2D> MainTexture;
-
-	UPROPERTY(EditAnywhere, Getter="GetTiling", Setter="SetTiling", Category="Style", meta=(Delta=0.1, EditCondition="ColoringStyle == EAvaTextColoringStyle::FromTexture", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	FVector2D Tiling;
-
-	UPROPERTY(EditAnywhere, Getter="GetCustomMaterial", Setter="SetCustomMaterial", Category="Style", meta=(EditCondition="ColoringStyle == EAvaTextColoringStyle::CustomMaterial", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	TObjectPtr<UMaterialInterface> CustomMaterial = nullptr;
-	
-	UPROPERTY(EditAnywhere, Getter="GetTranslucencyStyle", Setter="SetTranslucencyStyle", Category="Style", meta=(EditCondition="ColoringStyle != EAvaTextColoringStyle::CustomMaterial", EditConditionHides,DisplayAfter="Text", AllowPrivateAccess="true"))
-	EAvaTextTranslucency TranslucencyStyle = EAvaTextTranslucency::None;
-
-	UPROPERTY(EditAnywhere, Getter="GetOpacity", Setter="SetOpacity", Category="Style", meta=(ClampMin=0.0, ClampMax=1.0, EditCondition="TranslucencyStyle == EAvaTextTranslucency::Translucent && ColoringStyle != EAvaTextColoringStyle::CustomMaterial", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	float Opacity = 0.75f;
-
-	UPROPERTY(EditAnywhere, Getter="GetMaskOrientation", Setter="SetMaskOrientation", Category="Style", meta=(EditCondition="TranslucencyStyle == EAvaTextTranslucency::GradientMask", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	EAvaMaterialMaskOrientation MaskOrientation = EAvaMaterialMaskOrientation::LeftRight;
-
-	UPROPERTY(EditAnywhere, Getter="GetMaskSmoothness", Setter="SetMaskSmoothness", Category="Style", meta=(ClampMin=0.0, ClampMax=1.0, EditCondition="TranslucencyStyle == EAvaTextTranslucency::GradientMask", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	float MaskSmoothness = 0.1f;
-
-	UPROPERTY(EditAnywhere, Getter="GetMaskOffset", Setter="SetMaskOffset", Category="Style", meta=(ClampMin=0.0, ClampMax=1.0, EditCondition="TranslucencyStyle == EAvaTextTranslucency::GradientMask", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	float MaskOffset = 0.5f;
-
-	UPROPERTY(EditAnywhere, Getter="GetMaskRotation", Setter="SetMaskRotation", Category="Style", meta=(ClampMin=0.0, ClampMax=1.0, EditCondition="MaskOrientation == EAvaMaterialMaskOrientation::Custom && TranslucencyStyle == EAvaTextTranslucency::GradientMask", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	float MaskRotation = 0.0f;
-
-	UPROPERTY(EditAnywhere, Getter="GetIsUnlit", Setter="SetIsUnlit", Category="Style", meta=(EditCondition="ColoringStyle != EAvaTextColoringStyle::Custom", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
-	bool bIsUnlit = true;
-
 protected:
 	void RefreshText3D();
 	void MarkRenderDirty();
@@ -272,34 +169,6 @@ protected:
 	/** Will schedule a Text Refresh to be executed by Core Ticker */
 	void ScheduleTextRefreshOnTicker();
 
-	TWeakObjectPtr<USceneComponent> ParentRootComponent;
-
-	/**
-	 * UMaterialInstanceDynamic references are transient.
-	 * We need to retrieve them from material when loading assets using this component, in order to avoid having to create the material again.
-	 * Refer to UAvaText3DComponent::RetrieveCurrentMaterials().
-	 *
-	 * Additionally, this helps when loading old versions of the AAvaTextActor, which was hosting all the custom properties now handled by this component (UAvaText3DComponent)
-	 */
-
-	UPROPERTY(Transient)
-	TObjectPtr<UMaterialInstanceDynamic> TextMaterialInstance;
-
-	UPROPERTY(Transient)
-	TObjectPtr<UMaterialInstanceDynamic> ExtrudeTextMaterialInstance;
-
-	UPROPERTY(Transient)
-	TObjectPtr<UMaterialInstanceDynamic> BevelTextMaterialInstance;
-
-	UPROPERTY(Transient)
-	TObjectPtr<UMaterialInstanceDynamic> TexturedTextMaterialInstance;
-
-	UPROPERTY(Transient)
-	TObjectPtr<UMaterialInstanceDynamic> GradientTextMaterialInstance;
-
-	FOnGetMaterialWithSettings OnGetMaterialWithSettingsDelegate;
-	UMaterialInstanceDynamic* GetMIDWithSettings(const UMaterialInterface* InPreviousMaterial, const FAvaTextMaterialSettings& InSettings);
-
 	void OnTextGeneratedActions();
 	void OnTextGeometryGenerated();
 
@@ -327,6 +196,7 @@ protected:
 	void RetrieveCurrentMaterials();
 
 	void RetrieveCurrentColoringStyle();
+
 	void RetrieveSolidMaterialInstances();
 	void RetrieveGradientMaterialInstance();
 	void RetrieveTexturedMaterialInstance();
@@ -336,7 +206,36 @@ protected:
 	void RetrieveMaskedMaterialParameters(const UMaterialInstanceDynamic* InMaterialInstance);
 	void RetrieveSimpleTranslucentMaterialParameters(const UMaterialInstanceDynamic* InMaterialInstance);
 
+	UMaterialInstanceDynamic* GetMIDWithSettings(const UMaterialInterface* InPreviousMaterial, const FAvaTextMaterialSettings& InSettings);
+
 	EAvaTextMaterialFeatures GetMaterialFeaturesFromProperties() const;
+
+	/**
+	 * UMaterialInstanceDynamic references are transient.
+	 * We need to retrieve them from material when loading assets using this component, in order to avoid having to create the material again.
+	 * Refer to UAvaText3DComponent::RetrieveCurrentMaterials().
+	 *
+	 * Additionally, this helps when loading old versions of the AAvaTextActor, which was hosting all the custom properties now handled by this component (UAvaText3DComponent)
+	 */
+
+	UPROPERTY(Transient)
+	TObjectPtr<UMaterialInstanceDynamic> TextMaterialInstance;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UMaterialInstanceDynamic> ExtrudeTextMaterialInstance;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UMaterialInstanceDynamic> BevelTextMaterialInstance;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UMaterialInstanceDynamic> TexturedTextMaterialInstance;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UMaterialInstanceDynamic> GradientTextMaterialInstance;
+
+	TWeakObjectPtr<USceneComponent> ParentRootComponent;
+
+	FOnGetMaterialWithSettings OnGetMaterialWithSettingsDelegate;
 
 	/** Used to filter which refreshes need be performed when calling RefreshText3D */
 	EAvaTextRefreshReason TextRefreshReason;
@@ -349,6 +248,107 @@ protected:
 	FTSTicker::FDelegateHandle TickerHandle;
 
 private:
+#if WITH_EDITOR
+	static void RegisterOnPropertyChangeFunctions();
+#endif
+
+	void RefreshOpacity() const;
+	void RefreshColor() const;
+	void RefreshExtrudeColor() const;
+	void RefreshBevelColor() const;
+
+	void RefreshAlignment();
+	void RefreshMotionDesignFont();
+	void RefreshHorizontalAlignment();
+	void RefreshVerticalAlignment();
+	void RefreshUnlit();
+	void RefreshMasked();
+	void RefreshGeometry();
+	void RefreshLayout();
+	void RefreshGradientValues();
+	AVALANCHETEXT_API void RefreshGradient();
+	void RefreshMaskValues();
+	void RefreshMask();
+	void RefreshMaterialGeometryParameterValues();
+
+	void RefreshFormatting();
+
+	void SetMaterialGeometryParameterValues(UMaterialInstanceDynamic* InMaterial, const FVector& TextScaleFactor) const;
+	void SetMaskedMaterialValues(UMaterialInstanceDynamic* InMaterial);
+
+	//~ Begin UObject
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	//~ End UObject
+#endif
+
+	//~ Begin USceneComponent
+#if WITH_EDITOR
+	virtual void PostEditComponentMove(bool bFinished) override;
+#endif
+	virtual void OnUpdateTransform(EUpdateTransformFlags UpdateTransformFlags, ETeleportType Teleport) override;
+	virtual void OnAttachmentChanged() override;
+	virtual void OnRegister() override;
+	//~ End USceneComponent
+
+	//~ Begin UText3DComponent
+	virtual void FormatText(FText& InOutText) const override;
+	//~ End UText3DComponent
+
+	UPROPERTY(EditAnywhere, Getter="IsEnforcingUpperCase", Setter="SetEnforceUpperCase", Category="Text", meta=(DisplayAfter="Text", AllowPrivateAccess="true"))
+	bool bEnforceUpperCase = false;
+
+	UPROPERTY(EditAnywhere, Getter="GetMotionDesignFont", Setter="SetMotionDesignFont", Category="Text", meta=(DisplayAfter="Text", AllowPrivateAccess="true"))
+	FAvaFont MotionDesignFont;
+
+	UPROPERTY(EditAnywhere, Getter="GetAlignment", Setter="SetAlignment", Category="Layout", meta=(DisplayAfter="VerticalAlignment", AllowPrivateAccess="true"))
+	FAvaTextAlignment Alignment;
+
+	UPROPERTY(EditAnywhere, Getter="GetColoringStyle", Setter="SetColoringStyle", Category="Style", meta=(DisplayAfter="Text", AllowPrivateAccess="true"))
+	EAvaTextColoringStyle ColoringStyle = EAvaTextColoringStyle::Solid;
+
+	UPROPERTY(EditAnywhere, Getter="GetColor", Setter="SetColor", Category="Style", meta=(EditCondition="ColoringStyle == EAvaTextColoringStyle::Solid", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	FLinearColor Color = FLinearColor::White;
+
+	UPROPERTY(EditAnywhere, Getter="GetExtrudeColor", Setter="SetExtrudeColor", Category="Style", meta=(EditCondition="ColoringStyle == EAvaTextColoringStyle::Solid && Extrude > 0.0", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	FLinearColor ExtrudeColor = FLinearColor::Gray;
+
+	UPROPERTY(EditAnywhere, Getter="GetBevelColor", Setter="SetBevelColor", Category="Style", meta=(EditCondition="ColoringStyle == EAvaTextColoringStyle::Solid && Bevel > 0.0", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	FLinearColor BevelColor = FLinearColor::Gray;
+
+	UPROPERTY(EditAnywhere, Getter="GetGradientSettings", Setter="SetGradientSettings", Category="Style", meta=(EditCondition="ColoringStyle == EAvaTextColoringStyle::Gradient", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	FAvaLinearGradientSettings GradientSettings;
+
+	UPROPERTY(EditAnywhere, Getter="GetMainTexture", Setter="SetMainTexture", Category="Style", meta=(EditCondition="ColoringStyle == EAvaTextColoringStyle::FromTexture", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	TObjectPtr<UTexture2D> MainTexture;
+
+	UPROPERTY(EditAnywhere, Getter="GetTiling", Setter="SetTiling", Category="Style", meta=(Delta=0.1, EditCondition="ColoringStyle == EAvaTextColoringStyle::FromTexture", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	FVector2D Tiling;
+
+	UPROPERTY(EditAnywhere, Getter="GetCustomMaterial", Setter="SetCustomMaterial", Category="Style", meta=(EditCondition="ColoringStyle == EAvaTextColoringStyle::CustomMaterial", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	TObjectPtr<UMaterialInterface> CustomMaterial = nullptr;
+
+	UPROPERTY(EditAnywhere, Getter="GetTranslucencyStyle", Setter="SetTranslucencyStyle", Category="Style", meta=(EditCondition="ColoringStyle != EAvaTextColoringStyle::CustomMaterial", EditConditionHides,DisplayAfter="Text", AllowPrivateAccess="true"))
+	EAvaTextTranslucency TranslucencyStyle = EAvaTextTranslucency::None;
+
+	UPROPERTY(EditAnywhere, Getter="GetOpacity", Setter="SetOpacity", Category="Style", meta=(ClampMin=0.0, ClampMax=1.0, EditCondition="TranslucencyStyle == EAvaTextTranslucency::Translucent && ColoringStyle != EAvaTextColoringStyle::CustomMaterial", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	float Opacity = 0.75f;
+
+	UPROPERTY(EditAnywhere, Getter="GetMaskOrientation", Setter="SetMaskOrientation", Category="Style", meta=(EditCondition="TranslucencyStyle == EAvaTextTranslucency::GradientMask", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	EAvaMaterialMaskOrientation MaskOrientation = EAvaMaterialMaskOrientation::LeftRight;
+
+	UPROPERTY(EditAnywhere, Getter="GetMaskSmoothness", Setter="SetMaskSmoothness", Category="Style", meta=(ClampMin=0.0, ClampMax=1.0, EditCondition="TranslucencyStyle == EAvaTextTranslucency::GradientMask", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	float MaskSmoothness = 0.1f;
+
+	UPROPERTY(EditAnywhere, Getter="GetMaskOffset", Setter="SetMaskOffset", Category="Style", meta=(ClampMin=0.0, ClampMax=1.0, EditCondition="TranslucencyStyle == EAvaTextTranslucency::GradientMask", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	float MaskOffset = 0.5f;
+
+	UPROPERTY(EditAnywhere, Getter="GetMaskRotation", Setter="SetMaskRotation", Category="Style", meta=(ClampMin=0.0, ClampMax=1.0, EditCondition="MaskOrientation == EAvaMaterialMaskOrientation::Custom && TranslucencyStyle == EAvaTextTranslucency::GradientMask", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	float MaskRotation = 0.0f;
+
+	UPROPERTY(EditAnywhere, Getter="GetIsUnlit", Setter="SetIsUnlit", Category="Style", meta=(EditCondition="ColoringStyle != EAvaTextColoringStyle::Custom", EditConditionHides, DisplayAfter="Text", AllowPrivateAccess="true"))
+	bool bIsUnlit = true;
+
 #if WITH_EDITOR
 	static inline TMap<FName, TFunction<void(UAvaText3DComponent*)>> PropertyChangeFunctions;
 #endif
