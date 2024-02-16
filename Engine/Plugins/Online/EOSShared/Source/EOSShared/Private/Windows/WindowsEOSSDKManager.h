5f6b6467657fcd972372a6683037b65582760714
jake niman <jake.niman@epicgames.com>
2024-02-16 16:01:11 -0500

    Remove reference to change not for release in 5.4
    [CL 31579322 by jake niman in 5.4 branch]

--- Engine/Plugins/Online/EOSShared/Source/EOSShared/Private/Windows/WindowsEOSSDKManager.h
+++ Engine/Plugins/Online/EOSShared/Source/EOSShared/Private/Windows/WindowsEOSSDKManager.h
@@ -21,9 +21,6 @@ protected:
 
 
 	EOS_IntegratedPlatform_Steam_Options PlatformSteamOptions;
-
-public: 
-	virtual FString GetCacheDirBase() const override;
 };
 
 using FPlatformEOSSDKManager = FWindowsEOSSDKManager;
