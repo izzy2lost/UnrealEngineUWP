e4e4dc953f062393e4541f3966fcd6bb9b4079a1
aaron mcleran <Aaron.McLeran@epicgames.com>
2024-02-16 12:24:58 -0500

    Fixing live attenuation updates.
    Made the max distance value use the attenuation setting asset and not use the cached value.
    
    #jira UE-197840
    #rb Jimmy.Smith
    #fyi sondra.moyls
    
    [CL 31569851 by aaron mcleran in 5.4 branch]

--- Engine/Source/Runtime/Engine/Private/AudioDevice.cpp
+++ Engine/Source/Runtime/Engine/Private/AudioDevice.cpp
@@ -6025,7 +6025,7 @@ bool FAudioDevice::SoundIsAudible(const FActiveSound& NewActiveSound)
 		return true;
 	}
 
-	const float ApparentMaxDistance = NewActiveSound.MaxDistance * NewActiveSound.FocusData.DistanceScale;
+	const float ApparentMaxDistance = NewActiveSound.GetMaxDistance() * NewActiveSound.FocusData.DistanceScale;
 	if (LocationIsAudible(NewActiveSound.Transform.GetLocation(), ApparentMaxDistance))
 	{
 		return true;
@@ -6398,8 +6398,6 @@ void FAudioDevice::PlaySoundAtLocation(USoundBase* Sound, UWorld* World, float V
 			NewActiveSound.FocusData.PriorityScale = AttenuationSettingsToApply->GetFocusPriorityScale(FocusSettings, FocusFactor);
 			NewActiveSound.FocusData.DistanceScale = AttenuationSettingsToApply->GetFocusDistanceScale(FocusSettings, FocusFactor);
 		}
-
-		NewActiveSound.MaxDistance = MaxDistance;
 
 		if (Concurrency)
 		{
