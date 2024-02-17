ed5e35b42c19d9ff8f60fadd3801c40fbcdc170c
eric knapik <Eric.Knapik@epicgames.com>
2024-02-17 10:13:10 -0500

    Add validator so nonGameFeaturePlugins do not depend on GameFeaturePlugins
    #presubmit 65cfea1b05690e8376a10025
    #rb will.brown
    
    [CL 31600108 by eric knapik in ue5-main branch]

--- Engine/Plugins/Runtime/GameFeatures/Source/GameFeaturesEditor/GameFeaturesEditor.Build.cs
+++ Engine/Plugins/Runtime/GameFeatures/Source/GameFeaturesEditor/GameFeaturesEditor.Build.cs
@@ -20,6 +20,7 @@ namespace UnrealBuildTool.Rules
 					"AssetTools",
 					"AssetRegistry",
 					"DataLayerEditor",
+					"DataValidation",
 					"DeveloperSettings",
 					"Engine",
 					"ModularGameplay",
