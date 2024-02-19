091cb1a923afbdccef4d98b906b34ba7e21671f7
max chen <Max.Chen@epicgames.com>
2024-02-19 15:28:33 -0500

    Sequencer: Back out unnecessary change that removes spaces and makes a unique object name because it makes the spawnable no longer match the spawnable name in the level sequence.
    #jira UE-207283
    #rb andrew.rodham
    #rnx
    
    [CL 31622118 by max chen in ue5-main branch]

--- Engine/Source/Runtime/LevelSequence/Private/LevelSequenceActorSpawner.cpp
+++ Engine/Source/Runtime/LevelSequence/Private/LevelSequenceActorSpawner.cpp
@@ -143,8 +143,7 @@ UObject* FLevelSequenceActorSpawner::SpawnObject(FMovieSceneSpawnable& Spawnable
 	// Spawn the puppet actor
 	FActorSpawnParameters SpawnInfo;
 	{
-		SpawnInfo.Name = *SpawnName.ToString().Replace(TEXT(" "), TEXT("_"));
-		SpawnInfo.Name = MakeUniqueObjectName(WorldContext->PersistentLevel, ObjectTemplate->GetClass(), SpawnInfo.Name);
+		SpawnInfo.Name = SpawnName;
 		SpawnInfo.ObjectFlags = ObjectFlags;
 		SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 		// @todo: Spawning with a non-CDO template is fraught with issues
