82c961e519706d6cb9919df3bd9814e65563bbe9
patrick boutot <Patrick.Boutot@epicgames.com>
2024-02-19 11:53:46 -0500

    MVVM: Add the UserWidget to the message log. The UserWidget is a link that the user can click on. #jira UE-164353 #rb editor-ui-systems
    [CL 31613893 by patrick boutot in ue5-main branch]

--- Engine/Plugins/Runtime/ModelViewViewModel/Source/ModelViewViewModel/Public/View/MVVMViewClass.h
+++ Engine/Plugins/Runtime/ModelViewViewModel/Source/ModelViewViewModel/Public/View/MVVMViewClass.h
@@ -127,6 +127,7 @@ public:
 		bool bAddFlags = true;
 		bool bAddBindingId = true;
 		bool bAddBindingFields = true;
+		bool bAddSources = true;
 
 		MODELVIEWVIEWMODEL_API static FToStringArgs Short();
 		MODELVIEWVIEWMODEL_API static FToStringArgs All();
@@ -398,6 +399,7 @@ private:
 		HasTickBindings = 1 << 8,
 		IsViewModel = 1 << 9,
 		IsViewModelInstanceExposed = 1 << 10,
+		GlobalViewModelCollectionRetry = 1 << 11,
 	};
 
 	UPROPERTY(VisibleAnywhere, Category = "View")
