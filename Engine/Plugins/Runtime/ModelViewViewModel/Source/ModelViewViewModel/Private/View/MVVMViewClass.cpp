bc54fcf54c734c1c7cd2eb039b0a20ec83fe45bd
hilda cruz <hilda.cruz@epicgames.com>
2024-02-19 22:33:06 -0500

    [Backout] - CL31610257 [FYI] Patrick.Boutot #rnx Original CL Desc ----------------------------------------------------------------- MVVM: Add the UserWidget to the message log. The UserWidget is a link that the user can click on. #jira UE-164353 #rb editor-ui-systems
    [CL 31637723 by hilda cruz in ue5-main branch]

--- Engine/Plugins/Runtime/ModelViewViewModel/Source/ModelViewViewModel/Private/View/MVVMViewClass.cpp
+++ Engine/Plugins/Runtime/ModelViewViewModel/Source/ModelViewViewModel/Private/View/MVVMViewClass.cpp
@@ -372,9 +372,6 @@ FMVVMViewClass_Binding::FToStringArgs FMVVMViewClass_Binding::FToStringArgs::Sho
 	FToStringArgs Result;
 	Result.bUseDisplayName = false;
 	Result.bAddFlags = false;
-	Result.bAddBindingId = false;
-	Result.bAddBindingFields = true;
-	Result.bAddSources = false;
 	return Result;
 }
 
@@ -387,22 +384,12 @@ FString FMVVMViewClass_Binding::ToString(const UMVVMViewClass* ViewClass, FToStr
 {
 	TStringBuilder<1024> StringBuilder;
 
-	bool bAddNewLine = false;
-	auto AddNewLine = [&StringBuilder, &bAddNewLine]()
-		{
-			if (bAddNewLine)
-			{
-				StringBuilder << TEXT("\n    ");
-			}
-			bAddNewLine = true;
-		};
-
 #if WITH_EDITOR
 	if (Args.bAddBindingId)
 	{
-		AddNewLine();
 		StringBuilder << TEXT("BindingId: ");
 		EditorId.AppendString(StringBuilder);
+		StringBuilder << TEXT("\n    ");
 	}
 #endif
 
@@ -410,7 +397,6 @@ FString FMVVMViewClass_Binding::ToString(const UMVVMViewClass* ViewClass, FToStr
 	{
 		if (GetBinding().IsValid())
 		{
-			AddNewLine();
 			StringBuilder << TEXT("Binding: ");
 
 			FString SourceString;
@@ -489,23 +475,21 @@ FString FMVVMViewClass_Binding::ToString(const UMVVMViewClass* ViewClass, FToStr
 			{
 				StringBuilder << TEXT("()");
 			}
+
+			StringBuilder << TEXT("\n    ");
 		}
 		else
 		{
 			StringBuilder << TEXT("!!Invalid Binding Error!!");
+			StringBuilder << TEXT("\n    ");
 		}
 	}
 
-	if (Args.bAddSources)
-	{
-		AddNewLine();
-		StringBuilder << TEXT("Sources: 0x");
-		StringBuilder << FString::Printf(TEXT("%x"), SourceBitField);
-	}
+	StringBuilder << TEXT("Sources: 0x");
+	StringBuilder << FString::Printf(TEXT("%x"), SourceBitField);
 
 	if (Args.bAddFlags)
 	{
-		AddNewLine();
 		if ((Flags & (uint8)EFlags::OverrideExecuteMode) != 0)
 		{
 			StringBuilder << TEXT(", Mode: ");
