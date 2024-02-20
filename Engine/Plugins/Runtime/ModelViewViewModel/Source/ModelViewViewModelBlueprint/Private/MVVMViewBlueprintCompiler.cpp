07aaf3ea51957c841fb4d2957eef3b4e4716c849
bob tellez <Bob.Tellez@epicgames.com>
2024-02-20 00:06:07 -0500

    [Backout] - CL31568442 [FYI] Patrick.Boutot Original CL Desc ----------------------------------------------------------------- MVVM: Enable compilation warning when there are 2 bindings trying to change the same value. Since the order is not configurable that can lead to binding executing in different orders and do run in an unexpected behavior. #rb editor-ui-systems
    [CL 31638824 by bob tellez in ue5-main branch]

--- Engine/Plugins/Runtime/ModelViewViewModel/Source/ModelViewViewModelBlueprint/Private/MVVMViewBlueprintCompiler.cpp
+++ Engine/Plugins/Runtime/ModelViewViewModel/Source/ModelViewViewModelBlueprint/Private/MVVMViewBlueprintCompiler.cpp
@@ -1216,11 +1216,12 @@ void FMVVMViewBlueprintCompiler::CreateWriteFieldContexts(const FWidgetBlueprint
 					});
 				if (Found)
 				{
-					AddMessageForBinding(Binding
-						, FText::Format(LOCTEXT("PropertyPathAlreadyUsed", "The property path '{0}' is already used by another binding."), PropertyPathToText(NewSkeletonClass, BlueprintView.Get(), DestinationPropertyPath))
-						, EMessageType::Warning
-						, FMVVMBlueprintPinId()
-					);
+					// todo Temporary removing this message until the assets are fixed.
+					//AddMessageForBinding(Binding
+					//	, FText::Format(LOCTEXT("PropertyPathAlreadyUsed", "The property path '{0}' is already used by another binding."), PropertyPathToText(NewSkeletonClass, BlueprintView.Get(), DestinationPropertyPath))
+					//	, EMessageType::Warning
+					//	, FMVVMBlueprintPinId()
+					//);
 					ValidBinding->WritePath = *Found;
 					continue;
 				}
