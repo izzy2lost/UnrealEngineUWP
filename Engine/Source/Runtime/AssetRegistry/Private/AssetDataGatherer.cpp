68cf7f26039cc3bcdb559045de2ab5ada0b61730
alex kahn <alex.kahn@epicgames.com>
2024-02-20 18:35:08 -0500

    [Backout] - CL31650971 - TeamCity MCP Error #jira UE-207602 #rnx [FYI] Matt.Peters Original CL Desc ----------------------------------------------------------------- AssetRegistry: Downgrade error "FPathExistence failed to gather correct capitalization from disk" to verbose. It is a spurious error and does not indicate a problem. #jira UE-207481 #rnx #rb Zousar.Shaker
    [CL 31661636 by alex kahn in 5.4 branch]

--- Engine/Source/Runtime/AssetRegistry/Private/AssetDataGatherer.cpp
+++ Engine/Source/Runtime/AssetRegistry/Private/AssetDataGatherer.cpp
@@ -2339,7 +2339,7 @@ void FPathExistence::LoadExistenceData()
 		}
 		else
 		{
-			UE_LOG(LogAssetRegistry, Verbose,
+			UE_LOG(LogAssetRegistry, Error,
 				TEXT("FPathExistence failed to gather correct capitalization from disk for %s, because GetFilenameOnDisk returned a non-matching filename"),
 				*LocalAbsPath);
 		}
@@ -2363,7 +2363,7 @@ void FPathExistence::LoadExistenceData()
 			}
 			else
 			{
-				UE_LOG(LogAssetRegistry, Verbose,
+				UE_LOG(LogAssetRegistry, Error,
 					TEXT("FPathExistence failed to gather correct capitalization from disk for %s, because GetFilenameOnDisk returned a non-matching filename"),
 					*LocalAbsPath);
 			}
