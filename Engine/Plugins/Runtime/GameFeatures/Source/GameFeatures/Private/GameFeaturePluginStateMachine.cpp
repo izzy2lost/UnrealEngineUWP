ca13abd5f8a8e9b29854f640590d6238e963e399
justin marcus <Justin.Marcus@epicgames.com>
2024-02-20 11:41:26 -0500

    CIS fix - only the generic version of CountBits is constexpr
    [CL 31647589 by justin marcus in ue5-main branch]

--- Engine/Plugins/Runtime/GameFeatures/Source/GameFeatures/Private/GameFeaturePluginStateMachine.cpp
+++ Engine/Plugins/Runtime/GameFeatures/Source/GameFeatures/Private/GameFeaturePluginStateMachine.cpp
@@ -3438,7 +3438,8 @@ FString FInstallBundlePluginProtocolMetaData::ToString() const
 	});
 	ReturnedString.Append(BundlesList);
 
-	static_assert(FMath::CountBits(static_cast<uint64>(EGameFeatureURLOptions::All)) == 1, "Update this function to handle the newly added EGameFeatureInstallBundleProtocolOptions value!");
+	// Only the generic version of CountBits is constexpr...
+	static_assert(FGenericPlatformMath::CountBits(static_cast<uint64>(EGameFeatureURLOptions::All)) == 1, "Update this function to handle the newly added EGameFeatureInstallBundleProtocolOptions value!");
 
 	return ReturnedString;
 }
