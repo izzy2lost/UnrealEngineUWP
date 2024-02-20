e530194c592b0127737ee59f4bbfbe907a4fef2c
dzeligman <dzeligman@gmail.com>
2024-02-19 19:14:01 -0500

    PR #11492: Add UObject/CoreNet.h include to fix -Nopch build error
    #jira UE-207116
    
    [CL 31634057 by dzeligman in ue5-main branch]

--- Engine/Plugins/Compression/OodleNetwork/Source/Public/OodleNetworkHandlerComponent.h
+++ Engine/Plugins/Compression/OodleNetwork/Source/Public/OodleNetworkHandlerComponent.h
@@ -5,6 +5,7 @@
 // @todo #JohnB: Separate module-based header code, from other class implementations, so that you can setup the PCH.h file correctly
 
 #include "Net/Core/Analytics/NetAnalytics.h"
+#include "UObject/CoreNet.h"
 #include "PacketHandler.h"
 #include "OodleNetworkArchives.h"
 #include "OodleNetworkFaultHandler.h"
