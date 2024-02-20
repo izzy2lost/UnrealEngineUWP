bc7638b9f090adfd3db4d6da5fe9c6a6387d8706
jimmy andrews <Jimmy.Andrews@epicgames.com>
2024-02-20 17:29:13 -0500

    fix crash when a geometry collection selected by fracture mode is invalidated / becomes null
    #jira UE-207357
    
    #ushell-cherrypick of 31613415 by swarm
    #rb jimmy.andrews
    
    [CL 31659000 by jimmy andrews in ue5-main branch]

--- Engine/Plugins/Experimental/ChaosEditor/Source/FractureEditor/Private/FractureEditorModeToolkit.cpp
+++ Engine/Plugins/Experimental/ChaosEditor/Source/FractureEditor/Private/FractureEditorModeToolkit.cpp
@@ -1696,6 +1696,10 @@ void FFractureEditorModeToolkit::SetOutlinerComponents(const TArray<UGeometryCol
 	ComponentsToEdit.Reserve(InNewComponents.Num());
 	for (UGeometryCollectionComponent* Component : InNewComponents)
 	{
+		if (!Component)
+		{
+			continue;
+		}
 		FGeometryCollectionEdit RestCollection = Component->EditRestCollection(GeometryCollection::EEditUpdate::None);
 		UGeometryCollection* FracturedGeometryCollection = RestCollection.GetRestCollection();
 
