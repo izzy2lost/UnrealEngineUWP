3e39ff37688092811c0f15e727183bbfb0e52ea1
zach bethel <zach.bethel@epicgames.com>
2024-02-21 03:51:20 -0500

    Wrapped particle system world space uniform buffer update in a mutex to fix race condition when multiple views are processing the same particle system.
    #rb luke.thatcher
    #localization none
    #tests Lyra
    
    [CL 31676710 by zach bethel in 5.4 branch]

--- Engine/Source/Runtime/Engine/Private/Particles/ParticleSystemRender.cpp
+++ Engine/Source/Runtime/Engine/Private/Particles/ParticleSystemRender.cpp
@@ -6895,6 +6895,8 @@ void FParticleSystemSceneProxy::UpdateWorldSpacePrimitiveUniformBuffer(FRHIComma
 		NewWorldSpaceUBHash = FCrc::MemCrc32(LocalCustomPrimitiveData->Data.GetData(), LocalCustomPrimitiveData->Data.Num() * LocalCustomPrimitiveData->Data.GetTypeSize());
 	}
 
+	UE::TScopeLock Lock(WorldSpacePrimitiveUniformBufferMutex);
+
 	const bool bNeedsInit = !WorldSpacePrimitiveUniformBuffer.IsInitialized();
 
 	if (bNeedsInit || (WorldSpaceUBHash != NewWorldSpaceUBHash))
