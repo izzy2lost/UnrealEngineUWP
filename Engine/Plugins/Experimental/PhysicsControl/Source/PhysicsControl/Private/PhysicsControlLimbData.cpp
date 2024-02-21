3d7fe5373c02399b0d6303a7ff84cb9a6c76a842
danny chapman <danny.chapman@epicgames.com>
2024-02-21 09:32:43 -0500

    Trivial typo fix - initialising RigidBodyWithControl controls
    [CL 31680465 by danny chapman in ue5-main branch]

--- Engine/Plugins/Experimental/PhysicsControl/Source/PhysicsControl/Private/PhysicsControlLimbData.cpp
+++ Engine/Plugins/Experimental/PhysicsControl/Source/PhysicsControl/Private/PhysicsControlLimbData.cpp
@@ -7,7 +7,7 @@ FPhysicsControlCharacterSetupData& FPhysicsControlCharacterSetupData::operator+=
 	const FPhysicsControlCharacterSetupData& Other)
 {
 	LimbSetupData += Other.LimbSetupData;
-	DefaultWorldSpaceControlData = Other.DefaultParentSpaceControlData;
+	DefaultWorldSpaceControlData = Other.DefaultWorldSpaceControlData;
 	DefaultParentSpaceControlData = Other.DefaultParentSpaceControlData;
 	DefaultBodyModifierData = Other.DefaultBodyModifierData;
 	return *this;
