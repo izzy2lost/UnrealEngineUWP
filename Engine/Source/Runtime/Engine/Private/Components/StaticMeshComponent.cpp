96b945c3a56e6394027e8a1b6491647cfa70a4e6
yoan stamant <Yoan.Stamant@epicgames.com>
2024-02-23 11:28:37 -0500

    [EntityFramework] added ai_navigation_relevant_component that handles static_mesh_component affecting navigatio data #rb Aris.Theophanidis, kurtis.schmidt #jira UE-186525 #okforversepublic
    [CL 31765421 by yoan stamant in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Components/StaticMeshComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/StaticMeshComponent.cpp
@@ -3166,15 +3166,7 @@ FBox UStaticMeshComponent::GetNavigationBounds() const
 	{
 		if (ensureMsgf(!Mesh->IsCompiling(), TEXT("%s is not considered relevant to navigation until associated mesh is compiled."), *GetFullName()))
 		{
-			if (const UNavCollisionBase* NavCollision = Mesh->GetNavCollision())
-			{
-				FBox NavBounds = NavCollision->GetBounds();
-				if (!NavBounds.IsValid)
-				{
-					NavBounds = Mesh->GetBounds().GetBox();
-				}
-				return NavBounds.TransformBy(GetComponentTransform());
-			}
+			return Mesh->GetNavigationBounds(GetComponentTransform());
 		}
 	}
 
