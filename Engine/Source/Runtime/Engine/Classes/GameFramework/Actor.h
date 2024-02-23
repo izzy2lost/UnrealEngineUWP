0132f1037aa6e8ae2eb38736f09a785134ffd141
jeremy moore <Jeremy.Moore@epicgames.com>
2024-02-23 18:25:55 -0500

    Add OnActorVisibilityChanged virtual function for UActorComponent. SetActorHiddenInGame() now ends up calling this. Override on GeometryCollectionComponent to update any custom renderer. #rb aidan.mcdu, cedric.caillaud
    [CL 31782941 by jeremy moore in ue5-main branch]

--- Engine/Source/Runtime/Engine/Classes/GameFramework/Actor.h
+++ Engine/Source/Runtime/Engine/Classes/GameFramework/Actor.h
@@ -3088,6 +3088,9 @@ public:
 	/** Update all components transforms */
 	ENGINE_API void UpdateComponentTransforms();
 
+	/** Update all components visibility state */
+	ENGINE_API void UpdateComponentVisibility();
+
 	/** Iterate over components array and call InitializeComponent, which happens once per actor */
 	ENGINE_API void InitializeComponents();
 
