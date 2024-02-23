c08bb4a2329af589b22d279eb05e4a1a3022a75d
kissSimple <kissSimple@users.noreply.github.com>
2024-02-23 15:48:32 -0500

    PR #11457: [GAS] fix up a comment about PostGameplayEffectExecute in AttributeSet.h
    #jira UE-206467
    
    [CL 31775364 by kissSimple in ue5-main branch]

--- Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Public/AttributeSet.h
+++ Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Public/AttributeSet.h
@@ -195,7 +195,7 @@ public:
 	virtual bool PreGameplayEffectExecute(struct FGameplayEffectModCallbackData &Data) { return true; }
 	
 	/**
-	 *	Called just before a GameplayEffect is executed to modify the base value of an attribute. No more changes can be made.
+	 *	Called just after a GameplayEffect is executed to modify the base value of an attribute. No more changes can be made.
 	 *	Note this is only called during an 'execute'. E.g., a modification to the 'base value' of an attribute. It is not called during an application of a GameplayEffect, such as a 5 ssecond +10 movement speed buff.
 	 */
 	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData &Data) { }
