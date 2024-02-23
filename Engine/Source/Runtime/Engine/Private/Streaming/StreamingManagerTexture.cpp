24b65393c60cfb58c23178e72eb419ef03847014
zach bethel <zach.bethel@epicgames.com>
2024-02-23 17:25:57 -0500

    Changed streaming asset task back to foreground priority to fix hitching on certain platforms.
    [CL 31780335 by zach bethel in 5.4 branch]

--- Engine/Source/Runtime/Engine/Private/Streaming/StreamingManagerTexture.cpp
+++ Engine/Source/Runtime/Engine/Private/Streaming/StreamingManagerTexture.cpp
@@ -1729,7 +1729,7 @@ public:
 	}
 	static FORCEINLINE ENamedThreads::Type GetDesiredThread()
 	{
-		return ENamedThreads::AnyBackgroundThreadNormalTask;
+		return ENamedThreads::AnyNormalThreadNormalTask;
 	}
 	static FORCEINLINE ESubsequentsMode::Type GetSubsequentsMode()
 	{
