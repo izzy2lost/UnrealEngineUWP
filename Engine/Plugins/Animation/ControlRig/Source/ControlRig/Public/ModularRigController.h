85a5f7e119b5df54508d0ef7b7953313606946b4
sara schvartzman <sara.schvartzman@epicgames.com>
2024-02-26 09:38:26 -0500

    Control Rig: Fix mirror modules sometimes crashing #jira UE-208028 #rb helge.mathee #rnx
    [CL 31802538 by sara schvartzman in 5.4 branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRig/Public/ModularRigController.h
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRig/Public/ModularRigController.h
@@ -31,7 +31,7 @@ class CONTROLRIG_API UModularRigController : public UObject
 	bool CanConnectConnectorToElement(const FRigElementKey& InConnectorKey, const FRigElementKey& InTargetKey, FText& OutErrorMessage);
 
 	UFUNCTION(BlueprintCallable, Category = "Control Rig | Modules")
-	bool ConnectConnectorToElement(const FRigElementKey& InConnectorKey, const FRigElementKey& InTargetKey, bool bSetupUndo = true, bool bAutoResolveOtherConnectors = true);
+	bool ConnectConnectorToElement(const FRigElementKey& InConnectorKey, const FRigElementKey& InTargetKey, bool bSetupUndo = true, bool bAutoResolveOtherConnectors = true, bool bCheckValidConnection = true);
 
 	UFUNCTION(BlueprintCallable, Category = "Control Rig | Modules")
 	bool DisconnectConnector(const FRigElementKey& InConnectorKey, bool bDisconnectSubModules = false, bool bSetupUndo = true);
