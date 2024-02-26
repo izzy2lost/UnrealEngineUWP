36327090c991db79e81f7ac0db3a6e539d2269ce
jeremy moore <Jeremy.Moore@epicgames.com>
2024-02-26 04:22:28 -0500

    #jira UE-207834 Add additional validation to prevent incompatible streaming mips with runtime virtual texture. This is extra defence against any code logic issues that could generate an incompatible SVT format. If that does happen it will generate D3D device errors when trying to copy the incompatible SVT format to the physical texture. #rb Laura.Hermanns
    [CL 31797104 by jeremy moore in 5.4 branch]

--- Engine/Source/Runtime/Engine/Private/Components/RuntimeVirtualTextureComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/RuntimeVirtualTextureComponent.cpp
@@ -14,6 +14,7 @@
 #include "UObject/UObjectIterator.h"
 #include "UObject/UnrealType.h"
 #include "VT/RuntimeVirtualTexture.h"
+#include "VT/VirtualTexture.h"
 #include "VT/VirtualTextureBuilder.h"
 #include "RenderUtils.h"
 #include "RHIGlobals.h"
@@ -241,6 +242,13 @@ bool URuntimeVirtualTextureComponent::IsStreamingLowMips(EShadingPath ShadingPat
 	return VirtualTexture != nullptr && StreamingTexture != nullptr && StreamingTexture->GetVirtualTexture(ShadingPath) != nullptr;
 }
 
+bool IsCompatibleFormat(URuntimeVirtualTexture const& RuntimeVirtualTexture, UVirtualTexture2D const& StreamingVirtualTexture)
+{
+	// During texture compilation we can't validate anything other than first layer, so restrict validation to that.
+	// This should catch any 99% of issues anyway. 
+	return (RuntimeVirtualTexture.GetLayerFormat(0) == StreamingVirtualTexture.GetPixelFormat(0));
+}
+
 bool URuntimeVirtualTextureComponent::IsStreamingTextureInvalid(EShadingPath ShadingPath) const
 {
 	checkf(IsActiveInWorld(), TEXT("This function should never be called for a world where we're inactive"));
@@ -249,19 +257,14 @@ bool URuntimeVirtualTextureComponent::IsStreamingTextureInvalid(EShadingPath Sha
 		VirtualTexture != nullptr && 
 		StreamingTexture != nullptr && 
 		StreamingTexture->GetVirtualTexture(ShadingPath) != nullptr && 
-		StreamingTexture->BuildHash != CalculateStreamingTextureSettingsHash();
+		(StreamingTexture->BuildHash != CalculateStreamingTextureSettingsHash() || !IsCompatibleFormat(*VirtualTexture, *StreamingTexture->GetVirtualTexture(ShadingPath)));
 }
 
 #if WITH_EDITOR
 
 bool URuntimeVirtualTextureComponent::IsStreamingTextureInvalid() const
 {
-	return 
-		VirtualTexture != nullptr && 
-		StreamingTexture != nullptr && 
-		StreamingTexture->GetVirtualTexture(EShadingPath::Deferred) != nullptr && 
-		StreamingTexture->GetVirtualTexture(EShadingPath::Mobile) != nullptr && 
-		StreamingTexture->BuildHash != CalculateStreamingTextureSettingsHash();
+	return IsStreamingTextureInvalid(EShadingPath::Mobile) || IsStreamingTextureInvalid(EShadingPath::Deferred);
 }
 
 FLinearColor URuntimeVirtualTextureComponent::GetStreamingMipsFixedColor() const 
