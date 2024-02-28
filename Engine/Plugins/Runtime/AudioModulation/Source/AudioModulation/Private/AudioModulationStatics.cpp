072e4e70c6ea42deeb87e097aae8cc4d9f728ba6
alfaroh corneyiii <alfaroh.corneyiii@epicgames.com>
2024-02-27 19:11:15 -0500

    Remove Deprecation-related issues that came from BP Modulation Destination #jira UE-198084 UE-205909 #rb Rob.Gay #lockdown julien.marchand
    [CL 31858246 by alfaroh corneyiii in ue5-main branch]

--- Engine/Plugins/Runtime/AudioModulation/Source/AudioModulation/Private/AudioModulationStatics.cpp
+++ Engine/Plugins/Runtime/AudioModulation/Source/AudioModulation/Private/AudioModulationStatics.cpp
@@ -352,11 +352,6 @@ UAudioModulationDestination* UAudioModulationStatics::CreateModulationDestinatio
 	return nullptr;
 }
 
-UAudioModulationDestination* UAudioModulationStatics::CreateModulationWatcher(UObject* WorldContextObject, FName Name, USoundModulatorBase* Modulator)
-{
-	return CreateModulationDestination(WorldContextObject, Name, Modulator);
-}
-
 void UAudioModulationStatics::DeactivateBus(const UObject* WorldContextObject, USoundControlBus* Bus)
 {
 	if (Bus)
