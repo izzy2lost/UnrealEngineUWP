26450fd972ef28c66c7dd582ff09676420e5ea5c
danny couture <Danny.Couture@epicgames.com>
2024-02-29 07:34:47 -0500

    [EditorPerf]   - Enable shader job cache in -game
    #rnx
    #rb Jason.Nadro
    
    [CL 31903909 by danny couture in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
+++ Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
@@ -248,7 +248,7 @@ bool IsShaderJobCacheDDCEnabled()
 #endif
 
 	// For now we only support the editor and not commandlets like the cooker.
-	if (GIsEditor && (!IsRunningCommandlet() || bForceAllowShaderCompilerJobCache))
+	if ((GIsEditor || IsRunningGame()) && (!IsRunningCommandlet() || bForceAllowShaderCompilerJobCache))
 	{
 		// job cache itself must be enabled first
 		return GShaderCompilerJobCache && CVarJobCacheDDC.GetValueOnAnyThread();
