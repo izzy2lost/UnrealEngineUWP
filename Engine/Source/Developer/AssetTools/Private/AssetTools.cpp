8c78548e84cd6b0dd912b377e530c68b3b594574
yafes sahin <yafes.sahin@epicgames.com>
2024-02-29 19:11:14 -0500

    - IAssetTools.h expose bPrompt and create bIgnoreDependencies - AssetTools.cpp add a case to only copy the selected assets and ignore the dependencies (usecase for automation) - Added BP example that implements the scripted asset action with the new migrationOption
    #rb Julien.StJean, Takashi.Kuribayashi
    [FYI] Michael.Lentine
    #tests
    
    [CL 31930249 by yafes sahin in ue5-main branch]

--- Engine/Source/Developer/AssetTools/Private/AssetTools.cpp
+++ Engine/Source/Developer/AssetTools/Private/AssetTools.cpp
@@ -4873,6 +4873,21 @@ void UAssetToolsImpl::PerformMigratePackages(TArray<FName> PackageNamesToMigrate
 				}
 			}
 
+
+			// Ignore dependencies and only migrate the given assets
+			if (Options.bIgnoreDependencies)
+			{
+				if (PackageNamesToMigrate.Contains(PackageName))
+				{
+					bShouldMigratePackage = true;
+				}
+				else
+				{
+					bShouldMigratePackage = false;
+				}
+			}
+
+
 			FilteredPackageNamesToMove.Add(PackageName);
 
 			if (bShouldMigratePackage)
