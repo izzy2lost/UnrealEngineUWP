8a58f1761ffd21826a9c6263fd47e3fd2a9ec3c2
halfdan ingvarsson <halfdan.ingvarsson@epicgames.com>
2024-03-01 16:07:39 -0500

    Skeletal Mesh: When converting from import model to mesh description, don't ditch all normals/tangents even if some are broken. Instead, re-create the broken normals/tangents as needed.
    #rb Alexis.Matte
    #rnx
    
    [CL 31955209 by halfdan ingvarsson in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/SkeletalMesh.cpp
+++ Engine/Source/Runtime/Engine/Private/SkeletalMesh.cpp
@@ -2585,7 +2585,7 @@ void USkeletalMesh::SaveLODImportedData(const int32 LODIndex, const FSkeletalMes
 	
 	FMeshDescription MeshDescription;
 
-	if (InMesh.GetMeshDescription(nullptr, MeshDescription))
+	if (InMesh.GetMeshDescription(nullptr, LODIndex, MeshDescription))
 	{
 		CreateMeshDescription(LODIndex, MoveTemp(MeshDescription));
 		CommitMeshDescription(LODIndex);
@@ -3277,7 +3277,7 @@ void USkeletalMesh::BeginPostLoadInternal(FSkinnedAssetPostLoadContext& Context)
 						}
 
 						FMeshDescription MeshDescription;
-						if (SkeletalMeshImportData.GetMeshDescription(this, MeshDescription))
+						if (SkeletalMeshImportData.GetMeshDescription(this, LODIndex, MeshDescription))
 						{
 							CreateMeshDescription(LODIndex, MoveTemp(MeshDescription));
 							CommitMeshDescription(LODIndex);
