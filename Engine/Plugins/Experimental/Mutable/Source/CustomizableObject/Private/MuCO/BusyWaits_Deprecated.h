040fded1393aa49bf9f1cb2215306b8a85a20704
gerard martin <gerard.martin@epicgames.com>
2024-03-01 04:57:50 -0500

    [Mutable] Moved GetImages and GetMesh to a new file in preparation for BusyWait changes.
    [REVIEW] [at]genis.sole, [at]alexei.lebedev
    #rnx
    #rb alexei.lebedev, genis.sole
    
    [CL 31939122 by gerard martin in ue5-main branch]

---
+++ Engine/Plugins/Experimental/Mutable/Source/CustomizableObject/Private/MuCO/BusyWaits_Deprecated.h
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+// ---------------------------------------------------------------
+// THIS FILE SHOULD ONLY CONTAIN CODE BEING DEPRECATED BY UE-207754
+// ---------------------------------------------------------------
+
+#include "Templates/SharedPointer.h"
+
+class FUpdateContextPrivate;
+namespace mu
+{
+	class Model;
+}
+
+
+namespace impl_deprecated
+{
+	// This runs in the mutable thread.
+	void Subtask_Mutable_GetImages(const TSharedRef<FUpdateContextPrivate>& OperationData);
+
+	// This runs in a worker thread.
+	void Task_Mutable_Update_GetImages(const TSharedRef<FUpdateContextPrivate>& OperationData);
+
+	// This runs in the mutable thread.
+	void Subtask_Mutable_BeginUpdate_GetMesh(const TSharedRef<FUpdateContextPrivate>& OperationData, TSharedPtr<mu::Model> Model);
+
+	// This runs in a worker thread.
+	void Task_Mutable_Update_GetMesh(const TSharedRef<FUpdateContextPrivate>& OperationData, const TSharedPtr<mu::Model>& Model);
+}
