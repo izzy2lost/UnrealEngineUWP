d1c3bf85c6e45302c30cc4b96d1bf68c5f1c880d
helge mathee <Helge.Mathee@epicgames.com>
2024-03-05 09:49:34 -0500

    ControlRig / RigVM: Make sure to only apply pin factory on matching assets
    #rb sara.schvartzman
    #jira UE-208900
    #rnx
    
    [CL 32019286 by helge mathee in 5.4 branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRigEditor/Private/Graph/ControlRigGraphPanelPinFactory.cpp
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRigEditor/Private/Graph/ControlRigGraphPanelPinFactory.cpp
@@ -25,10 +25,25 @@
 
 TSharedPtr<SGraphPin> FControlRigGraphPanelPinFactory::CreatePin(UEdGraphPin* InPin) const
 {
+	if(InPin == nullptr)
+	{
+		return nullptr;
+	}
+	
 	TSharedPtr<SGraphPin> InternalResult = CreatePin_Internal(InPin);
 	if(InternalResult.IsValid())
 	{
 		return InternalResult;
+	}
+
+	// if the graph we are looking at is not a control rig graph - let's not do this
+	if (const UEdGraphNode* OwningNode = InPin->GetOwningNode())
+	{
+		// only create pins within control rig graphs
+		if (Cast<UControlRigGraph>(OwningNode->GetGraph()) == nullptr)
+		{
+			return nullptr;
+		}
 	}
 	
 	TSharedPtr<SGraphPin> K2PinWidget = FNodeFactory::CreateK2PinWidget(InPin);
