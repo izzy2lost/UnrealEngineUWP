9e2d90ae37359d73c06ab8958d5ed8037ac8e114
tim smith <tim.smith@epicgames.com>
2024-03-05 07:55:24 -0500

    Fixed verse build phase to happen after new UObjects have been registered in the engine.  Wrong delegate was being used causing the build to happen prior to UObject definitions in some instances.
    #rb Markus.Breyer
    
    [CL 32016832 by tim smith in ue5-main branch]

--- Engine/Source/Runtime/Launch/Private/LaunchEngineLoop.cpp
+++ Engine/Source/Runtime/Launch/Private/LaunchEngineLoop.cpp
@@ -4398,10 +4398,8 @@ bool FEngineLoop::LoadCoreModules()
 {
 	// Always attempt to load CoreUObject. It requires additional pre-init which is called from its module's StartupModule method.
 #if WITH_COREUOBJECT
-#if USE_PER_MODULE_UOBJECT_BOOTSTRAP // otherwise do it later
 	// Always register the UObjects callback for VNI and general consistency with the callbacks ProcessNewlyLoadedUObjects calls.
 	RegisterProcessNewlyLoadedUObjects();
-#endif
 	return FModuleManager::Get().LoadModule(TEXT("CoreUObject")) != nullptr;
 #else
 	return true;
