9674e2cfda1fb85e4fb4ff3a238b7133ec24629a
richard malo <Richard.Malo@epicgames.com>
2024-03-06 12:09:44 -0500

    AsyncLoading2 instancing context fix : Don't pass down the request's instancing context to imported packages (FAsyncPackageDesc2::FromPackageImport). This change has no impact in editor as imports are already handled through InitializeLinkerLoadState. #rb Andrew.Rodham, JeanFrancois.Dube, Patrick.Enfedaque #tests Validated the fix in internal projects using instancing context in cooked builds + Validated the fix in in a cooked/staged gym. [FYI] Francis.Hurteau, danny.couture
    [CL 32057309 by richard malo in 5.4 branch]

--- Engine/Source/Runtime/CoreUObject/Private/Serialization/AsyncLoading2.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/Serialization/AsyncLoading2.cpp
@@ -625,7 +625,7 @@ struct FAsyncPackageDesc2
 			LOAD_None,
 			INDEX_NONE,
 #endif
-			ImportingPackageDesc.InstancingContext,
+			FLinkerInstancingContext(),
 			ImportedPackageId,
 			PackageIdToLoad,
 			UPackageName,
@@ -5205,6 +5205,7 @@ void FAsyncPackage2::ImportPackagesRecursiveInner(FAsyncLoadingThreadState2& Thr
 				else
 #endif
 				{
+					// TODO: Here we should probably also propagate the instancing context if the imported package is also instanced (similar to the call to InitializeLinkerLoadState done above)
 					AsyncLoadingThread.InitializeAsyncPackageFromPackageStore(ThreadState, &IoBatch, ImportedPackage, ImportedPackageEntry);
 				}
 				ImportedPackage->StartLoading(ThreadState, IoBatch);
