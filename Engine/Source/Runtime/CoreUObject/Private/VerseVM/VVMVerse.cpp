1c4b6ad2d315bfae45b31e08ad95f7ccc5d9668c
tim smith <tim.smith@epicgames.com>
2024-03-06 12:42:08 -0500

    Fixed VerseVM to always enable RTFM if it is being compiled in.  This is now required by recent changes in VerseVM.
    #rb Brandon.Schaefer, neil.henning
    
    [CL 32058850 by tim smith in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Private/VerseVM/VVMVerse.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/VerseVM/VVMVerse.cpp
@@ -2,6 +2,7 @@
 
 #if WITH_VERSE_VM || defined(__INTELLISENSE__)
 #include "VerseVM/VVMVerse.h"
+#include "AutoRTFM/AutoRTFM.h"
 #include "VerseVM/VVMEmergentTypeCreator.h"
 #include "VerseVM/VVMFalse.h"
 #include "VerseVM/VVMHeap.h"
@@ -16,6 +17,11 @@ void VerseVM::Startup()
 
 	// We initialize the global True/False ptr's at module startup to avoid checking if they are initialized elsewhere
 	Verse::VFalse::InitializeGlobals();
+
+	// VerseVM requires RTFM enabled
+#if UE_AUTORTFM || defined(__INTELLISENSE__)
+	AutoRTFM::ForTheRuntime::SetAutoRTFMRuntime(AutoRTFM::ForTheRuntime::EAutoRTFMEnabledState::AutoRTFM_Enabled);
+#endif
 }
 
 void VerseVM::Shutdown()
