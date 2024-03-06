a80fb4bef207dfeaa2c55c1891d55bbc477e4ea9
robert taylor <robert.taylor@epicgames.com>
2024-03-06 16:56:52 -0500

    Stopgap fix for HISM issue on server where instances are inadvertently cleared as the instance manager reports that it has changes.
    Original CL: https://p4-swarm.epicgames.net/reviews/32049078
    Created this CL for 29.01+ since 29.00 shelf is being held for a server build
    
    #rb Benn.Gallagher
    #rnx
    
    [CL 32068519 by robert taylor in ue5-main branch]

--- Engine/Source/Runtime/Engine/Public/InstancedStaticMesh/ISMInstanceDataManager.h
+++ Engine/Source/Runtime/Engine/Public/InstancedStaticMesh/ISMInstanceDataManager.h
@@ -99,11 +99,20 @@ public:
 
 	ENGINE_API bool HasAnyInstanceChanges() const;
 
+
+#if USE_NULL_RHI
+	/**
+	 * Stopgap fix for server, where the manager reporting that it has changes causes (inadvertent) clearing of the instances in the HISM.
+	 * TODO: Remove when proper fix (not clearing instances) is implemented.
+	 */
+	inline bool HasAnyChanges() const { return false;}
+#else
 	/** 
 	 * Returns true if there are explicitly tracked instance changes, or the state is not tracked (because no proxy has been created yet),
 	 * and the tracking state is not Disabled.
 	 */
 	inline bool HasAnyChanges() const { return GetState() != ETrackingState::Disabled && (GetState() != ETrackingState::Tracked || HasAnyInstanceChanges());}
+#endif
 	bool FlushChanges(FInstanceUpdateComponentDesc &&ComponentData, bool bNewPrimitiveProxy);
 
 	// 
