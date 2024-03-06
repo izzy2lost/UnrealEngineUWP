5751f3b14296ac5e5c7f42169181b07cd1905fb3
jason nadro <jason.nadro@epicgames.com>
2024-03-06 14:02:44 -0500

    [Shaders] - Discard non-stripped, original shader source to save on memory.  Saves ~2x of the memory tracked by the LLM tag ShaderCompiler in small projects.  For Lyra this saved ~600MB during a cook.
    - If we need the shader source (i.e. material stats in the editor) we still keep the data around.  The editor compiles shaders on demand and doesn't try to load these from the DDC so we don't have to worry about cache pollution.
    - Shader Debug Info also needs this information so we keep it around if developers are running with this enabled.
    - Added a helper function to the job input which determines if we need to store the original source code. NeedsOriginalShaderSource()
    
    #rb dan.elksnitis
    #tests Lyra Cook, Lyra Editor, UEFN Cooks
    [FYI] sebastian.schoner, Graeme.Thornton
    
    [CL 32061613 by jason nadro in ue5-main branch]

--- Engine/Source/Runtime/RenderCore/Public/ShaderPreprocessTypes.h
+++ Engine/Source/Runtime/RenderCore/Public/ShaderPreprocessTypes.h
@@ -265,7 +265,7 @@ private:
 
 	// Strips comments/whitespace/line directives from the preprocessed source, replacing the contents of PreprocessedSource
 	// and saving the original source in the OriginalPreprocessedSource member
-	void StripCode();
+	void StripCode(bool bCopyOriginalPreprocessdSource);
 
 	void RemapErrors(FShaderCompilerOutput& Output) const;
 
