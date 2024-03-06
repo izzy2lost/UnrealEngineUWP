2253798fa1322de98ea081ac20081bd9696f864a
bob tellez <Bob.Tellez@epicgames.com>
2024-03-05 23:53:17 -0500

    [Backout] - CL32040017 [FYI] aidan.mcdu Original CL Desc ----------------------------------------------------------------- [Lime][UEFN]
    Adding a validator that evaluates all actors with prevents overlaps and determines if their overlaps are acceptable for submission.
    
    Has two modes, one of which only evalutes same type building actor->actor collision, or the much more aggresive All Building types->Props.
    That proved too aggressive for the GDC timeline, so instead we are going with the Same Type collision only.
    
    Also enabling Physics for GC's so that any of this works in the first place.
    Also increasing the penetration threshold for UEFN so that we get a little more grace in calculating overlaps.
    
    
    #rb Cory.Kolek, Rex.Hill, Shaun.Kime
    
    [CL 32046583 by bob tellez in 5.4 branch]

--- Engine/Plugins/Editor/DataValidation/Source/DataValidation/Public/EditorValidatorBase.h
+++ Engine/Plugins/Editor/DataValidation/Source/DataValidation/Public/EditorValidatorBase.h
@@ -140,10 +140,6 @@ protected:
 	UPROPERTY(Config)
 	bool bIsConfigDisabled;
 
-	/* Whether we should also print out the source validator when printing validation errors.*/
-	UPROPERTY(EditAnywhere, Category = "Asset Validation", meta = (BlueprintProtected = "true"))
-	bool bOnlyPrintCustomMessage = false;
-
 private:
 	EDataValidationResult ValidationResult;
 	TArray<FText> AllWarnings;
