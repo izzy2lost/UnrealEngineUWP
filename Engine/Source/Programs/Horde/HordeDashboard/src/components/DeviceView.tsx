36f6c9e6553bdf537982798034c156cc76e76434
josh engebretson <Josh.Engebretson@epicgames.com>
2024-03-08 11:06:42 -0500

    Horde: Fix device view margin, default with some device platforms and pools #rnx
    [CL 32121302 by josh engebretson in ue5-main branch]

--- Engine/Source/Programs/Horde/HordeDashboard/src/components/DeviceView.tsx
+++ Engine/Source/Programs/Horde/HordeDashboard/src/components/DeviceView.tsx
@@ -1029,7 +1029,7 @@ export const DeviceView: React.FC = () => {
       <Stack horizontal>
          <div key={`windowsize_streamview_${windowSize.width}_${windowSize.height}`} style={{ width: vw / 2 - (1440 / 2), flexShrink: 0, backgroundColor: modeColors.background }} />
          <Stack tokens={{ childrenGap: 0 }} styles={{ root: { backgroundColor: modeColors.background, width: "100%" } }}>
-            <Stack style={{ maxWidth: 1440, paddingTop: 6, marginLeft: 4, height: 'calc(100vh - 8px)' }}>
+            <Stack style={{ maxWidth: 1440, paddingTop: 30, marginLeft: 4, height: 'calc(100vh - 8px)' }}>
                <Stack horizontal className={hordeClasses.raised}>
                   <Stack style={{ width: "100%", height: 'calc(100vh - 228px)' }} tokens={{ childrenGap: 18 }}>
                      <DevicePanel />
