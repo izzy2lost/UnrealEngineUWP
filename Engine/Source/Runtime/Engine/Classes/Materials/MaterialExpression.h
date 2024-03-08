a44bd0f9ce45cc34d9751bddcafa60b27d44cf98
massimo tristano <massimo.tristano@epicgames.com>
2024-03-08 12:03:41 -0500

    [Material Editor] Fixed crash by removing debugging data that was used to  printing missing expressions on material function post-load. #rb joe.kilner
    [CL 32122910 by massimo tristano in ue5-main branch]

--- Engine/Source/Runtime/Engine/Classes/Materials/MaterialExpression.h
+++ Engine/Source/Runtime/Engine/Classes/Materials/MaterialExpression.h
@@ -168,11 +168,6 @@ struct FMaterialExpressionCollection
 	UPROPERTY()
 	TArray<TObjectPtr<UMaterialExpression>> Expressions;
 	
-	/** Temporary debug info strings for each expression to help debugging the sporadic
-	 *  unexpected occurrence of nulls in Expressions on PostLoad() (UE-198712) */
-	UPROPERTY()
-	TArray<FString> DebugExpressionInfos;
-
 	/** Array of comments associated with this material; viewed in the material editor. */
 	UPROPERTY()
 	TArray<TObjectPtr<UMaterialExpressionComment>> EditorComments;
