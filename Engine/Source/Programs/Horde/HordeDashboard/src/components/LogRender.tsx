768cf98d1351c08f9a3031dd7887ae9249bf21ed
josh engebretson <Josh.Engebretson@epicgames.com>
2024-03-08 11:42:15 -0500

    Horde: Fix log source rendering #rnx
    [CL 32122348 by josh engebretson in ue5-main branch]

--- Engine/Source/Programs/Horde/HordeDashboard/src/components/LogRender.tsx
+++ Engine/Source/Programs/Horde/HordeDashboard/src/components/LogRender.tsx
@@ -217,7 +217,7 @@ const renderTags = (navigate: NavigateFunction, line: LogLine, lineNumber: numbe
             depotPath += `&line=${tagLine}`;
          }
 
-         return <a key={key} href={`ugs://timelapse?depotPath=${(depotPath)}`} onClick={(ev) => ev.stopPropagation()}><Highlight search={search ? search : ""} className={logStyle.logLine}>{record.relativePath ? record.relativePath : text}</Highlight></a>;
+         return <a key={key} href={`ugs://timelapse?depotPath=${(depotPath)}`} onClick={(ev) => ev.stopPropagation()}><Highlight search={search ? search : ""} className={logStyle.logLine}>{text}</Highlight></a>;
       } else if (tagType === TagType.Link) {
          
          return <a key={key} rel="noreferrer" href={record.target} onClick={(ev) => ev.stopPropagation()}><Highlight search={search ? search : ""} className={logStyle.logLine}>{text}</Highlight></a>;
