5a03bfe1eb2aa6f5cbfa790647f657546d5fd49c
jack cai <jack.cai@epicgames.com>
2024-03-08 17:25:28 -0500

    [SkeletalMesh & Deformer Graph] follow up fix to merge conflict for CL32132480
    hode link https://horde.devtools.epicgames.com/job/65eb886aa60fac6697945898?step=8862
    
    [CL 32134321 by jack cai in ue5-main branch]

--- Engine/Source/Runtime/Engine/Public/Rendering/SkeletalMeshHalfEdgeBuffer.h
+++ Engine/Source/Runtime/Engine/Public/Rendering/SkeletalMeshHalfEdgeBuffer.h
@@ -7,7 +7,7 @@
 
 class FSkeletalMeshLODRenderData;
 class FRHIShaderResourceView;
-struct FRHIResourceUpdateBatcher;
+class FRHIResourceReplaceBatcher;
 
 namespace SkeletalMeshHalfEdgeUtility
 {
@@ -30,8 +30,8 @@ public:
 
 	FRHIInfo CreateRHIBuffer(FRHICommandListBase& RHICmdList);
 
-	void InitRHIForStreaming(FRHIInfo RHIInfo, FRHIResourceUpdateBatcher& Batcher);
-	void ReleaseRHIForStreaming(FRHIResourceUpdateBatcher& Batcher);
+	void InitRHIForStreaming(FRHIInfo RHIInfo, FRHIResourceReplaceBatcher& Batcher);
+	void ReleaseRHIForStreaming(FRHIResourceReplaceBatcher& Batcher);
 	
 	void ENGINE_API InitRHI(FRHICommandListBase& RHICmdList) override;
 
