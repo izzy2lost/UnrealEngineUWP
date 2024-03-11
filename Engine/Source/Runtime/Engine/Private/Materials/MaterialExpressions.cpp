46ae104f08b729c2096700a861c1b92e20ccca1f
robert millar <robert.millar@epicgames.com>
2024-03-11 18:36:48 -0400

    Add material function name to error messages.
    [CL 32168473 by robert millar in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialExpressions.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialExpressions.cpp
@@ -16953,7 +16953,7 @@ int32 UMaterialExpressionMaterialFunctionCall::Compile(class FMaterialCompiler* 
 	{
 		if (!FunctionInputs[i].ExpressionInput)
 		{
-			return Compiler->Errorf(TEXT("Function call input with index %d is unset."), i);
+			return Compiler->Errorf(TEXT("Function (%s) call input with index %d is unset."), *MaterialFunction->GetPathName(), i);
 		}
 	}
 
@@ -16961,13 +16961,13 @@ int32 UMaterialExpressionMaterialFunctionCall::Compile(class FMaterialCompiler* 
 	{
 		if (!FunctionOutputs[i].ExpressionOutput)
 		{
-			return Compiler->Errorf(TEXT("Function call output with index %d is unset."), i);
+			return Compiler->Errorf(TEXT("Function (%s) call output with index %d is unset."), *MaterialFunction->GetPathName(), i);
 		}
 	}
 
 	if (!FunctionOutputs.IsValidIndex(OutputIndex))
 	{
-		return Compiler->Errorf(TEXT("Invalid function output"));
+		return Compiler->Errorf(TEXT("Invalid function (%s) output"), *MaterialFunction->GetPathName());
 	}
 
 	// Link the function's inputs into the caller graph before entering
