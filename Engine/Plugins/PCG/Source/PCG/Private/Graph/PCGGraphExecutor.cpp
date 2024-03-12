ba49a89698bc7550f2d2563b75d73f7d7fdad649
julien lheureux <julien.lheureux@epicgames.com>
2024-03-12 13:14:20 -0400

    Removed unneeded warnings/error logging when executing disabled nodes
    #rnx
    #jira UE-208719
    #rb adrien.logut, ryan.buehler
    [FYI] jeansebastien.guay
    
    [CL 32184341 by julien lheureux in ue5-main branch]

--- Engine/Plugins/PCG/Source/PCG/Private/Graph/PCGGraphExecutor.cpp
+++ Engine/Plugins/PCG/Source/PCG/Private/Graph/PCGGraphExecutor.cpp
@@ -1265,6 +1265,13 @@ void FPCGGraphExecutor::BuildTaskInput(const FPCGGraphTask& Task, FPCGDataCollec
 	auto LogDiscardedData = [&Task](const UPCGPin* InPin)
 	{
 #if !(UE_BUILD_SHIPPING || UE_BUILD_TEST) || USE_LOGGING_IN_SHIPPING
+		// Turn off eventual errors/warnings when the node is disabled, as this is irrelevant.
+		const bool bNodeIsDisabled = (Task.Context && Task.Context->GetOriginalSettings<UPCGSettings>() && !Task.Context->GetOriginalSettings<UPCGSettings>()->bEnabled);
+		if (bNodeIsDisabled)
+		{
+			return;
+		}
+
 		const FString Message = FString::Printf(
 			TEXT("[%s] %s - BuildTaskInput - too many data items arriving on single data pin '%s', only first data item will be used"),
 			(Task.SourceComponent.Get() && Task.SourceComponent->GetOwner()) ? *Task.SourceComponent->GetOwner()->GetName() : TEXT("MissingComponent"),
