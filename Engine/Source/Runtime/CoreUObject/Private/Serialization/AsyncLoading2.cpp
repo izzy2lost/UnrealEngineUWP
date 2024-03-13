a595a41259a80d4e475ed4042e760d027fb91ca0
matt breindel <matt.breindel@epicgames.com>
2024-03-13 17:56:59 -0400

    Improved surfacing of skipped package diagnostics. Messages will now show up in the editor Message Log as well as the output log.
    #jira UE-205029
    [REVIEW] [at]*eric.knapik [at]will.brown
    #rb Eric.Knapik
    
    [CL 32230932 by matt breindel in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Private/Serialization/AsyncLoading2.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/Serialization/AsyncLoading2.cpp
@@ -84,6 +84,7 @@
 #include "Serialization/ZenPackageHeader.h"
 #include "Trace/Trace.h"
 #include "Containers/Ticker.h"
+#include "Logging/MessageLog.h"
 
 #include <atomic>
 
@@ -5194,17 +5195,18 @@ void FAsyncPackage2::ImportPackagesRecursiveInner(FAsyncLoadingThreadState2& Thr
 						FPackageName::GetExplanationForUnavailablePackage(ImportedPackageNameToLoad, Explanation);
 						if (Explanation.Len())
 						{
-							UE_LOG(LogStreaming, Warning, TEXT("While trying to load package %s, a dependent package %s was not available. Additional explanatory information follows:\n%s"), 
-								*PackagePathToLoadString,
-								*ImportedPackageNameToLoadString,
-								Explanation.ToString());
+							FMessageLog("LoadErrors").Warning(FText::Format(NSLOCTEXT("Core", "AsyncLoading_SkippedPackage_Explanation", "While trying to load package {MissingPackage}, a dependent package {DependentPackage} was not available. Additional explanatory information follows:\n{Explanation}"),
+								FText::FromString(PackagePathToLoadString),
+								FText::FromString(ImportedPackageNameToLoadString),
+								FText::FromString(Explanation.ToString())));
 						}
 						else
 						{
-							UE_LOG(LogStreaming, Warning, TEXT("While trying to load package %s, a dependent package %s was not available. No additional explanation was available."),
-								*PackagePathToLoadString,
-								*ImportedPackageNameToLoadString);
+							FMessageLog("LoadErrors").Warning(FText::Format(NSLOCTEXT("Core", "AsyncLoading_SkippedPackage_NoExplanation", "While trying to load package {MissingPackage}, a dependent package {DependentPackage} was not available. No additional explanation was available."),
+								FText::FromString(PackagePathToLoadString),
+								FText::FromString(ImportedPackageNameToLoadString)));
 						}
+
 						return false;
 					});
 				}
