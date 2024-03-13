4db3c61f538b38e3a67afbf1b27e64e8315b17cd
ben marsh <Ben.Marsh@epicgames.com>
2024-03-13 08:54:47 -0400

    Horde: Add a -CleanOutput command line option to the bundle create command, which cleans the output directory before writing anything.
    #rnx
    
    [CL 32210427 by ben marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde/Commands/Bundles/BundleCreate.cs
+++ Engine/Source/Programs/Horde/Horde/Commands/Bundles/BundleCreate.cs
@@ -31,6 +31,10 @@ namespace Horde.Commands.Bundles
 		[Description("Filter for files to include, in P4 syntax (eg. Foo/...).")]
 		public string Filter { get; set; } = "...";
 
+		[CommandLine("-CleanOutput")]
+		[Description("Clean the output folder before writing any data")]
+		public bool CleanOutput { get; set; }
+
 		public BundleCreate(HttpStorageClientFactory storageClientFactory, BundleCache bundleCache, IOptions<CmdConfig> config)
 			: base(storageClientFactory, bundleCache, config)
 		{
@@ -38,6 +42,12 @@ namespace Horde.Commands.Bundles
 
 		public override async Task<int> ExecuteAsync(ILogger logger)
 		{
+			if (CleanOutput && File != null)
+			{
+				logger.LogInformation("Cleaning {Directory}...", File.Directory);
+				FileUtils.ForceDeleteDirectoryContents(File.Directory);
+			}
+
 			if (File != null)
 			{
 				using IStorageClient store = BundleStorageClient.CreateFromDirectory(File.Directory, BundleCache, logger);
