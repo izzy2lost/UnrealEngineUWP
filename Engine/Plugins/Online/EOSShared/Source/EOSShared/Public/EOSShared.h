f13d395b4aaea1a1df17e297dfa17af66a153610
chris varnsverry <Chris.Varnsverry@epicgames.com>
2024-03-14 12:56:46 -0400

    - Re-add missing LexFromString(EOS_EExternalAccountType, const TCHAR* InString) removed through bad merge
    #jira UE-208035
    [FYI] Alejandro.Aguilar
    
    [CL 32249306 by chris varnsverry in ue5-main branch]

--- Engine/Plugins/Online/EOSShared/Source/EOSShared/Public/EOSShared.h
+++ Engine/Plugins/Online/EOSShared/Source/EOSShared/Public/EOSShared.h
@@ -91,6 +91,7 @@ EOSSHARED_API const TCHAR* LexToString(const EOS_ENetworkStatus NetworkStatus);
 EOSSHARED_API const TCHAR* LexToString(const EOS_Presence_EStatus PresenceStatus);
 
 EOSSHARED_API bool LexFromString(EOS_EAuthScopeFlags& OutEnum, const FStringView& InString);
+EOSSHARED_API bool LexFromString(EOS_EExternalAccountType& OutEnum, const TCHAR* InString);
 EOSSHARED_API bool LexFromString(EOS_EIntegratedPlatformManagementFlags& OutEnum, const TCHAR* InString);
 EOSSHARED_API bool LexFromString(EOS_ELoginCredentialType& OutEnum, const TCHAR* InString);
 EOSSHARED_API bool LexFromString(EOS_EPacketReliability& OutEnum, const TCHAR* InString);
