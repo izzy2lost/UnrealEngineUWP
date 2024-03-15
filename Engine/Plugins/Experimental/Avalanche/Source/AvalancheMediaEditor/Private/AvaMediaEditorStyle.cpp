19e8ad71e211d4db10396575e6645329b8a83b80
juan portillo <juan.portillo@geotech.one>
2024-03-15 17:05:31 -0400

    Motion Design: Fix Media Style Toolbars showing invalid entries in Toolbars when collapsed
    #jira UE-209938
    
    [CL 32284353 by juan portillo in ue5-main branch]

--- Engine/Plugins/Experimental/Avalanche/Source/AvalancheMediaEditor/Private/AvaMediaEditorStyle.cpp
+++ Engine/Plugins/Experimental/Avalanche/Source/AvalancheMediaEditor/Private/AvaMediaEditorStyle.cpp
@@ -17,6 +17,8 @@
 FAvaMediaEditorStyle::FAvaMediaEditorStyle()
 	: FSlateStyleSet(TEXT("AvaMediaEditor"))
 {
+	SetParentStyleName(FAppStyle::GetAppStyleSetName());
+
 	const FVector2D Icon12x12(12.0f, 12.0f);
 	const FVector2D Icon16x16(16.0f, 16.0f);
 	const FVector2D Icon20x20(20.0f, 20.0f);
