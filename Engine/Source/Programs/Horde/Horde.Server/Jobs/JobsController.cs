6a847a233628407541e6eb4105cc016f432708d6
ben marsh <Ben.Marsh@epicgames.com>
2024-03-19 15:28:54 -0400

    Horde: Propagate artifact keys through to step requests.
    #rnx
    
    [CL 32335865 by ben marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Server/Jobs/JobsController.cs
+++ Engine/Source/Programs/Horde/Horde.Server/Jobs/JobsController.cs
@@ -507,7 +507,7 @@ namespace Horde.Server.Jobs
 						string? stepKey = artifact.Keys.FirstOrDefault(x => x.StartsWith(artifactStepKeyPrefix, StringComparison.Ordinal));
 						if (stepKey != null && JobStepId.TryParse(stepKey.Substring(artifactStepKeyPrefix.Length), out JobStepId jobStepId))
 						{
-							response.Artifacts.Add(new GetJobArtifactResponse(artifact.Id, artifact.Name, artifact.Type, artifact.Description, jobStepId));
+							response.Artifacts.Add(new GetJobArtifactResponse(artifact.Id, artifact.Name, artifact.Type, artifact.Description, artifact.Keys.ToList(), jobStepId));
 							addedArtifacts.Add((artifact.Name, jobStepId));
 						}
 					}
@@ -530,7 +530,7 @@ namespace Horde.Server.Jobs
 							IGraphArtifact? graphArtifact;
 							if (outputNameToArtifact.TryGetValue(outputName, out graphArtifact) && !addedArtifacts.Contains((graphArtifact.Name, step.Id)))
 							{
-								response.Artifacts.Add(new GetJobArtifactResponse(null, graphArtifact.Name, graphArtifact.Type, graphArtifact.Description, step.Id));
+								response.Artifacts.Add(new GetJobArtifactResponse(null, graphArtifact.Name, graphArtifact.Type, graphArtifact.Description, graphArtifact.Keys.ToList(), step.Id));
 							}
 						}
 					}
