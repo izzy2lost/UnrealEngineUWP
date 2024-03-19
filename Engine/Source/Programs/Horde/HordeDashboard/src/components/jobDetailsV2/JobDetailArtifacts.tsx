22f8c5e02fffa76bfa16ea2af2aa2d7d11caa6e1
josh engebretson <Josh.Engebretson@epicgames.com>
2024-03-19 09:27:28 -0400

    Horde: Update job artifacts status colors #rnx
    [CL 32323483 by josh engebretson in ue5-main branch]

--- Engine/Source/Programs/Horde/HordeDashboard/src/components/jobDetailsV2/JobDetailArtifacts.tsx
+++ Engine/Source/Programs/Horde/HordeDashboard/src/components/jobDetailsV2/JobDetailArtifacts.tsx
@@ -4,10 +4,11 @@ import { DetailsList, DetailsListLayoutMode, FontIcon, IColumn, SelectionMode, S
 import { observer } from 'mobx-react-lite';
 import React, { useEffect, useState } from 'react';
 import { GetJobArtifactResponse, JobStepState } from '../../backend/Api';
+import dashboard, { StatusColor } from '../../backend/Dashboard';
 import { ISideRailLink } from '../../base/components/SideRail';
 import { getStepETA, getStepFinishTime } from '../../base/utilities/timeUtils';
 import { getHordeStyling } from '../../styles/Styles';
-import { StepStatusIcon } from '../StatusIcon';
+import { getStepStatusColor } from '../../styles/colors';
 import { JobArtifactsModal } from '../artifacts/ArtifactsModal';
 import { JobDataView, JobDetailsV2 } from "./JobDetailsViewCommon";
 
@@ -192,8 +193,15 @@ export const JobArtifactsPanel: React.FC<{ jobDetails: JobDetailsV2 }> = observe
       }
 
       if (column.key === 'column_desc') {
+
+         let color = getStepStatusColor(step?.state, step?.outcome);
+         if (step?.state === JobStepState.Waiting || step?.state === JobStepState.Ready || step?.state === JobStepState.Running) {
+            const colors = dashboard.getStatusColors();
+            color = colors.get(StatusColor.Running)!;
+         }
+         
          return <Stack horizontal verticalAlign="center" verticalFill={true} style={{cursor: cursor}} onClick={() => { if (stepFinished) setSelected(item) }}>
-            {!!step && <StepStatusIcon step={step} style={{paddingTop: 2}} />}
+            {!!step && <FontIcon iconName="Square" style={{color: color, paddingTop: 2, fontSize: 13, paddingRight: 8}} />}
             <Text style={{ color: modeColors.text, fontFamily: "Horde Open Sans SemiBold" }}>{item.description ?? item.name}</Text>
          </Stack>
       }
