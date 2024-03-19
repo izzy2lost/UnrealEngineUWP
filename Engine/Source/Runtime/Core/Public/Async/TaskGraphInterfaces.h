b016769edc37640fb204cb134e00d7e03c6cdce0
danny couture <Danny.Couture@epicgames.com>
2024-03-19 05:47:35 -0400

    [TaskGraph]  - Add missing CORE_API export for WaitForAnyTaskCompleted and AnyTaskCompleted
    #rb kevin.macaulayvacher
    
    [CL 32320774 by danny couture in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Async/TaskGraphInterfaces.h
+++ Engine/Source/Runtime/Core/Public/Async/TaskGraphInterfaces.h
@@ -1381,10 +1381,10 @@ private:
 // Blocks the current thread until any of the given tasks is completed.
 // Is slightly more efficient than `AnyTaskCompleted()->Wait()` and supports timeout while `FGraphEvent::Wait()` doesn't.
 // Returns the index of the first completed task, or `INDEX_NONE` on timeout.
-int32 WaitForAnyTaskCompleted(const FGraphEventArray& GraphEvents, FTimespan Timeout = FTimespan::MaxValue());
+CORE_API int32 WaitForAnyTaskCompleted(const FGraphEventArray& GraphEvents, FTimespan Timeout = FTimespan::MaxValue());
 
 // Returns a graph event that gets completed as soon as any of the given tasks gets completed
-FGraphEventRef AnyTaskCompleted(const FGraphEventArray& GraphEvents);
+CORE_API FGraphEventRef AnyTaskCompleted(const FGraphEventArray& GraphEvents);
 
 /** 
  *	FReturnGraphTask is a task used to return flow control from a named thread back to the original caller of ProcessThreadUntilRequestReturn
