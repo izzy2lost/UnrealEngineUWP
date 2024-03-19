a3995544b8db663053ed45b1f43df7b36a5156f4
steve robb <Steve.Robb@epicgames.com>
2024-03-19 07:54:16 -0400

    Added UE_REQUIRES_EXPR for wrapping up C++20 requires expressions. Fixed up TSharedPtr and TUniquePtr to use this.
    #rb devin.doucette
    
    [CL 32322030 by steve robb in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Templates/Requires.h
+++ Engine/Source/Runtime/Core/Public/Templates/Requires.h
@@ -15,9 +15,38 @@
 		UE_REQUIRES(std::is_integral_v<T> && sizeof(U) <= 4)
 	>
 	void IntegralUpTo32Bit(T Lhs, U Rhs) {}
+
+	UE_REQUIRES_EXPR() wraps the effects of a requires expression, used to test the
+	compilability of an expression based on a deduced template parameter.  Usage:
+
+	template <typename Type>
+	struct TSmartPtr
+	{
+		// Only enable this constructor if the incoming pointer type is a UObject
+		// convertible to the smart pointer type:
+		template <
+			typename OtherType
+			UE_REQUIRES(
+				std::is_base_of_v<UObject, OtherType> &&
+				UE_REQUIRES_EXPR(ImplicitConv<Type*>((OtherType*)nullptr)))
+			)
+		>
+		explicit TSmartPtr(OtherType* OtherPtr)
+			: Ptr(Cast<Type>(OtherPtr))
+		{
+		}
+
+	private:
+		BaseType* Ptr;
+	};
+
+	Unlike a C++20 requires expression, UE_REQUIRES_EXPR() can only be used in
+	the body of a UE_REQUIRES() macro - standalone concept checks must still be
+	done via something like a TModels-type concept.
  -----------------------------------------------------------------------------*/
 #if __cplusplus < 202000
 	#define UE_REQUIRES(...) , std::enable_if_t<(__VA_ARGS__), int> = 0
+    #define UE_REQUIRES_EXPR(...) ((decltype((void)(__VA_ARGS__), 0))0 == 0)
 #else
 	namespace UE::Core::Private
 	{
@@ -27,6 +56,7 @@
 	}
 
 	#define UE_REQUIRES(...) > requires (!!(__VA_ARGS__)) && UE::Core::Private::BoolIdentityConcept<true
+    #define UE_REQUIRES_EXPR(...) requires { (__VA_ARGS__); }
 #endif
 
 // This should be regarded as deprecated - please use UE_REQUIRES instead.
