149cab890de947ac286cd03460e18a7c4d5b53fb
sebastien lussier <Sebastien.Lussier@epicgames.com>
2024-03-20 11:44:39 -0400

    ProxyLOD - Fix corner case where processing was skipped if input geometry had no volume. What we really want to test is for an area. #rb JeanFrancois.Dube
    [CL 32368160 by sebastien lussier in ue5-main branch]

--- Engine/Plugins/Editor/ProxyLODPlugin/Source/ProxyLOD/Private/ProxyLODPlugin.cpp
+++ Engine/Plugins/Editor/ProxyLODPlugin/Source/ProxyLOD/Private/ProxyLODPlugin.cpp
@@ -676,7 +676,7 @@ void FVoxelizeMeshMerging::ProxyLOD(FMeshDescriptionArrayAdapter& InSrcGeometryA
 		{
 			const auto& BBox = InSrcGeometryAdapter.GetBBox();
 
-			if (!BBox.hasVolume())
+			if (BBoxMajorAxisLength(BBox) == 0.f)
 			{
 				UE_LOG(LogProxyLODMeshReduction, Warning, TEXT("Empty bounding box for all static meshes input, no output mesh will be generated."));
 
