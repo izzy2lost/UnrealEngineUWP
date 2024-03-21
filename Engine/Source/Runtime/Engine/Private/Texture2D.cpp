c4fd40c4c43a4ca5ffa33a036424ff27e32d9135
simon orr <simon.orr@epicgames.com>
2024-03-20 23:34:31 -0400

    [Backout] - CL32373598 [FYI] simon.orr Original CL Desc ----------------------------------------------------------------- Add in some Texture information to the Content Browser as columns that can be searched and sorted. Max In Game X, Y, Z (where appropriate) as well as calculated resource size for memory. #rb Jamie.Dale #tests Loaded assets in Editor and viewed the columns. Also ensured other workflows weren't affected.
    [CL 32391746 by simon orr in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Texture2D.cpp
+++ Engine/Source/Runtime/Engine/Private/Texture2D.cpp
@@ -719,24 +719,6 @@ void UTexture2D::GetAssetRegistryTags(FAssetRegistryTagsContext Context) const
 	Context.AddTag(FAssetRegistryTag("MaxTextureSize", FString::FromInt(MaxTextureSize), FAssetRegistryTag::TT_Numerical));
 #endif
 
-#if WITH_EDITORONLY_DATA
-	// In game max bias and dimensions
-	const int32 NumMips = GetNumMips();
-
-	// We don't want to call GetNumMipsAllowed() as that causes a stall under some workflows, and we can get the data ourselves.
-	const int32 LODBiasNoCinematics = UDeviceProfileManager::Get().GetActiveProfile()->GetTextureLODSettings()->CalculateLODBias(this, false);
-	const int32 CookedMips = FMath::Clamp<int32>(NumMips - LODBiasNoCinematics, 1, GMaxTextureMipCount);
-
-	const int32 SurfaceWidth = static_cast<int32>(GetSurfaceWidth());
-	const int32 SurfaceHeight = static_cast<int32>(GetSurfaceHeight());
-	const int32 MaxResMipBias = NumMips - CookedMips;
-	const int32 MaxInGameWidth = SurfaceWidth ? FMath::Max(SurfaceWidth >> MaxResMipBias, 1) : 0;
-	const int32 MaxInGameHeight = SurfaceHeight ? FMath::Max(SurfaceHeight >> MaxResMipBias, 1) : 0;
-
-	Context.AddTag(FAssetRegistryTag("MaxInGameX", FString::FromInt(MaxInGameWidth), FAssetRegistryTag::TT_Numerical));
-	Context.AddTag(FAssetRegistryTag("MaxInGameY", FString::FromInt(MaxInGameHeight), FAssetRegistryTag::TT_Numerical));
-#endif
-
 	Super::GetAssetRegistryTags(Context);
 }
 
