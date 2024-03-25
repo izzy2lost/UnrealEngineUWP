24f58990979414061d83edd78da5f9514f587871
logan buchy <logan.buchy@epicgames.com>
2024-03-25 19:30:49 -0400

    Convert usage of MakeShareable to MakeShared
    #rb sebastian.arleryd
    
    [CL 32492006 by logan buchy in 5.4 branch]

--- Engine/Source/Editor/PropertyEditor/Private/PropertyNode.h
+++ Engine/Source/Editor/PropertyEditor/Private/PropertyNode.h
@@ -800,7 +800,7 @@ public:
 
 		if (CurrentNode != nullptr && CurrentNode->AsCategoryNode() != nullptr)
 		{
-			TSharedRef< FPropertyPath > NewPath = MakeShareable(new FPropertyPath());
+			TSharedRef< FPropertyPath > NewPath = MakeShared<FPropertyPath>();
 			return NewPath;
 		}
 
@@ -812,7 +812,7 @@ public:
 				NewPropInfo.Property = CurrentNode->GetProperty();
 				if (!NewPropInfo.Property.IsValid())
 				{
-					return MakeShareable(new FPropertyPath());
+					return MakeShared<FPropertyPath>();
 				}
 				NewPropInfo.ArrayIndex = CurrentNode->GetArrayIndex();
 
@@ -822,7 +822,7 @@ public:
 			CurrentNode = CurrentNode->GetParentNode();
 		}
 
-		TSharedRef< FPropertyPath > NewPath = MakeShareable(new FPropertyPath());
+		TSharedRef< FPropertyPath > NewPath = MakeShared<FPropertyPath>();
 
 		for (int PropertyIndex = Properties.Num() - 1; PropertyIndex >= 0; --PropertyIndex)
 		{
