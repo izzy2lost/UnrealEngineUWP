2059505debdd7ea994d5f5f474166fd4d9d1f958
noah presser <noah.presser@epicgames.com>
2024-03-25 13:38:54 -0400

    Add native implementation for water interactions in buoyancy subsystem
    [REVIEW] steven.barnett
    #rb Steven.Barnett
    
    #changelist validated
    #virtualized
    
    [CL 32475937 by noah presser in 5.4 branch]

--- Engine/Plugins/Experimental/Buoyancy/Source/Runtime/Public/BuoyancyEventInterface.h
+++ Engine/Plugins/Experimental/Buoyancy/Source/Runtime/Public/BuoyancyEventInterface.h
@@ -20,12 +20,46 @@ class BUOYANCY_API IBuoyancyEventInterface
 public:
 
 	/**
+	* Called when part of a submerged simulated actor first comes in
+	* contact with a water surface. This can be called for multiple parts
+	* of a complex body.
+	*/
+	virtual void OnSurfaceTouchBegin_Native(
+		UPrimitiveComponent* WaterComponent,
+		UPrimitiveComponent* SubmergedComponent,
+		float SubmergedVolume,
+		const FVector& SubmergedCenterOfMass,
+		const FVector& SubmergedVelocity) = 0;
+
+	/**
+	 * Called continually while objects maintain contact with a water surface.
+	 * May be called multiple times for different parts of an object.
+	 */
+	virtual void OnSurfaceTouching_Native(
+		UPrimitiveComponent* WaterComponent,
+		UPrimitiveComponent* SubmergedComponent,
+		float SubmergedVolume,
+		const FVector& SubmergedCenterOfMass,
+		const FVector& SubmergedVelocity) = 0;
+
+	/**
+	 * Called when a submerged body loses contact with all water surfaces. This
+	 * can result from total submersion or from coming completely out of water.
+	 */
+	virtual void OnSurfaceTouchEnd_Native(
+		UPrimitiveComponent* WaterComponent,
+		UPrimitiveComponent* SubmergedComponent,
+		float SubmergedVolume,
+		const FVector& SubmergedCenterOfMass,
+		const FVector& SubmergedVelocity) = 0;
+	
+	/**
 	 * Called when part of a submerged simulated actor first comes in
 	 * contact with a water surface. This can be called for multiple parts
 	 * of a complex body.
 	 */
 	UFUNCTION(BlueprintImplementableEvent, Category = WaterBody)
-	void  OnSurfaceTouchBegin(
+	void OnSurfaceTouchBegin(
 		class AWaterBody* WaterBodyActor,
 		UPrimitiveComponent* WaterComponent,
 		UPrimitiveComponent* SubmergedComponent,
@@ -38,7 +72,7 @@ public:
 	 * May be called multiple times for different parts of an object.
 	 */
 	UFUNCTION(BlueprintImplementableEvent, Category = WaterBody)
-	void  OnSurfaceTouching(
+	void OnSurfaceTouching(
 		class AWaterBody* WaterBodyActor,
 		UPrimitiveComponent* WaterComponent,
 		UPrimitiveComponent* SubmergedComponent,
@@ -51,7 +85,7 @@ public:
 	 * can result from total submersion or from coming completely out of water.
 	 */
 	UFUNCTION(BlueprintImplementableEvent, Category = WaterBody)
-	void  OnSurfaceTouchEnd(
+	void OnSurfaceTouchEnd(
 		class AWaterBody* WaterBodyActor,
 		UPrimitiveComponent* WaterComponent,
 		UPrimitiveComponent* SubmergedComponent,
