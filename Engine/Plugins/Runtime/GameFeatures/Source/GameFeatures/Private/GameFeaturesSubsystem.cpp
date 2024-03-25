7f11ae8bfcacf34899722f4b6283e7c876df29b9
will brown <will.brown@epicgames.com>
2024-03-25 18:26:10 -0400

    Added new function suite to return found plugins that are enabled. Updated the LoadBuiltInGameFeaturePlugins sort function to skip plugin dependencies that are not enabled for correctness however it does not impact the final EnabledPlugins list
    #rb Eric.Knapik
    
    [CL 32488936 by will brown in 5.4 branch]

--- Engine/Plugins/Runtime/GameFeatures/Source/GameFeatures/Private/GameFeaturesSubsystem.cpp
+++ Engine/Plugins/Runtime/GameFeatures/Source/GameFeatures/Private/GameFeaturesSubsystem.cpp
@@ -1864,7 +1864,7 @@ void UGameFeaturesSubsystem::LoadBuiltInGameFeaturePlugins(FBuiltInPluginAdditio
 		{
 			if (Dependency.bEnabled)
 			{
-				if (TSharedPtr<IPlugin> FoundPlugin = PluginManager.FindPlugin(Dependency.Name))
+				if (TSharedPtr<IPlugin> FoundPlugin = PluginManager.FindEnabledPlugin(Dependency.Name))
 				{
 					Dependencies.Add(FoundPlugin.ToSharedRef());
 				}
