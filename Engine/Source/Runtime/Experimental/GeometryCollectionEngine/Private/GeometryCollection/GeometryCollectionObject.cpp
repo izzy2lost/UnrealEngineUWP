bf1173c5a8cb20257893c1d5c108b833cf55f63d
cedric caillaud <cedric.caillaud@epicgames.com>
2024-03-25 18:07:07 -0400

    Geometry Collection : Fix potential crash in FillAutoInstanceMeshesInstancesIfNeeded if the index attribute is not fopund but the asset has auto instance meshes assigned
    #jira UE-207298
    
    [CL 32488040 by cedric caillaud in 5.4 branch]

--- Engine/Source/Runtime/Experimental/GeometryCollectionEngine/Private/GeometryCollection/GeometryCollectionObject.cpp
+++ Engine/Source/Runtime/Experimental/GeometryCollectionEngine/Private/GeometryCollection/GeometryCollectionObject.cpp
@@ -1285,19 +1285,25 @@ void UGeometryCollection::FillAutoInstanceMeshesInstancesIfNeeded()
 		}
 
 		const GeometryCollection::Facades::FCollectionInstancedMeshFacade InstancedMeshFacade(*GeometryCollection);
-
-		const int32 NumTransforms = GeometryCollection->Children.Num();
-		for (int32 TransformIndex = 0; TransformIndex < NumTransforms; TransformIndex++)
+		if (InstancedMeshFacade.IsValid())
 		{
-			// only applies to leaves nodes
-			if (GeometryCollection->Children[TransformIndex].Num() == 0)
+			const int32 NumTransforms = GeometryCollection->Children.Num();
+			for (int32 TransformIndex = 0; TransformIndex < NumTransforms; TransformIndex++)
 			{
-				const int32 AutoInstanceMeshIndex = InstancedMeshFacade.GetIndex(TransformIndex);
-				if (AutoInstanceMeshes.IsValidIndex(AutoInstanceMeshIndex))
+				// only applies to leaves nodes
+				if (GeometryCollection->Children[TransformIndex].Num() == 0)
 				{
-					AutoInstanceMeshes[AutoInstanceMeshIndex].NumInstances++;
+					const int32 AutoInstanceMeshIndex = InstancedMeshFacade.GetIndex(TransformIndex);
+					if (AutoInstanceMeshes.IsValidIndex(AutoInstanceMeshIndex))
+					{
+						AutoInstanceMeshes[AutoInstanceMeshIndex].NumInstances++;
+					}
 				}
 			}
+		}
+		else
+		{
+			UE_LOG(LogGeometryCollectionInternal, Warning, TEXT("[%s] Could not find AutoInstanceMeshIndex attribute but the asset as instanced meshes assigned, you may need to regenerate this asset"), *GetPathName());
 		}
 	}
 }
