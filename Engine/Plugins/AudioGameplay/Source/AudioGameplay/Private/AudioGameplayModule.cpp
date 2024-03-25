30198f1bb4c5186e0543b7a01af8bb5a50914cf9
igor dallavanzi <igor.dallavanzi@psyonix.com>
2024-03-25 19:57:29 -0400

    enable csv profiling for audio gameplay on non shipping builds
    [CL 32493188 by igor dallavanzi in 5.4 branch]

--- Engine/Plugins/AudioGameplay/Source/AudioGameplay/Private/AudioGameplayModule.cpp
+++ Engine/Plugins/AudioGameplay/Source/AudioGameplay/Private/AudioGameplayModule.cpp
@@ -13,6 +13,7 @@ DEFINE_LOG_CATEGORY(AudioGameplayLog);
 // This should only be defined here. Modules who wish to use this category should contain the line
 // 		CSV_DECLARE_CATEGORY_MODULE_EXTERN(AUDIOGAMEPLAY_API, AudioGameplay);
 //
+
 #if UE_BUILD_SHIPPING
 CSV_DEFINE_CATEGORY_MODULE(AUDIOGAMEPLAY_API, AudioGameplay, false);
 #else
