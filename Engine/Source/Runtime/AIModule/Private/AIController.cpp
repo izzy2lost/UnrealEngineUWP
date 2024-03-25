aa52c8b10387ae60338d2a60770fb94c3e55c329
louis thorpemonaghan <louis.thorpemonaghan@epicgames.com>
2024-03-25 17:58:11 -0400

    Added an additional option to SuggestProjectileVelocity, allowing a test with no results to return a velocity which will get the projectile as close to the target as possible. #rb panos.grigoropoulos
    [CL 32487447 by louis thorpemonaghan in 5.4 branch]

--- Engine/Source/Runtime/AIModule/Private/AIController.cpp
+++ Engine/Source/Runtime/AIModule/Private/AIController.cpp
@@ -1086,7 +1086,13 @@ bool AAIController::SuggestTossVelocity(FVector& OutTossVelocity, FVector Start,
 	float const GravityOverride = PhysicsVolume ? PhysicsVolume->GetGravityZ() : 0.f;
 	ESuggestProjVelocityTraceOption::Type const TraceOption = bOnlyTraceUp ? ESuggestProjVelocityTraceOption::OnlyTraceWhileAscending : ESuggestProjVelocityTraceOption::TraceFullPath;
 
-	return UGameplayStatics::SuggestProjectileVelocity(this, OutTossVelocity, Start, End, TossSpeed, bPreferHighArc, CollisionRadius, GravityOverride, TraceOption);
+	UGameplayStatics::FSuggestProjectileVelocityParameters VelocityParams = UGameplayStatics::FSuggestProjectileVelocityParameters(this, Start, End, TossSpeed);
+	VelocityParams.bFavorHighArc = bPreferHighArc;
+	VelocityParams.CollisionRadius = CollisionRadius;
+	VelocityParams.OverrideGravityZ = GravityOverride;
+	VelocityParams.TraceOption = TraceOption;
+
+	return UGameplayStatics::SuggestProjectileVelocity(VelocityParams, OutTossVelocity);
 }
 bool AAIController::PerformAction(UDEPRECATED_PawnAction& Action, EAIRequestPriority::Type Priority, UObject* const InInstigator /*= NULL*/)
 {
