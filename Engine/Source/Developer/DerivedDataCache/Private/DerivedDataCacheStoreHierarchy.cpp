6bddcafc0689a9bedbc0478e5ddff22ce1dd1f30
kevin macaulayvacher <kevin.macaulayvacher@epicgames.com>
2024-03-26 15:15:39 -0400

    Ignore cancelations when tracking DDC usage stats as our legacy stat tracking system doesn't represent cancellations and instead gives a false perception about how many cache misses are occurring. The cancellations do have an overhead, but in practice most cancellations are done before network traffic has begun and has very little cost, such that we aren't at a major lossby not tracking this time.
    #jira UE-206398
    #rb Zousar.Shaker
    [FYI] Francis.Hurteau
    
    [CL 32519384 by kevin macaulayvacher in ue5-main branch]

--- Engine/Source/Developer/DerivedDataCache/Private/DerivedDataCacheStoreHierarchy.cpp
+++ Engine/Source/Developer/DerivedDataCache/Private/DerivedDataCacheStoreHierarchy.cpp
@@ -1637,9 +1637,16 @@ void FCacheStoreHierarchy::RecordStats(FCacheBucket Bucket, ERequestType Type, E
 	using EStatType = FCallStats::EStatType;
 
 	const bool bIsInGameThread = IsInGameThread();
+	const bool bIsCanceled = Status == EStatus::Canceled;
 	const EHitOrMiss HitOrMiss = Status == EStatus::Ok ? EHitOrMiss::Hit : EHitOrMiss::Miss;
-	FCallStats& CallStats = (Op == ERequestOp::Put) ? BucketStats.PutStats : BucketStats.GetStats;
-	CallStats.Accumulate(HitOrMiss, EStatType::Counter, 1, bIsInGameThread);
+
+	// FCallStats is a legacy stat codepath that isn't aware of cancellations. In order to avoid skewing miss% rates
+	// by accumulating cancellation requests as misses, we simply ignore cancellation requests for stat tracking
+	if (!bIsCanceled)
+	{
+		FCallStats& CallStats = (Op == ERequestOp::Put) ? BucketStats.PutStats : BucketStats.GetStats;
+		CallStats.Accumulate(HitOrMiss, EStatType::Counter, 1, bIsInGameThread);
+	}
 #endif
 }
 
