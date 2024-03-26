d9b036e62c659cad39db4104a65358b9c7e174f3
camille gendreau <camille.gendreau@xa.epicgames.com>
2024-03-26 01:23:41 -0400

    [GFN][Engine] GFN Module Shutdown cleanup v2. * The module is now in charge of the lifespan of the GFNWrapper via the Initialize and Shutdown functions * Said functions have now been made static so they can be called by the wrapper's module * The singleton has been converted to a pointer that is created and destroyed in said functions. * Renamed IsInitialised to IsSdkInitialised to clarify its intent and use it as a gate to the gfnShutdownSdk function * IsRunningInGFN and its dependencies has been made static to gate anything that might somehow try using the singleton in external code.
    #rb bernard.lambert, robert.millar
    
    [CL 32497784 by camille gendreau in 5.4 branch]

--- Engine/Plugins/Runtime/Nvidia/GeForceNOWWrapper/Source/Private/GeForceNOWWrapper.cpp
+++ Engine/Plugins/Runtime/Nvidia/GeForceNOWWrapper/Source/Private/GeForceNOWWrapper.cpp
@@ -24,21 +24,16 @@ extern "C"
 THIRD_PARTY_INCLUDES_END
 UE_POP_MACRO("TEXT")
 
-GeForceNOWWrapper::GeForceNOWWrapper()
-	: bIsInitialized(false)
-{
+bool GeForceNOWWrapper::bIsSdkInitialized = false;
 
-}
+TOptional<bool> GeForceNOWWrapper::bIsRunningInCloud = TOptional<bool>();
 
-GeForceNOWWrapper::~GeForceNOWWrapper()
-{
-	Shutdown();
-}
+GeForceNOWWrapper* GeForceNOWWrapper::Singleton = nullptr;
 
 GeForceNOWWrapper& GeForceNOWWrapper::Get()
 {
-	static GeForceNOWWrapper Singleton;
-	return Singleton;
+	check(Singleton); //Crashing here means we're fetching the wrapper too soon.
+	return *(Singleton);
 }
 
 /*static*/ const FString GeForceNOWWrapper::GetGfnOsTypeString(GfnOsType OsType)
@@ -75,12 +70,12 @@ GeForceNOWWrapper& GeForceNOWWrapper::Get()
 }
 
 
-bool GeForceNOWWrapper::IsRunningInGFN()
+/*static*/ bool GeForceNOWWrapper::IsRunningInGFN()
 {
-	return IsRunningMockGFN() || IsInitialized() && IsRunningInCloud();
+	return IsRunningMockGFN() || IsSdkInitialized() && IsRunningInCloud();
 }
 
-bool GeForceNOWWrapper::IsRunningMockGFN() const
+/*static*/ bool GeForceNOWWrapper::IsRunningMockGFN()
 {
 #if !UE_BUILD_SHIPPING
 	static bool bIsMockGFN = FParse::Param(FCommandLine::Get(), TEXT("MockGFN"));
@@ -94,16 +89,21 @@ bool GeForceNOWWrapper::IsRunningMockGFN() const
 	return bIsMockGFN;
 }
 
-GfnRuntimeError GeForceNOWWrapper::Initialize()
+/*static*/ GfnRuntimeError GeForceNOWWrapper::Initialize()
 {
-	if (IsRunningMockGFN())
+	if (!Singleton)
 	{
-		bIsInitialized = true;
+		Singleton = new GeForceNOWWrapper();
+	}
+
+	if (bIsSdkInitialized)
+	{
 		return gfnSuccess;
 	}
 
-	if (bIsInitialized)
+	if (IsRunningMockGFN())
 	{
+		bIsSdkInitialized = true;
 		return gfnSuccess;
 	}
 
@@ -111,14 +111,14 @@ GfnRuntimeError GeForceNOWWrapper::Initialize()
 	FString GFNDllFullPath = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*GFNDllPath);
 	GFNDllFullPath.ReplaceInline(TEXT("/"), TEXT("\\"), ESearchCase::CaseSensitive);
 	const GfnRuntimeError ErrorCode = GfnInitializeSdkFromPathW(gfnDefaultLanguage, *GFNDllFullPath);
-	bIsInitialized = ErrorCode == gfnSuccess || ErrorCode == gfnInitSuccessClientOnly;
+	bIsSdkInitialized = ErrorCode == gfnSuccess || ErrorCode == gfnInitSuccessClientOnly;
 
 	return ErrorCode;
 }
 
 bool GeForceNOWWrapper::InitializeActionZoneProcessor()
 {
-	if (bIsInitialized)
+	if (bIsSdkInitialized)
 	{
 		ActionZoneProcessor = MakeShared<GeForceNOWActionZoneProcessor>();
 		return ActionZoneProcessor->Initialize();
@@ -126,25 +126,40 @@ bool GeForceNOWWrapper::InitializeActionZoneProcessor()
 	return false;
 }
 
-GfnRuntimeError GeForceNOWWrapper::Shutdown()
+/*static*/ GfnRuntimeError GeForceNOWWrapper::Shutdown()
 {
-	bIsInitialized = false;
-	if (ActionZoneProcessor.IsValid())
+	if (Singleton)
 	{
-		ActionZoneProcessor->Terminate();
-		ActionZoneProcessor.Reset();
+		GeForceNOWWrapper& Wrapper = GeForceNOWWrapper::Get();
+		if (Wrapper.ActionZoneProcessor.IsValid())
+		{
+			Wrapper.ActionZoneProcessor->Terminate();
+			Wrapper.ActionZoneProcessor.Reset();
+		}
+
+		delete Singleton;
+		Singleton = nullptr;
 	}
-	return GfnShutdownSdk();
+
+	if (bIsSdkInitialized)
+	{
+		bIsSdkInitialized = false;
+		return GfnShutdownSdk();
+	}
+	else
+	{
+		return gfnSuccess;
+	}
 }
 
-bool GeForceNOWWrapper::IsRunningInCloud()
+/*static*/ bool GeForceNOWWrapper::IsRunningInCloud()
 {
 	if (IsRunningMockGFN())
 	{
 		return true;
 	}
 
-	if (!bIsInitialized)
+	if (!bIsSdkInitialized)
 	{
 		return false;
 	}
