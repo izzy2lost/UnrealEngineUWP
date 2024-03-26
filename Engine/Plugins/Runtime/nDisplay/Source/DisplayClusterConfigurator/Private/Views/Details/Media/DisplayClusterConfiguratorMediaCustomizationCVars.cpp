9f47f53d14f5d969a2a4a5c9638dce0876a21cb4
andrey yamashev <andrey.yamashev@xa.epicgames.com>
2024-03-26 02:02:00 -0400

    Fixed CIS build issue
    [CL 32498669 by andrey yamashev in 5.4 branch]

--- Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterConfigurator/Private/Views/Details/Media/DisplayClusterConfiguratorMediaCustomizationCVars.cpp
+++ Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterConfigurator/Private/Views/Details/Media/DisplayClusterConfiguratorMediaCustomizationCVars.cpp
@@ -1,6 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "Views\Details\Media\DisplayClusterConfiguratorMediaCustomizationCVars.h"
+#include "Views/Details/Media/DisplayClusterConfiguratorMediaCustomizationCVars.h"
 
 
 TAutoConsoleVariable<bool> CVarMediaAutoInitializationEnabled(
