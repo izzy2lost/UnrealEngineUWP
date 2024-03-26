74c44ab3cb93a9cd41f7e3090740fcdcc0b44e14
louisphilippe seguin <LouisPhilippe.Seguin@epicgames.com>
2024-03-26 00:58:39 -0400

    Fix typo in TagNetSerializer IsEqual
    #rnx
    #rb jon.fairchild
    
    [CL 32497167 by louisphilippe seguin in 5.4 branch]

--- Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Private/Serialization/MinimalReplicationTagCountNetSerializer.cpp
+++ Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Private/Serialization/MinimalReplicationTagCountNetSerializer.cpp
@@ -171,7 +171,7 @@ bool FMinimalReplicationTagCountMapNetSerializer::IsEqual(FNetSerializationConte
 		TempValue0.ClampTagCount(MaxTagCount);
 
 		TempValue1.CopyReplicatedFieldsFrom(Source1);
-		TempValue0.ClampTagCount(MaxTagCount);
+		TempValue1.ClampTagCount(MaxTagCount);
 
 		FNetIsEqualArgs IsEqualArgs = Args;
 		IsEqualArgs.Source0 = NetSerializerValuePointer(&TempValue0);
