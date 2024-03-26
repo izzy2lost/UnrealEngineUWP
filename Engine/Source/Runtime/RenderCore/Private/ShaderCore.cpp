0c86f4d9fc8ed4c635249e49eca19f5fa0a13a44
dan elksnitis <dan.elksnitis@epicgames.com>
2024-03-26 02:42:11 -0400

    [shaders] add compression for FShaderSource objects and enable by default (Mermaid SuperFast compression). This halves the high watermark for the ShaderCompiler LLM scope in my testing (QAGame cold cook), with minimal measurable impact to preprocessing time (<3% on average, but this may just be noise).
    #rb Jason.Nadro, Laura.Hermanns
    #jira UE-209037
    
    [CL 32499932 by dan elksnitis in 5.4 branch]

--- Engine/Source/Runtime/RenderCore/Private/ShaderCore.cpp
+++ Engine/Source/Runtime/RenderCore/Private/ShaderCore.cpp
@@ -1286,6 +1286,9 @@ public:
 			// if the preprocessed job cache is enabled we need to strip the preprocessed code, this removes comments, line directives
 			// and blank lines to improve deduplication (and populates data required to remap diagnostic messages to correct line numbers)
 			Job.PreprocessOutput.StripCode(Job.Input.NeedsOriginalShaderSource());
+
+			// always compress the code after stripping to minimize memory footprint
+			Job.PreprocessOutput.CompressCode();
 		}
 
 		Job.PreprocessOutput.ElapsedTime = FPlatformTime::Seconds() - StartPreprocessTime;
@@ -1351,6 +1354,9 @@ public:
 		{
 			PreprocessShaderInternal(Compiler, Job);
 		}
+
+		// decompress if necessary; this is a no-op if source is not compressed.
+		Job.PreprocessOutput.DecompressCode();
 
 		if (Job.PreprocessOutput.bSucceeded)
 		{
@@ -3816,7 +3822,8 @@ void FShaderCompileJob::OnComplete()
 		// (jobs deserialized from the cache/wait list/ddc will have a compiletime of 0.0)
 		&& (CVarDumpDebugInfoForCacheHits.GetValueOnAnyThread() || Output.CompileTime > 0.0f))
 	{
-		
+		// need to decompress the source code before dumping debug data
+		PreprocessOutput.DecompressCode();
 		if (SecondaryPreprocessOutput.IsValid() && SecondaryOutput.IsValid())
 		{
 			ShaderFormat->OutputDebugData(Input, PreprocessOutput, *SecondaryPreprocessOutput, Output, *SecondaryOutput);
