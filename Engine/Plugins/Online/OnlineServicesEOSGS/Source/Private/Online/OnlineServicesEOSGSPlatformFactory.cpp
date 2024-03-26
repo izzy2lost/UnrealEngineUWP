c45bee534c0343291f93f0d063f0bd270ffa1da9
chris varnsverry <Chris.Varnsverry@epicgames.com>
2024-03-26 02:19:43 -0400

    - Don't create a default platform during StartupModule
    #jira UE-209526
    [REVIEW] [at]Ben.Marsh
    #rb Ben.Marsh
    
    [CL 32499335 by chris varnsverry in 5.4 branch]

--- Engine/Plugins/Online/OnlineServicesEOSGS/Source/Private/Online/OnlineServicesEOSGSPlatformFactory.cpp
+++ Engine/Plugins/Online/OnlineServicesEOSGS/Source/Private/Online/OnlineServicesEOSGSPlatformFactory.cpp
@@ -86,11 +86,6 @@ namespace UE::Online {
 
 FOnlineServicesEOSGSPlatformFactory::FOnlineServicesEOSGSPlatformFactory()
 {
-	// If a fork is requested, we need to wait for post-fork to create the default platform
-	if (!FForkProcessHelper::IsForkRequested() || FForkProcessHelper::IsForkedChildProcess())
-	{
-		GetDefaultPlatform();
-	}
 }
 
 FOnlineServicesEOSGSPlatformFactory& FOnlineServicesEOSGSPlatformFactory::Get()
