260449155db38c132acf3eade96573e0132a1484
pere rifa <pere.rifa@epicgames.com>
2024-03-26 01:54:47 -0400

    [Mutable] Basic implementation of UCustomizableObjectSkeletalMesh and FCustomizableObjectMeshStreamIn classes to allow streaming of mesh LODs.
    #rnx
    #rb alexei.lebedev, gerard.martin
    
    [CL 32498438 by pere rifa in 5.4 branch]

---
+++ Engine/Plugins/Experimental/Mutable/Source/CustomizableObject/Private/MuCO/CustomizableObjectMeshUpdate.cpp
@@ -0,0 +1,262 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+/*=============================================================================
+CustomizableObjectMeshUpdate.cpp: Helpers to stream in CustomizableObject skeletal mesh LODs.
+=============================================================================*/
+
+
+#include "MuCO/CustomizableObjectMeshUpdate.h"
+
+#include "MuCO/CustomizableObjectSystem.h"
+#include "MuCO/CustomizableObjectSystemPrivate.h"
+#include "MuCO/CustomizableObjectSkeletalMesh.h"
+#include "MuCO/UnrealConversionUtils.h"
+
+#include "MuR/Model.h"
+#include "MuR/MeshBufferSet.h"
+
+#include "Engine/SkeletalMesh.h"
+#include "Streaming/RenderAssetUpdate.inl"
+#include "Rendering/SkeletalMeshRenderData.h"
+
+template class TRenderAssetUpdate<FSkelMeshUpdateContext>;
+
+
+FCustomizableObjectMeshStreamIn::FCustomizableObjectMeshStreamIn(const UCustomizableObjectSkeletalMesh* InMesh, bool bInHighPrio, bool bInRenderThread)
+	: FSkeletalMeshStreamIn(InMesh),
+	bHighPriority(bInHighPrio),
+	bRenderThread(bInRenderThread)
+{
+	OperationData = MakeShared<FMutableMeshOperationData>();
+
+	// This must run in the mutable thread.
+	check(UCustomizableObjectSystem::IsCreated());
+	OperationData->System = UCustomizableObjectSystem::GetInstance()->GetPrivate()->MutableSystem;
+
+	OperationData->Model = InMesh->Model;
+	OperationData->Parameters = InMesh->Parameters;
+	OperationData->State = InMesh->State;
+
+	OperationData->MeshIDs = InMesh->MeshIDs;
+	OperationData->Meshes.SetNum(InMesh->MeshIDs.Num());
+
+	OperationData->CurrentFirstLODIdx = CurrentFirstLODIdx;
+	OperationData->PendingFirstLODIdx = PendingFirstLODIdx;
+
+	PushTask(FContext(InMesh, TT_None), TT_Async, SRA_UPDATE_CALLBACK(DoInitiate), TT_None, nullptr);
+}
+
+void FCustomizableObjectMeshStreamIn::DoInitiate(const FContext& Context)
+{
+	check(Context.CurrentThread == TT_Async);
+
+	if (IsCancelled())
+	{
+		return;
+	}
+
+	// Launch MutableTask
+	RequestMeshUpdate(Context);
+
+	PushTask(Context, TT_Async, SRA_UPDATE_CALLBACK(DoConvertResources), TT_Async, SRA_UPDATE_CALLBACK(DoCancelMeshUpdate));
+}
+
+void FCustomizableObjectMeshStreamIn::DoConvertResources(const FContext& Context)
+{
+	check(Context.CurrentThread == TT_Async);
+
+	ConvertMesh(Context);
+
+	const EThreadType TThread = bRenderThread ? TT_Render : TT_Async;
+	const EThreadType CThread = (EThreadType)Context.CurrentThread;
+	PushTask(Context, TThread, SRA_UPDATE_CALLBACK(DoCreateBuffers), CThread, SRA_UPDATE_CALLBACK(DoCancel));
+}
+
+void FCustomizableObjectMeshStreamIn::DoCreateBuffers(const FContext& Context)
+{
+	if (bRenderThread)
+	{
+		CreateBuffers_RenderThread(Context);
+	}
+	else
+	{
+		CreateBuffers_Async(Context);
+	}
+	check(!TaskSynchronization.GetValue());
+	PushTask(Context, TT_Render, SRA_UPDATE_CALLBACK(DoFinishUpdate), (EThreadType)Context.CurrentThread, SRA_UPDATE_CALLBACK(DoCancel));
+}
+
+void FCustomizableObjectMeshStreamIn::DoCancelMeshUpdate(const FContext& Context)
+{
+	CancelMeshUpdate(Context);
+	PushTask(Context, TT_None, nullptr, (EThreadType)Context.CurrentThread, SRA_UPDATE_CALLBACK(DoCancel));
+}
+
+
+namespace impl
+{
+	void Task_Mutable_UpdateMesh(const TSharedPtr<FMutableMeshOperationData> OperationData)
+	{
+		MUTABLE_CPUPROFILER_SCOPE(Task_Mutable_UpdateMesh);
+
+		// This runs in a worker thread.
+		check(OperationData.IsValid());
+		check(OperationData->System.get());
+		check(OperationData->Model);
+		check(OperationData->Parameters.get());
+
+		if (!OperationData.IsValid())
+		{
+			return;
+		}
+
+		mu::SystemPtr System = OperationData->System;
+		const TSharedPtr<mu::Model, ESPMode::ThreadSafe> Model = OperationData->Model;
+
+#if WITH_EDITOR
+		// Recompiling a CO in the editor will invalidate the previously generated Model. Check that it is valid before accessing the streamed data.
+		if (!Model || !Model->IsValid())
+		{
+			return;
+		}
+#endif
+
+		// For now, we are forcing the recreation of mutable-side instances with every update.
+		mu::Instance::ID InstanceID = System->NewInstance(Model);
+		UE_LOG(LogMutable, Verbose, TEXT("Creating Mutable instance with id [%d] for a mesh update"), InstanceID);
+
+		// LOD mask, set to all ones to build all LODs
+		const uint32 LODMask = 0xFFFFFFFF;
+
+		// Main instance generation step
+		const mu::Instance* Instance = System->BeginUpdate(InstanceID, OperationData->Parameters, OperationData->State, LODMask);
+		check(Instance);
+
+		// Generate the required meshes
+		for (int32 Index = OperationData->PendingFirstLODIdx; Index < OperationData->CurrentFirstLODIdx; ++Index)
+		{
+			const mu::FResourceID MeshID = OperationData->MeshIDs[Index];
+			OperationData->Meshes[Index] = System->GetMesh(InstanceID, MeshID);
+		}
+
+		// End update
+		System->EndUpdate(InstanceID);
+		System->ReleaseInstance(InstanceID);
+
+		if (CVarClearWorkingMemoryOnUpdateEnd.GetValueOnAnyThread())
+		{
+			System->ClearWorkingMemory();
+		}
+	}
+} // namespace
+
+
+void FCustomizableObjectMeshStreamIn::RequestMeshUpdate(const FContext& Context)
+{
+	if (!UCustomizableObjectSystem::IsActive())
+	{
+		Abort();
+		return;
+	}
+
+	check(OperationData.IsValid());
+
+#if WITH_EDITOR
+	// Recompiling a CO in the editor will invalidate the previously generated Model. Check that it is valid before accessing the streamed data.
+	if (!OperationData->Model || !OperationData->Model->IsValid())
+	{
+		Abort();
+		return;
+	}
+#endif
+
+	UCustomizableObjectSystemPrivate* CustomizableObjectSystem = UCustomizableObjectSystem::GetInstance()->GetPrivate();
+	
+	TaskSynchronization.Increment();
+
+	TRefCountPtr<FCustomizableObjectMeshStreamIn> RefThis = this;
+	TSharedPtr<FMutableMeshOperationData> SharedOperationData = OperationData;
+
+	MutableTaskId = CustomizableObjectSystem->MutableTaskGraph.AddMutableThreadTaskLowPriority(
+		TEXT("Mutable_MeshUpdate"),
+		[SharedOperationData, RefThis]()
+		{
+			impl::Task_Mutable_UpdateMesh(SharedOperationData);
+
+			// At this point task synchronization would hold the number of pending requests.
+			RefThis->TaskSynchronization.Decrement();
+
+			// The tick here is intended to schedule the success or cancel callback.
+			// Using TT_None ensure gets which could create a dead lock.
+			RefThis->Tick(FSkeletalMeshUpdate::TT_None);
+		});
+}
+
+
+void FCustomizableObjectMeshStreamIn::CancelMeshUpdate(const FContext& Context)
+{
+	UCustomizableObjectSystemPrivate* CustomizableObjectSystem = UCustomizableObjectSystem::GetInstance()->GetPrivate();
+	if (CustomizableObjectSystem && MutableTaskId != FMutableTaskGraph::INVALID_ID)
+	{
+		// Cancel task if not launched yet.
+		const bool bMutableTaskCancelledBeforeRun = CustomizableObjectSystem->MutableTaskGraph.CancelMutableThreadTaskLowPriority(MutableTaskId);
+		if (bMutableTaskCancelledBeforeRun)
+		{
+			// Clear MeshUpdate data
+			OperationData = nullptr;
+
+			// At this point task synchronization would hold the number of pending requests.
+			TaskSynchronization.Decrement();
+			check(TaskSynchronization.GetValue() == 0);
+		}
+	}
+	else
+	{
+		check(TaskSynchronization.GetValue() == 0);
+	}
+
+	// The tick here is intended to schedule the success or cancel callback.
+	// Using TT_None ensure gets which could create a dead lock.
+	Tick(FSkeletalMeshUpdate::TT_None);
+}
+
+
+void FCustomizableObjectMeshStreamIn::ConvertMesh(const FContext& Context)
+{
+	MUTABLE_CPUPROFILER_SCOPE(ConvertMesh);
+
+	check(!TaskSynchronization.GetValue());
+
+	const USkeletalMesh* Mesh = Context.Mesh;
+	FSkeletalMeshRenderData* RenderData = Context.RenderData;
+	if (IsCancelled() || !Mesh || !RenderData)
+	{
+		return;
+	}
+
+	for (int32 LODIndex = PendingFirstLODIdx; LODIndex < CurrentFirstLODIdx; ++LODIndex)
+	{
+		mu::MeshPtrConst MutableMesh = OperationData->Meshes[LODIndex];
+
+		if (!MutableMesh)
+		{
+			check(false);
+			Abort();
+			return;
+		}
+
+		FSkeletalMeshLODRenderData& LODResource = *Context.LODResourcesView[LODIndex];
+		UnrealConversionUtils::CopyMutableVertexBuffers(LODResource, MutableMesh, true);
+		UnrealConversionUtils::CopyMutableIndexBuffers(LODResource, MutableMesh);
+
+		// TODO PERE: Implement missing buffers
+		// SkinWeightProfilesData
+		// MorphTargets (MorphTargetVertexInfoBuffers)
+		// Clothing (ClothVertexBuffer)
+
+		UnrealConversionUtils::UpdateSkeletalMeshLODRenderDataBuffersSize(LODResource);
+	}
+
+	// Clear MeshUpdate data
+	OperationData = nullptr;
+}
