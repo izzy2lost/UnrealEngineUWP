541685d8a9fac2cf6619a5850787410c1d1d8165
jodon karlik <jodon.karlik@epicgames.com>
2024-03-26 02:50:59 -0400

    Ensure FPredictionKeys only attempt to "catch-up" if they were locally generated.  If they are Server Initiated, then ignore them for catch-up purposes.
    This would have had very subtle bugs in the past, where a server-initiated prediction key would "catch-up" client keys, possibly before (or after) rejecting them.  The new FPredictionKey handling makes these bugs more apparent.
    
    #rb vivien.wei
    
    [CL 32500084 by jodon karlik in 5.4 branch]

--- Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Public/GameplayPrediction.h
+++ Engine/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Public/GameplayPrediction.h
@@ -371,7 +371,7 @@ struct GAMEPLAYABILITIES_API FPredictionKey
 
 	FString ToString() const
 	{
-		return FString::Printf(TEXT("[%d/%d]"), Current, Base);
+		return bIsServerInitiated ? FString::Printf(TEXT("[Srv: %d]"), Current) : FString::Printf(TEXT("[%d/%d]"), Current, Base);
 	}
 
 	friend uint32 GetTypeHash(const FPredictionKey& InKey)
