ad6b9217f64ba65d098e11ecf4a28f213392d789
sebastien hillaire <Sebastien.Hillaire@epicgames.com>
2024-03-26 02:08:20 -0400

    Lumen - fixed translucent lighting volume to not offset ALL samples behind the depth buffer. Now we only offset those that are N slice behind it. (N behind 1 by default) This avoid all samples beinh an object to mvoe closer and result in which can result in volumetric fog distortion around a mesh edges.
    [FYI] Krzysztof.Narkowicz
    
    [CL 32498907 by sebastien hillaire in 5.4 branch]

--- Engine/Source/Runtime/Renderer/Private/Lumen/LumenTranslucencyVolumeLighting.cpp
+++ Engine/Source/Runtime/Renderer/Private/Lumen/LumenTranslucencyVolumeLighting.cpp
@@ -234,11 +234,19 @@ FAutoConsoleVariableRef CVarTranslucencyVolumeRadianceCacheStats(
 	ECVF_RenderThreadSafe
 );
 
-float GTranslucencyVolumeRadianceCacheGridCenterOffsetFromDepthBuffer = 0.5f;
-FAutoConsoleVariableRef CVarTranslucencyVolumeRadianceCacheGridCenterOffsetFromDepthBuffer(
-	TEXT("r.Lumen.TranslucencyVolume.RadianceCache.GridCenterOffsetFromDepthBuffer"),
-	GTranslucencyVolumeRadianceCacheGridCenterOffsetFromDepthBuffer,
+float GTranslucencyVolumeGridCenterOffsetFromDepthBuffer = 0.5f;
+FAutoConsoleVariableRef CVarTranslucencyVolumeGridCenterOffsetFromDepthBuffer(
+	TEXT("r.Lumen.TranslucencyVolume.GridCenterOffsetFromDepthBuffer"),
+	GTranslucencyVolumeGridCenterOffsetFromDepthBuffer,
 	TEXT("Offset in grid units to move grid center sample out form the depth buffer along the Z direction. -1 means disabled. This reduces sample self intersection with geometry when tracing the global distance field buffer, and thus reduces flickering in those areas, as well as results in less leaking sometimes."),
+	ECVF_RenderThreadSafe
+);
+
+float GTranslucencyVolumeOffsetThresholdToAcceptDepthBufferOffset = 1.0f;
+FAutoConsoleVariableRef CVarTranslucencyVolumeOffsetThresholdToAcceptDepthBufferOffset(
+	TEXT("r.Lumen.TranslucencyVolume.OffsetThresholdToAcceptDepthBufferOffset"),
+	GTranslucencyVolumeOffsetThresholdToAcceptDepthBufferOffset,
+	TEXT("Offset in grid units to accept a sample to be moved forward in front of the depth buffer. This is to avoid moving all samples behind the depth buffer forward which would affect the lighting of translucent and volumetric at edges of mesh."),
 	ECVF_RenderThreadSafe
 );
 
@@ -581,7 +589,8 @@ FLumenTranslucencyLightingVolumeParameters GetTranslucencyLightingVolumeParamete
 	Parameters.UseJitter = GTranslucencyVolumeJitter;
 	Parameters.FrameJitterOffset = (FVector3f)TranslucencyVolumeTemporalRandom(View.ViewState ? View.ViewState->GetFrameIndex() : 0);
 	Parameters.UnjitteredClipToTranslatedWorld = FMatrix44f(View.ViewMatrices.ComputeInvProjectionNoAAMatrix() * View.ViewMatrices.GetTranslatedViewMatrix().GetTransposed());		// LWC_TODO: Precision loss?
-	Parameters.GridCenterOffsetFromDepthBuffer = GTranslucencyVolumeRadianceCacheGridCenterOffsetFromDepthBuffer;
+	Parameters.GridCenterOffsetFromDepthBuffer = GTranslucencyVolumeGridCenterOffsetFromDepthBuffer;
+	Parameters.GridCenterOffsetThresholdToAcceptDepthBufferOffset = FMath::Max(0, GTranslucencyVolumeOffsetThresholdToAcceptDepthBufferOffset);
 
 	Parameters.SceneTexturesStruct = View.GetSceneTextures().UniformBuffer;
 		
