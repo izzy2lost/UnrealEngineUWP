e6554be1b8d6bcf41aa642ea656878db22168f04
zousar shaker <zousar.shaker@epicgames.com>
2024-03-26 18:45:00 -0400

    Ensure that zenserver http requests from the zen server interface (not the DDC layer) bypass proxies when communicating with zenserver.
    [FYI] dan.engelbrecht, Devin.Doucette
    #jira UE-210567
    
    [CL 32527896 by zousar shaker in ue5-main branch]

--- Engine/Source/Developer/Zen/Private/ZenServerHttp.cpp
+++ Engine/Source/Developer/Zen/Private/ZenServerHttp.cpp
@@ -95,6 +95,7 @@ namespace UE::Zen {
 		curl_easy_setopt(Curl, CURLOPT_NOSIGNAL, 1L);
 		curl_easy_setopt(Curl, CURLOPT_DNS_CACHE_TIMEOUT, -1L); // Don't re-resolve names mid-session
 		curl_easy_setopt(Curl, CURLOPT_BUFFERSIZE, 256 * 1024L);
+		curl_easy_setopt(Curl, CURLOPT_NOPROXY, "*");
 		//curl_easy_setopt(Curl, CURLOPT_UPLOAD_BUFFERSIZE, 256 * 1024L);
 		// Response functions
 		curl_easy_setopt(Curl, CURLOPT_HEADERDATA, this);
