f207c2ff72ef962a7a0018ab5b4ad37e9e997fa1
mike zyracki <Mike.Zyracki@epicgames.com>
2024-03-26 02:16:54 -0400

    Fix fallout from CIS Fix for Static Analysis from new auto tangent tweak #rb mike.zyracki #jira UE-184577
    [CL 32499246 by mike zyracki in 5.4 branch]

--- Engine/Source/Runtime/MovieScene/Private/Channels/MovieSceneCurveChannelImpl.cpp
+++ Engine/Source/Runtime/MovieScene/Private/Channels/MovieSceneCurveChannelImpl.cpp
@@ -1083,7 +1083,7 @@ void TMovieSceneCurveChannelImpl<ChannelType>::AutoSetTangents(ChannelType* InCh
 						CurveValueType ValueNewTangent = 0.0;
 						AutoCalcTangent(PrevKey.Value, ThisKey.Value, NextKey.Value, Tension, ValueNewTangent);
 						NewTangent = ValueNewTangent;
-						if (GCachedSequencerAutoTangentInterpolation < 2)
+						//fix for now if (GCachedSequencerAutoTangentInterpolation < 2)
 						{
 							NewTangent /= PrevToNextTimeDiff;
 							//if within 0 to 15% or 85% to 100% range we gradually weight tangent to zero
@@ -1104,6 +1104,7 @@ void TMovieSceneCurveChannelImpl<ChannelType>::AutoSetTangents(ChannelType* InCh
 							}
 
 						}
+						/*
 						else if (GCachedSequencerAutoTangentInterpolation == 2) //use flattening, no overshoot
 						{
 							const double TimeToPrevious = FMath::Max<double>(KINDA_SMALL_NUMBER, InChannel->Times[Index].Value - InChannel->Times[Index - 1].Value);
@@ -1113,7 +1114,7 @@ void TMovieSceneCurveChannelImpl<ChannelType>::AutoSetTangents(ChannelType* InCh
 							NewTangent = ClampTangent<double>(NewTangent, PreviousSlope, NextSlope);
 							NewTangent /= PrevToNextTimeDiff;
 						}
-						
+						*/
 					}
 				}
 			}
