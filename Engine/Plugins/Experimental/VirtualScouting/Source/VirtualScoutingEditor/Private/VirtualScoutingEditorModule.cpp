ded615b6fd70ca763ae376b2b0a9a56bdf70ed06
zach brockway <zach.brockway@epicgames.com>
2024-03-26 12:24:19 -0400

    Virtual Scouting: Ensure we default to "Scouting Default" mode when enabling the plugin for the first time.
    #jira UE-208777
    #rb jason.walter, richard.graham
    #rnx
    
    [CL 32512453 by zach brockway in 5.4 branch]

--- Engine/Plugins/Experimental/VirtualScouting/Source/VirtualScoutingEditor/Private/VirtualScoutingEditorModule.cpp
+++ Engine/Plugins/Experimental/VirtualScouting/Source/VirtualScoutingEditor/Private/VirtualScoutingEditorModule.cpp
@@ -2,6 +2,7 @@
 
 #include "VirtualScoutingEditorModule.h"
 #include "Modules/ModuleManager.h"
+#include "VRModeSettings.h"
 
 
 DEFINE_LOG_CATEGORY(LogVirtualScoutingEditor);
@@ -12,6 +13,13 @@ class FVirtualScoutingEditorModule : public IModuleInterface
 private:
 	virtual void StartupModule() override
 	{
+		// When the user has no previous selection upon enabling this plugin
+		// for the first time, ensure we default to our preferred mode.
+		UVRModeSettings* Settings = GetMutableDefault<UVRModeSettings>();
+		if (Settings->ModeClass.IsNull())
+		{
+			Settings->ModeClass = FSoftObjectPath("/VirtualScouting/Core/Scouting_Default.Scouting_Default_C");
+		}
 	}
 
 	virtual void ShutdownModule() override
