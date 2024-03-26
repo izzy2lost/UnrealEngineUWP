d595ce65cb85ecd734066c09b2b1536b1a335933
jamie dale <Jamie.Dale@epicgames.com>
2024-03-26 00:50:54 -0400

    Don't mount localization data for explicitly loaded plugins by default
    This allows them to separate their mounting and content loading into two phases, such as allowing a GFP to mount the plugin when "registered" and defer mounting the localization data until "activating".
    
    Note: Anything that was calling one of the MountExplicitlyLoadedPlugin functions to load a plugin, will now need to also call MountExplicitlyLoadedPluginLocalizationData if it wants to also mount the localization data.
    
    #rb anton.dunchev, Leon.Huang
    
    [CL 32496881 by jamie dale in 5.4 branch]

--- Engine/Plugins/Runtime/GameFeatures/Source/GameFeatures/Private/GameFeaturePluginStateMachine.cpp
+++ Engine/Plugins/Runtime/GameFeatures/Source/GameFeatures/Private/GameFeaturePluginStateMachine.cpp
@@ -2923,6 +2923,8 @@ struct FGameFeaturePluginState_Activating : public FGameFeaturePluginState
 
 		FGameFeatureActivatingContext Context;
 
+		const bool bIsLoadingLocalizationData = IPluginManager::Get().MountExplicitlyLoadedPluginLocalizationData(StateProperties.PluginName);
+
 		if (AllowIniLoading())
 		{
 			TRACE_CPUPROFILER_EVENT_SCOPE(GFP_Activating_InitIni);
@@ -2936,8 +2938,7 @@ struct FGameFeaturePluginState_Activating : public FGameFeaturePluginState
 
 		// @TODO: non-blocking wait here?
 		// If this plugin caused localization data to load, wait for that here before marking it as active
-		if (TSharedPtr<IPlugin> Plugin = IPluginManager::Get().FindPlugin(StateProperties.PluginName);
-			Plugin && Plugin->GetDescriptor().bExplicitlyLoaded && Plugin->GetDescriptor().LocalizationTargets.Num() > 0)
+		if (bIsLoadingLocalizationData)
 		{
 			TRACE_CPUPROFILER_EVENT_SCOPE(GFP_Activating_WaitForLoc);
 			FTextLocalizationManager::Get().WaitForAsyncTasks();
