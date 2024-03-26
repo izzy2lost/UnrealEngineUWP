be3a00d90222e92153797e69894ff338297d014f
andrey yamashev <andrey.yamashev@xa.epicgames.com>
2024-03-26 02:20:27 -0400

    [nDisplay] Fixed media capture initialization to prevent log spam  - Media capture always start, even if camera is disabled  - No more log spam trying to re-start capture every second  - Special initialization case for tiles when the owner is disabled on start
    #jira UE-209425
    #rb Alejandro.Arango
    
    [CL 32499353 by andrey yamashev in 5.4 branch]

--- Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterMedia/Private/Capture/DisplayClusterMediaCaptureCamera.cpp
+++ Engine/Plugins/Runtime/nDisplay/Source/DisplayClusterMedia/Private/Capture/DisplayClusterMediaCaptureCamera.cpp
@@ -3,20 +3,52 @@
 #include "Capture/DisplayClusterMediaCaptureCamera.h"
 
 #include "Components/DisplayClusterICVFXCameraComponent.h"
-#include "Config/IDisplayClusterConfigManager.h"
-#include "DisplayClusterConfigurationTypes_Viewport.h"
+#include "DisplayClusterConfigurationTypes.h"
 #include "DisplayClusterRootActor.h"
 #include "IDisplayCluster.h"
-#include "IDisplayClusterCallbacks.h"
 #include "Game/IDisplayClusterGameManager.h"
-
-#include "Render/Viewport/IDisplayClusterViewportManagerProxy.h"
-#include "RHICommandList.h"
-#include "RHIResources.h"
 
 
 FDisplayClusterMediaCaptureCamera::FDisplayClusterMediaCaptureCamera(const FString& InMediaId, const FString& InClusterNodeId, const FString& InCameraId, const FString& InViewportId, UMediaOutput* InMediaOutput, UDisplayClusterMediaOutputSynchronizationPolicy* SyncPolicy)
 	: FDisplayClusterMediaCaptureViewport(InMediaId, InClusterNodeId, InViewportId, InMediaOutput, SyncPolicy)
 	, CameraId(InCameraId)
 {
+}
+
+bool FDisplayClusterMediaCaptureCamera::GetCaptureSizeFromConfig(FIntPoint& OutSize) const
+{
+	if (const ADisplayClusterRootActor* const ActiveRootActor = IDisplayCluster::Get().GetGameMgr()->GetRootActor())
+	{
+		TArray<UActorComponent*> ICVFXCameraComponents;
+		IDisplayClusterViewportConfiguration* ViewportConfiguration = ActiveRootActor->GetViewportConfiguration();
+		if (const ADisplayClusterRootActor* const ConfigurationRootActor = ViewportConfiguration ? ViewportConfiguration->GetRootActor(EDisplayClusterRootActorType::Configuration) : nullptr)
+		{
+			ConfigurationRootActor->GetComponents(UDisplayClusterICVFXCameraComponent::StaticClass(), ICVFXCameraComponents);
+		}
+
+		// Get capture size from camera config
+		for (const UActorComponent* const Component : ICVFXCameraComponents)
+		{
+			if (const UDisplayClusterICVFXCameraComponent* const ICVFXCamera = Cast<UDisplayClusterICVFXCameraComponent>(Component))
+			{
+				if (ICVFXCamera->GetName() == CameraId)
+				{
+					const FDisplayClusterConfigurationICVFX_CameraSettings& CameraSettings = ICVFXCamera->GetCameraSettingsICVFX();
+
+					if (CameraSettings.CustomFrustum.bEnable)
+					{
+						OutSize = CameraSettings.CustomFrustum.EstimatedOverscanResolution;
+					}
+					else
+					{
+						OutSize = CameraSettings.CustomFrustum.InnerFrustumResolution;
+					}
+
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
 }
