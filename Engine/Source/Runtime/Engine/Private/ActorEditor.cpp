cc4d979cc49d219c0dbe585d5fb04b5e7713f777
patrick enfedaque <Patrick.Enfedaque@epicgames.com>
2024-03-26 02:15:31 -0400

    FStaticMeshComponentBulkReregisterContext::SanitizeMeshComponents : Fix crash with Unregistered PCG Components while moving an actor
    New code in PCG (CL 32221745) now listens to OnObjectsReplaced which can cause PCG to unregister a component while ReRunConstructionScript is executed.
    When the FStaticMeshComponentBulkReregisterContext exits scope we need to remove those unregistered components from the bulk register list as they can't be registered to the render anymore (they are unregistered from the world)
    
    #rb  jason.hoerner, huw.bowles
    #rnx
    
    [CL 32499202 by patrick enfedaque in 5.4 branch]

--- Engine/Source/Runtime/Engine/Private/ActorEditor.cpp
+++ Engine/Source/Runtime/Engine/Private/ActorEditor.cpp
@@ -256,8 +256,8 @@ void AActor::PostEditMove(bool bFinished)
 
 			FNavigationLockContext NavLock(GetWorld(), ENavigationLockReason::AllowUnregister);
 			RerunConstructionScripts();
-			// Construction scripts can have all manner of side effects, including creation of proxies..
-			// remove any static mesh components that have had their proxies created by RerunConstructionScripts:
+			// Construction scripts can have all manner of side effects, including creation of render proxies, unregistering / destruction of components
+			// Remove any static mesh components that are no longer valid for proxy registration after RerunConstructionScripts
 			ReregisterContext.SanitizeMeshComponents();
 		}
 	}
