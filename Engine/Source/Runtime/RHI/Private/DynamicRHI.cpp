129dce05c5b2d66b9d4041bcb6d30c89fea09796
daniele vettorel <daniele.vettorel@epicgames.com>
2024-03-28 13:15:49 -0400

    Do not attempt to cleanup the pipeline state cache twice.
    The cache gets cleaned up in the rendering thread.
    
    #rb Kenzo.Terelst
    #tests tested locally
    
    [CL 32583467 by daniele vettorel in ue5-main branch]

--- Engine/Source/Runtime/RHI/Private/DynamicRHI.cpp
+++ Engine/Source/Runtime/RHI/Private/DynamicRHI.cpp
@@ -420,9 +420,6 @@ void RHIExit()
 {
 	if (!GUsingNullRHI && GDynamicRHI != NULL)
 	{
-		// Clean up all cached pipelines
-		PipelineStateCache::Shutdown();
-
 		// Flush any potential commands queued before we shut things down.
 		FRHICommandListExecutor::GetImmediateCommandList().ImmediateFlush(EImmediateFlushType::FlushRHIThread);
 
