9348d70ba14e8be3fbaf3679e761739f58672475
ryan buehler <ryan.buehler@epicgames.com>
2024-03-29 21:28:28 -0400

    [PCG] Fix Mac compile error in PCGInputOutputSettings
    #rnx
    #rb
    
    [CL 32628356 by ryan buehler in ue5-main branch]

--- Engine/Plugins/PCG/Source/PCG/Public/PCGInputOutputSettings.h
+++ Engine/Plugins/PCG/Source/PCG/Public/PCGInputOutputSettings.h
@@ -65,7 +65,7 @@ public:
 	// Add a new custom pin
 	// Note that you should use the return value of this function, since it can be different from
 	// the one passed as argument. It will change if its label collides with existing pins.
-	PCG_API [[nodiscard]] const FPCGPinProperties& AddPin(const FPCGPinProperties& NewCustomPinProperties);
+	[[nodiscard]] PCG_API const FPCGPinProperties& AddPin(const FPCGPinProperties& NewCustomPinProperties);
 
 protected:
 	TArray<FPCGPinProperties> DefaultPinProperties(bool bInvisiblePin) const;
