ff3468a8d3ffc4d76d9bf8ae64d53585186ff4a6
jake burga <jake.burga@epicgames.com>
2024-04-01 19:18:33 -0400

    MIDI Writer Node - MidiStreamWriter class which contains the actual functionality - MidiStreamWriterNode which wraps the functionality - MIDI files are written out to "AudioCaptures/../MIDICaptures" - MIDI files are timestamped for uniqueness
    The Midi Stream Writer uses the "Clock Advance" events to determine the relative tick of the midi events to write out.
    
    [REVIEW] [at]buzz.burrowes [at]charlie.huguenard
    #rb charlie.huguenard
    #tests Harmonix Functional Tests
    
    [CL 32655117 by jake burga in ue5-main branch]

---
+++ Engine/Plugins/Runtime/Harmonix/Source/HarmonixMetasound/Private/HarmonixMetasound/MidiOps/MidiStreamWriter.cpp
@@ -0,0 +1,56 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HarmonixMetasound/MidiOps/MidiStreamWriter.h"
+#include "HarmonixMetasound/DataTypes/MidiStream.h"
+#include "HarmonixMidi/MidiWriter.h"
+
+namespace Harmonix::Midi::Ops
+{
+	using namespace HarmonixMetasound;
+	FMidiStreamWriter::FMidiStreamWriter(TUniquePtr<FArchive>&& InArchive)
+		: Archive(MoveTemp(InArchive))
+	{}
+
+	void FMidiStreamWriter::Process(const FMidiStream& InStream)
+	{
+		ensureMsgf(InStream.GetClock(), TEXT("Midi stream must have a midi clock for the MidiStreamWriter to process it"));
+		if (TSharedPtr<const FMidiClock, ESPMode::NotThreadSafe> MidiClock = InStream.GetClock())
+		{
+			for (const FMidiClockEvent& ClockEvent : MidiClock->GetMidiClockEventsInBlock())
+			{
+				if (ClockEvent.Msg.Type == FMidiClockMsg::EType::AdvanceThru && !ClockEvent.Msg.AsAdvanceThru().IsPreRoll)
+				{
+					Process(InStream, ClockEvent.Msg.FromTick(), ClockEvent.Msg.ThruTick());
+				}
+			}
+		}
+	}
+
+
+	void FMidiStreamWriter::Process(const FMidiStream& InStream, int32 FromTick, int32 ThruTick)
+	{
+		bool AddedEvents = false;
+		for (const FMidiStreamEvent& MidiEvent : InStream.GetEventsInBlock())
+		{
+			if (MidiEvent.CurrentMidiTick > FromTick && MidiEvent.CurrentMidiTick <= ThruTick)
+			{
+				const int32 OffsetTick = MidiEvent.CurrentMidiTick - FromTick;
+				const int32 MidiTick = CurrentWriteTick + OffsetTick;
+				FMidiTrack& MidiTrack = MidiTracks.FindOrAdd(MidiEvent.TrackIndex);
+				MidiTrack.AddEvent(FMidiEvent(MidiTick, MidiEvent.MidiMessage));
+				AddedEvents = true;
+			}
+		}
+		CurrentWriteTick += ThruTick - FromTick;
+
+		if (AddedEvents)
+		{
+			Archive->Seek(0);
+			FMidiWriter MidiWriter = FMidiWriter(*Archive);
+			for (TPair<int32, const FMidiTrack&> Pair : MidiTracks)
+			{
+				Pair.Value.WriteStdMidi(MidiWriter);
+			}
+		}
+	}
+};
