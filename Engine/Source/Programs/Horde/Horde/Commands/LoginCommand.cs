00575944f45857148e5358ab76723f22157f1589
carl bystrom <carl.bystrom@epicgames.com>
2024-04-03 19:10:48 -0400

    Horde: Fix compile warning for LoginCommand
    [CL 32714982 by carl bystrom in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde/Commands/LoginCommand.cs
+++ Engine/Source/Programs/Horde/Horde/Commands/LoginCommand.cs
@@ -1,7 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using System.ComponentModel;
-using System.Net.Http.Headers;
 using EpicGames.Core;
 using EpicGames.Horde;
 using EpicGames.Horde.Server;
