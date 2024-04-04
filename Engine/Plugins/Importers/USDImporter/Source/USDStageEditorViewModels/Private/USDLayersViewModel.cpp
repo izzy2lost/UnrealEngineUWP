e45877ea5c5f12e8bcb14a89742d09187a00647c
daniel coelho <daniel.coelho@xa.epicgames.com>
2024-04-04 06:09:08 -0400

    USD: Fix strange hang/crash when opening any USD stage.
    #jira UE-211302
    [FYI] Anousack.Kitisa
    
    [CL 32722829 by daniel coelho in ue5-main branch]

--- Engine/Plugins/Importers/USDImporter/Source/USDStageEditorViewModels/Private/USDLayersViewModel.cpp
+++ Engine/Plugins/Importers/USDImporter/Source/USDStageEditorViewModels/Private/USDLayersViewModel.cpp
@@ -164,7 +164,10 @@ void FUsdLayerViewModel::RefreshData()
 	const pxr::SdfLayerHandle& EditTargetLayer = UsdStageRef->GetEditTarget().GetLayer();
 	const pxr::SdfLayerHandle& FocusedEditTarget = IsolatedStageRef ? IsolatedStageRef->GetEditTarget().GetLayer() : pxr::SdfLayerHandle{};
 
-	LayerModel->bIsEditTarget = (IsolatedStageRef) ? LayerModel->bIsInIsolatedStage && FocusedEditTarget == UsdLayer : EditTargetLayer == UsdLayer;
+	// For whatever reason, doing the "EditTargetLayer == UsdLayer" comparison direcly leads to a hang on Windows and a crash
+	// on the Mac? It could be related to the USD 24.03 update somehow
+	LayerModel->bIsEditTarget = (IsolatedStageRef) ? LayerModel->bIsInIsolatedStage && FocusedEditTarget == UsdLayer
+												   : (EditTargetLayer && UsdLayer && EditTargetLayer->GetIdentifier() == UsdLayer->GetIdentifier());
 
 	LayerModel->bIsMuted = (IsolatedStageRef && LayerModel->bIsInIsolatedStage)
 							   // If we isolating, we're only muted if we're muted on the isolated stage
