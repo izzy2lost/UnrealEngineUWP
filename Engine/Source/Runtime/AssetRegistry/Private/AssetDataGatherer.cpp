33095a4c5fc278bad8ff3e1c07c2865b1767a49d
francis hurteau <Francis.Hurteau@epicgames.com>
2024-04-04 16:43:30 -0400

    Temporarily disable multithreaded data processing for Asset Registry.
    #jira UE-211455
    [FYI] Matt.Breindel
    
    [CL 32741906 by francis hurteau in ue5-main branch]

--- Engine/Source/Runtime/AssetRegistry/Private/AssetDataGatherer.cpp
+++ Engine/Source/Runtime/AssetRegistry/Private/AssetDataGatherer.cpp
@@ -85,7 +85,7 @@ static FAutoConsoleVariableRef CVarIgnoreEmptyDirectories(
 	bIgnoreEmptyDirectories,
 	TEXT("If true, completely empty leaf directories are ignored by the asset registry while scanning"));
 
-bool bTickGatherOnGTOnly = false;
+bool bTickGatherOnGTOnly = true;
 static FAutoConsoleVariableRef CVarTickGatherOnGTOnly(
 	TEXT("AssetRegistry.TickGatherOnGTOnly"),
 	bTickGatherOnGTOnly,
