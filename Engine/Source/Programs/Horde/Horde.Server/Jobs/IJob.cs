db8bf6f6d3a7dd23c410965981286221756ad1aa
ben marsh <Ben.Marsh@epicgames.com>
2024-04-07 08:06:23 -0400

    Horde: Move JobStepError into EpicGames.Horde.
    #rnx
    
    [CL 32785281 by ben marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Server/Jobs/IJob.cs
+++ Engine/Source/Programs/Horde/Horde.Server/Jobs/IJob.cs
@@ -159,32 +159,6 @@ namespace Horde.Server.Jobs
 	}
 
 	/// <summary>
-	/// Systemic error codes for a job step failing
-	/// </summary>
-	public enum JobStepError
-	{
-		/// <summary>
-		/// No systemic error
-		/// </summary>
-		None,
-
-		/// <summary>
-		/// Step did not complete in the required amount of time
-		/// </summary>
-		TimedOut,
-
-		/// <summary>
-		/// Step is in is paused state so was skipped
-		/// </summary>
-		Paused,
-
-		/// <summary>
-		/// Step did not complete because the batch exited
-		/// </summary>
-		Incomplete
-	}
-
-	/// <summary>
 	/// Extension methods for job steps
 	/// </summary>
 	public static class JobStepExtensions
