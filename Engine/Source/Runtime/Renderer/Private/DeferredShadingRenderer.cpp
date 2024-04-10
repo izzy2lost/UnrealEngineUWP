3d46fb9abd5687ae9eb165a71d7032baa74b0ffd
justin moe <Justin.Moe@epicgames.com>
2024-04-10 18:58:18 -0400

    [Backout] - CL32855665 [FYI] Sebastien.Hillaire Original CL Desc ----------------------------------------------------------------- Updated light function processing and added more debug information when out of slots. Fixed a bug wehre one slot was always marked as used, always removing 1 material form the atlas.
    [CL 32867346 by justin moe in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.cpp
+++ Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.cpp
@@ -3117,8 +3117,6 @@ void FDeferredShadingSceneRenderer::Render(FRDGBuilder& GraphBuilder)
 			}
 		}
 
-		LightFunctionAtlas.RenderDebugInfo(GraphBuilder, Views);
-
 		if (VirtualShadowMapArray.IsEnabled())
 		{
 			VirtualShadowMapArray.RenderDebugInfo(GraphBuilder, Views);
