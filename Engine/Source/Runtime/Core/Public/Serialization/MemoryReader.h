fb6334a7c25337113b4b5c31c948198d7f3fb25b
joe kirchoff <joe.kirchoff@epicgames.com>
2024-04-10 11:20:19 -0400

    [Backout] - CL32838690 [FYI] Joe.Kirchoff Original CL Desc ----------------------------------------------------------------- Prevent clang analysis warnings for uninitialized data from FArchive derived readers
    #rnx
    
    [CL 32857977 by joe kirchoff in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Serialization/MemoryReader.h
+++ Engine/Source/Runtime/Core/Public/Serialization/MemoryReader.h
@@ -34,10 +34,6 @@ public:
 
 	void Serialize( void* Data, int64 Num )
 	{
-#if defined(__clang_analyzer__)
-		// Suppress uninitialized data static analysis warning
-		FMemory::Memzero(Data, Num);
-#endif
 		if (Num && !IsError())
 		{
 			// Only serialize if we have the requested amount of data
--- Engine/Source/Runtime/Online/BuildPatchServices/Private/Tests/Fake/FileSystem.fake.h
+++ Engine/Source/Runtime/Online/BuildPatchServices/Private/Tests/Fake/FileSystem.fake.h
@@ -36,10 +36,6 @@ namespace BuildPatchServices
 
 		void Serialize(void* Data, int64 Num)
 		{
-#if defined(__clang_analyzer__)
-			// Suppress uninitialized data static analysis warning
-			FMemory::Memzero(Data, Num);
-#endif
 			if (Num && !IsError())
 			{
 				if (Offset + Num <= TotalSize())
