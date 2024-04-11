d012207297b882d3791d6c19407faae1a9545291
joakim lindqvist <Joakim.Lindqvist@epicgames.com>
2024-04-11 07:31:57 -0400

    Unreal Cloud DDC - Fixed build errors in references controller
    [CL 32883875 by joakim lindqvist in ue5-main branch]

--- Engine/Source/Programs/UnrealCloudDDC/Jupiter/Controllers/ReferencesController.cs
+++ Engine/Source/Programs/UnrealCloudDDC/Jupiter/Controllers/ReferencesController.cs
@@ -297,7 +297,7 @@ namespace Jupiter.Controllers
 							byte[] packageBytes;
 							{
 								using TelemetrySpan _ = _tracer.StartActiveSpan("cbpackage.buffer").SetAttribute("operation.name", "cbpackage.buffer");
-								packageBytes = await writer.ToByteArray();
+								packageBytes = await writer.ToByteArrayAsync();
 							}
 							await using BlobContents contents = new BlobContents(packageBytes);
 							await WriteBody(contents, CustomMediaTypeNames.UnrealCompactBinaryPackage);
@@ -744,11 +744,11 @@ namespace Jupiter.Controllers
 			_diagnosticContext.Set("Content-Length", Request.ContentLength ?? -1);
 
 			byte[] b = await RequestUtil.ReadRawBodyAsync(Request);
-			CbPackageReader packageReader = await CbPackageReader.Create(new MemoryStream(b));
+			CbPackageReader packageReader = await CbPackageReader.CreateAsync(new MemoryStream(b));
 
 			try
 			{
-				await foreach ((CbPackageAttachmentEntry entry, byte[] blob) in packageReader.IterateAttachments())
+				await foreach ((CbPackageAttachmentEntry entry, byte[] blob) in packageReader.IterateAttachmentsAsync())
 				{
 					if (entry.Flags.HasFlag(CbPackageAttachmentFlags.IsError))
 					{
