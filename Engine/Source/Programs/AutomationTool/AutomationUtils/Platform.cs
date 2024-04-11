1eac2aebe22825b3cfeb1b84bc08674d9711d758
bob tellez <Bob.Tellez@epicgames.com>
2024-04-11 02:05:38 -0400

    Merging CL#32865058
    [preflight] Move UpdatePackagingParameters into it's own file.
    
    #rb brendan.lienau
    
    [CL 32872909 by bob tellez in ue5-main branch]

--- Engine/Source/Programs/AutomationTool/AutomationUtils/Platform.cs
+++ Engine/Source/Programs/AutomationTool/AutomationUtils/Platform.cs
@@ -12,6 +12,7 @@ using UnrealBuildBase;
 using Microsoft.Extensions.Logging;
 
 using static AutomationTool.CommandUtils;
+using IdentityModel.Client;
 
 namespace AutomationTool
 {
@@ -814,6 +815,12 @@ namespace AutomationTool
 		{
 			Logger.LogWarning("GetExecutableSize() has not been implemented for {Arg0}", PlatformType.ToString());
 			return -1;
+		}
+
+		public virtual bool UpdatePatchPackagingParameters( string ProjectRoot, string BuildToUse, string AdditionalOptions )
+		{
+			Logger.LogWarning("UpdatePackagingParameters() has not been implemented for {Arg0}", PlatformType.ToString());
+			return false;
 		}
 
 		/// <summary>
