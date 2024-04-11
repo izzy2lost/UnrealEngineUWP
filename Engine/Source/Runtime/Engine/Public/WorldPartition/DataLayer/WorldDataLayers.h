472e47ec156307a5c9a9762a529f08f1a095a1bb
dominic couture <dominic.couture@epicgames.com>
2024-04-11 16:34:26 -0400

    #rb richard.malo
    [CL 32905886 by dominic couture in ue5-main branch]

--- Engine/Source/Runtime/Engine/Public/WorldPartition/DataLayer/WorldDataLayers.h
+++ Engine/Source/Runtime/Engine/Public/WorldPartition/DataLayer/WorldDataLayers.h
@@ -58,6 +58,7 @@ class AWorldDataLayers : public AInfo, public IDataLayerInstanceProvider
 public:
 	//~ Begin UObject Interface
 	ENGINE_API virtual void PostLoad() override;
+	ENGINE_API virtual void PostRegisterAllComponents() override;
 	virtual void Serialize(FArchive& Ar) override;
 	//~ End UObject Interface
 
