3bb843714be9260bdd6ea48c84d0d71ebc35771b
ben hoffman <ben.hoffman@epicgames.com>
2024-04-12 10:29:23 -0400

    [Backout] - CL32829489
    Original CL Desc
    -----------------------------------------------------------------
    Auto-expand Instanced Struct properties
    
    #rb Mikko.Mononen
    #jira none
    #ushell-cherrypick of 32823971 by kristof.morva1
    
    [CL 32929142 by ben hoffman in ue5-main branch]

--- Engine/Plugins/Experimental/StructUtils/Source/StructUtilsEditor/Private/Customizations/InstancedStructDetails.cpp
+++ Engine/Plugins/Experimental/StructUtils/Source/StructUtilsEditor/Private/Customizations/InstancedStructDetails.cpp
@@ -431,7 +431,6 @@ void FInstancedStructDetails::CustomizeHeader(TSharedRef<class IPropertyHandle> 
 	}
 
 	HeaderRow
-		.ShouldAutoExpand(true)
 		.NameContent()
 		[
 			StructPropertyHandle->CreatePropertyNameWidget()
