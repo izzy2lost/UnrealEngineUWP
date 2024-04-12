59c0c6218421bafcea4eeba21a110f4aa12c8b99
paul chipchase <paul.chipchase@epicgames.com>
2024-04-12 07:08:30 -0400

    Fix typo in log message when switching workspaces.
    #rb Per.Larsson
    #rnx
    
    [CL 32925564 by paul chipchase in ue5-main branch]

--- Engine/Plugins/Developer/PerforceSourceControl/Source/PerforceSourceControl/Private/PerforceSourceControlProvider.cpp
+++ Engine/Plugins/Developer/PerforceSourceControl/Source/PerforceSourceControl/Private/PerforceSourceControlProvider.cpp
@@ -910,7 +910,7 @@ ECommandResult::Type FPerforceSourceControlProvider::SwitchWorkspace(FStringView
 		P4Settings.SetWorkspace(WorkspaceName);
 	}
 
-	UE_LOG(LogSourceControl, Log, TEXT("Switched workspaces from '%s' to '%s%%'"), *OldWorkspaceName, *WorkspaceName);
+	UE_LOG(LogSourceControl, Log, TEXT("Switched workspaces from '%s' to '%s'"), *OldWorkspaceName, *WorkspaceName);
 
 	return ECommandResult::Succeeded;
 }
