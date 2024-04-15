12446ee336f9fece432cbea58ec312a25ad30b0f
jeanfrancois dube <JeanFrancois.Dube@epicgames.com>
2024-04-15 08:59:14 -0400

    [Backout] - CL32924228 [FYI] wouter.dek Original CL Desc ----------------------------------------------------------------- Perform DF shadows distance-culling based on full-resolution depth. Previously, undersampling depth meant some pixels were not shadowed by DF shadows or CSM, causing glint-like artifacts in fully shadowed regions at the boundary between these techniques. #jira UE-210854 #rb tiago.costa
    [CL 32969955 by jeanfrancois dube in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/DistanceFieldShadowing.cpp
+++ Engine/Source/Runtime/Renderer/Private/DistanceFieldShadowing.cpp
@@ -388,8 +388,6 @@ class FDistanceFieldShadowingUpsamplePS : public FGlobalShader
 		SHADER_PARAMETER(float, NearFadePlaneOffset)
 		SHADER_PARAMETER(float, InvNearFadePlaneLength)
 		SHADER_PARAMETER(float, OneOverDownsampleFactor)
-		SHADER_PARAMETER(float, MinDepth)
-		SHADER_PARAMETER(float, MaxDepth)
 	END_SHADER_PARAMETER_STRUCT()
 
 	class FUpsample : SHADER_PERMUTATION_BOOL("SHADOW_FACTORS_UPSAMPLE_REQUIRED");
@@ -761,22 +759,6 @@ bool FSceneRenderer::ShouldPrepareHeightFieldScene() const
 		&& SupportsHeightFieldShadows(Scene->GetFeatureLevel(), Scene->GetShaderPlatform());
 }
 
-void GetDistanceFieldShadowRange(const FProjectedShadowInfo* ProjectedShadowInfo, EDistanceFieldPrimitiveType PrimitiveType, float& OutMinDepth, float& OutMaxDepth)
-{
-	if (ProjectedShadowInfo->bDirectionalLight)
-	{
-		OutMinDepth = ProjectedShadowInfo->CascadeSettings.SplitNear - ProjectedShadowInfo->CascadeSettings.SplitNearFadeRegion;
-		OutMaxDepth = ProjectedShadowInfo->CascadeSettings.SplitFar;
-	}
-	else
-	{
-		check(PrimitiveType != DFPT_HeightField);
-		//@todo - set these up for point lights as well
-		OutMinDepth = 0.0f;
-		OutMaxDepth = HALF_WORLD_MAX;
-	}
-}
-
 void RayTraceShadows(
 	FRDGBuilder& GraphBuilder,
 	bool bAsyncCompute,
@@ -850,7 +832,18 @@ void RayTraceShadows(
 		PassParameters->TwoSidedMeshDistanceBiasScale = GDFShadowTwoSidedMeshDistanceBiasScale;
 		PassParameters->Substrate = Substrate::BindSubstrateGlobalUniformParameters(View);
 
-		GetDistanceFieldShadowRange(ProjectedShadowInfo, PrimitiveType, PassParameters->MinDepth, PassParameters->MaxDepth);
+		if (ProjectedShadowInfo->bDirectionalLight)
+		{
+			PassParameters->MinDepth = ProjectedShadowInfo->CascadeSettings.SplitNear - ProjectedShadowInfo->CascadeSettings.SplitNearFadeRegion;
+			PassParameters->MaxDepth = ProjectedShadowInfo->CascadeSettings.SplitFar;
+		}
+		else
+		{
+			check(!bHeightfield);
+			//@todo - set these up for point lights as well
+			PassParameters->MinDepth = 0.0f;
+			PassParameters->MaxDepth = HALF_WORLD_MAX;
+		}
 
 		PassParameters->DownsampleFactor = GetDFShadowDownsampleFactor();
 		const FIntPoint OutputBufferSize = OutputTexture->Desc.Extent;
@@ -1120,8 +1113,6 @@ void FProjectedShadowInfo::RenderRayTracedDistanceFieldProjection(
 		PassParameters->PS.ShadowFactorsSampler = TStaticSamplerState<SF_Bilinear>::GetRHI();
 		PassParameters->PS.ScissorRectMinAndSize = FIntRect(ScissorRect.Min, ScissorRect.Size());
 		PassParameters->PS.OneOverDownsampleFactor = 1.0f / GetDFShadowDownsampleFactor();
-
-		GetDistanceFieldShadowRange(this, DFPT_SignedDistanceField, PassParameters->PS.MinDepth, PassParameters->PS.MaxDepth);
 
 		if (bDirectionalLight && CascadeSettings.FadePlaneLength > 0)
 		{
