b22b94ae5311c67863cc584f09f149dcf1a4a069
christian sirois <christian.sirois@epicgames.com>
2024-04-15 13:53:24 -0400

    Modification to the Visualize Attribute PCG node to support larger point number display using the distance from the camera as a limit. #changelist validated [FYI] Wyatt.Marvil, julien.lheureux
    [CL 32974257 by christian sirois in ue5-main branch]

--- Engine/Plugins/PCG/Source/PCG/Private/Elements/PCGVisualizeAttribute.cpp
+++ Engine/Plugins/PCG/Source/PCG/Private/Elements/PCGVisualizeAttribute.cpp
@@ -8,6 +8,9 @@
 #include "Data/PCGPointData.h"
 #include "Metadata/Accessors/PCGAttributeAccessorHelpers.h"
 
+#include "LevelEditorViewport.h"
+#include "Math/UnrealMathUtility.h"
+
 #define LOCTEXT_NAMESPACE "PCGVisualizeAttributeElement"
 
 FPCGElementPtr UPCGVisualizeAttributeSettings::CreateElement() const
@@ -112,27 +115,51 @@ bool FPCGVisualizeAttribute::ExecuteInternal(FPCGContext* Context) const
 		TArray<FDebugRenderSceneProxy::FText3d> DebugStrings;
 		DebugStrings.Reserve(FMath::Min(Points.Num(), Settings->PointLimit));
 
-		// TODO: Optimize which points will be drawn by distance, or other measures, rather than by Index
+		// TODO: Optimize which points will be drawn by other measures, rather than only by Index and Distance
 		// Debug messages are already culled by camera frustum, but a point limit is desired to prevent overloading
-		for (int I = 0; I < Points.Num() && I < Settings->PointLimit; ++I)
+
+		// Store the number of points we add debug info too
+		int count = 0;
+
+		// Get the current camera location to use with the distance check
+		FViewportCameraTransform& ViewTransform = GCurrentLevelEditingViewportClient->GetViewTransform();
+		FVector CurrentCameraLocation = ViewTransform.GetLocation();
+
+		for (int I = 0; I < Points.Num(); ++I)
 		{
-			FStringBuilderBase CompoundedString;
-
-			CompoundedString += Settings->CustomPrefixString;
-
-			if (Settings->bPrefixWithIndex)
-			{
-				CompoundedString += FString::Printf(TEXT("[%d]"), I);
+			// Break when the number of points exceed the point limit
+			if (count > Settings->PointLimit) {
+				break;
 			}
 
-			if (Settings->bPrefixWithAttributeName)
+			// Get the current point location and the distance between it and the camera
+			FVector CurrentPointLocation = Points[I].Transform.GetLocation();
+			float Distance = FVector::DistSquared(CurrentPointLocation, CurrentCameraLocation);
+
+			// If the distance is smaller than the max allowed distance, do the display of attribute
+			if (Distance < FMath::Square(Settings->MaxDistance))
 			{
-				CompoundedString += FString::Printf(TEXT("[%s]"), *InputSource.GetDisplayText().ToString());
+				FStringBuilderBase CompoundedString;
+
+				CompoundedString += Settings->CustomPrefixString;
+
+				if (Settings->bPrefixWithIndex)
+				{
+					CompoundedString += FString::Printf(TEXT("[%d]"), I);
+				}
+
+				if (Settings->bPrefixWithAttributeName)
+				{
+					CompoundedString += FString::Printf(TEXT("[%s]"), *InputSource.GetDisplayText().ToString());
+				}
+
+				CompoundedString = FString::Printf(TEXT("%s %s"), *CompoundedString, *ValuesToString[I]);
+
+				DebugStrings.Emplace(CompoundedString.ToString(), Points[I].Transform.GetLocation() + Settings->LocalOffset, Settings->Color);
+
+				// Increment the count
+				count += 1;
 			}
-
-			CompoundedString = FString::Printf(TEXT("%s %s"), *CompoundedString, *ValuesToString[I]);
-
-			DebugStrings.Emplace(CompoundedString.ToString(), Points[I].Transform.GetLocation() + Settings->LocalOffset, Settings->Color);
 		}
 
 		check(DebugDrawComponent);
