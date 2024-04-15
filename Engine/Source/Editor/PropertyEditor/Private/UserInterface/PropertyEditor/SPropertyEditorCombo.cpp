7082a884053271a467753acec7f81a445ef9f426
logan buchy <logan.buchy@epicgames.com>
2024-04-15 17:19:06 -0400

    Fix SPropertyEditorCombo firing extra OnChanged event when opened * Introduced with the new GetOptions feature to use alternative display names instead of values for FText and FStrings.  Root cause is that the PropertyHandle uses a new GeneratePossibleValues API which returns FStrings and then later converts them to TSharedPtr<FStrings> for the combo box. The value equality check with currently selected item needed to be re-addressed since it was using the OutComboBoxStrings which had not yet been filled-in in the new code.
    #rb brooke.hubert
    #jira UE-205226
    
    [CL 32976976 by logan buchy in ue5-main branch]

--- Engine/Source/Editor/PropertyEditor/Private/UserInterface/PropertyEditor/SPropertyEditorCombo.cpp
+++ Engine/Source/Editor/PropertyEditor/Private/UserInterface/PropertyEditor/SPropertyEditorCombo.cpp
@@ -156,27 +156,6 @@ void SPropertyEditorCombo::GenerateComboBoxStrings( TArray< TSharedPtr<FString> 
 	
 	bUsesAlternateDisplayValues = ComboArgs.PropertyHandle->GeneratePossibleValues(ValueStrings, BasicTooltips, OutRestrictedItems, &DisplayNames);
 
-	// If we regenerate the entries, let's make sure that the currently selected item has the same shared pointer as
-	// the newly generated item with the same value, so that the generation of elements won't immediately result in a
-	// value changed event (i.e. at every single `OnComboOpening`).
-	if (ComboBox)
-	{
-		if (const TSharedPtr<FString>& SelectedItem = ComboBox->GetSelectedItem())
-		{
-			for (TSharedPtr<FString>& Item : OutComboBoxStrings)
-			{
-				if (Item)
-				{
-					if (*SelectedItem.Get() == *Item.Get())
-					{
-						Item = SelectedItem;
-						break;
-					}
-				}
-			}
-		}
-	}
-
 	// Build the reverse LUT for alternate display values
 	AlternateDisplayValueToInternalValue.Reset();
 	InternalValueToAlternateDisplayValue.Reset();
@@ -203,6 +182,27 @@ void SPropertyEditorCombo::GenerateComboBoxStrings( TArray< TSharedPtr<FString> 
 	else
 	{
 		Algo::Transform(ValueStrings, OutComboBoxStrings, [](const FString& Str) { return MakeShared<FString>(Str); });
+	}
+	
+	// If we regenerate the entries, let's make sure that the currently selected item has the same shared pointer as
+	// the newly generated item with the same value, so that the generation of elements won't immediately result in a
+	// value changed event (i.e. at every single `OnComboOpening`).
+	if (ComboBox)
+	{
+		if (const TSharedPtr<FString>& SelectedItem = ComboBox->GetSelectedItem())
+		{
+			for (TSharedPtr<FString>& Item : OutComboBoxStrings)
+			{
+				if (Item)
+				{
+					if (*SelectedItem.Get() == *Item.Get())
+					{
+						Item = SelectedItem;
+						break;
+					}
+				}
+			}
+		}
 	}
 
 	// For enums, look for rich tooltip information
