ed06c2389e207f1007b9a4c7550e2a6bee464137
Yuriy ODonnell <Yuriy.ODonnell@epicgames.com>
2024-04-16 15:40:51 -0400

    Workaround for pixel shader UAV write unit test affecting some platforms
    Platform shader compiler generates unsafe instructions under certain conditions when writing to a typed buffer. The workaround is to use a structured buffer, which avoids the problematic edge case.
    
    #jira UE-212251
    #rb dave.barrett, mihnea.balta
    
    [CL 33011414 by Yuriy ODonnell in ue5-main branch]

--- Engine/Plugins/Tests/RHITests/Source/RHITests/Private/RHIGraphicsUAVTests.cpp
+++ Engine/Plugins/Tests/RHITests/Source/RHITests/Private/RHIGraphicsUAVTests.cpp
@@ -103,14 +103,15 @@ bool FRHIGraphicsUAVTests::Test_GraphicsUAV_PixelShader(FRHICommandListImmediate
 	static constexpr uint32 OutputBufferSize = OutputBufferStride * MaxInstances;
 	FRHIResourceCreateInfo OutputBufferCreateInfo(TEXT("GraphicsUAVTests_PixelShaderOutput"));
 
+	// NOTE: using a structured buffer here as workaround for UE-212251
 	FBufferRHIRef OutputBuffer = RHICmdList.CreateBuffer(OutputBufferSize,
-		EBufferUsageFlags::UnorderedAccess | EBufferUsageFlags::SourceCopy,
+		EBufferUsageFlags::UnorderedAccess | EBufferUsageFlags::SourceCopy | EBufferUsageFlags::StructuredBuffer,
 		OutputBufferStride, ERHIAccess::UAVCompute, OutputBufferCreateInfo);
 
 	FUnorderedAccessViewRHIRef OutputBufferUAV = RHICmdList.CreateUnorderedAccessView(OutputBuffer,
 		FRHIViewDesc::CreateBufferUAV()
-			.SetType(FRHIViewDesc::EBufferType::Typed)
-			.SetFormat(PF_R32_UINT));
+			.SetType(FRHIViewDesc::EBufferType::Structured)
+			.SetStride(4));
 
 	RHICmdList.ClearUAVUint(OutputBufferUAV, FUintVector4(~0u));
 
