c2c66e16ed389af0ae7542768a38ec21c9d3e38b
charles bloom <charles.bloom@epicgames.com>
2024-04-18 18:38:36 -0400

    Texture2D : nop comment only
    [CL 33089428 by charles bloom in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Texture2D.cpp
+++ Engine/Source/Runtime/Engine/Private/Texture2D.cpp
@@ -711,6 +711,8 @@ void UTexture2D::GetAssetRegistryTags(FAssetRegistryTagsContext Context) const
 	//	it does not tell you if the texture actually has non-opaque alpha
 	Context.AddTag( FAssetRegistryTag("HasAlphaChannel", HasAlphaChannel() ? TEXT("True") : TEXT("False"), FAssetRegistryTag::TT_Alphabetical) );
 	Context.AddTag( FAssetRegistryTag("Format", GPixelFormats[GetPixelFormat()].Name, FAssetRegistryTag::TT_Alphabetical) );
+	
+	// @todo Oodle : this should be in UTexture:: , not 2D::
 #if WITH_EDITORONLY_DATA
 	const FString PowerOfTwoModeStr = StaticEnum<ETexturePowerOfTwoSetting::Type>()->GetNameStringByValue(static_cast<int64>(PowerOfTwoMode));
 	Context.AddTag(FAssetRegistryTag("PowerOfTwoMode", *PowerOfTwoModeStr, FAssetRegistryTag::TT_Alphabetical));
