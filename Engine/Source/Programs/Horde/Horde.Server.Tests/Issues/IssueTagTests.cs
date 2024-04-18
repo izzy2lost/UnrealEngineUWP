b5a47e70e3068570aa3bfde22bd27deee5d98d29
ben marsh <Ben.Marsh@epicgames.com>
2024-04-18 19:05:25 -0400

    Horde: Fix static analysis warnings.
    #rnx
    
    [CL 33090317 by ben marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/Horde.Server.Tests/Issues/IssueTagTests.cs
+++ Engine/Source/Programs/Horde/Horde.Server.Tests/Issues/IssueTagTests.cs
@@ -1,10 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Horde.Server.Issues;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
