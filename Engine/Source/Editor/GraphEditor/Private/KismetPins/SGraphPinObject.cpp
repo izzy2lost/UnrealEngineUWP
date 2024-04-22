a98b5cde73654355ab4e8e24133b0572a5856bec
miro salminen <miro.salminen@xa.epicgames.com>
2024-04-22 02:51:10 -0400

    Selector drop down doesn't highlight current sound for quick playback #rb brooke.hubert #jira UE-171028
    [CL 33133975 by miro salminen in ue5-main branch]

--- Engine/Source/Editor/GraphEditor/Private/KismetPins/SGraphPinObject.cpp
+++ Engine/Source/Editor/GraphEditor/Private/KismetPins/SGraphPinObject.cpp
@@ -244,6 +244,7 @@ TSharedRef<SWidget> SGraphPinObject::GenerateAssetPicker()
 	AssetPickerConfig.OnAssetEnterPressed = FOnAssetEnterPressed::CreateSP(this, &SGraphPinObject::OnAssetEnterPressedInPicker);
 	AssetPickerConfig.InitialAssetViewType = EAssetViewType::List;
 	AssetPickerConfig.bAllowDragging = false;
+	AssetPickerConfig.InitialAssetSelection = CachedAssetData.GetAsset();
 
 	UObject* OwningAsset = GraphPinObj->GetOwningNode();
 	while (OwningAsset && !OwningAsset->IsAsset())
