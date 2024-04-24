ff2fbf124f348e424bbacf926b42a061272a44e6
jurre debaare <jurre.debaare@epicgames.com>
2024-04-24 05:47:45 -0400

    Add override for details view creation to BaseAssetToolkit #rb aditya.ravichandran
    [CL 33195025 by jurre debaare in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Public/Tools/BaseAssetToolkit.h
+++ Engine/Source/Editor/UnrealEd/Public/Tools/BaseAssetToolkit.h
@@ -59,6 +59,7 @@ protected:
 	UNREALED_API virtual TSharedPtr<FEditorViewportClient> CreateEditorViewportClient() const;
 	UNREALED_API virtual TSharedRef<SDockTab> SpawnTab_Viewport(const FSpawnTabArgs& Args);
 	UNREALED_API virtual TSharedRef<SDockTab> SpawnTab_Details(const FSpawnTabArgs& Args);
+	UNREALED_API virtual TSharedRef<IDetailsView> CreateDetailsView();
 
 protected:
 	/** Property View */
