d6b45687f8001fee92aa198c6eba3f3296f8b152
yoan stamant <Yoan.Stamant@epicgames.com>
2024-04-25 12:20:47 -0400

    [Backout] - CL33232683 [FYI] Yoan.StAmant Original CL Desc ----------------------------------------------------------------- [StateTreeDebugger] enabled debugger on all desktop platforms #jira UE-192305 #rb mikko.mononen
    [CL 33234412 by yoan stamant in ue5-main branch]

--- Engine/Plugins/Runtime/StateTree/Source/StateTreeModule/StateTreeModule.Build.cs
+++ Engine/Plugins/Runtime/StateTree/Source/StateTreeModule/StateTreeModule.Build.cs
@@ -1,7 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-using System.Linq;
-
 namespace UnrealBuildTool.Rules
 {
 	public class StateTreeModule : ModuleRules
@@ -46,8 +44,8 @@ namespace UnrealBuildTool.Rules
 				);
 			}
 
-			// Until we split use of TraceServices and TraceAnalysis in the debugger we enable only on platforms supporting both at the moment
-			if (Target.Platform.IsInGroup(UnrealPlatformGroup.Desktop) && (Target.Configuration != UnrealTargetConfiguration.Shipping || Target.bBuildEditor))
+			if (Target.Platform == UnrealTargetPlatform.Win64 && 
+				(Target.Configuration != UnrealTargetConfiguration.Shipping || Target.bBuildEditor))
 			{
 				PublicDefinitions.Add("WITH_STATETREE_DEBUGGER=1");
 				PublicDependencyModuleNames.AddRange(
