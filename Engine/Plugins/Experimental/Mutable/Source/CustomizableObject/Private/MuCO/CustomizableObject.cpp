d00f5f59961586dc15512308af64007f37492ada
gerard martin <gerard.martin@epicgames.com>
2024-04-26 05:24:50 -0400

    [Mutable] Fix missing implicit FMutableParameterData and FIntegerParameterUIData constructors on some compilers.
    #rnx
    
    [CL 33259244 by gerard martin in ue5-main branch]

--- Engine/Plugins/Experimental/Mutable/Source/CustomizableObject/Private/MuCO/CustomizableObject.cpp
+++ Engine/Plugins/Experimental/Mutable/Source/CustomizableObject/Private/MuCO/CustomizableObject.cpp
@@ -2442,6 +2442,19 @@ bool FMutableSkinWeightProfileInfo::operator==(const FMutableSkinWeightProfileIn
 }
 
 
+FIntegerParameterUIData::FIntegerParameterUIData(const FMutableParamUIMetadata& InParamUIMetadata)
+{
+	ParamUIMetadata = InParamUIMetadata;
+}
+
+
+FMutableParameterData::FMutableParameterData(const FMutableParamUIMetadata& InParamUIMetadata, EMutableParameterType InType)
+{
+	ParamUIMetadata = InParamUIMetadata;
+	Type = InType;
+}
+
+
 #if WITH_EDITORONLY_DATA
 FArchive& operator<<(FArchive& Ar, FMutableRemappedBone& RemappedBone)
 {
