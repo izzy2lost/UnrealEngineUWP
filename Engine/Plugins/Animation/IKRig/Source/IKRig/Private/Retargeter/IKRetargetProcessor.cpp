8d9848b0dd507f6be3d3523039f3f9be1e16463f
kiaran ritchie <kiaran.ritchie@epicgames.com>
2024-04-29 17:44:28 -0400

    Fixes issue, retargeter freezes after editing retarget pose.
    #rb halfdan.ingvarsson
    #JIRA UE-213551
    
    [CL 33316546 by kiaran ritchie in ue5-main branch]

--- Engine/Plugins/Animation/IKRig/Source/IKRig/Private/Retargeter/IKRetargetProcessor.cpp
+++ Engine/Plugins/Animation/IKRig/Source/IKRig/Private/Retargeter/IKRetargetProcessor.cpp
@@ -2302,6 +2302,12 @@ bool UIKRetargetProcessor::WasInitializedWithTheseAssets(
 void UIKRetargetProcessor::SetNeedsInitialized()
 {
 	bIsInitialized = false;
+
+	if (RetargeterAsset)
+	{
+		RetargeterAsset->IncrementVersion(); // triggers re-init
+	}
+	
 	if (IKRigProcessor)
 	{
 		// may not be initialized yet (during setup as prerequisites are being created)
