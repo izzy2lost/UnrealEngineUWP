25d83fecdbfdc3cc34405996dc59fa038f927537
chris constantinescu <chris.constantinescu@epicgames.com>
2024-04-29 13:20:08 -0400

    Merging
    Main/Engine/Build/InstalledEngineFilters.xml
    Main/Engine/Source/Developer/LowLevelTestsRunner/LowLevelTestsRunner.Build.cs
    Main/Engine/Source/ThirdParty/Catch2/Catch2Extras.Build.cs
    
    to //UE5/Release-5.4/Engine/Source/
    
    #jira UE-210257
    
    [CL 33306012 by chris constantinescu in ue5-main branch]

--- Engine/Source/Developer/LowLevelTestsRunner/LowLevelTestsRunner.Build.cs
+++ Engine/Source/Developer/LowLevelTestsRunner/LowLevelTestsRunner.Build.cs
@@ -42,12 +42,6 @@ namespace UnrealBuildTool.Rules
 			{
 				PrivateDependencyModuleNames.Add("CoreUObject");
 			}
-
-			if (Target.Platform == UnrealTargetPlatform.Android)
-			{
-				string ModulePath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
-				AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(ModulePath, "LowLevelTestsAndroid_UPL.xml"));
-			}
 		}
 	}
 }
