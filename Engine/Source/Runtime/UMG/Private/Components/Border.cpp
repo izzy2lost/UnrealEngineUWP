54025a3f93d826130fcc22a2a77593b39250108c
nickdarnell <nickdarnell@users.noreply.github.com>
2024-04-29 12:15:06 -0400

    [Backout] - CL32624676. The assumption that the image is always the outer for the material is not true.
    Original CL Desc
    -----------------------------------------------------------------
    PR #11546: Fixing UMG Image and Border's GetDynamicMaterial functions not checking MID outer
    
    #jira UE-208062
    #rb daren.cheng
    
    [CL 33303387 by nickdarnell in ue5-main branch]

--- Engine/Source/Runtime/UMG/Private/Components/Border.cpp
+++ Engine/Source/Runtime/UMG/Private/Components/Border.cpp
@@ -313,7 +313,7 @@ UMaterialInstanceDynamic* UBorder::GetDynamicMaterial()
 	{
 		UMaterialInstanceDynamic* DynamicMaterial = Cast<UMaterialInstanceDynamic>(Material);
 
-		if ( !DynamicMaterial || DynamicMaterial->GetOuter() != this)
+		if ( !DynamicMaterial )
 		{
 			DynamicMaterial = UMaterialInstanceDynamic::Create(Material, this);
 			Background.SetResourceObject(DynamicMaterial);
@@ -427,3 +427,4 @@ const FText UBorder::GetPaletteCategory()
 /////////////////////////////////////////////////////
 
 #undef LOCTEXT_NAMESPACE
+
--- Engine/Source/Runtime/UMG/Private/Components/Image.cpp
+++ Engine/Source/Runtime/UMG/Private/Components/Image.cpp
@@ -391,7 +391,7 @@ void UImage::SetBrushFromSoftMaterial(TSoftObjectPtr<UMaterialInterface> SoftMat
 PRAGMA_DISABLE_DEPRECATION_WARNINGS
 UMaterialInstanceDynamic* UImage::GetDynamicMaterial()
 {
-	UMaterialInterface* Material = nullptr;
+	UMaterialInterface* Material = NULL;
 
 	UObject* Resource = Brush.GetResourceObject();
 	Material = Cast<UMaterialInterface>(Resource);
@@ -400,7 +400,7 @@ UMaterialInstanceDynamic* UImage::GetDynamicMaterial()
 	{
 		UMaterialInstanceDynamic* DynamicMaterial = Cast<UMaterialInstanceDynamic>(Material);
 
-		if ( !DynamicMaterial || DynamicMaterial->GetOuter() != this )
+		if ( !DynamicMaterial )
 		{
 			DynamicMaterial = UMaterialInstanceDynamic::Create(Material, this);
 			SetBrushResourceObject(DynamicMaterial);
@@ -415,7 +415,7 @@ UMaterialInstanceDynamic* UImage::GetDynamicMaterial()
 
 	//TODO UMG can we do something for textures?  General purpose dynamic material for them?
 
-	return nullptr;
+	return NULL;
 }
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -449,3 +449,4 @@ const FText UImage::GetPaletteCategory()
 /////////////////////////////////////////////////////
 
 #undef LOCTEXT_NAMESPACE
+
