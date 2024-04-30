82758d07bfc3752ffc029580748e270f97b4aba1
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2024-04-30 04:28:21 -0400

    Do not invalidate skylights on scalability changes for platforms that do not support realtime reflection capture
    [CL 33325733 by dmitriy dyomin in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Components/SkyLightComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/SkyLightComponent.cpp
@@ -85,8 +85,18 @@ FAutoConsoleVariableRef CVarSkylightIntensityMultiplier(
 	ECVF_Scalability | ECVF_RenderThreadSafe
 	);
 
+static bool FeatureLevelSupportsRealTimeReflectionCapture(FStaticFeatureLevel FeatureLevel)
+{
+	return FeatureLevel > ERHIFeatureLevel::ES3_1;
+}
+
 void OnChangeSkylightRealTimeReflectionCapture(IConsoleVariable* Var)
 {
+	if (!FeatureLevelSupportsRealTimeReflectionCapture(GMaxRHIFeatureLevel))
+	{
+		return;
+	}
+	
 	// r.SkyLight.RealTimeReflectionCapture is set based on the "Effect" quality level (to be supported, or not, on some different platofrms).
 	// When that quality level changes, real-time sky capture can become disabled. In this case, sky light recapture should be scheduled to match the current quality level.
 	for (TObjectIterator<USkyLightComponent> It; It; ++It)
@@ -1121,8 +1131,8 @@ bool USkyLightComponent::IsRealTimeCaptureEnabled() const
 	// We currently disable realtime capture on mobile, OGL requires an additional texture to read SkyIrradianceEnvironmentMap which can break materials already at the texture limit.
 	// See FORT-301037, FORT-302324	
 	// Don't call in PostLoad and SetCaptureIsDirty, because the LocalScene could be null and sky wouldn't be updated on mobile.
-	const bool bIsMobile = LocalScene && LocalScene->GetFeatureLevel() <= ERHIFeatureLevel::ES3_1;
-	return bRealTimeCapture && (Mobility == EComponentMobility::Movable || Mobility == EComponentMobility::Stationary) && GSkylightRealTimeReflectionCapture >0 && !bIsMobile;
+	const bool bFeatureLevelSupport = !LocalScene || FeatureLevelSupportsRealTimeReflectionCapture(LocalScene->GetFeatureLevel());
+	return bRealTimeCapture && (Mobility == EComponentMobility::Movable || Mobility == EComponentMobility::Stationary) && GSkylightRealTimeReflectionCapture >0 && bFeatureLevelSupport;
 }
 
 void USkyLightComponent::SetRealTimeCaptureEnabled(bool bNewRealTimeCaptureEnabled)
