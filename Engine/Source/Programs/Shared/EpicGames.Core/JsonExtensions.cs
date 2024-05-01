1f783f9eb6b614a8548548b710f3ba288258621c
joe kirchoff <joe.kirchoff@epicgames.com>
2024-05-01 17:13:02 -0400

    Various fixes to allow for compiling shared projects with target framework newer than .net6 where apis have changed
    #rnx
    [FYI] ben.marsh
    
    [CL 33383137 by joe kirchoff in ue5-main branch]

--- Engine/Source/Programs/Shared/EpicGames.Core/JsonExtensions.cs
+++ Engine/Source/Programs/Shared/EpicGames.Core/JsonExtensions.cs
@@ -4,8 +4,6 @@ using System;
 using System.Diagnostics;
 using System.Text.Json;
 
-#pragma warning disable CA1045 // Do not pass types by reference
-
 namespace EpicGames.Core
 {
 	/// <summary>
@@ -19,7 +17,7 @@ namespace EpicGames.Core
 		/// <param name="reader">Token reader</param>
 		/// <param name="propertyName">Receives the property name on success</param>
 		/// <returns>True if the read succeeded</returns>
-		public static bool TryReadNextPropertyName(ref Utf8JsonReader reader, out ReadOnlySpan<byte> propertyName)
+		public static bool TryReadNextPropertyName(this Utf8JsonReader reader, out ReadOnlySpan<byte> propertyName)
 		{
 			if (reader.Read() && reader.TokenType == JsonTokenType.PropertyName)
 			{
