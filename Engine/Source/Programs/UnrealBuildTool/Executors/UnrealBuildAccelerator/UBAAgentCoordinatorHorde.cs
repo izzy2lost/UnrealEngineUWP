1ae7601467b1e078b6b8de1a764782ab76b47d41
joe kirchoff <joe.kirchoff@epicgames.com>
2024-05-01 19:03:27 -0400

    [Backout] - CL33375955 [FYI] henrik.karlsson Original CL Desc ----------------------------------------------------------------- [UBA] * Fixed so if no horde server url is set we early out and write a log entry saying horde is disabled because no url
    [CL 33385949 by joe kirchoff in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildTool/Executors/UnrealBuildAccelerator/UBAAgentCoordinatorHorde.cs
+++ Engine/Source/Programs/UnrealBuildTool/Executors/UnrealBuildAccelerator/UBAAgentCoordinatorHorde.cs
@@ -373,12 +373,6 @@ namespace UnrealBuildTool
 				return null;
 			}
 
-			if (String.IsNullOrEmpty(hordeConfig.HordeServer))
-			{
-				logger.LogInformation("Horde disabled. Url not set.");
-				return null;
-			}
-
 			Uri? server = (hordeConfig.HordeServer == null) ? null : new Uri(hordeConfig.HordeServer);
 			string? token = hordeConfig.HordeToken;
 
