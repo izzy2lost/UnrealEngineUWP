efd94c4e4b6da5d4c4cdea2e175cf59e126de2f4
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2024-05-02 02:26:59 -0400

    Metal: End current render pass on RHIEndRenderPass #rb Jack.Porter [FYI] carl.lloyd
    [CL 33391493 by dmitriy dyomin in ue5-main branch]

--- Engine/Source/Runtime/Apple/MetalRHI/Private/MetalRHIContext.cpp
+++ Engine/Source/Runtime/Apple/MetalRHI/Private/MetalRHIContext.cpp
@@ -130,6 +130,8 @@ void FMetalRHICommandContext::RHIEndRenderPass()
 	{
 		RHIEndOcclusionQueryBatch();
 	}
+    
+    Context->EndRenderPass();
 
 	UE::RHICore::ResolveRenderPassTargets(RenderPassInfo, [this](UE::RHICore::FResolveTextureInfo Info)
 	{
