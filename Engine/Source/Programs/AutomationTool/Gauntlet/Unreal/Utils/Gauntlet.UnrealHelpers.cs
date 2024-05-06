ef364e883f6b48bd6dd4500b058ae1af8145df73
jerome delattre <Jerome.Delattre@epicgames.com>
2024-05-06 12:37:12 -0400

    Add DebugGame configuration as discoverable executable
    #rb rob.huyett, sean.sweeney, sebastian.lewicki
    #rnx
    
    [CL 33460100 by jerome delattre in ue5-main branch]

--- Engine/Source/Programs/AutomationTool/Gauntlet/Unreal/Utils/Gauntlet.UnrealHelpers.cs
+++ Engine/Source/Programs/AutomationTool/Gauntlet/Unreal/Utils/Gauntlet.UnrealHelpers.cs
@@ -351,7 +351,7 @@ namespace Gauntlet
 			{
 				ProjectNameRegEx += string.Format("|{0}", ModuleAndRole.Key);
 			}
-			string RegExMatch = string.Format(@"^(?:.+[_-])?(({0}(Game|Client|Server|CookedEditor)){1})(?:-(.+?)-(Debug|Test|Shipping))?(?:[_-](.+))?$", ShortName, ProjectNameRegEx);
+			string RegExMatch = string.Format(@"^(?:.+[_-])?(({0}(Game|Client|Server|CookedEditor)){1})(?:-(.+?)-(Debug|DebugGame|Test|Shipping))?(?:[_-](.+))?$", ShortName, ProjectNameRegEx);
 
 			// Format should be something like
 			// FortniteClient
