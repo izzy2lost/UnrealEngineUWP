bcb35e71d0c3cbc5b277023132fe49a4d24821e3
roland munguia <roland.munguia@epicgames.com>
2024-05-06 12:56:26 -0400

    RESOLUTION: Conflict in FPoseSearchDatabaseAnimationAssetBase::GetEffectiveSamplingRange(). Took repo changes and applied the fix manually (handling cacse where minimum is greated than the maximum).
    Fixed SamplingRange not being clamped to the assset's play length in the details panel. Additionally, fixed the GetEffectiveSamplingRange() method handle the case where the SamplingRange's minimum is greater than the maximum.
    
    #jira UE-213089
    [RN] minor
    #rb samuele.rigamonti
    
    [CL 33460652 by roland munguia in ue5-main branch]

--- Engine/Plugins/Animation/PoseSearch/Source/Runtime/Private/PoseSearchDatabase.cpp
+++ Engine/Plugins/Animation/PoseSearch/Source/Runtime/Private/PoseSearchDatabase.cpp
@@ -399,6 +399,14 @@ FFloatInterval FPoseSearchDatabaseAnimationAssetBase::GetEffectiveSamplingRange(
 	FFloatInterval Range;
 	Range.Min = bSampleAll ? 0.0f : SamplingRange.Min;
 	Range.Max = bSampleAll ? PlayLength : FMath::Min(PlayLength, SamplingRange.Max);
+
+	if (Range.Min > Range.Max)
+	{
+		UE_LOG(LogPoseSearch, Warning, TEXT("Sampling range minimum (%f) is greated than max (%f). Setting min to be equal to max."), Range.Min, Range.Max)
+		
+		Range.Min = Range.Max;
+	}
+
 	return Range;
 }
 #endif // WITH_EDITORONLY_DATA
