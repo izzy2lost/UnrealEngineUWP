3cd7b9bfd0930275f268ec05b3ea03b6f386842d
henrik karlsson <henrik.karlsson@epicgames.com>
2024-05-08 17:54:05 -0400

    [UBA] * Fix so Rpc_GetEntryOffset handle paths that has no path separators in them.
    [CL 33530543 by henrik karlsson in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildAccelerator/Detours/Private/UbaDetoursFileMappingTable.cpp
+++ Engine/Source/Programs/UnrealBuildAccelerator/Detours/Private/UbaDetoursFileMappingTable.cpp
@@ -180,7 +180,11 @@ namespace uba
 			return dirTableOffset;
 
 		const tchar* lastPathSeparator = TStrrchr(entryName, PathSeparator);
-		UBA_ASSERTF(lastPathSeparator, TC("No path separator found in %s"), TStrlen(entryName) > 0 ? entryName : TC("(NULL)"));
+		if (!lastPathSeparator)
+		{
+			UBA_ASSERTF(lastPathSeparator, TC("No path separator found in %s"), TStrlen(entryName) > 0 ? entryName : TC("(NULL)"));
+			return ~u32(0);
+		}
 
 		#if PLATFORM_WINDOWS
 		UBA_ASSERT(wcsncmp(entryName, g_systemTemp.data, g_systemTemp.count) != 0);
