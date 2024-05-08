fe12837fe8fda9ad806d0b447e78df2780e6c63a
charles bloom <charles.bloom@epicgames.com>
2024-05-08 11:21:06 -0400

    TextureImportSettings AutoLimitDimension option to resize textures on import to limit texture source sizes #rb dan.thompson
    [CL 33514513 by charles bloom in ue5-main branch]

--- Engine/Source/Runtime/TextureUtilitiesCommon/Private/TextureImportSettings.cpp
+++ Engine/Source/Runtime/TextureUtilitiesCommon/Private/TextureImportSettings.cpp
@@ -52,6 +52,53 @@ ETextureImportPNGInfill UTextureImportSettings::GetPNGInfillMapDefault() const
 	}
 }
 
+bool UTextureImportSettings::IsImportAutoVTEnabled() const
+{
+	if ( AutoVTSize <= 0 )
+	{
+		return false;
+	}
+
+	static const TConsoleVariableData<int32>* CVarVirtualTexturesEnabled = IConsoleManager::Get().FindTConsoleVariableDataInt(TEXT("r.VirtualTextures"));
+	check(CVarVirtualTexturesEnabled);
+
+	static const auto CVarVirtualTexturesAutoImportEnabled = IConsoleManager::Get().FindTConsoleVariableDataInt(TEXT("r.VT.EnableAutoImport"));
+	check(CVarVirtualTexturesAutoImportEnabled);
+
+	return ( CVarVirtualTexturesEnabled->GetValueOnAnyThread() && CVarVirtualTexturesAutoImportEnabled->GetValueOnAnyThread() );
+}
+
+int64 UTextureImportSettings::GetAutoLimitPixelCount() const
+{
+	if ( AutoLimitDimension <= 0 )
+	{
+		// no limit
+		return 0;
+	}
+	
+	int64 UseAutoLimitDimension = AutoLimitDimension;
+
+	UseAutoLimitDimension = FMath::Min<int64>(UseAutoLimitDimension, UTexture::GetMaximumDimensionOfNonVT());
+
+	if ( IsImportAutoVTEnabled() )
+	{
+		if ( AutoVTSize != AutoLimitDimension )
+		{
+			// AutoVTSize and AutoLimitDimension cannot both be enabled and be different
+
+			UE_CALL_ONCE( [&](){	
+				UE_LOG(LogCore, Warning, TEXT("VT is enabled with AutoVTSize (%d) not equal AutoLimitDimension (%d); they must be equal or zero, fix config!  Ignoring AutoLimitDimension and using AutoVTSize."), 
+					AutoVTSize,AutoLimitDimension
+					);	
+			} );
+
+			UseAutoLimitDimension = AutoVTSize;
+		}
+	}
+
+	return UseAutoLimitDimension*UseAutoLimitDimension;
+}
+
 #if WITH_EDITOR
 void UTextureImportSettings::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
 {
@@ -151,38 +198,34 @@ namespace UE::TextureUtilitiesCommon
 	{
 		// If the texture is larger than a certain threshold make it VT.
 		// Note that previously for re-imports we still checked size and potentially changed the VT status.
-		// But that was unintuitive for many users so now for re-imports we will end up ignoring this and respecting the existing setting below.
+		// But that was unintuitive for many users so now for re-imports we will end up ignoring this and respecting the existing setting.
 		
-		static const TConsoleVariableData<int32>* CVarVirtualTexturesEnabled = IConsoleManager::Get().FindTConsoleVariableDataInt(TEXT("r.VirtualTextures"));
-		check(CVarVirtualTexturesEnabled);
-
-		static const auto CVarVirtualTexturesAutoImportEnabled = IConsoleManager::Get().FindTConsoleVariableDataInt(TEXT("r.VT.EnableAutoImport"));
-		check(CVarVirtualTexturesAutoImportEnabled);
-
-		if (CVarVirtualTexturesEnabled->GetValueOnAnyThread() && CVarVirtualTexturesAutoImportEnabled->GetValueOnAnyThread())
+		if ( ! GetDefault<UTextureImportSettings>()->IsImportAutoVTEnabled() )
 		{
-			const int64 VirtualTextureAutoEnableThreshold = GetDefault<UTextureImportSettings>()->AutoVTSize;
+			return false;
+		}
 
-			if ( VirtualTextureAutoEnableThreshold == 0 )
-			{
-				return false;
-			}
+		const int64 VirtualTextureAutoEnableThreshold = GetDefault<UTextureImportSettings>()->AutoVTSize;
 
-			const int64 VirtualTextureAutoEnableThresholdPixels = VirtualTextureAutoEnableThreshold * VirtualTextureAutoEnableThreshold;
+		if ( VirtualTextureAutoEnableThreshold <= 0 )
+		{
+			return false;
+		}
+			
+		const int64 VirtualTextureAutoEnableThresholdPixels = VirtualTextureAutoEnableThreshold * VirtualTextureAutoEnableThreshold;
 
-			// We do this in pixels so a 8192 x 128 texture won't get VT enabled 
-			// We use the Source size instead of simple Texture2D->GetSizeX() as this uses the size of the platform data
-			// however for a new texture platform data may not be generated yet, and for an reimport of a texture this is the size of the
-			// old texture. 
-			// Using source size gives one small caveat. It looks at the size before mipmap power of two padding adjustment.
-			// Textures with more than 1 block (UDIM textures) must be imported as VT
-			if (Texture->Source.GetNumBlocks() > 1 ||
-				( (int64) Texture->Source.GetSizeX() * Texture->Source.GetSizeY() ) >= VirtualTextureAutoEnableThresholdPixels ||
-				Texture->Source.GetSizeX() > UTexture::GetMaximumDimensionOfNonVT() ||
-				Texture->Source.GetSizeY() > UTexture::GetMaximumDimensionOfNonVT() )
-			{
-				return true;
-			}
+		// We do this in pixels so a 8192 x 128 texture won't get VT enabled 
+		// We use the Source size instead of simple Texture2D->GetSizeX() as this uses the size of the platform data
+		// however for a new texture platform data may not be generated yet, and for an reimport of a texture this is the size of the
+		// old texture. 
+		// Using source size gives one small caveat. It looks at the size before mipmap power of two padding adjustment.
+		// Textures with more than 1 block (UDIM textures) must be imported as VT
+		if (Texture->Source.GetNumBlocks() > 1 ||
+			( (int64) Texture->Source.GetSizeX() * Texture->Source.GetSizeY() ) >= VirtualTextureAutoEnableThresholdPixels ||
+			Texture->Source.GetSizeX() > UTexture::GetMaximumDimensionOfNonVT() ||
+			Texture->Source.GetSizeY() > UTexture::GetMaximumDimensionOfNonVT() )
+		{
+			return true;
 		}
 
 		return false;
