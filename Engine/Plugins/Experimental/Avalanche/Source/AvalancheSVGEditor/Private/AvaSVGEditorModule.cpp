a3e9dbed8c267a31a47c591318a7ef5228925eec
loris rossi <loris.rossi@geotech.one>
2024-05-08 17:56:48 -0400

    MotionDesign : SVG - Fixed crash when SVG Importer module is not yet loaded when AvaSVGActorTool gets FSVGImporterEditorCommands
    #jira UE-213977
    #rb Dario.Mazzanti
    [FYI] Dario.Mazzanti
    
    [CL 33530733 by loris rossi in ue5-main branch]

--- Engine/Plugins/Experimental/Avalanche/Source/AvalancheSVGEditor/Private/AvaSVGEditorModule.cpp
+++ Engine/Plugins/Experimental/Avalanche/Source/AvalancheSVGEditor/Private/AvaSVGEditorModule.cpp
@@ -7,6 +7,7 @@
 #include "Modifiers/ActorModifierCoreStack.h"
 #include "Modifiers/AvaBevelModifier.h"
 #include "Modifiers/AvaExtrudeModifier.h"
+#include "Modules/ModuleObserver.h"
 #include "ProceduralMeshes/SVGDynamicMeshComponent.h"
 #include "SVGEngineSubsystem.h"
 #include "SVGShapesParentActor.h"
@@ -33,9 +34,22 @@ void FAvaSVGEditorModule::ShutdownModule()
 
 void FAvaSVGEditorModule::RegisterTools(IAvalancheInteractiveToolsModule* InModule)
 {
-	InModule->RegisterTool(
-		IAvalancheInteractiveToolsModule::Get().CategoryNameActor,
-		GetDefault<UAvaSVGActorTool>()->GetToolParameters()
+	// Use module observer to make sure SVGImporterEditor is loaded
+	FModuleObserver ModuleObserver(
+		"SVGImporterEditor",
+		FSimpleDelegate::CreateLambda([InModule]()
+		{
+			if (!InModule)
+			{
+				return;
+			}
+
+			InModule->RegisterTool(
+				IAvalancheInteractiveToolsModule::Get().CategoryNameActor,
+				GetDefault<UAvaSVGActorTool>()->GetToolParameters()
+			);
+		}),
+		FSimpleDelegate()
 	);
 }
 
