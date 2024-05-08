473b476c90fcc84de0ea1e44b250795edae4d438
cedric caillaud <cedric.caillaud@epicgames.com>
2024-05-08 14:18:26 -0400

    Fix broken chaos cache replay - symptoms : Cache would fail to set the geometry collection to be kinematic when replaying , causing also the pose to be reset regardless of the start time - fix : make sure to note recreate the physics state if it was already created, if it was , make sure to recreate the physics proxy as well
    #rb Benn.Gallagher, vincent.robert
    #jira UE-214150
    
    [CL 33520578 by cedric caillaud in ue5-main branch]

--- Engine/Source/Runtime/Experimental/GeometryCollectionEngine/Private/GeometryCollection/GeometryCollectionComponent.cpp
+++ Engine/Source/Runtime/Experimental/GeometryCollectionEngine/Private/GeometryCollection/GeometryCollectionComponent.cpp
@@ -1262,7 +1262,11 @@ void UGeometryCollectionComponent::SetSimulatePhysics(bool bEnabled)
 
 	if (bEnabled && !PhysicsProxy && RestCollection)
 	{
-		RecreatePhysicsState();
+		EnsurePhysicsStateCreated();
+		if (!PhysicsProxy)
+		{
+			RegisterAndInitializePhysicsProxy();
+		}
 	}
 }
 
