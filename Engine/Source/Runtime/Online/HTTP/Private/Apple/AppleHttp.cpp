30acb16593b43ee264a6dca7a30e58f3ffda2507
michael kirzinger <Michael.Kirzinger@epicgames.com>
2024-05-08 15:56:47 -0400

    AppleHttp - Fix an ensure caused by a cvar being accessed in a non-ue created thread
    #jira UE-213273
    [REVIEW] [at]lorry.li
    #rb lorry.li, Rafa.Lecina
    
    [CL 33525188 by michael kirzinger in ue5-main branch]

--- Engine/Source/Runtime/Online/HTTP/Private/Apple/AppleHttp.cpp
+++ Engine/Source/Runtime/Online/HTTP/Private/Apple/AppleHttp.cpp
@@ -194,7 +194,6 @@ static bool ShouldReadHeadersWhenComplete(const FString& Url)
 {
 	if (TSharedPtr<FAppleHttpRequest> Request = SourceRequest.Pin())
 	{
-		AppleHTTPRequestInternal::UpdateConfigFromCVar();
 		if (!AppleHTTPRequestInternal::ShouldReadHeadersWhenComplete(Request->GetURL()))
 		{
 			if (Request->GetDelegateThreadPolicy() == EHttpRequestDelegateThreadPolicy::CompleteOnHttpThread)
@@ -795,6 +794,8 @@ bool FAppleHttpRequest::ProcessRequest()
 	{
 		return false;
 	}
+	
+	AppleHTTPRequestInternal::UpdateConfigFromCVar();
 
 	return true;
 }
