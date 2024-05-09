490e7aae352f06b8388245aac29fb7492fb33081
dominik peacock <dominik.peacock@geotech.one>
2024-05-09 04:03:00 -0400

    Fix crash in ConcertInsightsClient due to adding status bar widget even though slate application is not initialized (e.g. running commandlet). Fixes blocker in Build Engine Localization build farm.
    #jira UE-214370
    
    [CL 33540656 by dominik peacock in ue5-main branch]

--- Engine/Plugins/Developer/Concert/ConcertInsights/ConcertInsightsClient/Source/ConcertInsightsClient/Private/ConcertInsightsClient.cpp
+++ Engine/Plugins/Developer/Concert/ConcertInsights/ConcertInsightsClient/Source/ConcertInsightsClient/Private/ConcertInsightsClient.cpp
@@ -5,6 +5,7 @@
 #include "StatusBar/SMultiUserStatusBar.h"
 #include "StatusBar/StatusBarExtension.h"
 
+#include "Application/SlateApplicationBase.h"
 #include "Misc/CoreDelegates.h"
 
 #define LOCTEXT_NAMESPACE "FConcertInsightsEditorModule"
@@ -21,11 +22,14 @@ namespace UE::ConcertInsightsClient
 
 	void FConcertInsightsClientModule::PostEngineInit()
 	{
-		// Multi User does not add any status bar by itself. For now, we'll add one but in the future we may want this to be in the MU module.
-		ExtendEditorStatusBarWithMultiUserWidget();
+		if (FSlateApplicationBase::IsInitialized())
+		{
+			// Multi User does not add any status bar by itself. For now, we'll add one but in the future we may want this to be in the MU module.
+			ExtendEditorStatusBarWithMultiUserWidget();
 		
-		TraceControls = ConcertInsightsCore::FTraceControls::Make<FClientTraceControls>();
-		ExtendMultiUserStatusBarWithInsights(*TraceControls);
+			TraceControls = ConcertInsightsCore::FTraceControls::Make<FClientTraceControls>();
+			ExtendMultiUserStatusBarWithInsights(*TraceControls);
+		}
 	}
 }
 
