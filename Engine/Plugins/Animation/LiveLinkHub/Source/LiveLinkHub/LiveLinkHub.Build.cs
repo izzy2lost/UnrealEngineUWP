3949d3efccd6e92d78d98f11761a3df70e46b2d3
robert hillary <robert.hillary@cubicmotion.com>
2024-05-09 06:49:25 -0400

    Move LiveLinkHub ModuleRules out of the UnrealBuildTool.Rules namespace.
    #rb zach.brockway
    
    [CL 33541973 by robert hillary in ue5-main branch]

--- Engine/Plugins/Animation/LiveLinkHub/Source/LiveLinkHub/LiveLinkHub.Build.cs
+++ Engine/Plugins/Animation/LiveLinkHub/Source/LiveLinkHub/LiveLinkHub.Build.cs
@@ -1,47 +1,46 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-namespace UnrealBuildTool.Rules
-{
-	public class LiveLinkHub : ModuleRules
-	{
-		public LiveLinkHub(ReadOnlyTargetRules Target) : base(Target)
-		{
-			PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				"CoreUObject",
-			});
+using UnrealBuildTool;
 
-			PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"ApplicationCore",
-				"AssetRegistry",
-				"AssetTools",
-				"ContentBrowser",
-				"ContentBrowserAssetDataSource",
-				"ContentBrowserData",
-				"EditorStyle",
-				"Engine",
-				"InputCore",
-				"Json",
-				"JsonUtilities",
-				"LiveLink",
-				"LiveLinkEditor",
-				"LiveLinkHubMessaging",
-				"LiveLinkInterface",
-				"LiveLinkMessageBusFramework",
-				"Slate",
-				"SlateCore",
-				"StandaloneRenderer",
-				"StructUtils",
-				"TimeManagement",
-				"ToolMenus",
-				"ToolWidgets",
-				"UnrealEd",
-				"OutputLog",
-			});
-		}
+public class LiveLinkHub : ModuleRules
+{
+	public LiveLinkHub(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PublicDependencyModuleNames.AddRange(
+		new string[]
+		{
+			"Core",
+			"CoreUObject",
+		});
+
+		PrivateDependencyModuleNames.AddRange(
+		new string[]
+		{
+			"ApplicationCore",
+			"AssetRegistry",
+			"AssetTools",
+			"ContentBrowser",
+			"ContentBrowserAssetDataSource",
+			"ContentBrowserData",
+			"EditorStyle",
+			"Engine",
+			"InputCore",
+			"Json",
+			"JsonUtilities",
+			"LiveLink",
+			"LiveLinkEditor",
+			"LiveLinkHubMessaging",
+			"LiveLinkInterface",
+			"LiveLinkMessageBusFramework",
+			"Slate",
+			"SlateCore",
+			"StandaloneRenderer",
+			"StructUtils",
+			"TimeManagement",
+			"ToolMenus",
+			"ToolWidgets",
+			"UnrealEd",
+			"OutputLog",
+		});
 	}
 }
