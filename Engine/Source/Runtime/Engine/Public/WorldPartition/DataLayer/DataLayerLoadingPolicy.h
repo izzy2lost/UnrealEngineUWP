969910552f015302b73ad5ed84056317420bbfac
richard malo <Richard.Malo@epicgames.com>
2024-05-10 13:53:47 -0400

    WorldPartition DataLayerLogicOperator now also affects loading of actors using data layers #rb JeanFrancois.Dube #jira UE-214494
    [CL 33580981 by richard malo in ue5-main branch]

--- Engine/Source/Runtime/Engine/Public/WorldPartition/DataLayer/DataLayerLoadingPolicy.h
+++ Engine/Source/Runtime/Engine/Public/WorldPartition/DataLayer/DataLayerLoadingPolicy.h
@@ -4,16 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Object.h"
+#include "WorldPartition/WorldPartition.h"
 #include "DataLayerLoadingPolicy.generated.h"
 
 class UDataLayerInstance;
 
-UCLASS()
+UCLASS(Within = DataLayerManager)
 class ENGINE_API UDataLayerLoadingPolicy : public UObject
 {
 	GENERATED_BODY()
-public:
 #if WITH_EDITOR
+public:
 	virtual bool ResolveIsLoadedInEditor(TArray<const UDataLayerInstance*>& InDataLayers) const;
+
+protected:
+	EWorldPartitionDataLayersLogicOperator GetDataLayersLogicOperator() const;
 #endif
 };
