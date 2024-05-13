3e47e7360b6b1991b1561a8a300ea00638b2686f
ben marsh <Ben.Marsh@epicgames.com>
2024-05-13 17:54:02 -0400

    Horde: Add missing entry point for driver app.
    #rnx
    
    [CL 33610005 by ben marsh in ue5-main branch]

---
+++ Engine/Source/Programs/Horde/Horde.Agent.Driver/DriverApp.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using EpicGames.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Horde.Agent.Driver
+{
+	class DriverApp
+	{
+		public static async Task<int> Main(string[] args)
+		{
+			CommandLineArguments arguments = new CommandLineArguments(args);
+
+			IServiceCollection services = new ServiceCollection();
+			services.AddCommandsFromAssembly(Assembly.GetExecutingAssembly());
+
+			await using ServiceProvider serviceProvider = services.BuildServiceProvider();
+			return await CommandHost.RunAsync(arguments, serviceProvider, null);
+		}
+	}
+}
