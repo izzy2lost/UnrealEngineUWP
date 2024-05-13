fa89891e52e9f9b4d681bc0f3f35183339d06e71
max chen <Max.Chen@epicgames.com>
2024-05-13 13:29:05 -0400

    [Backout] - CL33605064 [FYI] max.chen Original CL Desc ----------------------------------------------------------------- Sequencer: Fix deprecated include warning
    #rb ludovic.chabant
    #jira UE-214722
    #rnx
    
    [CL 33606256 by max chen in ue5-main branch]

--- Engine/Source/Runtime/MovieScene/Public/Evaluation/PreAnimatedState/MovieScenePreAnimatedEntityCaptureSource.h
+++ Engine/Source/Runtime/MovieScene/Public/Evaluation/PreAnimatedState/MovieScenePreAnimatedEntityCaptureSource.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
-UE_DEPRECATED_HEADER(5.4, "Use MovieScenePreAnimatedCaptureSources.h instead of MovieScenePreAnimatedEntityCaptureSource.h")
-
 #include "Evaluation/PreAnimatedState/MovieScenePreAnimatedCaptureSources.h"
+
+COMPILE_WARNING("FPreAnimatedEntityCaptureSource has been moved to MovieScenePreAnimatedCaptureSources.h")
+
