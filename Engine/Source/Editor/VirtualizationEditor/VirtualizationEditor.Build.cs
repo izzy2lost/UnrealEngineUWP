51d8962cfd5d0db37af899ac89c7b029d7add4f2
paul chipchase <paul.chipchase@epicgames.com>
2024-05-13 08:47:27 -0400

    Enable casting warnings for the VirtualizationEditor module
    #rb Per.Larsson
    #rnx
    
    [CL 33602460 by paul chipchase in ue5-main branch]

--- Engine/Source/Editor/VirtualizationEditor/VirtualizationEditor.Build.cs
+++ Engine/Source/Editor/VirtualizationEditor/VirtualizationEditor.Build.cs
@@ -7,6 +7,8 @@ public class VirtualizationEditor : ModuleRules
 	public VirtualizationEditor(ReadOnlyTargetRules Target)
 		 : base(Target)
 	{
+		UnsafeTypeCastWarningLevel = WarningLevel.Error;
+
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
