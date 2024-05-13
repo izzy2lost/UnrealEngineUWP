311b7b912ef54aa0e63263f0c7a501107a453990
euan carmichael <euan.carmichael@epicgames.com>
2024-05-13 18:01:36 -0400

    Expose functions on the various cached anim state machine types to blueprint #jira UE-214247 #rb Jose.Villarroel
    [CL 33610106 by euan carmichael in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Animation/CachedAnimDataLibrary.cpp
+++ Engine/Source/Runtime/Engine/Private/Animation/CachedAnimDataLibrary.cpp
@@ -40,3 +40,92 @@ float UCachedAnimDataLibrary::StateMachine_GetGlobalWeight(UAnimInstance* InAnim
 	return 0.0f;
 }
 
+float UCachedAnimDataLibrary::StateMachine_GetTotalWeight(UAnimInstance* InAnimInstance, const FCachedAnimStateArray& CachedAnimStateArray)
+{
+	if (InAnimInstance)
+	{
+		return CachedAnimStateArray.GetTotalWeight(*InAnimInstance);
+	}
+
+	return 0.0f;
+}
+
+bool UCachedAnimDataLibrary::StateMachine_IsFullWeight(UAnimInstance* InAnimInstance, const FCachedAnimStateArray& CachedAnimStateArray)
+{
+	if (InAnimInstance)
+	{
+		return CachedAnimStateArray.IsFullWeight(*InAnimInstance);
+	}
+
+	return false;
+}
+
+bool UCachedAnimDataLibrary::StateMachine_IsRelevant(UAnimInstance* InAnimInstance, const FCachedAnimStateArray& CachedAnimStateArray)
+{
+	if (InAnimInstance)
+	{
+		return CachedAnimStateArray.IsRelevant(*InAnimInstance);
+	}
+
+	return false;
+}
+
+float UCachedAnimDataLibrary::StateMachine_GetAssetPlayerTime(UAnimInstance* InAnimInstance, const FCachedAnimAssetPlayerData& CachedAnimAssetPlayerData)
+{
+	if (InAnimInstance)
+	{
+		return CachedAnimAssetPlayerData.GetAssetPlayerTime(*InAnimInstance);
+	}
+
+	return 0.0f;
+}
+
+float UCachedAnimDataLibrary::StateMachine_GetAssetPlayerTimeRatio(UAnimInstance* InAnimInstance, const FCachedAnimAssetPlayerData& CachedAnimAssetPlayerData)
+{
+	if (InAnimInstance)
+	{
+		return CachedAnimAssetPlayerData.GetAssetPlayerTimeRatio(*InAnimInstance);
+	}
+
+	return 0.0f;
+}
+
+float UCachedAnimDataLibrary::StateMachine_GetRelevantAnimTime(UAnimInstance* InAnimInstance, const FCachedAnimRelevancyData& CachedAnimRelevancyData)
+{
+	if (InAnimInstance)
+	{
+		return CachedAnimRelevancyData.GetRelevantAnimTime(*InAnimInstance);
+	}
+
+	return 0.0f;
+}
+
+float UCachedAnimDataLibrary::StateMachine_GetRelevantAnimTimeRemaining(UAnimInstance* InAnimInstance, const FCachedAnimRelevancyData& CachedAnimRelevancyData)
+{
+	if (InAnimInstance)
+	{
+		return CachedAnimRelevancyData.GetRelevantAnimTimeRemaining(*InAnimInstance);
+	}
+
+	return 0.0f;
+}
+
+float UCachedAnimDataLibrary::StateMachine_GetRelevantAnimTimeRemainingFraction(UAnimInstance* InAnimInstance, const FCachedAnimRelevancyData& CachedAnimRelevancyData)
+{
+	if (InAnimInstance)
+	{
+		return CachedAnimRelevancyData.GetRelevantAnimTimeRemainingFraction(*InAnimInstance);
+	}
+
+	return 0.0f;
+}
+
+float UCachedAnimDataLibrary::StateMachine_GetCrossfadeDuration(UAnimInstance* InAnimInstance, const FCachedAnimTransitionData& CachedAnimTransitionData)
+{
+	if (InAnimInstance)
+	{
+		return CachedAnimTransitionData.GetCrossfadeDuration(*InAnimInstance);
+	}
+
+	return 0.0f;
+}
