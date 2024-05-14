6513eb73fe84bdbc9d7710e2460de4d96e7a2c3d
gary yuan <gary.yuan@epicgames.com>
2024-05-14 13:13:47 -0400

    [UnsyncUI] Fix user-unhandled exception crash from async task
    #rb https://epicgames.slack.com/archives/C0440HNJFJT/p1710441945770729
    [REVIEW]
    #rnx
    
    [CL 33632756 by gary yuan in ue5-main branch]

--- Engine/Source/Programs/UnsyncUI/UnsyncUI/BuildSelectorControl.xaml.cs
+++ Engine/Source/Programs/UnsyncUI/UnsyncUI/BuildSelectorControl.xaml.cs
@@ -151,37 +151,44 @@ namespace UnsyncUI
 			if (!currentDir.Parse(path, ref template))
 				return;
 
-			foreach (var childDir in await dirEnum.EnumerateDirectories(path, cancellationToken.Token))
+			try
 			{
-				if (System.IO.Path.GetFileName(childDir) == ".unsync")
+				foreach (var childDir in await dirEnum.EnumerateDirectories(path, cancellationToken.Token))
 				{
-					List<Config.BuildTemplate> fileGroups = null;
-					if (currentDir.FileGroups?.Any() == true)
+					if (System.IO.Path.GetFileName(childDir) == ".unsync")
 					{
-						var files = await dirEnum.EnumerateFiles(path, cancellationToken.Token);
-						currentDir.ParseFileGroups(files.ToList(), template, out fileGroups);
-					}
-					if (fileGroups?.Any() == true)
-					{
-						foreach (var fileGroup in fileGroups)
+						List<Config.BuildTemplate> fileGroups = null;
+						if (currentDir.FileGroups?.Any() == true)
 						{
-							Platforms.Add(new BuildPlatformModel(this, fileGroup.Platform, path, fileGroup.Flavor, fileGroup.Include));
+							var files = await dirEnum.EnumerateFiles(path, cancellationToken.Token);
+							currentDir.ParseFileGroups(files.ToList(), template, out fileGroups);
 						}
-						Platforms.Add(new BuildPlatformModel(this, template.Platform, path, "All", null));
+						if (fileGroups?.Any() == true)
+						{
+							foreach (var fileGroup in fileGroups)
+							{
+								Platforms.Add(new BuildPlatformModel(this, fileGroup.Platform, path, fileGroup.Flavor, fileGroup.Include));
+							}
+							Platforms.Add(new BuildPlatformModel(this, template.Platform, path, "All", null));
+						}
+						else
+						{
+							// This folder is a valid build
+							Platforms.Add(new BuildPlatformModel(this, template.Platform, path, template.Flavor, null));
+						}
 					}
 					else
 					{
-						// This folder is a valid build
-						Platforms.Add(new BuildPlatformModel(this, template.Platform, path, template.Flavor, null));
+						foreach (var subDir in currentDir.SubDirectories)
+						{
+							await EnumeratePlatforms(dirEnum, subDir, childDir, template, cancellationToken);
+						}
 					}
 				}
-				else
-				{
-					foreach (var subDir in currentDir.SubDirectories)
-					{
-						await EnumeratePlatforms(dirEnum, subDir, childDir, template, cancellationToken);
-					}
-				}
+			}
+			catch (Exception ex)
+			{
+				App.Current.LogMessage($"Platforms enumeration failed with exception: {ex}");
 			}
 		}
 
