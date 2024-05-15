fab9aea4596ef7cf531e3f65ff73b0b9d75a8906
marc audy <Marc.Audy@epicgames.com>
2024-05-15 17:45:37 -0400

    Use header deprecation macro rather than bespoke warnings [FYI] jack.porter, max.chen, ludovic.chabant #rnx
    [CL 33672260 by marc audy in ue5-main branch]

--- Engine/Source/Runtime/Engine/Public/PerPlatformProperties.h
+++ Engine/Source/Runtime/Engine/Public/PerPlatformProperties.h
@@ -6,6 +6,7 @@ Redirect PerPlatformProperties.h include to UObject
 
 #pragma once
 
-#pragma message("WARNING: Do not #include PerPlatformProperties.h. Please use #include UObject/PerPlatformProperties.h instead. Deprecated in 5.5")
-
 #include "UObject/PerPlatformProperties.h"
+
+UE_DEPRECATED_HEADER(5.5, "WARNING: Do not #include PerPlatformProperties.h. Please use #include UObject/PerPlatformProperties.h instead.")
+
--- Engine/Source/Runtime/MovieScene/Public/Evaluation/PreAnimatedState/MovieScenePreAnimatedEntityCaptureSource.h
+++ Engine/Source/Runtime/MovieScene/Public/Evaluation/PreAnimatedState/MovieScenePreAnimatedEntityCaptureSource.h
@@ -4,5 +4,5 @@
 
 #include "Evaluation/PreAnimatedState/MovieScenePreAnimatedCaptureSources.h"
 
-COMPILE_WARNING("FPreAnimatedEntityCaptureSource has been moved to MovieScenePreAnimatedCaptureSources.h")
+UE_DEPRECATED_HEADER(5.4, "FPreAnimatedEntityCaptureSource has been moved to MovieScenePreAnimatedCaptureSources.h")
 
