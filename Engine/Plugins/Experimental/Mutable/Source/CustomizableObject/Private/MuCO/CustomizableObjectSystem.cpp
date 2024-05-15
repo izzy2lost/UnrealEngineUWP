f99decb5d9e4bbc94651f11ba2c539dbf669e033
gerard martin <gerard.martin@epicgames.com>
2024-05-15 06:08:40 -0400

    [Mutable] Enable split Mutable Task (remove BusyWaits).
    [FYI] [at]genis.sole, [at]alexei.lebedev
    #rnx
    
    [CL 33653561 by gerard martin in ue5-main branch]

--- Engine/Plugins/Experimental/Mutable/Source/CustomizableObject/Private/MuCO/CustomizableObjectSystem.cpp
+++ Engine/Plugins/Experimental/Mutable/Source/CustomizableObject/Private/MuCO/CustomizableObjectSystem.cpp
@@ -115,7 +115,7 @@ TAutoConsoleVariable<bool> CVarEnableMeshCache(
 
 TAutoConsoleVariable<bool> CVarEnableNewSplitMutableTask(
 	TEXT("mutable.EnableNewSplitMutableTask"),
-	false,
+	true,
 	TEXT("Enables or disables the then new split GetImages and GetMesh tasks that remove BusyWaits."),
 	ECVF_Scalability);
 
