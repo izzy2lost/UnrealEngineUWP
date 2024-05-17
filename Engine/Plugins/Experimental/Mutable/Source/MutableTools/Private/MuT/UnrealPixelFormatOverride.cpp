cca19a0697e79a623350ccc5398e3a9b18b156d5
alex kahn <alex.kahn@epicgames.com>
2024-05-17 17:45:04 -0400

    [Backout] - CL33725844 - Builds Cook Client Error #rnx [FYI] jordi.rovira Original CL Desc ----------------------------------------------------------------- [mutable] Avoid unnecessary pixel conversion when converting single image mips. [REVIEW] [at]genis.sole #rnx
    [CL 33738891 by alex kahn in ue5-main branch]

--- Engine/Plugins/Experimental/Mutable/Source/MutableTools/Private/MuT/UnrealPixelFormatOverride.cpp
+++ Engine/Plugins/Experimental/Mutable/Source/MutableTools/Private/MuT/UnrealPixelFormatOverride.cpp
@@ -156,9 +156,7 @@ void MutableToImageCore(const mu::Image* InMutable, FImage& CoreImage, int32 LOD
 	{
 		// Unsupported format: force conversion
 		mu::FImageOperator ImOp = mu::FImageOperator::GetDefault(mu::FImageOperator::FImagePixelFormatFunc());
-		Mutable = ImOp.ImagePixelFormat(4, InMutable, mu::EImageFormat::IF_BGRA_UBYTE,LOD);
-		// We are extracting one LOD, so always access LOD 0 of the resulting mutable image 
-		LOD = 0;
+		Mutable = ImOp.ImagePixelFormat(4, InMutable, mu::EImageFormat::IF_BGRA_UBYTE);
 		CoreImageFormat = ERawImageFormat::BGRA8;
 		break;
 	}
