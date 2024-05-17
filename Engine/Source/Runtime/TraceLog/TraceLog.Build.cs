d7190498335b8686c3d2422bc375c8d8fe16cc71
michael nicolella <michael.nicolella@epicgames.com>
2024-05-17 12:43:06 -0400

    autortfm: remove instrumentation from TraceLog
    - TraceLog is intentionally meant to not depend on Core, but autortfm instrumentation requires Core
    - if this causes issues with autortfm code, that stuff needs to be isolated from autortfm, outside of TraceLog itself
    #rb Brandon.Schaefer, Markus.Breyer
    
    [CL 33727798 by michael nicolella in ue5-main branch]

--- Engine/Source/Runtime/TraceLog/TraceLog.Build.cs
+++ Engine/Source/Runtime/TraceLog/TraceLog.Build.cs
@@ -13,8 +13,6 @@ public class TraceLog : ModuleRules
 		bRequiresImplementModule = false;
 		PublicIncludePathModuleNames.Add("Core");
 
-		bAllowAutoRTFMInstrumentation = true;
-
 		PrivateDefinitions.Add("SUPPRESS_PER_MODULE_INLINE_FILE"); // This module does not use core's standard operator new/delete overloads
 	}
 }
