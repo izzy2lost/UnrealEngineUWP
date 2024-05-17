1f589dd12ee94bb2f2b4bbf8ef291ed111660f5a
dan oconnor <dan.oconnor@epicgames.com>
2024-05-17 15:40:08 -0400

    K2Node_Variable is more careful abound member reference usage to avoid resetting member name due to a stale guid. K2Node_FunctionEntry now generates a fresh local guid when it is duplicated. This fixes a phantom compile error after pasting local variable nodes, which resulted in NodeGuid collisions #rb dave.jones2, Phillip.Kavan #jira UE-214774
    [RN] Fixed one cause of phantom blueprint compilation errors after duplicating a blueprint graph
    
    [CL 33734182 by dan oconnor in ue5-main branch]

--- Engine/Source/Editor/BlueprintGraph/Private/K2Node_FunctionEntry.cpp
+++ Engine/Source/Editor/BlueprintGraph/Private/K2Node_FunctionEntry.cpp
@@ -884,6 +884,11 @@ void UK2Node_FunctionEntry::PostPasteNode()
 	// If a function entry is being pasted, it should be editable in it's new graph
 	bIsEditable = true;
 
+	for (FBPVariableDescription& LocalVariable : LocalVariables)
+	{
+		LocalVariable.VarGuid = FGuid::NewGuid();
+	}
+
 	// ensure there are UserDefinedPins for all pins except the 'then' pin
 	for (int32 PinIdx = 1; PinIdx < Pins.Num(); ++PinIdx)
 	{
