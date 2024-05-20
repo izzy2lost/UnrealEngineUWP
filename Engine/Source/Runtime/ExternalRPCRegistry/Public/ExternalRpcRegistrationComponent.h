4fc750759620bd635d8dc1f155037cb612dfa193
ben salem <Ben.Salem@epicgames.com>
2024-05-20 16:31:23 -0400

    Engine functionality for RPC Registration #rb John.Wilson
    [CL 33778245 by ben salem in ue5-main branch]

---
+++ Engine/Source/Runtime/ExternalRPCRegistry/Public/ExternalRpcRegistrationComponent.h
@@ -0,0 +1,48 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "HttpRequestHandler.h"
+#include "Serialization/JsonWriter.h"
+#include "ExternalRpcRegistry.h"
+
+
+#include "ExternalRpcRegistrationComponent.generated.h"
+class FJsonObject;
+enum class EHttpServerRequestVerbs : uint16;
+struct FExternalRouteInfo;
+struct FExternalRpcArgumentDesc;
+struct FHttpPath;
+struct FHttpServerRequest;
+struct FHttpServerResponse;
+struct FKey;
+UCLASS()
+class UExternalRpcRegistrationComponent : public UObject
+{
+
+	GENERATED_BODY()
+public:
+	TArray<FName> RegisteredRoutes;
+	FString SecuritySecret;
+
+
+	// Outgoing RPC info
+	FString SenderID;
+	FString ListenerAddress;
+
+	virtual void DeregisterHttpCallbacks();	
+	virtual void RegisterAlwaysOnHttpCallbacks();
+	void BroadcastRpcListChanged();
+	void RegisterHttpCallback(FExternalRouteInfo InRouteInfo, const FHttpRequestHandler& Handler, bool bOverrideIfBound = false);
+	void RegisterHttpCallback(FName RouteName, const FHttpPath& HttpPath, const EHttpServerRequestVerbs& RequestVerbs, const FHttpRequestHandler& Handler, bool bOverrideIfBound = false, FString OptionalCategory = TEXT(""), FString OptionalContentType = {}, TArray<FExternalRpcArgumentDesc> OptionalInArguments = TArray<FExternalRpcArgumentDesc>());
+
+
+	// Send Message to Listener RPC
+	bool HttpSendMessageToListener(FString MessageCategory, FString MessagePayload);
+	bool HttpUpdateIpOnListener(FString TargetName, FString MessagePayload);
+	FHttpRequestHandler CreateRouteHandle(TDelegate<bool(const FHttpServerRequest& Request, const FHttpResultCallback& OnComplete)>	 InFunc);
+
+	/** Creates a simple Http response with boolean and optional value */
+	TUniquePtr<FHttpServerResponse> CreateSimpleResponse(bool bInWasSuccessful, FString InValue = "");
+
+};
