971febfd0bf12be8e3c009febc78af6d6794fc82
Michael Forot <Michael.Forot@epicgames.com>
2024-05-21 09:18:23 -0400

    Fix solver nodes copyright #jira none
    [CL 33792341 by Michael Forot in ue5-main branch]

--- Engine/Plugins/Experimental/Dataflow/Source/DataflowNodes/Private/Dataflow/DataflowSolverNodes.cpp
+++ Engine/Plugins/Experimental/Dataflow/Source/DataflowNodes/Private/Dataflow/DataflowSolverNodes.cpp
@@ -1,3 +1,5 @@
-﻿#include "Dataflow/DataflowSolverNodes.h"
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Dataflow/DataflowSolverNodes.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(DataflowSolverNodes)
