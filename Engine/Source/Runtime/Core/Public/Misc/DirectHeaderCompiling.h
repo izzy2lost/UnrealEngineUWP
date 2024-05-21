fb3f9ebe19a741d71b75e96efd870df88efc3f24
marc audy <Marc.Audy@epicgames.com>
2024-05-21 12:47:05 -0400

    Move pragma outside ifndef in attempt to appease compiler #rnx
    [CL 33799592 by marc audy in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Misc/DirectHeaderCompiling.h
+++ Engine/Source/Runtime/Core/Public/Misc/DirectHeaderCompiling.h
@@ -2,11 +2,12 @@
 
 #pragma once
 
+// C5105: macro expansion producing 'defined' has undefined behavior
+// however, in all compilers we use, the behavior is as desired
+#pragma warning(disable : 5105) 
+
 // Used to allow control flow when direct header compiling to avoid issues with headers
 // that typically expect to be included after another header
 #ifndef UE_DIRECT_HEADER_COMPILING
-	// C5105: macro expansion producing 'defined' has undefined behavior
-	// however, in all compilers we use, the behavior is as desired
-	#pragma warning(disable : 5105) 
 	#define UE_DIRECT_HEADER_COMPILING(id) defined __COMPILING_##id
 #endif
