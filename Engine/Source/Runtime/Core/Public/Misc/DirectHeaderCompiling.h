38a8d9fdfcb2807a152875c54db6caa8a4876d66
marc audy <Marc.Audy@epicgames.com>
2024-05-21 15:49:19 -0400

    Remove the Platform.h experiment as it fails if files are in modules that don't include Core and put back the one off pragma disable for the single file cases in to DirectHeaderCompiling.h, leaving the global case in place for most use cases #rnx
    [CL 33806551 by marc audy in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Misc/DirectHeaderCompiling.h
+++ Engine/Source/Runtime/Core/Public/Misc/DirectHeaderCompiling.h
@@ -2,6 +2,10 @@
 
 #pragma once
 
+// C5105: macro expansion producing 'defined' has undefined behavior
+// however, in all compilers we use, the behavior is as desired
+#pragma warning(disable : 5105) 
+
 // Used to allow control flow when direct header compiling to avoid issues with headers
 // that typically expect to be included after another header
 #ifndef UE_DIRECT_HEADER_COMPILING
