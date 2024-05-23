6561363c2bfc1e8e681b913830ff1d5a57084222
stu mckenna <Stu.McKenna@epicgames.com>
2024-05-23 13:23:31 -0400

    - Fix static bool material parameter bindings
    #rb Michael.Galetzka
    #jira UE-215373
    #rnx
    
    [CL 33867005 by stu mckenna in ue5-main branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/Customizations/NiagaraTypeCustomizations.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/Customizations/NiagaraTypeCustomizations.cpp
@@ -2462,15 +2462,17 @@ TSharedRef<SWidget> FNiagaraRendererMaterialStaticBoolParameterCustomization::On
 				}
 			};
 
-		if (UNiagaraSystem* NiagaraSystem = RenderProperties->GetTypedOuter<UNiagaraSystem>())
-		{
-			AddStaticVariables(NiagaraSystem->GetSystemSpawnScript());
-			AddStaticVariables(NiagaraSystem->GetSystemUpdateScript());
-		}
+		// We must add the emitter alias context before the system scripts as they will contain the emitter spawn & update script
 		if (FVersionedNiagaraEmitterData* EmitterData = NiagaraEmitter.GetEmitterData())
 		{
 			AliasContext.ChangeEmitterNameToEmitter(NiagaraEmitter.Emitter->GetUniqueEmitterName());
 			EmitterData->ForEachScript(AddStaticVariables);
+		}
+
+		if (UNiagaraSystem* NiagaraSystem = RenderProperties->GetTypedOuter<UNiagaraSystem>())
+		{
+			AddStaticVariables(NiagaraSystem->GetSystemSpawnScript());
+			AddStaticVariables(NiagaraSystem->GetSystemUpdateScript());
 		}
 	}
 
