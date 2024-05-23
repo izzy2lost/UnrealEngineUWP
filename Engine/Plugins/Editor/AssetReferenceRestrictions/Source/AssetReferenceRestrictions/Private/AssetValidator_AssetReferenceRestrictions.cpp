6b631c416f1d6596f50c98a2dc7310ab94843f4e
robert millar <robert.millar@epicgames.com>
2024-05-23 16:05:03 -0400

    Validator to check asset referencing restrictions for assets in plugins when plugins descriptor changes.
    [CL 33872869 by robert millar in ue5-main branch]

--- Engine/Plugins/Editor/AssetReferenceRestrictions/Source/AssetReferenceRestrictions/Private/AssetValidator_AssetReferenceRestrictions.cpp
+++ Engine/Plugins/Editor/AssetReferenceRestrictions/Source/AssetReferenceRestrictions/Private/AssetValidator_AssetReferenceRestrictions.cpp
@@ -27,13 +27,7 @@ bool UAssetValidator_AssetReferenceRestrictions::CanValidateAsset_Implementation
 {
 	if (InAsset)
 	{
-		TSharedPtr<FDomainData> DomainData = GEditor->GetEditorSubsystem<UAssetReferencingPolicySubsystem>()->GetDomainDB()->FindDomainFromAssetData(AssetData);
-
-		const bool bIsInUnrestrictedFolder = DomainData && DomainData->IsValid() && DomainData->bCanSeeEverything;
-		if (!bIsInUnrestrictedFolder)
-		{
-			return true;
-		}
+		return GEditor->GetEditorSubsystem<UAssetReferencingPolicySubsystem>()->ShouldValidateAssetReferences(AssetData);
 	}
 
 	return false;
@@ -65,79 +59,13 @@ EDataValidationResult UAssetValidator_AssetReferenceRestrictions::ValidateLoaded
 
 void UAssetValidator_AssetReferenceRestrictions::ValidateAssetInternal(const FAssetData& InAssetData, const IAssetRegistry& InAssetRegistry)
 {
-	static const FName TransientName = GetTransientPackage()->GetFName();
-
-	// Check for missing soft or hard references to cinematic and developers content
-	FName PackageFName = InAssetData.PackageName;
-	TArray<FName> SoftDependencies;
-	TArray<FAssetData> AllDependencyAssets;
-
-	const UAssetReferencingPolicySettings* Settings = GetDefault<UAssetReferencingPolicySettings>();
-	UE::AssetRegistry::EDependencyQuery QueryFlags = UE::AssetRegistry::EDependencyQuery::NoRequirements;
-
-	if (Settings->bIgnoreEditorOnlyReferences)
+    UAssetReferencingPolicySubsystem* Subsystem = GEditor->GetEditorSubsystem<UAssetReferencingPolicySubsystem>();
+	TValueOrError<void, TArray<FAssetReferenceError>> Result = Subsystem->ValidateAssetReferences(InAssetData);
+	if (Result.HasError())
 	{
-		// If the rules allow ignoring editor-only referencers, restrict the dependencies to game references
-		QueryFlags = UE::AssetRegistry::EDependencyQuery::Game;
-	}
-
-	InAssetRegistry.GetDependencies(PackageFName, SoftDependencies, UE::AssetRegistry::EDependencyCategory::Package, UE::AssetRegistry::EDependencyQuery::Soft | QueryFlags);
-	for (FName SoftDependency : SoftDependencies)
-	{
-		const FString SoftDependencyStr = SoftDependency.ToString();
-		if (!FPackageName::IsScriptPackage(SoftDependencyStr) && !FPackageName::IsVersePackage(SoftDependencyStr))
+		for (const FAssetReferenceError& Error : Result.GetError())
 		{
-			TArray<FAssetData> DependencyAssets;
-			InAssetRegistry.GetAssetsByPackageName(SoftDependency, DependencyAssets, true);
-			if (DependencyAssets.Num() == 0)
-			{
-				if (SoftDependency != TransientName)
-				{
-					AssetMessage(InAssetData, EMessageSeverity::Error, FText::Format(LOCTEXT("IllegalReference_MissingSoftRef", "Soft references {0} which does not exist"), FText::FromString(SoftDependencyStr)));
-				}
-			}
-			else
-			{
-				AllDependencyAssets.Append(DependencyAssets);
-			}
-		}
-	}
-
-	// Now check hard references to cinematic and developers content
-	TArray<FName> HardDependencies;
-	InAssetRegistry.GetDependencies(PackageFName, HardDependencies, UE::AssetRegistry::EDependencyCategory::Package, UE::AssetRegistry::EDependencyQuery::Hard | QueryFlags);
-	for (FName HardDependency : HardDependencies)
-	{
-		//@TODO: Probably not needed anymore?
-#if 0
-		const FString HardDependencyStr = HardDependency.ToString();
-		FString UncookedFolderName;
-		if (IsInUncookedFolder(HardDependencyStr, &UncookedFolderName))
-		{
-			AssetMessage(InAssetData, EMessageSeverity::Error, FText::Format(LOCTEXT("IllegalReference_HardDependency", "Illegally hard references {0} asset {1}"), FText::FromString(UncookedFolderName), FText::FromString(HardDependencyStr)));
-		}
-#endif
-
-		InAssetRegistry.GetAssetsByPackageName(HardDependency, AllDependencyAssets, true);
-	}
-
-	if ((GetValidationResult() != EDataValidationResult::Invalid) && (AllDependencyAssets.Num() > 0))
-	{
-		FAssetReferenceFilterContext AssetReferenceFilterContext;
-		AssetReferenceFilterContext.ReferencingAssets = { InAssetData };
-		TSharedPtr<IAssetReferenceFilter> AssetReferenceFilter = GEditor ? GEditor->MakeAssetReferenceFilter(AssetReferenceFilterContext) : nullptr;
-		if (ensure(AssetReferenceFilter.IsValid()))
-		{
-			for (const FAssetData& Dependency : AllDependencyAssets)
-			{
-				FText FailureReason;
-				if (!AssetReferenceFilter->PassesFilter(Dependency, &FailureReason))
-				{
-					AssetMessage(InAssetData, EMessageSeverity::Error, FText::Format(LOCTEXT("IllegalReference_AssetFilterFail", "Illegal reference:"), FailureReason))
-						->AddToken(FAssetDataToken::Create(Dependency))
-						->AddText(FText::Format(LOCTEXT("IllegalReference_FailureReason", ". {0}"), FailureReason));
-				}
-			}
+			AssetMessage(InAssetData, EMessageSeverity::Error, Error.Message)->AddToken(FAssetDataToken::Create(Error.ReferencedAsset));
 		}
 	}
 }
