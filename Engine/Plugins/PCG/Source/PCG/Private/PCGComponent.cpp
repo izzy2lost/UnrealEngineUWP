0110a467feafffd1de0a3e5a690bbd5cdbf15f2c
julien lheureux <julien.lheureux@epicgames.com>
2024-05-23 17:26:27 -0400

    [PCG] Temporarily forced cleanup in a single frame to prevent issues with filling update pipeline on other systems - will introduce a managed components list resource to enforce synchronous release soon.
    #rnx
    #rb ryan.buehler, wyatt.marvil
    #jira none
    
    [CL 33875759 by julien lheureux in ue5-main branch]

--- Engine/Plugins/PCG/Source/PCG/Private/PCGComponent.cpp
+++ Engine/Plugins/PCG/Source/PCG/Private/PCGComponent.cpp
@@ -1362,7 +1362,22 @@ FPCGTaskId UPCGComponent::CreateCleanupTask(bool bRemoveComponents, const TArray
 		return true;
 	};
 
-	return GetSubsystem()->ScheduleGeneric(CleanupTask, this, *AllDependencies);
+#if WITH_EDITOR
+	if (!PCGHelpers::IsRuntimeOrPIE())
+	{
+		auto SuperCleanupTask = [CleanupTask]()
+		{
+			while (!CleanupTask()) {}
+			return true;
+		};
+
+		return GetSubsystem()->ScheduleGeneric(SuperCleanupTask, this, *AllDependencies);
+	}
+	else
+#endif
+	{
+		return GetSubsystem()->ScheduleGeneric(CleanupTask, this, *AllDependencies);
+	}
 }
 
 void UPCGComponent::CleanupUnusedManagedResources()
