ca385c558d3c53a9417b5e0f2d6c10e54a5fea78
justin peterson <justin.peterson@epicgames.com>
2024-05-23 14:05:32 -0400

    [Backout] - CL33856648 - CIS Cook Warning [FYI] mikko.mononen Original CL Desc ----------------------------------------------------------------- StateTree: Updated FStateTreePropertyFunctionBase comment.
    [CL 33868581 by justin peterson in ue5-main branch]

--- Engine/Plugins/Runtime/StateTree/Source/StateTreeModule/Public/StateTreePropertyFunctionBase.h
+++ Engine/Plugins/Runtime/StateTree/Source/StateTreeModule/Public/StateTreePropertyFunctionBase.h
@@ -10,29 +10,7 @@ struct FStateTreeExecutionContext;
 /**
  * Base struct for all property functions.
  * PropertyFunction is a node which is executed just before evaluating owner's bindings.
- *
- * The property function's instance data is expected to have one property marked as output.
- * This property is used to find which properties the function can be used for,
- * and that property is hidden in the UI. It is expected that there's just one output property.
- *
- * Example:
- *
- *	USTRUCT()
- *	struct FStateTreeBooleanOperationPropertyFunctionInstanceData
- *	{
- *		GENERATED_BODY()
- *
- *		UPROPERTY(EditAnywhere, Category = Param)
- *		bool bLeft = false;
- *
- *		UPROPERTY(EditAnywhere, Category = Param)
- *		bool bRight = false;
- *
- *      // This property is used to find which properties the function can be used for.
- *		UPROPERTY(EditAnywhere, Category = Output)
- *		bool bResult = false;
- *	};
- *
+ * Only Output properties are allowed to be bound to.
  */
 USTRUCT(meta = (Hidden))
 struct STATETREEMODULE_API FStateTreePropertyFunctionBase : public FStateTreeNodeBase
