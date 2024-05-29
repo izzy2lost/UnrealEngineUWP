c5f31709a7d0a6b7d74626edb02e073618f05965
Alejandro Arango <alejandro.arango@epicgames.com>
2024-05-29 04:46:42 -0400

    NvmlWrapper: Added functions to query GPU clocks, temperatures, and usage.
    These will be used by SwitchboardListener to replace equivalent NvApi calls.
    
    #ushell-cherrypick of 33964233 by Alejandro.Arango
    #rb alejandro.arango
    #jira none
    #rnx
    
    [CL 33966383 by Alejandro Arango in ue5-main branch]

--- Engine/Source/ThirdParty/NVIDIA/NVML/Public/GpuClocker.h
+++ Engine/Source/ThirdParty/NVIDIA/NVML/Public/GpuClocker.h
@@ -10,15 +10,27 @@ class EXPORTLIB FGpuClocker
 public:
 
 	/** Locks the gpu clocks to the maximum allowed */
-	bool LockGpuClocks();
+	bool LockGpuClocks() const;
 
 	/** Unlocks the gpu and memory clocks */
-	bool UnlockGpuClocks();
+	bool UnlockGpuClocks() const;
 
 	/** Locks the given gpu and memory clocks to the maximum allowed */
-	bool GpuUnlockDevice(const uint32_t GpuIdx);
+	bool GpuUnlockDevice(const uint32_t GpuIdx) const;
 
 	/** Unlocks the given gpu and memory clocks */
-	bool GpuLockDevice(const uint32_t GpuIdx);
+	bool GpuLockDevice(const uint32_t GpuIdx) const;
+
+	/** Returns number of GPUs */
+	bool GetGpuCount(uint32_t& OutNumGpus) const;
+
+	/** Returns the temperature in Celsius of the indexed GPU */
+	bool GetGpuCelsius(const uint32_t GpuIdx, uint32_t& OutCelsius) const;
+
+	/** Returns the GPU usage in percentage of running kernels and memory operations */
+	bool GetGpuUsage(const uint32_t GpuIdx, uint32_t& OutUsageKernel, uint32_t& OutUsageMemory) const;
+
+	/** Return GPU clock frequency in MHz */
+	bool GetGpuMHz(const uint32_t GpuIdx, uint32_t& GraphicsMHz, uint32_t& MemoryMHz) const;
 };
 
