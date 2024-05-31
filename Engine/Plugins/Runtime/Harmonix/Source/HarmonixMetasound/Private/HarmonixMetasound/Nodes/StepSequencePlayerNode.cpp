5e6ef905362fd24f8aba74063fd9cb54cbc5babb
buzz burrowes <buzz.burrowes@epicgames.com>
2024-05-31 11:10:57 -0400

    Fix inadvertent regression of a previous fix. Minor change BACK to the correct code.
    [CL 34032903 by buzz burrowes in ue5-main branch]

--- Engine/Plugins/Runtime/Harmonix/Source/HarmonixMetasound/Private/HarmonixMetasound/Nodes/StepSequencePlayerNode.cpp
+++ Engine/Plugins/Runtime/Harmonix/Source/HarmonixMetasound/Private/HarmonixMetasound/Nodes/StepSequencePlayerNode.cpp
@@ -820,7 +820,7 @@ namespace HarmonixMetasound::Nodes::StepSequencePlayer
 				}
 			}
 
-			if (CellInRow != CurrentCellIndex/* || CurrentMaxColumns == 1*/)
+			if (CellInRow != CurrentCellIndex || CurrentMaxColumns == 1)
 			{	
 				for (int32 i = 0; i < CurrentPage->Rows.Num(); ++i)
 				{
