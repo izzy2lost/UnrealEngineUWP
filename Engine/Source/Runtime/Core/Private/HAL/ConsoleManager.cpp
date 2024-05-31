006cc29344b77337d3bda21be0b7e4c627909228
david harvey <David.Harvey@epicgames.com>
2024-05-31 09:44:23 -0400

    fix for localized text clash
    #jira UE-215916
    #rnx
    #rb Josh.Adams
    
    [CL 34031356 by david harvey in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/HAL/ConsoleManager.cpp
+++ Engine/Source/Runtime/Core/Private/HAL/ConsoleManager.cpp
@@ -4579,7 +4579,7 @@ protected:
 				}
 				else
 				{
-					Message = FText::Format(LOCTEXT("ShadowCVarUsage_Deprecated", "Using a shadowed CVar '{ThisName}'. It is recommended to change all uses to '{ShadowName}' instead."), Arguments);
+					Message = FText::Format(LOCTEXT("ShadowCVarUsage", "Using a shadowed CVar '{ThisName}'. It is recommended to change all uses to '{ShadowName}' instead."), Arguments);
 				}
 				switch (UsageBehavior)
 				{
