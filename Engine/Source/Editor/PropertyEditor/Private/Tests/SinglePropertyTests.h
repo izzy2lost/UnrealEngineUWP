ccaa5102675b2413138951b72651929a721cc62d
logan buchy <logan.buchy@epicgames.com>
2024-06-03 16:22:39 -0400

    [Backout] - CL34073126 [FYI] logan.buchy Original CL Desc ----------------------------------------------------------------- Fix include paths Mac compilation issues warning on monolithic include.
    #rnx
    #jira UE-21638
    
    [CL 34075640 by logan buchy in ue5-main branch]

--- Engine/Source/Editor/PropertyEditor/Private/Tests/SinglePropertyTests.h
+++ Engine/Source/Editor/PropertyEditor/Private/Tests/SinglePropertyTests.h
@@ -8,7 +8,7 @@
 #include "Modules/ModuleManager.h"
 #include "Misc/AutomationTest.h"
 #include "PropertyEditorModule.h"
-#include "UObject/Object.h"
+#include "CoreUObject.h"
 
 #include "SinglePropertyTests.generated.h"
 
