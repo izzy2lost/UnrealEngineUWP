e7f95c64cca27d4c3b31b903f1f8af7cd51c61df
bob tellez <Bob.Tellez@epicgames.com>
2024-06-03 14:54:35 -0400

    [Backout] - CL34072936. Wasn't supposed to be in FNMain [FYI] henry.smith Original CL Desc ----------------------------------------------------------------- Fixed pre-SH bug with broken scrollbars
    #rnx
    [REVIEW] [at]Patrick.Boutot
    #jira UE-216275
    
    [CL 34073045 by bob tellez in ue5-main branch]

--- Engine/Source/Runtime/Slate/Public/Widgets/Views/SListView.h
+++ Engine/Source/Runtime/Slate/Public/Widgets/Views/SListView.h
@@ -114,7 +114,6 @@ public:
 		, _HandleSpacebarSelection(false)
 		, _IsFocusable(true)
 		, _ReturnFocusToSelection()
-		, _GenerateAllItems_Debug(false)
 		, _OnItemToString_Debug()
 		, _OnEnteredBadState()
 		{
