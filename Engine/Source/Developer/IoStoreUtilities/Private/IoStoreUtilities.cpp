9e0174130363d65b5d3393d54a14be0ef1993330
pj kack <PJ.Kack@epicgames.com>
2024-06-04 06:12:40 -0400

    UnrealPak: When the existing optional .uproject argument is provided first on the commandline, then let UnrealPak use that to set its project directory and override the monolithic engine program directory (Engine/Programs/UnrealPak). This will make UnrealPak read from and use the project-specific ini files. This will make UnrealPak write to and use the project-specific Saved directory, e.g. to save the UnrealPak.log file. In the BuildCookRun/CopyBuildToStagingDirectory scripts always invoke UnrealPak with the .uproject file argument.
    Motivation: When UnrealPak is run with -compressionEnableDDC it needs to use the project's ddc configuration instead of the engine's ddc configuration.
    
    Note: When UnrealPak is invoked without the project argument it will still use the default program directory, and there is no change in behavior when e.g. manually executing inspection commands like list, extract and describe.
    Note: All important packaging options are already passed on command line from BuildCookRun/CopyBuildToStagingDirectory, so from that perspective the change should be low risk, but in the future there is a possibility to reduce the number of command line options and rely on the project configuration instead.
    
    #rb per.larsson
    #tests identical binary output
    
    [CL 34089539 by pj kack in ue5-main branch]

--- Engine/Source/Developer/IoStoreUtilities/Private/IoStoreUtilities.cpp
+++ Engine/Source/Developer/IoStoreUtilities/Private/IoStoreUtilities.cpp
@@ -9503,6 +9503,12 @@ bool ParseContainerGenerationArguments(FIoStoreArguments& Arguments, FIoStoreWri
 	}
 
 	WriterSettings.bCompressionEnableDDC = FParse::Param(FCommandLine::Get(), TEXT("compressionEnableDDC"));
+	if (WriterSettings.bCompressionEnableDDC && !FPaths::IsProjectFilePathSet())
+	{
+		UE_LOG(LogIoStore, Warning,
+			TEXT("Ignoring -compressionEnableDDC due to missing .uproject file as the first unnamed argument."));
+		WriterSettings.bCompressionEnableDDC = false;
+	}
 
 	int32 CompressionMinSizeToConsiderDDC = 0;
 	if (FParse::Value(FCommandLine::Get(), TEXT("-compressionMinSizeToConsiderDDC="), CompressionMinSizeToConsiderDDC))
