8c76110c15607ec12bb64189f52e75cf23721f68
yoan stamant <Yoan.Stamant@epicgames.com>
2024-06-05 16:39:52 -0400

    [MockAI_BT] fixed component initialization when tests are run in a game world #jira UE-216565 #rb Aris.Theophanidis, Maxime.Mercier
    [CL 34139624 by yoan stamant in ue5-main branch]

--- Engine/Source/Developer/AITestSuite/Private/MockAI/MockAI_BT.cpp
+++ Engine/Source/Developer/AITestSuite/Private/MockAI/MockAI_BT.cpp
@@ -25,7 +25,14 @@ UMockAI_BT::UMockAI_BT(const FObjectInitializer& ObjectInitializer)
 		// We want our component to be ticked manually by the test framework, never by the TickFunction
 		BTComp->PrimaryComponentTick.bCanEverTick = false;
 		BTComp->RegisterComponent();
-		BTComp->InitializeComponent();
+
+		// Test running in a game world will get initialized automatically by
+		// the previous call to RegisterComponent. Otherwise, we initialize it
+		// manually.
+		if (!BTComp->HasBeenInitialized() && BTComp->bWantsInitializeComponent)
+		{
+			BTComp->InitializeComponent();
+		}
 	}
 }
 
