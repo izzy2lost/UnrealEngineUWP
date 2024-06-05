613a6086e07dbab2ce30c5521bc8ce9785184d78
bob tellez <Bob.Tellez@epicgames.com>
2024-06-05 00:34:36 -0400

    [Backout] - CL34089817, 34114150, 34114248, and 34115050 [FYI] erica.stella,Dmitriy.Dyomin Original CL Desc ----------------------------------------------------------------- Add rect light support to mobile forward renderer
    #jira UE-211644
    #rb florian.penzkofer, Sebastien.Hillaire
    [FYI] sebastien.hillaire
    
    [CL 34116082 by bob tellez in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
+++ Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
@@ -7806,12 +7806,6 @@ void GlobalBeginCompileShader(
 			SET_SHADER_DEFINE(Input.Environment, USE_GLES_FBF_DEFERRED, bGLESDeferredShading ? 1 : 0);
 			SET_SHADER_DEFINE(Input.Environment, MOBILE_EXTENDED_GBUFFER, MobileUsesExtenedGBuffer((EShaderPlatform)Target.Platform) ? 1 : 0);
 		}
-		else
-		{
-			static const auto CVarRenderRectLightAsSpotLight = IConsoleManager::Get().FindTConsoleVariableDataInt(TEXT("r.Mobile.Forward.RenderRectLightsAsSpotLights"));
-			const bool bRenderAsSpotLight = (CVarRenderRectLightAsSpotLight && CVarRenderRectLightAsSpotLight->GetValueOnAnyThread() != 0);
-			SET_SHADER_DEFINE(Input.Environment, RECT_LIGHT_AS_SPOTLIGHT, bRenderAsSpotLight ? 1 : 0);
-		}
 
 		SET_SHADER_DEFINE(Input.Environment, USE_SCENE_DEPTH_AUX, MobileRequiresSceneDepthAux(ShaderPlatform) ? 1 : 0);
 
