7ac9aefcd02e4bc05d47ba5304685304fee3bef8
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2024-06-04 23:09:34 -0400

    Corrected a comment on r.Mobile.Forward.RenderRectLightsAsSpotLights cvar default value
    [CL 34114271 by dmitriy dyomin in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/HAL/ConsoleManager.cpp
+++ Engine/Source/Runtime/Core/Private/HAL/ConsoleManager.cpp
@@ -3775,8 +3775,8 @@ static TAutoConsoleVariable<int32> CVarMobileForwardEnableLocalLights(
 static TAutoConsoleVariable<int32> CVarMobileRenderRectLightsAsSpotLights(
 	TEXT("r.Mobile.Forward.RenderRectLightsAsSpotLights"),
 	0,
-	TEXT("0: Rect Lights are rendered as area lights\n"
-		"1: Rect Lights are rendered as Spot Lights (default)\n"),
+	TEXT("0: Rect Lights are rendered as area lights (default)\n"
+		"1: Rect Lights are rendered as Spot Lights\n"),
 	ECVF_ReadOnly | ECVF_MobileShaderChange);
 
 static TAutoConsoleVariable<int32> CVarMobileForwardEnableParticleLights(
