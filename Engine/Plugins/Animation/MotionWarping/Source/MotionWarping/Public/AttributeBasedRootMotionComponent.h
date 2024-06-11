3c3915b2d094b56356bb06f1e11ef6c5e9e56080
keith yerex <keith.yerex@epicgames.com>
2024-06-11 16:44:02 -0400

    Optional Velocity mode for AttributeBasedRootMotionComponent #rb samuele.rigamonti
    [CL 34290601 by keith yerex in ue5-main branch]

--- Engine/Plugins/Animation/MotionWarping/Source/MotionWarping/Public/AttributeBasedRootMotionComponent.h
+++ Engine/Plugins/Animation/MotionWarping/Source/MotionWarping/Public/AttributeBasedRootMotionComponent.h
@@ -8,6 +8,48 @@
 
 class ACharacter;
 
+USTRUCT()
+struct FAttributeBasedRootMotionComponentPrePhysicsTickFunction : public FTickFunction
+{
+	GENERATED_USTRUCT_BODY()
+
+	/** CharacterMovementComponent that is the target of this tick **/
+	class UAttributeBasedRootMotionComponent* Target;
+
+	/**
+	 * Abstract function actually execute the tick.
+	 * @param DeltaTime - frame time to advance, in seconds
+	 * @param TickType - kind of tick for this frame
+	 * @param CurrentThread - thread we are executing on, useful to pass along as new tasks are created
+	 * @param MyCompletionGraphEvent - completion event for this task. Useful for holding the completion of this task until certain child tasks are complete.
+	 **/
+	virtual void ExecuteTick(float DeltaTime, enum ELevelTick TickType, ENamedThreads::Type CurrentThread, const FGraphEventRef& MyCompletionGraphEvent) override;
+
+	/** Abstract function to describe this tick. Used to print messages about illegal cycles in the dependency graph **/
+	virtual FString DiagnosticMessage() override;
+	
+	/** Function used to describe this tick for active tick reporting. **/
+	virtual FName DiagnosticContext(bool bDetailed) override;
+};
+template<>
+struct TStructOpsTypeTraits<FAttributeBasedRootMotionComponentPrePhysicsTickFunction> : public TStructOpsTypeTraitsBase2<FAttributeBasedRootMotionComponentPrePhysicsTickFunction>
+{
+	enum
+	{
+		WithCopy = false
+	};
+};
+
+
+UENUM()
+enum class EAttributeBasedRootMotionMode
+{
+	ApplyDelta,
+	ApplyVelocity,
+};
+
+
+
 UCLASS(ClassGroup = Movement, meta = (BlueprintSpawnableComponent))
 class MOTIONWARPING_API UAttributeBasedRootMotionComponent : public UActorComponent
 {
@@ -17,7 +59,9 @@ public:
 	UAttributeBasedRootMotionComponent(const FObjectInitializer& ObjectInitializer);
 
 	virtual void InitializeComponent() override;
+	virtual void RegisterComponentTickFunctions(bool bRegister) override;
 
+	void PrePhysicsTickComponent(float DeltaTime);
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	
 	/** Gets the character this component belongs to */
@@ -26,7 +70,14 @@ public:
 	UPROPERTY(BlueprintReadWrite, Category = "Runtime")
 	bool bEnableRootMotion = true;
 	
+	
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	EAttributeBasedRootMotionMode Mode = EAttributeBasedRootMotionMode::ApplyDelta;
+	
 protected:
+
+	/** Pre-physics tick function for this character */
+	FAttributeBasedRootMotionComponentPrePhysicsTickFunction PrePhysicsTickFunction;
 
 	/** Character this component belongs to */
 	UPROPERTY(Transient)
@@ -34,4 +85,8 @@ protected:
 
 	FTransform PrevTransform;
 	bool PrevTransformValid = false;
+
+	FVector TranslationVelocity {0, 0, 0};
+	FVector RotationVelocity {0, 0, 0};
 };
+
