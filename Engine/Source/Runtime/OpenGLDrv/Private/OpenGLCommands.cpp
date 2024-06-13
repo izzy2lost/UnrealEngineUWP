9bd94062e32f48d30c2edbdd10ab45c938223e37
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2024-06-13 02:46:14 -0400

    OpenGL: Do not wrap compute dispatches into memory barriers GLES barriers seem to do nothing
    [FYI] allan.bentham
    
    [CL 34332861 by dmitriy dyomin in ue5-main branch]

--- Engine/Source/Runtime/OpenGLDrv/Private/OpenGLCommands.cpp
+++ Engine/Source/Runtime/OpenGLDrv/Private/OpenGLCommands.cpp
@@ -2833,9 +2833,7 @@ void FOpenGLDynamicRHI::RHIDispatchComputeShader(uint32 ThreadGroupCountX, uint3
 	SetupUAVsForCompute(ContextState, ComputeShader);
 	CommitComputeShaderConstants(ComputeShader);
 	
-	FOpenGL::MemoryBarrier(GL_ALL_BARRIER_BITS);
 	FOpenGL::DispatchCompute(ThreadGroupCountX, ThreadGroupCountY, ThreadGroupCountZ);
-	FOpenGL::MemoryBarrier(GL_ALL_BARRIER_BITS);
 }
 
 void FOpenGLDynamicRHI::RHIDispatchIndirectComputeShader(FRHIBuffer* ArgumentBufferRHI, uint32 ArgumentOffset)
@@ -2862,15 +2860,11 @@ void FOpenGLDynamicRHI::RHIDispatchIndirectComputeShader(FRHIBuffer* ArgumentBuf
 	
 	CommitComputeShaderConstants(ComputeShader);
 	
-	FOpenGL::MemoryBarrier(GL_ALL_BARRIER_BITS);
-
 	glBindBuffer( GL_DISPATCH_INDIRECT_BUFFER, ArgumentBuffer->Resource);
 	
 	FOpenGL::DispatchComputeIndirect(ArgumentOffset);
 
 	glBindBuffer( GL_DISPATCH_INDIRECT_BUFFER, 0);
-	
-	FOpenGL::MemoryBarrier(GL_ALL_BARRIER_BITS);
 }
 
 void FOpenGLDynamicRHI::RHISetMultipleViewports(uint32 Count, const FViewportBounds* Data)
