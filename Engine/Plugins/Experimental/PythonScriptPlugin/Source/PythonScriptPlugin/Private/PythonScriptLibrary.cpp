3d47caeaee871bfd225753e5ede791bb12983a2b
jamie dale <Jamie.Dale@epicgames.com>
2024-06-13 13:06:00 -0400

    Run ProcessPending after all the stack references to Python objects have been released
    This avoids a GC leak when the inputs/outputs are referencing an object that ProcessPending needs to reinstance
    
    [FYI] Bryce.Lumpkin
    #rnx
    
    [CL 34344805 by jamie dale in ue5-main branch]

--- Engine/Plugins/Experimental/PythonScriptPlugin/Source/PythonScriptPlugin/Private/PythonScriptLibrary.cpp
+++ Engine/Plugins/Experimental/PythonScriptPlugin/Source/PythonScriptPlugin/Private/PythonScriptLibrary.cpp
@@ -120,9 +120,6 @@ DEFINE_FUNCTION(UPythonScriptLibrary::execExecutePythonScript)
 
 		// Execute the Python command
 		FPyObjectPtr PyResult = FPyObjectPtr::StealReference(FPythonScriptPlugin::Get()->EvalString(*PythonScript, TEXT("<string>"), Py_file_input, PyTempGlobalDict, PyTempGlobalDict));
-		Py_BEGIN_ALLOW_THREADS
-		FPyWrapperTypeReinstancer::Get().ProcessPending();
-		Py_END_ALLOW_THREADS
 
 		// Read the output values from the Python context
 		if (PyResult)
@@ -156,6 +153,9 @@ DEFINE_FUNCTION(UPythonScriptLibrary::execExecutePythonScript)
 		FPyScopedGIL GIL;
 		if (ExecuteCustomPythonScriptImpl())
 		{
+			Py_BEGIN_ALLOW_THREADS
+			FPyWrapperTypeReinstancer::Get().ProcessPending();
+			Py_END_ALLOW_THREADS
 			*(bool*)RESULT_PARAM = true;
 		}
 		else
