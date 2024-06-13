162da83f7a9520b74eb390e774fef0ecca89a6b5
ben hoffman <ben.hoffman@epicgames.com>
2024-06-13 15:08:43 -0400

    Undo of backout 34177841 to bring back the UHT optimization. This time though, we check if "Func->GetOwnerClass()->HasAnyClassFlags(CLASS_Native)" is false to skip directly into the Native C++ implementation instead of the Script num. This sovles an issue with Python code not executing properly.
    This also makes the FName we are referncing const :)
    
    ======= Orignal CL Desc. =======
    Change the code generation of UHT for BP implementable events to check for any script implementations of the event.
    
    If there are none, then just call directly into the native C++ implemenation of the function, saving us the cost of copying the function params to the script VM.
    
    #rb dan.oconnor
    
    #jira UE-216491
    
    [CL 34350294 by ben hoffman in ue5-main branch]

--- Engine/Source/Programs/Shared/EpicGames.UHT/Exporters/CodeGen/UhtHeaderCodeGenerator.cs
+++ Engine/Source/Programs/Shared/EpicGames.UHT/Exporters/CodeGen/UhtHeaderCodeGenerator.cs
@@ -277,9 +277,29 @@ namespace EpicGames.UHT.Exporters.CodeGen
 			return builder;
 		}
 
-		protected static StringBuilder AppendEventFunctionPrologue(StringBuilder builder, UhtFunction function, string functionName, int tabs, string endl, bool addEventParameterStruct)
+		// Find the UFUNCTION associated with this event so that we can check
+		// if there is a BP implementation of it
+		protected static StringBuilder AppendFindUFunction(StringBuilder builder, UhtClass classObj, UhtFunction function, int tabs, string endl)
 		{
-			builder.AppendTabs(tabs).Append('{').Append(endl);
+			builder
+				.AppendTabs(tabs)
+				.Append("UFunction* Func = FindFunctionChecked(")
+				.Append("NAME_")
+				.Append(classObj.SourceName)
+				.Append('_')
+				.Append(function.EngineName)
+				.Append(");\r\n");
+
+			return builder;
+		}
+
+		protected static StringBuilder AppendEventFunctionPrologue(StringBuilder builder, UhtFunction function, string functionName, int tabs, string endl, bool addEventParameterStruct, bool bAddFunctionScopeBracket = true)
+		{
+			if (bAddFunctionScopeBracket)
+			{
+				builder.AppendTabs(tabs).Append('{').Append(endl);
+			}
+			
 			if (function.Children.Count == 0)
 			{
 				return builder;
@@ -322,7 +342,7 @@ namespace EpicGames.UHT.Exporters.CodeGen
 			return builder;
 		}
 
-		protected static StringBuilder AppendEventFunctionEpilogue(StringBuilder builder, UhtFunction function, int tabs, string endl)
+		protected static StringBuilder AppendEventFunctionEpilogue(StringBuilder builder, UhtFunction function, int tabs, string endl, bool bAddFunctionScopeBracket = true)
 		{
 			++tabs;
 
@@ -382,7 +402,12 @@ namespace EpicGames.UHT.Exporters.CodeGen
 			}
 
 			--tabs;
-			builder.AppendTabs(tabs).Append('}').Append(endl);
+
+			if (bAddFunctionScopeBracket)
+			{
+				builder.AppendTabs(tabs).Append('}').Append(endl);
+			}
+			
 			return builder;
 		}
 
