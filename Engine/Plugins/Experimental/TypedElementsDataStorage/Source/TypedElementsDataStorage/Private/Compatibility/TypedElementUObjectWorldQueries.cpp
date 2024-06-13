55bd666b83b4a47e76f4b714ba33cb2912451228
ross smith2 <ross.smith2@epicgames.com>
2024-06-13 14:02:13 -0400

    Add FTypedElementClassDefaultObjectTag and use it to exclude CDOs from the query "Add world column to UObject".
    #jira UE-215830
    #rb ronald.koppers
    
    [CL 34347479 by ross smith2 in ue5-main branch]

--- Engine/Plugins/Experimental/TypedElementsDataStorage/Source/TypedElementsDataStorage/Private/Compatibility/TypedElementUObjectWorldQueries.cpp
+++ Engine/Plugins/Experimental/TypedElementsDataStorage/Source/TypedElementsDataStorage/Private/Compatibility/TypedElementUObjectWorldQueries.cpp
@@ -38,7 +38,7 @@ void UTypedElementUObjectWorldFactory::RegisterAddWorldColumn(ITypedElementDataS
 		)
 		.Where()
 			.All<FTypedElementSyncFromWorldTag>()
-			.None<FTypedElementWorldColumn>()
+			.None<FTypedElementWorldColumn, FTypedElementClassDefaultObjectTag>()
 		.Compile()
 	);
 }
