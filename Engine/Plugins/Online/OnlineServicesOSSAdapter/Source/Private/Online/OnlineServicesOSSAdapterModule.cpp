57b2091f954a466cc2bfcd83e00a5d34671978ea
lorry li <lorry.li@epicgames.com>
2024-06-14 12:36:02 -0400

    Resubmit 34096212: Include InstanceName in the adapter OnlineSubsystem name Enables adapter to work in PIE #rb jay.nakai, Michael.Kirzinger #tests Tried in editor with different type of game modes.
    [CL 34375152 by lorry li in ue5-main branch]

--- Engine/Plugins/Online/OnlineServicesOSSAdapter/Source/Private/Online/OnlineServicesOSSAdapterModule.cpp
+++ Engine/Plugins/Online/OnlineServicesOSSAdapter/Source/Private/Online/OnlineServicesOSSAdapterModule.cpp
@@ -61,7 +61,8 @@ public:
 	virtual ~FOnlineServicesFactoryOSSAdapter() {}
 	virtual TSharedPtr<IOnlineServices> Create(FName InInstanceName) override
 	{
-		IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get(Config.OnlineSubsystem);
+		FName CombinedInstanceName = InInstanceName.IsNone() ? (/*NoSuffix*/Config.OnlineSubsystem) : FName(*FString::Printf(TEXT("%s:%s"), *Config.OnlineSubsystem.ToString(), *InInstanceName.ToString()));
+		IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get(CombinedInstanceName);
 		if (Subsystem != nullptr)
 		{
 			return MakeShared<FOnlineServicesOSSAdapter>(Config.Service, Config.ConfigName, InInstanceName, Subsystem);
