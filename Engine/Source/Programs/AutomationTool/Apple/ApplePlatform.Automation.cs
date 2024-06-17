68cc92460d9eb67c911930db91bd8707071432c7
josh adams <josh.adams@epicgames.com>
2024-06-17 13:50:42 -0400

    Brojught two CLs over from Main, in one change:
    CHANGE 1:
    - Made a script (ExportAppleBuild) for uploading or exporting .xcarchives (we had MakeIPA but this is more general purpose and uses a project param to get ASC params
      - This needs .plist files to control the export/upload process, added some premade default ones in Engine/Build/Xcode
    - Refactored some code for the Auth param stuff
    
    CHANGE 2:
    - Updated packaging with -distribution, to take use -archivedirectory param (if it ends with .xcarchive) as an override location to write the .xcarchive, instead of ~/Library/Developer/Xcode (when -archive isn't set)
    - Similarly if -archive and -archivedirectory ends with .xcarchive, then that is the name of the directory to archive to, instead of using Xcode's name
    - Improved logging of xcarchive info
    #rb zack.neyland
    #jira UE-211111
    
    [CL 34428868 by josh adams in 5.4 branch]

--- Engine/Source/Programs/AutomationTool/Apple/ApplePlatform.Automation.cs
+++ Engine/Source/Programs/AutomationTool/Apple/ApplePlatform.Automation.cs
@@ -10,6 +10,7 @@ using UnrealBuildBase;
 using System.Diagnostics;
 using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
+using System.Xml.Linq;
 
 public abstract class ApplePlatform : Platform
 {
@@ -144,45 +145,27 @@ public abstract class ApplePlatform : Platform
 
 	private DirectoryReference GetArchivePath(StageTarget Target, DeploymentContext SC)
 	{
-		DirectoryReference UserDir = new DirectoryReference(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
-		DirectoryReference Library = DirectoryReference.Combine(UserDir, "Library/Developer/Xcode/Archives");
-
-		// order date named folders (use creating data, not name, but same thing)
-		List<DirectoryReference> DateDirs = DirectoryReference.EnumerateDirectories(Library).ToList();
-		DateDirs.SortBy(x => Directory.GetCreationTime(x.FullName));
-		DateDirs.Reverse();
-
-		string ArchiveName = Target.Receipt.TargetName;
+		string TargetName = Target.Receipt.TargetName;
 		if (!SC.IsCodeBasedProject)
 		{
 			if (SC.RawProjectPath != null)
 			{
-				ArchiveName = MakeContentOnlyTargetName(Target.Receipt, SC.ShortProjectName);
+				TargetName = MakeContentOnlyTargetName(Target.Receipt, SC.ShortProjectName);
 			}
 		}
 
-		// go through each folder, starting at most recent, looking for an archive for the target
-		foreach (DirectoryReference DateDir in DateDirs)
+		// find the most recent archive for this target (based on name of target, this ignores Development vs Shipping, but 
+		// since Distribution is meant only for Shipping it's ok
+		string ArchiveName = AppleExports.MakeBinaryFileName(TargetName, Target.Receipt.Platform, UnrealTargetConfiguration.Development,
+		 	Target.Receipt.Architectures, UnrealTargetConfiguration.Development, null);
+
+		if (ArchiveName != TargetName)
 		{
-			// find the most recent archive for this target (based on name of target, this ignores Development vs Shipping, but 
-			// since Distribution is meant only for Shipping it's ok
-			string Wildcard = AppleExports.MakeBinaryFileName(ArchiveName, Target.Receipt.Platform, UnrealTargetConfiguration.Development,
-				Target.Receipt.Architectures, UnrealTargetConfiguration.Development, null) + " *.xcarchive";
-
-			Logger.LogInformation("Looking in Xcode archive dir {0} for {1}", DateDir, Wildcard);
-
-			List<DirectoryReference> XcArchives = DirectoryReference.EnumerateDirectories(DateDir, Wildcard).ToList();
-			if (XcArchives.Count > 0)
-			{
-				XcArchives.SortBy(x => Directory.GetCreationTime(x.FullName));
-				DirectoryReference XcArchive = XcArchives.Last();
-
-				Logger.LogInformation("Found xcarchive dir {0}", XcArchive);
-				return XcArchive;
-			}
+			// i don't know why we called MakeBinaryFileName - with Dev it seems to just return the TargetName passed in
+			throw new AutomationException($"GetArchivePath changed name of Target from {TargetName} to {ArchiveName} - tell Josh Adams");
 		}
 
-		return null;
+		return AppleExports.FindLatestXcArchive(TargetName);
 	}
 
 	private DirectoryReference GetFinalAppPath(StageTarget Target, DeploymentContext SC)
@@ -245,6 +228,15 @@ public abstract class ApplePlatform : Platform
 					}
 				}
 
+				// if we aren't going to archive right after this to another location, and then archivedirectory specifies a .xcarchive extension
+				// then we want to archive directly to that location instead of the standard ~/Library/Developer/Xcode/Archives. This is useful for
+				// build machines that want to make a .xcarchive and export/upload it, without needing to hunt down latest, or dirty up the Library dir
+				string XcArchivePath = "";
+				if (Params.Distribution && Params.Archive == false && Params.ArchiveDirectoryParam.EndsWith(".xcarchive"))
+				{
+					XcArchivePath = Params.ArchiveDirectoryParam;
+					ExtraOptions += $" -archivePath \"{XcArchivePath}\"";
+				}
 
 				// if we we packaging for distrbution, we will create a .xcarchive which can be used to submit to app stores, or exported for other distribution methods
 				// the archive will be created in the standard Archives location accessible via Xcode. Using -archive will copy it out into
@@ -252,17 +244,34 @@ public abstract class ApplePlatform : Platform
 				AppleExports.XcodeBuildMode BuildMode = Params.Distribution ? AppleExports.XcodeBuildMode.Distribute : AppleExports.XcodeBuildMode.Package;
 				if (AppleExports.BuildWithStubXcodeProject(Params.RawProjectPath, Receipt.Platform, Receipt.Architectures, Receipt.Configuration, TargetName, BuildMode, Logger, ExtraOptions) == 0)
 				{
-					Logger.LogInformation("=====================================================================================");
+					Logger.LogInformation("==================================================================================================================================================");
 					if (Params.Distribution)
 					{
-						Logger.LogInformation("Created .xcarchive in Xcode's Library, which can be seen in Xcode's Organizer window");
-						Logger.LogInformation("You may use this to validate and prepare for various distribution methods");
+						if (XcArchivePath == "")
+						{
+							XcArchivePath = AppleExports.FindLatestXcArchive(TargetName)?.FullName;
+							if (XcArchivePath == null)
+							{
+								Logger.LogError("Unable to discover a xcarchive for {TargetName} in ~/Library/Developer/Xcode/Archives.", TargetName);
+								Logger.LogError("This is unexpected since the command succeeded");
+							}
+							else
+							{
+								Logger.LogInformation("Created '{XcArchivePath}', which can be seen in Xcode's Organizer window.", XcArchivePath);
+								Logger.LogInformation("You may use this to validate and prepare for various distribution methods");
+							}
+						}
+						else
+						{
+							Logger.LogInformation("Created '{XcArchivePath}' using an overriden location - this will _not_ appear in Xcode's Organizer window.", XcArchivePath);
+							Logger.LogInformation("You may use this to validate and prepare for various distribution methods");
+						}
 					}
 					else
 					{
 						Logger.LogInformation("Finalized {App} for running fully self-contained", GetFinalAppPath(Target, SC));
 					}
-					Logger.LogInformation("=====================================================================================");
+					Logger.LogInformation("==================================================================================================================================================");
 				}
 			}
 		}
@@ -289,6 +298,7 @@ public abstract class ApplePlatform : Platform
 
 			// distribution mode we want to archive the .xcarchive that was created during Package
 			DirectoryReference ArchiveSource;
+			DirectoryReference ArchiveDestination = null;
 			if (Params.Distribution)
 			{
 				// find the most recent .xcarchive in the Xcode archives library
@@ -298,6 +308,15 @@ public abstract class ApplePlatform : Platform
 				{
 					Logger.LogError("Unable to find a .xcarchive in Xcode's Library to archive to {ArchiveDir}", SC.ArchiveDirectory);
 					return;
+				}
+
+				if (SC.ArchiveDirectory.HasExtension(".xcarchive"))
+				{
+					ArchiveDestination = SC.ArchiveDirectory;
+					if (DirectoryReference.Exists(ArchiveDestination))
+					{
+						DirectoryReference.Delete(ArchiveDestination);
+					}
 				}
 			}
 			else
@@ -310,11 +329,16 @@ public abstract class ApplePlatform : Platform
 				}
 			}
 
-			Logger.LogInformation("=====================================================================================");
-			Logger.LogInformation("Copying {Type} package {ArchiveSource} to archive directory {ArchiveDir}", Params.Distribution ? "Distribution" : "Development", ArchiveSource, SC.ArchiveDirectory);
-			Logger.LogInformation("=====================================================================================");
+			if (ArchiveDestination == null)
+			{
+				ArchiveDestination = DirectoryReference.Combine(SC.ArchiveDirectory, ArchiveSource.GetDirectoryName());
+			}
+			
+			Logger.LogInformation("==================================================================================================================================================");
+			Logger.LogInformation("Copying {Type} package {ArchiveSource} to archive directory {ArchiveDir}", Params.Distribution ? "Distribution" : "Development", ArchiveSource, ArchiveDestination);
+			Logger.LogInformation("==================================================================================================================================================");
 
-			Utils.RunLocalProcessAndReturnStdOut("/usr/bin/env", $"ditto \"{ArchiveSource}\" \"{SC.ArchiveDirectory}/{ArchiveSource.GetDirectoryName()}\"", null);
+			Utils.RunLocalProcessAndReturnStdOut("/usr/bin/env", $"ditto \"{ArchiveSource}\" \"{ArchiveDestination}\"", null);
 		}
 	}
 }
