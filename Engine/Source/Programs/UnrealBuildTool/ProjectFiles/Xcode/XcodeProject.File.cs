9ce42324b09bb7c38f3f5f09ff877c4e7bcd5d2c
adam kinge <adam.kinge@epicgames.com>
2024-06-17 17:47:33 -0400

    Add plists to the allowed resources that can be copied on iOS during staging
    #rnx
    
    [CL 34442857 by adam kinge in ue5-main branch]

--- Engine/Source/Programs/UnrealBuildTool/ProjectFiles/Xcode/XcodeProject.File.cs
+++ Engine/Source/Programs/UnrealBuildTool/ProjectFiles/Xcode/XcodeProject.File.cs
@@ -126,6 +126,7 @@ namespace UnrealBuildTool.XcodeProjectXcconfig
 			".lproj",
 			".png",
 			".xcprivacy",
+			".plist",
 			// no extensions will be folders, like "cookeddata" that we want to copy as a resource
 			"",
 		};
