7505d06b2dee366e782942848a9acf653a6503ee
julien lheureux <julien.lheureux@epicgames.com>
2024-06-18 15:55:41 -0400

    [PCG] Prevent crash when the render data is null, could happen in certain server situations.
    #jira UE-218196
    #rb adrien.logut, huw.bowles, Patrick.Enfedaque, ryan.buehler
    
    [CL 34475160 by julien lheureux in 5.4 branch]

--- Engine/Plugins/PCG/Source/PCG/Private/Helpers/PCGActorHelpers.cpp
+++ Engine/Plugins/PCG/Source/PCG/Private/Helpers/PCGActorHelpers.cpp
@@ -90,7 +90,7 @@ UPCGManagedISMComponent* UPCGActorHelpers::GetOrCreateManagedISMC(AActor* InTarg
 #if WITH_EDITOR
 	const bool bMeshHasNaniteData = StaticMesh->IsNaniteEnabled();
 #else
-	const bool bMeshHasNaniteData = StaticMesh->GetRenderData()->HasValidNaniteData();
+	const bool bMeshHasNaniteData = StaticMesh->GetRenderData() && StaticMesh->GetRenderData()->HasValidNaniteData();
 #endif
 
 	FString ComponentName;
