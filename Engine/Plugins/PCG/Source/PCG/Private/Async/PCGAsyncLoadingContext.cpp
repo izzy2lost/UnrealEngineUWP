75729665c582d81019ff01dc42b863e847646237
adrien logut <adrien.logut@epicgames.com>
2024-06-20 10:07:33 -0400

    [PCG] Fix extra frame taken by async loading when all objects are loaded
    * All already loaded objects are now gathered as strong ptr to keep a reference on them
    * Remove the condition of having all the paths not null, as they are now always checked (and removed if needed)
    
    #rnx
    #rb julien.lheureux
    [FYI] jeansebastien.guay, antoine.guilbaud
    
    [CL 34528089 by adrien logut in ue5-main branch]

--- Engine/Plugins/PCG/Source/PCG/Private/Async/PCGAsyncLoadingContext.cpp
+++ Engine/Plugins/PCG/Source/PCG/Private/Async/PCGAsyncLoadingContext.cpp
@@ -22,13 +22,38 @@ void IPCGAsyncLoadingContext::CancelLoading()
 	LoadHandle.Reset();
 }
 
+void IPCGAsyncLoadingContext::ResolveAlreadyLoadedObjects(TArray<FSoftObjectPath>& ObjectsToLoad)
+{
+	for (int32 i = ObjectsToLoad.Num() - 1; i >= 0; --i)
+	{
+		UObject* ResolvedObject = ObjectsToLoad[i].ResolveObject();
+		if (ResolvedObject || ObjectsToLoad[i].IsNull())
+		{
+			ObjectsToLoad.RemoveAtSwap(i);
+			if (ResolvedObject)
+			{
+				AlreadyLoadedObjects.Emplace(ResolvedObject);
+			}
+		}
+	}
+}
+
 bool IPCGAsyncLoadingContext::RequestResourceLoad(FPCGContext* ThisContext, TArray<FSoftObjectPath>&& ObjectsToLoad, bool bAsynchronous)
 {
 	if (!ObjectsToLoad.IsEmpty() && !bLoadRequested)
 	{
+		ResolveAlreadyLoadedObjects(ObjectsToLoad);
+
+		// If there is nothing to load, early out.
+		if (ObjectsToLoad.IsEmpty())
+		{
+			bLoadRequested = true;
+			return true;
+		}
+
 		if (!bAsynchronous)
 		{
-			LoadHandle = UAssetManager::GetStreamableManager().RequestSyncLoad(std::forward<TArray<FSoftObjectPath>>(ObjectsToLoad));
+			LoadHandle = UAssetManager::GetStreamableManager().RequestSyncLoad(std::move(ObjectsToLoad));
 			bLoadRequested = true;
 
 			return true;
@@ -38,7 +63,7 @@ bool IPCGAsyncLoadingContext::RequestResourceLoad(FPCGContext* ThisContext, TArr
 			ThisContext->bIsPaused = true;
 
 			// It is a bit unsafe to pass this to a delegate lambda. But if the context dies before the completion of the loading, the context will cancel the loading in its dtor.
-			LoadHandle = UAssetManager::GetStreamableManager().RequestAsyncLoad(std::forward<TArray<FSoftObjectPath>>(ObjectsToLoad), [ThisContext]() { ThisContext->bIsPaused = false; });
+			LoadHandle = UAssetManager::GetStreamableManager().RequestAsyncLoad(std::move(ObjectsToLoad), [ThisContext]() { ThisContext->bIsPaused = false; });
 			bLoadRequested = true;
 
 			// If the load handle is not active it means objects were invalid
