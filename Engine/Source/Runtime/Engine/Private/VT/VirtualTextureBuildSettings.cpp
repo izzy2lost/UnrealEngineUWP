f7f4fff7b3de6368a6a25254a5d24cac48ece0b4
jeremy moore <Jeremy.Moore@epicgames.com>
2024-06-21 09:45:53 -0400

    Add alignment to virtual texture tile size when initializing from cvars. This is already enforced in compilation, but there are now other parts of the code that access this structure, so we should do this as early as possible.
    [CL 34560786 by jeremy moore in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/VT/VirtualTextureBuildSettings.cpp
+++ Engine/Source/Runtime/Engine/Private/VT/VirtualTextureBuildSettings.cpp
@@ -15,12 +15,22 @@ static TAutoConsoleVariable<int32> CVarVTTileSize(
 static TAutoConsoleVariable<int32> CVarVTTileBorderSize(
 	TEXT("r.VT.TileBorderSize"),
 	4,
-	TEXT("Size in pixels to use for virtual texture tiles borders (rounded to next power-of-2)")
+	TEXT("Size in pixels to use for virtual texture tiles borders (rounded to multiple-of-2)")
 );
+
+int32 FVirtualTextureBuildSettings::ClampAndAlignTileSize(int32 InTileSize)
+{
+	return FMath::RoundUpToPowerOfTwo(FMath::Clamp<uint32>(InTileSize, 16u, 1024u));
+}
+
+int32 FVirtualTextureBuildSettings::ClampAndAlignTileBorderSize(int32 InTileBorderSize)
+{
+	// Round tile border size up to multiple of 2 to ensure that block compressed formats will be OK.
+	return (FMath::Clamp<uint32>(InTileBorderSize, 0u, 8u) + 1u) & ~1u;
+}
 
 void FVirtualTextureBuildSettings::Init()
 {
-	TileSize = CVarVTTileSize.GetValueOnAnyThread();
-	TileBorderSize = CVarVTTileBorderSize.GetValueOnAnyThread();
+	TileSize = ClampAndAlignTileSize(CVarVTTileSize.GetValueOnAnyThread());
+	TileBorderSize = ClampAndAlignTileBorderSize(CVarVTTileBorderSize.GetValueOnAnyThread());
 }
-
