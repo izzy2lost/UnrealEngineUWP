3b632a24888979c432d6b5d2292162c30ae9ebcc
serge bernier <Serge.Bernier@epicgames.com>
2024-06-25 17:31:12 -0400

    Fix SVT commandlets. Flush all rendering commands issued by UpdateAllPrimitiveSceneInfos inside the FGlobalComponentRecreateRenderStateContext. Some rendering commands may trigger some shader compilations that needs to be issued and wait for completion before rendering the RVT.
    #rb jonathan.bard
    #tests Editor + cmdlet
    
    
    [FYI] john.huelin
    
    [CL 34657774 by serge bernier in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Private/WorldPartition/WorldPartitionRuntimeVirtualTextureBuilder.cpp
+++ Engine/Source/Editor/UnrealEd/Private/WorldPartition/WorldPartitionRuntimeVirtualTextureBuilder.cpp
@@ -122,6 +122,10 @@ bool UWorldPartitionRuntimeVirtualTextureBuilder::RunInternal(UWorld* World, con
 			FGlobalComponentRecreateRenderStateContext Context;
 		}
 
+		// Flush all rendering commands issued by UpdateAllPrimitiveSceneInfos inside the FGlobalComponentRecreateRenderStateContext. 
+		// Some rendering commands may trigger some shader compilations that we need to be issued and wait for completion before rendering the RVT.
+		FlushRenderingCommands();
+
 		// FGlobalComponentRecreateRenderStateContext can create new shaderJobs, make sure to wait on them.
 		FAssetCompilingManager::Get().FinishAllCompilation();
 		FAssetCompilingManager::Get().ProcessAsyncTasks();
