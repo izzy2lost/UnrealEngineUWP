20295f3beb01bf8f9033d83982efe787d7165d0f
rob gay <Rob.Gay@epicgames.com>
2024-06-27 17:32:15 -0400

    Minor comment add per earlier feedback #rb rob.gay #rnx
    [CL 34724650 by rob gay in ue5-main branch]

--- Engine/Plugins/Runtime/Metasound/Source/MetasoundFrontend/Public/MetasoundFrontendDocumentIdGenerator.h
+++ Engine/Plugins/Runtime/Metasound/Source/MetasoundFrontend/Public/MetasoundFrontendDocumentIdGenerator.h
@@ -75,7 +75,10 @@ namespace Metasound
 			bool bIsDeterministic = false;
 		};
 
-		// For generating IDs that are not derived from a given document. 
+		// For generating IDs that are derived from a given class vertex.  Unlike
+		// document ID generation, the class ID generator's results are not unique
+		// upon each request and therefor can deterministically generate the same
+		// ID for the same provided vertex.
 		class FClassIDGenerator
 		{
 		public: 
