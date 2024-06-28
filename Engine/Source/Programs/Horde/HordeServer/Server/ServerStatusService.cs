218bbc5223435406becf717c5ff6cd91df31d023
ben marsh <Ben.Marsh@epicgames.com>
2024-06-28 01:43:02 -0400

    Horde: Move IHealthMonitor into HordeServer.Shared.
    #rnx
    
    [CL 34732698 by ben marsh in ue5-main branch]

--- Engine/Source/Programs/Horde/HordeServer/Server/ServerStatusService.cs
+++ Engine/Source/Programs/Horde/HordeServer/Server/ServerStatusService.cs
@@ -14,30 +14,6 @@ using StackExchange.Redis;
 
 namespace HordeServer.Server;
 
-/// <summary>
-/// Allows reporting the health of a subsystem
-/// </summary>
-public interface IHealthMonitor
-{
-	/// <summary>
-	/// Sets the name used for reporting status of this service
-	/// </summary>
-	void SetName(string name);
-
-	/// <summary>
-	/// Updates the current health of a system
-	/// </summary>
-	Task UpdateAsync(HealthStatus result, string? message = null, DateTimeOffset? timestamp = null);
-}
-
-/// <summary>
-/// Typed implementation of <see cref="IHealthMonitor"/>
-/// </summary>
-/// <typeparam name="T">Type of the subsystem</typeparam>
-public interface IHealthMonitor<T> : IHealthMonitor
-{
-}
-
 internal class HealthMonitor<T> : IHealthMonitor<T>
 {
 	private readonly ServerStatusService _statusService;
