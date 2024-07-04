953a095093051ca3760afe493bc37f5a0166533d
matt chapman <matt.chapman@geotech.one>
2024-07-04 16:38:34 -0400

    Material Designer - Changed wrong Editor Only Data version registeration variable name.
    [CL 34775389 by matt chapman in ue5-main branch]

--- Engine/Plugins/Experimental/DynamicMaterial/Source/DynamicMaterialEditor/Private/Model/DynamicMaterialModelEditorOnlyDataVersion.cpp
+++ Engine/Plugins/Experimental/DynamicMaterial/Source/DynamicMaterialEditor/Private/Model/DynamicMaterialModelEditorOnlyDataVersion.cpp
@@ -4,4 +4,4 @@
 #include "Serialization/CustomVersion.h"
 
 const FGuid FDynamicMaterialModelEditorOnlyDataVersion::GUID(0xFCF57AFC, 0x50764285, 0xB9A9E660, 0xFFA02D34);
-FCustomVersionRegistration GRegisterDMTextureUVVersion(FDynamicMaterialModelEditorOnlyDataVersion::GUID, static_cast<int32>(FDynamicMaterialModelEditorOnlyDataVersion::Type::LatestVersion), TEXT("DynamicMaterialModelEditorOnlyData"));
+FCustomVersionRegistration GRegisterDynamicMaterialModelEditorOnlyDataVersion(FDynamicMaterialModelEditorOnlyDataVersion::GUID, static_cast<int32>(FDynamicMaterialModelEditorOnlyDataVersion::Type::LatestVersion), TEXT("DynamicMaterialModelEditorOnlyData"));
