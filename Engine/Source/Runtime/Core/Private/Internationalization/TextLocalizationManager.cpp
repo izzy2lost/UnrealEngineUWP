ff998aa736fc6bf7cc81bd539c7a027220ec62e9
mark meijer <mark.meijer@xa.epicgames.com>
2024-07-15 12:21:34 -0400

    Restore ExecuteOnGameThread from a bad merge
    #rb Jamie.Dale, mihnea.balta
    #jira UE-218952
    
    [CL 34815492 by mark meijer in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/Internationalization/TextLocalizationManager.cpp
+++ Engine/Source/Runtime/Core/Private/Internationalization/TextLocalizationManager.cpp
@@ -15,6 +15,7 @@
 #include "Misc/FileHelper.h"
 #include "Misc/Parse.h"
 #include "Misc/ScopeLock.h"
+#include "Containers/Ticker.h"
 #include "Misc/ScopeRWLock.h"
 #include "Misc/CommandLine.h"
 #include "Misc/LazySingleton.h"
@@ -1972,7 +1973,7 @@ void FTextLocalizationManager::DirtyTextRevision()
 	}
 	else
 	{
-		AsyncTask(ENamedThreads::GameThread, []()
+		ExecuteOnGameThread(TEXT("OnTextRevisionChangedEventBroadcastGT"), []()
 		{
 			FTextLocalizationManager::Get().OnTextRevisionChangedEvent.Broadcast();
 		});
