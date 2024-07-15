2b4024f4e1ac2ac385d2f1885decacd85246e158
brad monahan <brad.monahan@xa.epicgames.com>
2024-07-15 10:19:31 -0400

    [Sequencer] Added interface for AControlRigShapeActor to interact with Sequencer and fix cursor display on hover of selectable control rig handle when Sequencer selection limiting. #rb Mike.Zyracki
    [CL 34812555 by brad monahan in ue5-main branch]

--- Engine/Source/Editor/Kismet/Private/SCSEditorViewportClient.cpp
+++ Engine/Source/Editor/Kismet/Private/SCSEditorViewportClient.cpp
@@ -1232,7 +1232,7 @@ void FSCSEditorViewportClient::UpdateHoverFromHitProxy(HHitProxy* const InHitPro
 		else if (InHitProxy->IsA(HActor::StaticGetType()))
 		{
 			const HActor* const ActorHitProxy = static_cast<HActor*>(InHitProxy);
-			if (ActorHitProxy && IsValid(ActorHitProxy->PrimComponent))
+			if (ActorHitProxy && IsValid(ActorHitProxy->Actor))
 			{
 				if (bIsViewportSelectionLimited)
 				{
@@ -1252,8 +1252,6 @@ void FSCSEditorViewportClient::UpdateHoverFromHitProxy(HHitProxy* const InHitPro
 		});
 
 	// Set mouse cursor after hovered primitive component list has been updated
-	MouseCursor.Reset();
-
 	if (bIsGizmoHit)
 	{
 		MouseCursor = EMouseCursor::CardinalCross;
@@ -1265,5 +1263,9 @@ void FSCSEditorViewportClient::UpdateHoverFromHitProxy(HHitProxy* const InHitPro
 	else if (bIsViewportSelectionLimited)
 	{
 		MouseCursor = EMouseCursor::SlashedCircle;
+	}
+	else
+	{
+		MouseCursor.Reset();
 	}
 }
--- Engine/Source/Editor/UnrealEd/Private/EditorViewportSelectability.cpp
+++ Engine/Source/Editor/UnrealEd/Private/EditorViewportSelectability.cpp
@@ -266,10 +266,14 @@ bool FEditorViewportSelectability::SelectActorsByPredicate(const bool bInSelect
 
 	if (InActors.IsEmpty())
 	{
-		for (FActorIterator Iter(ActorSelection->GetWorld()); Iter; ++Iter)
+		const UWorld* const World = ActorSelection->GetWorld();
+		if (IsValid(World))
 		{
-			AActor* const Actor = *Iter;
-			SelectIfPossible(Actor);
+			for (FActorIterator Iter(World); Iter; ++Iter)
+			{
+				AActor* const Actor = *Iter;
+				SelectIfPossible(Actor);
+			}
 		}
 	}
 	else
@@ -400,7 +404,7 @@ void FEditorViewportSelectability::UpdateHoverFromHitProxy(HHitProxy* const InHi
 		else if (InHitProxy->IsA(HActor::StaticGetType()))
 		{
 			const HActor* const ActorHitProxy = static_cast<HActor*>(InHitProxy);
-			if (ActorHitProxy && IsValid(ActorHitProxy->PrimComponent))
+			if (ActorHitProxy && IsValid(ActorHitProxy->Actor))
 			{
 				if (bSelectionLimited)
 				{
@@ -414,8 +418,6 @@ void FEditorViewportSelectability::UpdateHoverFromHitProxy(HHitProxy* const InHi
 	UpdateHoveredActorPrimitives(Actor);
 
 	// Set mouse cursor after hovered primitive component list has been updated
-	MouseCursor.Reset();
-
 	if (bIsGizmoHit)
 	{
 		MouseCursor = EMouseCursor::CardinalCross;
@@ -428,6 +430,10 @@ void FEditorViewportSelectability::UpdateHoverFromHitProxy(HHitProxy* const InHi
 	{
 		MouseCursor = EMouseCursor::SlashedCircle;
 	}
+	else
+	{
+		MouseCursor.Reset();
+	}
 }
 
 bool FEditorViewportSelectability::HandleClick(FEditorViewportClient* const InViewportClient, HHitProxy* const InHitProxy, const FViewportClick& InClick)
@@ -435,25 +441,21 @@ bool FEditorViewportSelectability::HandleClick(FEditorViewportClient* const InVi
 	// Disable actor selection when sequencer is limiting selection
 	const int32 HitX = InViewportClient->Viewport->GetMouseX();
 	const int32 HitY = InViewportClient->Viewport->GetMouseY();
-
 	HHitProxy* const HitResult = InViewportClient->Viewport->GetHitProxy(HitX, HitY);
 	if (!HitResult)
 	{
 		return false;
 	}
 
-	if (HitResult->IsA(HActor::StaticGetType()))
+	if (HitResult->IsA(HWidgetAxis::StaticGetType()) || !HitResult->IsA(HActor::StaticGetType()))
 	{
-		const HActor* const ActorHitProxy = static_cast<HActor*>(HitResult);
-		if (ActorHitProxy
-			&& IsValid(ActorHitProxy->Actor)
-			&& !IsObjectSelectableInViewport(ActorHitProxy->Actor))
-		{
-			return true;
-		}
+		return false;
 	}
 
-	return false;
+	const HActor* const ActorHitProxy = static_cast<HActor*>(HitResult);
+	return ActorHitProxy
+		&& IsValid(ActorHitProxy->Actor)
+		&& !IsObjectSelectableInViewport(ActorHitProxy->Actor);
 }
 
 bool FEditorViewportSelectability::BoxSelectWorldActors(FBox& InBox, FEditorViewportClient* const InEditorViewportClient, const bool bInSelect)
