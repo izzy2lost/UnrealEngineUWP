3d692b8c18953d7c951514cab538a63b699cba53
russell johnston <russell.johnston@epicgames.com>
2024-07-16 13:28:35 -0400

    Bind native Verse APIs during module initialization
    This will enable module top-levels to invoke native constructors, such as those used for localization.
    
    #rb Markus.Breyer, Tim.Smith
    
    [CL 34846395 by russell johnston in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Public/VerseVM/VVMEngineEnvironment.h
+++ Engine/Source/Runtime/CoreUObject/Public/VerseVM/VVMEngineEnvironment.h
@@ -8,11 +8,13 @@
 class FString;
 class UPackage;
 class UStruct;
+struct FTopLevelAssetPath;
 
 namespace uLang
 {
 class CTypeBase;
-}
+class CScope;
+} // namespace uLang
 
 namespace Verse
 {
@@ -26,17 +28,23 @@ enum class EPackageStage : uint8;
 class IEngineEnvironment
 {
 public:
-	// Create a new UPackage with the given package name
-	virtual UPackage* CreateUPackage(FAllocationContext Context, const TCHAR* PackageName) = 0;
+	// Collect property information during code generation.
+	virtual void CollectPropertyInfo(FAllocationContext Context, const uLang::CTypeBase* Type, VPropertyType** OutPropertyType) = 0;
+
+	// Build the key used to look up native binding info for a module, class, or struct.
+	virtual FTopLevelAssetPath GetAssetPathForScope(const uLang::CScope& Scope) = 0;
+
+	// Bind a native module, class, or struct.
+	virtual void TryBindNativeAsset(FAllocationContext Context, const FTopLevelAssetPath& Path) = 0;
 
 	// Given a UPackage name, adjust the name when the package stage is either DEAD or TEMP.
 	virtual const TCHAR* AdornPackageName(const TCHAR* PackageName, EPackageStage Stage, FString& ScratchSpace) = 0;
 
-	// Create a new UClass/UScriptStruct from an existing VClass
-	virtual UStruct* CreateUStruct(FAllocationContext Context, VClass* Class) = 0;
+	// Create a new UPackage with the given package name
+	virtual UPackage* CreateUPackage(FAllocationContext Context, const TCHAR* PackageName) = 0;
 
-	// Collect property information
-	virtual void CollectPropertyInfo(FAllocationContext Context, const uLang::CTypeBase* Type, VPropertyType** OutPropertyType) = 0;
+	// Create a new UClass/UScriptStruct from an existing VClass during native binding or for CVarUObjectProbability.
+	virtual UStruct* CreateUStruct(FAllocationContext Context, VClass* Class) = 0;
 };
 } // namespace Verse
 #endif // WITH_VERSE_VM
