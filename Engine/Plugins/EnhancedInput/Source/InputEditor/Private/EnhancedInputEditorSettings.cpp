dff3cb583a87f45e6ec951f97785716e6ba67aab
ben hoffman <ben.hoffman@epicgames.com>
2024-07-17 16:28:14 -0400

    Remove the additional "console variable" metadata from the EI editor settings as they did not really work intuitively. This will ensure that your editor prefs persist between sessions. If you wish to set these settings in a config file, then you can do so with the normal object sytax:
      [/Script/InputEditor.EnhancedInputEditorProjectSettings]
      bAutomaticallyStartConsumingInput=true
      bLogAllInput=true
    
    #jira UE-218081
    #rb jodon.karlik
    
    [CL 34884119 by ben hoffman in ue5-main branch]

--- Engine/Plugins/EnhancedInput/Source/InputEditor/Private/EnhancedInputEditorSettings.cpp
+++ Engine/Plugins/EnhancedInput/Source/InputEditor/Private/EnhancedInputEditorSettings.cpp
@@ -3,29 +3,8 @@
 #include "EnhancedInputEditorSettings.h"
 #include "EnhancedInputDeveloperSettings.h"
 #include "EnhancedPlayerInput.h"
-#include "HAL/IConsoleManager.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(EnhancedInputEditorSettings)
-
-namespace UE::EnhancedInput::Private
-{
-	namespace ConsoleVariables
-	{
-		static bool bShouldLogAllInputs = false;
-		static FAutoConsoleVariableRef CVarShouldLogAllInputs(
-			TEXT("EnhancedEditorInput.bShouldLogAllInputs"),
-			bShouldLogAllInputs,
-			TEXT("Should each InputKey call be logged?"),
-			ECVF_Default);
-
-		static bool bAutomaticallyStartConsumingInput = false;
-		static FAutoConsoleVariableRef CVarAutomaticallyStartConsumingInput(
-			TEXT("EnhancedEditorInput.bAutomaticallyStartConsumingInput"),
-			bAutomaticallyStartConsumingInput,
-			TEXT("Should the UEnhancedInputEditorSubsystem be started as soon as it is inialized?"),
-			ECVF_Default);	
-	}
-}
 
 ////////////////////////////////////////////////////////////////////////
 // UEnhancedInputEditorProjectSettings
