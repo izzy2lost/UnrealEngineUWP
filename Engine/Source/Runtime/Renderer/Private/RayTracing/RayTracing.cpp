748eb7cdd92bf7d26c0441fb608d6c893a99b8f6
kenzo terelst <Kenzo.Terelst@epicgames.com>
2024-07-19 08:51:39 -0400

    Use primary view to get the LumenHardwareRayTracingUniformBuffer during raytracing SBT setup
    [CL 34929946 by kenzo terelst in ue5-main branch]

--- Engine/Source/Runtime/Renderer/Private/RayTracing/RayTracing.cpp
+++ Engine/Source/Runtime/Renderer/Private/RayTracing/RayTracing.cpp
@@ -340,7 +340,7 @@ namespace RayTracing
 			StaticUniformBuffers.AddUniformBuffer(View.ViewUniformBuffer.GetReference());
 			StaticUniformBuffers.AddUniformBuffer(SceneUniformBuffer);
 			StaticUniformBuffers.AddUniformBuffer(Nanite::GRayTracingManager.GetUniformBuffer().GetReference());
-			StaticUniformBuffers.AddUniformBuffer(View.LumenHardwareRayTracingUniformBuffer->GetRHI());
+			StaticUniformBuffers.AddUniformBuffer(View.GetPrimaryView()->LumenHardwareRayTracingUniformBuffer->GetRHI());
 
 			StaticUniformBufferScope.Emplace(RHICmdList, StaticUniformBuffers);
 		}
