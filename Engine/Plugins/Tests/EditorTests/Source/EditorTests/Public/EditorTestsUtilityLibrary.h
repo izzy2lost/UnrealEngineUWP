c37551b3cbd5e4f08a7b224a65d3914ffe5d1c0d
sebastien lussier <Sebastien.Lussier@epicgames.com>
2024-07-19 10:19:45 -0400

    EngineTest - Added new actor merging test case which tries to merge meshes with different configurations that include empty sections
    #jira UE-189838
    #rb JeanFrancois.Dube
    
    #virtualized
    
    [CL 34931498 by sebastien lussier in ue5-main branch]

--- Engine/Plugins/Tests/EditorTests/Source/EditorTests/Public/EditorTestsUtilityLibrary.h
+++ Engine/Plugins/Tests/EditorTests/Source/EditorTests/Public/EditorTestsUtilityLibrary.h
@@ -4,7 +4,9 @@
 
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "Components/StaticMeshComponent.h"
+
 #include "MeshMerge/MeshMergingSettings.h"
+#include "MeshMerge/MeshProxySettings.h"
 
 #include "EditorTestsUtilityLibrary.generated.h"
 
@@ -26,12 +28,16 @@ public:
 
 	/** Merges meshes and bakes out materials into a atlas-material for the given set of static mesh components using the MergeSettings */
 	UFUNCTION(BlueprintCallable, Category = "MeshMergingLibrary|Test")
-	static void MergeStaticMeshComponents(TArray<UStaticMeshComponent*> InStaticMeshComponents, const FMeshMergingSettings& MergeSettings, const bool bReplaceActors, TArray<int32>& OutLODIndices);
+	static void MergeStaticMeshComponents(const TArray<UStaticMeshComponent*>& InStaticMeshComponents, const FMeshMergingSettings& MergeSettings, const bool bReplaceActors, TArray<int32>& OutLODIndices);
+
+	/** Simplify meshes and bakes out materials into a atlas-material for the given set of static mesh components using the ProxySettings */
+	UFUNCTION(BlueprintCallable, Category = "MeshMergingLibrary|Test")
+	static void CreateProxyMesh(const TArray<UStaticMeshComponent*>& InStaticMeshComponents, const struct FMeshProxySettings& ProxySettings);
 
 	/** Finds a UWidget object used by the editor - useful for testing widget editing */
 	UFUNCTION(BlueprintCallable, Category = "User Interface|Test")
 	static UWidget* GetChildEditorWidgetByName(UWidgetBlueprint* WidgetBlueprint, FString Name);
-	
+
 	/** Simple function for setting UWidget::Navigation, which has a details customization */
 	UFUNCTION(BlueprintCallable, Category = "User Interface|Test")
 	static void SetEditorWidgetNavigationRule(UWidget* Widget, EUINavigation Nav, EUINavigationRule Rule);
