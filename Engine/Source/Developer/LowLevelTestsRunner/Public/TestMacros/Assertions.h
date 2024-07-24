1d85c48d81650421fc7097ec1821711d395e8694
jonathan bard <jonathan.bard@epicgames.com>
2024-07-24 08:47:27 -0400

    Added REQUIRE_CHECK_SLOW to low-level test macros, that validates that checkSlow, checkfSlow... are called on configurations where they're effective
    #rb Chris.Constantinescu
    #tests FoundationTests debug+development
    
    [CL 35051388 by jonathan bard in ue5-main branch]

--- Engine/Source/Developer/LowLevelTestsRunner/Public/TestMacros/Assertions.h
+++ Engine/Source/Developer/LowLevelTestsRunner/Public/TestMacros/Assertions.h
@@ -20,8 +20,14 @@
 //requires that a UE `check` fails in this call
 #define REQUIRE_CHECK(...) INTERNAL_UE_CHECK( "REQUIRE_CHECK", DO_CHECK, Catch::ResultDisposition::Normal, #__VA_ARGS__, __VA_ARGS__ )
 
-//requires that a UE `check` fails in this call containg the supplies message
+//requires that a UE `check` fails in this call contains the supplies message
 #define REQUIRE_CHECK_MSG(msg, ...) INTERNAL_UE_CHECK_MSG(msg, "REQUIRE_CHECK", DO_CHECK, Catch::ResultDisposition::Normal, #__VA_ARGS__, __VA_ARGS__ )
+
+//requires that a UE `checkSlow` fails in this call
+#define REQUIRE_CHECK_SLOW(...) INTERNAL_UE_CHECK( "REQUIRE_CHECK_SLOW", DO_GUARD_SLOW, Catch::ResultDisposition::Normal, #__VA_ARGS__, __VA_ARGS__ )
+
+//requires that a UE `checkSlow` fails in this call contains the supplies message
+#define REQUIRE_CHECK_SLOW_MSG(msg, ...) INTERNAL_UE_CHECK_MSG(msg, "REQUIRE_CHECK_SLOW", DO_GUARD_SLOW, Catch::ResultDisposition::Normal, #__VA_ARGS__, __VA_ARGS__ )
 
 #define INTERNAL_UE_ENSURE( macroName, doEnsure, resultDisposition, ensureExpr, ... ) \
 	do { \
@@ -31,7 +37,7 @@
 			static_cast<void>(__VA_ARGS__); \
 			bool bEncounteredEnsure = scope.GetCount() > 0; \
 			if (doEnsure && !bEncounteredEnsure) \
-				catchAssertionHandler.handleMessage( Catch::ResultWas::ExplicitFailure ,"Expected failure of `ensure` not recieved."); \
+				catchAssertionHandler.handleMessage( Catch::ResultWas::ExplicitFailure ,"Expected failure of `ensure` not received."); \
 		} INTERNAL_CATCH_CATCH( catchAssertionHandler ) \
 		INTERNAL_CATCH_REACT( catchAssertionHandler ) \
 	} while(false) \
@@ -44,7 +50,7 @@
 			static_cast<void>(__VA_ARGS__); \
 			bool bEncounteredEnsure = scope.GetCount() > 0; \
 			if (doEnsure && !bEncounteredEnsure) \
-				catchAssertionHandler.handleMessage( Catch::ResultWas::ExplicitFailure, ( Catch::MessageStream() << "Expected failure of `ensure` with message '" << msg << "' not recieved" + ::Catch::StreamEndStop() ).m_stream.str() ); \
+				catchAssertionHandler.handleMessage( Catch::ResultWas::ExplicitFailure, ( Catch::MessageStream() << "Expected failure of `ensure` with message '" << msg << "' not received" + ::Catch::StreamEndStop() ).m_stream.str() ); \
 		} INTERNAL_CATCH_CATCH( catchAssertionHandler ) \
 		INTERNAL_CATCH_REACT( catchAssertionHandler ) \
 	} while(false) \
@@ -57,7 +63,7 @@
 			__VA_ARGS__; \
 			bool bEncounteredEnsure = scope.GetCount() > 0; \
 			if (doCheck && !bEncounteredEnsure) \
-				catchAssertionHandler.handleMessage( Catch::ResultWas::ExplicitFailure, "Expected failure of `check` not recieved"); \
+				catchAssertionHandler.handleMessage( Catch::ResultWas::ExplicitFailure, "Expected failure of `check` not received"); \
 		} INTERNAL_CATCH_CATCH( catchAssertionHandler ) \
 		INTERNAL_CATCH_REACT( catchAssertionHandler ) \
 	} while(false) \
@@ -70,7 +76,7 @@
 			__VA_ARGS__; \
 			bool bEncounteredEnsure = scope.GetCount() > 0; \
 			if (doCheck && !bEncounteredEnsure) \
-				catchAssertionHandler.handleMessage( Catch::ResultWas::ExplicitFailure, ( Catch::MessageStream() << "Expected failure of `check` containing message '" << msg << "' not recieved" + ::Catch::StreamEndStop() ).m_stream.str() ); \
+				catchAssertionHandler.handleMessage( Catch::ResultWas::ExplicitFailure, ( Catch::MessageStream() << "Expected failure of `check` containing message '" << msg << "' not received" + ::Catch::StreamEndStop() ).m_stream.str() ); \
 		} INTERNAL_CATCH_CATCH( catchAssertionHandler ) \
 		INTERNAL_CATCH_REACT( catchAssertionHandler ) \
 	} while(false) \
