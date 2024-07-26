a5b11b643660740cbb8e08f6e599cbe9fe76d0ea
stu mckenna <Stu.McKenna@epicgames.com>
2024-07-26 18:36:51 -0400

    - Disable async compilation for Niagara - There is an issue regarding rapid iteration parameters that needs to be resolved before this can be enabled again
    #rb Patrick.Kelly
    #rnx
    #tests Asset that previously was broken works as expected
    #lockdown bob.tellez
    
    [CL 35122186 by stu mckenna in ue5-main branch]

--- Engine/Source/Runtime/Core/Private/UObject/FortniteReleaseBranchCustomObjectVersion.cpp
+++ Engine/Source/Runtime/Core/Private/UObject/FortniteReleaseBranchCustomObjectVersion.cpp
@@ -6,7 +6,7 @@ TMap<FGuid, FGuid> FFortniteReleaseBranchCustomObjectVersion::GetSystemGuids()
 	TMap<FGuid, FGuid> SystemGuids;
 	const FDevSystemGuids& DevGuids = FDevSystemGuids::Get();
 
-	SystemGuids.Add(DevGuids.Niagara_LatestScriptCompileVersion, FGuid("5F589BAA37394A51BF718ECB10D73C19"));
+	SystemGuids.Add(DevGuids.Niagara_LatestScriptCompileVersion, FGuid("D35A655CD3FDA4408724D77E1316E35B"));
 	SystemGuids.Add(DevGuids.SkeletalMeshDerivedDataVersion, FGuid("5705956EC7134274A5A1BD3FC74DB3A9"));
 	SystemGuids.Add(DevGuids.MaterialTranslationDDCVersion, FGuid("CC1B3A336E144A0B9E78014E07775988"));
 	return SystemGuids;
