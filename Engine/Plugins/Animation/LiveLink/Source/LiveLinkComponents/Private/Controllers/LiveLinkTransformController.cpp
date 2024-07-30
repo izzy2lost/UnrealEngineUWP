5f2f0f493d04706e0ae637b2ae767d9f555d5e5f
richard graham <richard.graham@epicgames.com>
2024-07-30 11:43:58 -0400

    LiveLink/Performance Capture - Added bEvaluateLiveLink uproperty to livelinktransform controller so users can pause animation from details panel and from BP.
    #jira UE-151353
    #rb jason.walter
    
    [CL 35185574 by richard graham in ue5-main branch]

--- Engine/Plugins/Animation/LiveLink/Source/LiveLinkComponents/Private/Controllers/LiveLinkTransformController.cpp
+++ Engine/Plugins/Animation/LiveLink/Source/LiveLinkComponents/Private/Controllers/LiveLinkTransformController.cpp
@@ -20,7 +20,7 @@
 
 void FLiveLinkTransformControllerData::ApplyTransform(USceneComponent* SceneComponent, const FTransform& Transform, const FLiveLinkTransformStaticData& StaticData) const
 {
-	if (SceneComponent)
+	if (SceneComponent && bEvaluateLiveLink)
 	{
 		if (bUseLocation && StaticData.bIsLocationSupported)
 		{
--- Engine/Plugins/Animation/LiveLink/Source/LiveLinkComponents/Public/Controllers/LiveLinkTransformController.h
+++ Engine/Plugins/Animation/LiveLink/Source/LiveLinkComponents/Public/Controllers/LiveLinkTransformController.h
@@ -15,6 +15,10 @@ struct LIVELINKCOMPONENTS_API FLiveLinkTransformControllerData
 {
 	GENERATED_BODY()
 
+	/** Whether we should evaluate animation from Live Link */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LiveLink")
+	bool bEvaluateLiveLink = true;
+
 	/** Set the transform of the component in world space of in its local reference frame. */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "LiveLink")
 	bool bWorldTransform = false;
