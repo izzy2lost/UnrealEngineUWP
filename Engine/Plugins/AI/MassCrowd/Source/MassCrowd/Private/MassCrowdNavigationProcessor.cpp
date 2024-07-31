8997f2e5eea1aad14e2522c32c69019e3e6601f2
mieszko zielinski <mieszko.zielinski@epicgames.com>
2024-07-31 09:05:08 -0400

    [Mass] Fixed a check condition in UMassCrowdDynamicObstacleDeinitializer::Initialize that erronously triggered in inactive worlds.
    #rb mieszko.zielinski
    
    [CL 35210938 by mieszko zielinski in ue5-main branch]

--- Engine/Plugins/AI/MassCrowd/Source/MassCrowd/Private/MassCrowdNavigationProcessor.cpp
+++ Engine/Plugins/AI/MassCrowd/Source/MassCrowd/Private/MassCrowdNavigationProcessor.cpp
@@ -278,8 +278,10 @@ void UMassCrowdDynamicObstacleDeinitializer::Initialize(UObject& Owner)
 {
 	Super::Initialize(Owner);
 
-	ZoneGraphAnnotationSubsystem = UWorld::GetSubsystem<UZoneGraphAnnotationSubsystem>(Owner.GetWorld());
-	checkf(ZoneGraphAnnotationSubsystem != nullptr, TEXT("UZoneGraphAnnotationSubsystem is mandatory when using this processor."));
+	UWorld* World = Owner.GetWorld();
+	ZoneGraphAnnotationSubsystem = UWorld::GetSubsystem<UZoneGraphAnnotationSubsystem>(World);
+	checkf(ZoneGraphAnnotationSubsystem != nullptr || (World && World->WorldType == EWorldType::Inactive)
+		, TEXT("UZoneGraphAnnotationSubsystem is mandatory when using this processor."));
 }
 
 void UMassCrowdDynamicObstacleDeinitializer::ConfigureQueries()
