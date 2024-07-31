3fd2a40648845e6d2daef335b9551707a42e07ed
grant medine <grant.medine@epicgames.com>
2024-07-31 10:54:49 -0400

    [Backout] - CL35208372 [FYI] Andrew.Davidson Original CL Desc ----------------------------------------------------------------- Enable IDO serialization by default for Verse classes within the Scenegraph
    #jira UE-195284
    #rb Francis.Hurteau
    
    [CL 35213543 by grant medine in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Private/UObject/InstanceDataObjectUtils.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/UObject/InstanceDataObjectUtils.cpp
@@ -136,7 +136,7 @@ namespace UE
 	static const FName NAME_IDOMapKey(ANSITEXTVIEW("Key"));
 	static const FName NAME_IDOMapValue(ANSITEXTVIEW("Value"));
 
-	bool bEnableIDOSupport = true;
+	bool bEnableIDOSupport = false;
 	FAutoConsoleVariableRef EnableIDOSupportCVar(
 		TEXT("IDO.Enable"),
 		bEnableIDOSupport,
