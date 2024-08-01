396580b11eb5dd74411d955f45d7c448d8a63236
james singer <james.singer@epicgames.com>
2024-08-01 09:53:09 -0400

    Toolchain upgrade to llvm 18.1.0
    fix scripts
        - support Rocky Linux 8 as minimum supported OS
        - deprecate CentOS 7 support
        - update to using a vanilla released version of crosstool
            - configs should now be able to be upgraded when we go to a new version instead of having to be rebuilt
        - changes to script flow to make it easier to iterate on the build process when things break
            - stages of the build are checkpointed so that they don't have to be redone each run
    
    #fyi Arciel.Rekman
    #jira UE-217651
    #rb Brandon.Schaefer
    
    [CL 35243087 by james singer in ue5-main branch]

--- Engine/Build/BatchFiles/Linux/Toolchain/RunMe.bat
+++ Engine/Build/BatchFiles/Linux/Toolchain/RunMe.bat
@@ -1,22 +1,24 @@
 @REM @echo off
 
-set TOOLCHAIN_VERSION=v22
-set LLVM_VERSION=16.0.6
-set LLVM_BRANCH=release/16.x
-set LLVM_TAG=llvmorg-16.0.6
+set TOOLCHAIN_VERSION=v23
+set LLVM_VERSION=18.1.0
+set LLVM_BRANCH=release/18.x
+set LLVM_TAG=llvmorg-18.1.0
 
 set CMAKE_BINARY=%CD%\..\..\..\..\Extras\ThirdPartyNotUE\CMake\bin\cmake.exe
 set PYTHON_BINARY=%CD%\..\..\..\..\Binaries\ThirdParty\Python\Win64\python.exe
-set ZLIB_PATH=%CD%\..\..\..\..\..\Engine\Source\ThirdParty\zlib\v1.2.8
+set ZLIB_PATH=%CD%\..\..\..\..\..\Engine\Source\ThirdParty\zlib\1.3
 set "ZLIB_PATH=%ZLIB_PATH:\=/%"
 set NSIS_BINARY=C:\Program Files (x86)\NSIS\Bin\makensis.exe
 set PATCH_BINARY=C:\Program Files\Git\usr\bin\patch.exe
 set VS_VERSION="Visual Studio 17 2022"
 
-for %%i in (python.exe) do set PYTHON_BINARY="%%~$PATH:i"
-for %%i in (cmake.exe) do set CMAKE_BINARY="%%~$PATH:i"
+rem Use the following two lines if you want to override python or cmake with
+rem the version in your path
+rem for %%i in (python.exe) do set PYTHON_BINARY="%%~$PATH:i"
+rem for %%i in (cmake.exe) do set CMAKE_BINARY="%%~$PATH:i"
 
-set FILENAME=%TOOLCHAIN_VERSION%_clang-%LLVM_VERSION%-centos7
+set FILENAME=%TOOLCHAIN_VERSION%_clang-%LLVM_VERSION%-rockylinux8
 
 echo Building %FILENAME%.exe...
 
@@ -28,15 +30,19 @@ echo Using VisualStudio: %VS_VERSION%
 @REM We need to build in a directory with shorter path, so we avoid hitting path max limit.
 set ROOT_DIR=%CD%
 
-rmdir /S /Q %TEMP%\clang-build-%LLVM_VERSION%
+rmdir /S /Q %TEMP%\clang-build-%LLVM_VERSION% > nul
 mkdir %TEMP%\clang-build-%LLVM_VERSION%
 pushd %TEMP%\clang-build-%LLVM_VERSION%
 
 
-unzip -q -o %ROOT_DIR%\%FILENAME%-windows.zip -d OUTPUT
+rem The MS provided tar can be used to create and extract zip files on windows
+mkdir OUTPUT
+pushd OUTPUT
+tar xf %ROOT_DIR%\%FILENAME%-windows.zip
+popd
 
 echo Cloning rpmalloc to speed up clang/lld runtime operation
-git clone https://github.com/mjansson/rpmalloc rpmalloc --depth 1
+git clone https://github.com/mjansson/rpmalloc -b main rpmalloc --depth 1
 
 
 echo Cloning LLVM (tag %LLVM_TAG% only)
@@ -50,17 +56,20 @@ echo Applying patches
 pushd "source"
 rem set DRY_RUN=--dry-run
 set DRY_RUN=
-"%PATCH_BINARY%" %DRY_RUN% -p 1 -i %ROOT_DIR%\patches\clang\default-dwarf-4.patch 
-"%PATCH_BINARY%" %DRY_RUN% -p 1 -i %ROOT_DIR%\patches\compiler-rt\manually-define-AT_HWCAP2.diff 
-"%PATCH_BINARY%" %DRY_RUN% -p 1 -i %ROOT_DIR%\patches\llvm\disable-auto-upgrade-debug-info.patch 
+rem disabled but left in place in case testing reveals that they are still needed
+rem "%PATCH_BINARY%" %DRY_RUN% -p 1 -i %ROOT_DIR%\patches\clang\default-dwarf-4.patch 
+rem "%PATCH_BINARY%" %DRY_RUN% -p 1 -i %ROOT_DIR%\patches\compiler-rt\manually-define-AT_HWCAP2.diff 
+rem "%PATCH_BINARY%" %DRY_RUN% -p 1 -i %ROOT_DIR%\patches\llvm\disable-auto-upgrade-debug-info.patch 
 popd
 
 
 echo Building LLVM, clang, lld, bolt
 mkdir build_all
 pushd build_all
-%CMAKE_BINARY% -G %VS_VERSION% -DLLVM_ENABLE_PROJECTS=llvm;clang;lld -DLLVM_INTEGRATED_CRT_ALLOC=../rpmalloc -DLLVM_USE_CRT_RELEASE=MT -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="/O2 /Ob3 /DNDEBUG /Zi /Gy" -DCMAKE_CXX_FLAGS_RELEASE="/O2 /Ob3 /DNDEBUG /Zi /Gy" -DCMAKE_EXE_LINKER_FLAGS_RELEASE="/DEBUG /INCREMENTAL:NO /OPT:REF /OPT:ICF" -DCMAKE_INSTALL_PREFIX="..\install" -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_TARGETS_TO_BUILD="AArch64;X86" -DLLVM_ENABLE_ZLIB=FORCE_ON -DZLIB_LIBRARY="%ZLIB_PATH%/lib/Win64-llvm/Release/zlibstatic.lib" -DZLIB_INCLUDE_DIR="%ZLIB_PATH%/include/Win64/VS2015" -DCLANG_REPOSITORY_STRING="github.com/llvm/llvm-project" "..\source\llvm"
+%CMAKE_BINARY% -G %VS_VERSION% -DLLVM_ENABLE_PROJECTS=llvm;clang;lld -DLLVM_INTEGRATED_CRT_ALLOC=../rpmalloc -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="/O2 /Ob3 /DNDEBUG /Zi /Gy" -DCMAKE_CXX_FLAGS_RELEASE="/O2 /Ob3 /DNDEBUG /Zi /Gy" -DCMAKE_EXE_LINKER_FLAGS_RELEASE="/DEBUG /INCREMENTAL:NO /OPT:REF /OPT:ICF" -DCMAKE_INSTALL_PREFIX="..\install" -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_TARGETS_TO_BUILD="AArch64;X86" -DLLVM_ENABLE_ZLIB=FORCE_ON -DZLIB_LIBRARY="%ZLIB_PATH%/lib/Win64/Release/zlibstatic.lib" -DZLIB_INCLUDE_DIR="%ZLIB_PATH%/include/" -DCLANG_REPOSITORY_STRING="github.com/llvm/llvm-project" "..\source\llvm"
+if ERRORLEVEL 1 goto endscript
 %CMAKE_BINARY% --build . --target install --config Release
+IF ERRORLEVEL 1 goto endscript
 popd
 
 for %%G in (aarch64-unknown-linux-gnueabi x86_64-unknown-linux-gnu) do (
@@ -76,18 +85,20 @@ for %%G in (aarch64-unknown-linux-gnueabi x86_64-unknown-linux-gnu) do (
     copy "install\bin\llvm-profdata.exe" OUTPUT\%%G\bin
     copy "install\bin\llvm-symbolizer.exe" OUTPUT\%%G\bin
     copy "install\bin\llvm-objcopy.exe" OUTPUT\%%G\bin
+    copy "install\bin\llvm-cov.exe" OUTPUT\%%G\bin
     copy "install\bin\LTO.dll" OUTPUT\%%G\bin
     xcopy "install\lib\clang" OUTPUT\%%G\lib\clang /s /e /y
 )
 
 @REM Create version file
-echo %TOOLCHAIN_VERSION%_clang-%LLVM_VERSION%-centos7> OUTPUT\ToolchainVersion.txt
+echo %TOOLCHAIN_VERSION%_clang-%LLVM_VERSION%-rockylinux8> OUTPUT\ToolchainVersion.txt
 
 echo Packing final toolchain...
 
 pushd OUTPUT
 del /S /Q %ROOT_DIR%\%FILENAME%.zip
-zip -r %ROOT_DIR%\%FILENAME%.zip *
+@REM the MS provided tar can be used to create and extract zip files on windows
+tar caf %ROOT_DIR%\%FILENAME%.zip *
 popd
 
 if exist "%NSIS_BINARY%" (
@@ -102,5 +113,6 @@ if exist "%NSIS_BINARY%" (
 
 popd
 
+:endscript
 echo.
 echo Done.
