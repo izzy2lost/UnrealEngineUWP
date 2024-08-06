e2d4255cd675fe54e154b3bf7e2ef3b0a68f4cb4
lucas dower <lucas.dower@epicgames.com>
2024-08-06 10:19:05 -0400

    Update hierarchy table builtin plugin from TEDS module rename and remove accidental #pragma once from some c++ files
    #jira UE-221086
    
    [CL 35340288 by lucas dower in ue5-main branch]

--- Engine/Plugins/Experimental/Animation/HierarchyTableBuiltin/Source/Editor/Private/TimeProfile/TimeProfileTypeHandler.cpp
+++ Engine/Plugins/Experimental/Animation/HierarchyTableBuiltin/Source/Editor/Private/TimeProfile/TimeProfileTypeHandler.cpp
@@ -1,7 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#pragma once
-
 #include "TimeProfile/TimeProfileTypeHandler.h"
 #include "TimeProfile/HierarchyTableTypeTime.h"
 #include "TimeProfile/TimeProfileProxyColumn.h"
