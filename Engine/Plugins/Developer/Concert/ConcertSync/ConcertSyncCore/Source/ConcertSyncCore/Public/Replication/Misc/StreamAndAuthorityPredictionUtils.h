f69bad60d6b09a94ff9ef5a99fdbaf8f23fdc673
dominik peacock <dominik.peacock@geotech.one>
2024-08-07 07:34:02 -0400

    Add API for predicting client state based on LeaveReplication activity
    #jira UE-221063
    #rb jason.walter
    
    [CL 35365223 by dominik peacock in ue5-main branch]

--- Engine/Plugins/Developer/Concert/ConcertSync/ConcertSyncCore/Source/ConcertSyncCore/Public/Replication/Misc/StreamAndAuthorityPredictionUtils.h
+++ Engine/Plugins/Developer/Concert/ConcertSync/ConcertSyncCore/Source/ConcertSyncCore/Public/Replication/Misc/StreamAndAuthorityPredictionUtils.h
@@ -2,10 +2,59 @@
 
 #pragma once
 
-#include "ConcertMessageData.h"
+#include "Replication/Messages/RestoreContent.h"
+
+#include "Containers/ContainersFwd.h"
+#include "HAL/Platform.h"
+#include "Templates/FunctionFwd.h"
+
+struct FConcertBaseStreamInfo;
+struct FConcertClientInfo;
+struct FConcertObjectInStreamID;
+struct FConcertSessionActivity;
+struct FConcertSyncActivity;
+struct FConcertSyncReplicationEvent;
+
+template<typename OptionalType> struct TOptional;
 
 namespace UE::ConcertSyncCore::Replication
 {
+	using FExtractReplicationEventFunc = TFunctionRef<void(
+		const int64 EventId,
+		TFunctionRef<void(const FConcertSyncReplicationEvent& Event)> Callback
+		)>;
+	
+	/**
+	 * Walks back the activity history and finds the latest activity that sets the target client's state.
+	 *
+	 * For now, the state is only determined by EConcertSyncReplicationActivityType::LeaveReplication replication activities.
+	 * In the future, additional types may also affect it, e.g. PutState, may then each in turn produce activities.
+	 *
+	 * @param Activities The activities to analyze
+	 * @param IsTargetEndpointFunc Returns whether the endpoint ID corresponds to the client to which we want to get the state.
+	 *	Usually when restoring, you'll want to match the client's DisplayName and DeviceName to that in an activity.
+	 * @param GetReplicationEventFunc Gets the replication event for the given EventId. This is called when iterating Activities.
+	 * @param OutStreams The streams the client had, if successful
+	 * @param OutAuthority The authority the client had, if successful
+	 *
+	 * @return The activity ID that contained the state. Unset if not found.
+	 */
+	CONCERTSYNCCORE_API TOptional<int64> BacktrackActivityHistoryForActivityThatSetsContent(
+		TConstArrayView<FConcertSyncActivity> Activities,
+		TFunctionRef<bool(const FGuid& EndpointId)> IsTargetEndpointFunc,
+		FExtractReplicationEventFunc GetReplicationEventFunc,
+		TArray<FConcertBaseStreamInfo>& OutStreams,
+		TArray<FConcertObjectInStreamID>& OutAuthority
+		);
+	/** Equivalent version that accepts FConcertSessionActivity instead. This is useful if your activities come from IConcertClientWorkspace::GetActivities. */
+	CONCERTSYNCCORE_API TOptional<int64> BacktrackActivityHistoryForActivityThatSetsContent(
+		TConstArrayView<FConcertSessionActivity> Activities,
+		TFunctionRef<bool(const FGuid& EndpointId)> IsTargetEndpointFunc,
+		FExtractReplicationEventFunc GetReplicationEventFunc,
+		TArray<FConcertBaseStreamInfo>& OutStreams,
+		TArray<FConcertObjectInStreamID>& OutAuthority
+		);
+	
 	/**
 	 * Decides whether First and Second should be considered to represent the same user across several Concert sessions.
 	 *
