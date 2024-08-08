228007d4f5ca63de41d4ea3330a7f872ae5797c5
steve robb <Steve.Robb@epicgames.com>
2024-08-08 09:58:55 -0400

    Added deprecation guards and comment to highlight an issue that will cause a deprecation warning with new TSoftObjectPtr changes.
    #jira UE-220443
    [FYI] george.rolfe
    
    [CL 35394786 by steve robb in ue5-main branch]

--- Engine/Plugins/Experimental/Web/WebAPI/Source/WebAPIEditor/Private/Dom/WebAPIOperation.cpp
+++ Engine/Plugins/Experimental/Web/WebAPI/Source/WebAPIEditor/Private/Dom/WebAPIOperation.cpp
@@ -100,15 +100,22 @@ void UWebAPIOperation::Visit(TFunctionRef<void(IWebAPISchemaObjectInterface*&)> 
 	}
 }
 
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
 void UWebAPIOperation::BindToTypeInfo()
 {
 	check(Name.HasTypeInfo());
 
 	if(Name.TypeInfo->Model.IsNull())
 	{
+		// This has been assigning a UWebAPIOperation* to a TSoftObjectPtr<UWebAPIModelBase> since it was submitted,
+		// even though those tpyes are unrelated.  This is dangerous if someone uses the pointer, but work if only
+		// the path is used.
 		Name.TypeInfo->Model = this;
 	}
 }
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 #if WITH_EDITOR
 void UWebAPIOperation::SetCodeText(const FString& InCodeText)
