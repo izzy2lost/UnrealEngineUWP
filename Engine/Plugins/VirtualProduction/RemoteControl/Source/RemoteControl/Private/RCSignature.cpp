8248ea528467c2e5192c663578cfe8e4477b5f46
juan portillo <juan.portillo@xa.epicgames.com>
2024-08-08 17:29:54 -0400

    Remote Control : Revert fixed issue with signatures adding exposed properties
    [CL 35408824 by juan portillo in ue5-main branch]

--- Engine/Plugins/VirtualProduction/RemoteControl/Source/RemoteControl/Private/RCSignature.cpp
+++ Engine/Plugins/VirtualProduction/RemoteControl/Source/RemoteControl/Private/RCSignature.cpp
@@ -164,18 +164,7 @@ int32 FRCSignature::ApplySignature(URemoteControlPreset* InPreset, TConstArrayVi
 			FRCSignatureActionContext ActionContext;
 			ActionContext.Preset = InPreset;
 			ActionContext.Object = Object;
-
-			// Try to re-use existing property in Preset
-			const FName FieldName = InPreset->GetEntityName(NAME_None, Object, Path);
-			const FGuid EntityId = InPreset->GetExposedEntityId(FieldName);
-			ActionContext.Property = InPreset->GetExposedEntity<FRemoteControlProperty>(EntityId).Pin();
-
-			// if not found, create new one
-			if (!ActionContext.Property.IsValid())
-			{
-				ActionContext.Property = InPreset->ExposeProperty(Context, Field.FieldPath, ExposeArgs).Pin();
-			}
-
+			ActionContext.Property = InPreset->ExposeProperty(Context, Field.FieldPath, ExposeArgs).Pin();
 			if (!ActionContext.Property.IsValid())
 			{
 				continue;
