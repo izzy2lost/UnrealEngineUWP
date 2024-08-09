961159a0933b86936b7ec422a67f68d79e699527
steve robb <Steve.Robb@epicgames.com>
2024-08-09 06:24:51 -0400

    Fixed comparing TSoftObjectPtr with TObjectPtr.
    #jira UE-220443
    
    [CL 35418429 by steve robb in ue5-main branch]

--- Engine/Plugins/PCG/Source/PCG/Private/Tests/Elements/PCGStaticMeshSpawnerTest.cpp
+++ Engine/Plugins/PCG/Source/PCG/Private/Tests/Elements/PCGStaticMeshSpawnerTest.cpp
@@ -304,7 +304,7 @@ void TestMeshSelectorWeighted(
 		TObjectPtr<UStaticMesh> StaticMesh = ISMC->GetStaticMesh();
 
 		FPCGMeshSelectorWeightedEntry* Entry = MeshSelector->MeshEntries.FindByPredicate([StaticMesh](const FPCGMeshSelectorWeightedEntry& Entry) {
-			return Entry.Descriptor.StaticMesh == StaticMesh;
+			return Entry.Descriptor.StaticMesh == StaticMesh.Get();
 			});
 
 		if (Test->TestNotNull("Validate instanced mesh exists in MeshEntries", Entry))
