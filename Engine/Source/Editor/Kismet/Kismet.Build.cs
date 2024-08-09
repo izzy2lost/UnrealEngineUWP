fe30cbe427dae4f99b2f893f7a70d2de86baa5f1
dan oconnor <dan.oconnor@epicgames.com>
2024-08-09 15:18:32 -0400

    Add JsonObjectGraphFunctionLibrary, a set of edit time functions for stringifying objects and persisting those strings to temporary files. Includes two new console commands snapshotblueprints <label> and snapshotblueprintclasses <label> which will write snapshots of all loaded blueprints to a folder as specified by the label. Available to python in unreal.JsonObjectGraphFunctionLibrary, as all blueprint callable functions are.
    #rb ben.hoffman
    #jira UE-173782
    [RN] Added JsonObjectGraphFunctionLibrary, a set of edit time functions for stringifying objects and persisting those strings to temporary files. Includes two new console commands snapshotblueprints <label> and snapshotblueprintclasses <label> which will write snapshots of all loaded blueprints to a folder as specified by the label. Available to python in unreal.JsonObjectGraphFunctionLibrary, as all blueprint callable functions are.
    
    [CL 35432290 by dan oconnor in ue5-main branch]

--- Engine/Source/Editor/Kismet/Kismet.Build.cs
+++ Engine/Source/Editor/Kismet/Kismet.Build.cs
@@ -51,6 +51,7 @@ public class Kismet : ModuleRules
 				"JsonUtilities",
 				"DesktopPlatform",
 				"HotReload",
+				"JsonObjectGraph",
 				"UMGEditor",
 				"UMG", // for SBlueprintDiff
 				"WorkspaceMenuStructure",
