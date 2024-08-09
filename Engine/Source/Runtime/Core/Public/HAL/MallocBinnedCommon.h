cbca371b6d56c7941d7235b0631deaa5c9ba1946
dmytro vovk <dmytro.vovk@epicgames.com>
2024-08-09 10:26:24 -0400

    Cleaned up MallocBinnedCommon.h from FArenaParams that's used by the MallocBinnedGPU only as it creates circular includes
    [CL 35421513 by dmytro vovk in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/HAL/MallocBinnedCommon.h
+++ Engine/Source/Runtime/Core/Public/HAL/MallocBinnedCommon.h
@@ -13,9 +13,7 @@
 
 #if PLATFORM_HAS_FPlatformVirtualMemoryBlock
 
-#include "Containers/Array.h"
 #include "HAL/PlatformMemory.h"
-#include "Templates/Function.h"
 
 // A project can define it's own UE_MBC_MAX_LISTED_SMALL_POOL_SIZE and UE_MBC_NUM_LISTED_SMALL_POOLS to reduce runtime memory usage
 // MallocBinnedCommon.cpp has a list of predefined bins that go up to 28672
@@ -106,43 +104,6 @@ struct FSizeTableEntry
 		return BinSize < Other.BinSize;
 	}
 	static uint8 FillSizeTable(uint64 PlatformPageSize, FSizeTableEntry* SizeTable, uint32 BasePageSize, uint32 MinimumAlignment, uint32 MaxSize, uint32 SizeIncrement);
-};
-
-struct FArenaParams
-{
-	// these are parameters you set
-	uint64 AddressLimit = 1024 * 1024 * 1024; // this controls the size of the root hash table
-	uint32 BasePageSize = 4096; // this is used to make sensible calls to malloc and figures into the standard pool sizes if bUseStandardSmallPoolSizes is true
-	uint32 AllocationGranularity = 4096; // this is the granularity of the commit and decommit calls used on the VM slabs
-	uint32 MaxSizePerBundle = 8192;
-	uint32 MaxStandardPoolSize = 128 * 1024; // these are added to the standard pool sizes, mainly to use the TLS caches, they are typically one block per slab
-	uint16 MaxBlocksPerBundle = 64;
-	uint8 MaxMemoryPerBlockSizeShift = 29;
-	uint8 EmptyCacheAllocExtra = 32;
-	uint8 MaxGlobalBundles = 32;
-	uint8 MinimumAlignmentShift = 4;
-	uint8 PoolCount;
-	bool bUseSeparateVMPerPool = !!(BINNEDCOMMON_USE_SEPARATE_VM_PER_POOL);
-	bool bPerThreadCaches = true;
-	bool bUseStandardSmallPoolSizes = true;
-	bool bAttemptToAlignSmallBocks = true;
-	TArray<uint32> AdditionalBlockSizes;
-
-	// This lambdas is similar to the platform virtual memory HAL and by default just call that. 
-	TFunction<FPlatformMemory::FPlatformVirtualMemoryBlock(SIZE_T)> ReserveVM;
-
-	// These allow you to override the large block allocator. The value add here is that MBA tracks the metadata for you and call tell the difference between a large block pointer and a small block pointer.
-	// By defaults these just use the platform VM interface to allocate some committed memory
-	TFunction<void*(SIZE_T, SIZE_T, SIZE_T&, uint32&)> LargeBlockAlloc;
-	TFunction<void(void*, uint32)> LargeBlockFree;
-
-
-	// these are parameters are derived from other parameters
-	uint64 MaxMemoryPerBlockSize;
-	uint32 MaxPoolSize;
-	uint32 MinimumAlignment;
-	uint32 MaximumAlignmentForSmallBlock;
-
 };
 
 #endif	//~PLATFORM_HAS_FPlatformVirtualMemoryBlock
