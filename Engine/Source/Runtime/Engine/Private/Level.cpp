728bf571ffee52498a99a9892dc12f4f2a826de2
gary yuan <gary.yuan@epicgames.com>
2024-08-13 18:41:55 -0400

    [Backout] - CL35497867 [FYI] JeanFrancois.Dube Original CL Desc ----------------------------------------------------------------- Changed an assert to an ensure when serializing a level with an actor that has its outer mismatching the owning level.
    #jira UE-220279
    #rb Sebastien.Lussier
    
    [CL 35514016 by gary yuan in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/Level.cpp
+++ Engine/Source/Runtime/Engine/Private/Level.cpp
@@ -638,10 +638,7 @@ void ULevel::Serialize( FArchive& Ar )
 				return false;
 			}
 
-			if (!ensureMsgf(Actor->GetLevel() == this, TEXT("Actor %s is in wrong level:") LINE_TERMINATOR TEXT("\tOwner Level=%s") LINE_TERMINATOR TEXT("\tActor Level=%s)"), *GetPathNameSafe(Actor), *GetPathNameSafe(this), *GetPathNameSafe(Actor->GetLevel())))
-			{
-				return false;
-			}
+			check(Actor->GetLevel() == this);
 
 			if (Actor->HasAnyFlags(RF_Transient))
 			{
@@ -4325,7 +4322,7 @@ UPackage* ULevel::CreateActorPackage(UPackage* InLevelPackage, EActorPackagingSc
 	const FString PackageName = ULevel::GetActorPackageName(InLevelPackage, InActorPackagingScheme, InActorPath, InTargetContextObject);
 	UPackage* ActorPackage = CreateActorPackageInternal(PackageName, InActorPath);
 	// Should be prevented upstream but we propagate the flag to prevent issues in asset enumeration
-	
+	if (!ensureMsgf(!(InLevelPackage->GetPackageFlags() & PKG_PlayInEditor), TEXT("Actor packages should not be created on PlayInEditor levels")))
 	{
 		ActorPackage->SetPackageFlags(PKG_PlayInEditor);
 	}
