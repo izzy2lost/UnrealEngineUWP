ccd6686972a0a135f1e9c24cdd4fcf6a3eec6944
colonelsalt <sondre@sondre.io>
2024-08-15 12:10:43 -0400

    ensure() instead of check() if OpenXRPath cannot be converted to a string #jira UE-219156 #rb Arciel.Rekman, Josie.Yang
    [CL 35566304 by colonelsalt in ue5-main branch]

--- Engine/Plugins/Runtime/OpenXR/Source/OpenXRHMD/Private/OpenXRCore.cpp
+++ Engine/Plugins/Runtime/OpenXR/Source/OpenXRHMD/Private/OpenXRCore.cpp
@@ -41,7 +41,7 @@ FString FOpenXRPath::ToString() const
 	uint32 PathCount = 0;
 	char PathChars[XR_MAX_PATH_LENGTH];
 	XrResult Result = xrPathToString(Instance, Path, XR_MAX_PATH_LENGTH, &PathCount, PathChars);
-	check(XR_SUCCEEDED(Result));
+	ensure(XR_SUCCEEDED(Result));
 	return XR_SUCCEEDED(Result) ? FString::ConstructFromPtrSize(PathChars, PathCount - 1) : FString();
 }
 
