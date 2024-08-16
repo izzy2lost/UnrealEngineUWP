fbc05b72bb1994a905810e7303d9a73e13c2f28c
sean gugler <sean.gugler@epicgames.com>
2024-08-16 11:21:40 -0400

    TSpscQueue support for holding UniquePtr.
    #rb Devin.Doucette
    [FYI] ryan.mangin
    
    [CL 35595662 by sean gugler in ue5-main branch]

--- Engine/Source/Runtime/Core/Public/Containers/SpscQueue.h
+++ Engine/Source/Runtime/Core/Public/Containers/SpscQueue.h
@@ -86,7 +86,7 @@ public:
 		TOptional<ElementType> LocalElement = Dequeue();
 		if (LocalElement.IsSet())
 		{
-			OutElem = LocalElement.GetValue();
+			OutElem = MoveTempIfPossible(LocalElement.GetValue());
 			return true;
 		}
 		
