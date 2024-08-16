62bcbc44ec43c592e7c63412df68778bbd1f37fb
charlie huguenard <charlie.huguenard@epicgames.com>
2024-08-16 11:05:40 -0400

    [Metasound] Include LOC_DEFINE_REGION so LOCTEXT works
    #jira UE-222020
    #tests local build
    #rnx
    
    [CL 35595169 by charlie huguenard in ue5-main branch]

--- Engine/Plugins/Runtime/Metasound/Source/MetasoundGraphCore/Public/MetasoundParamHelper.h
+++ Engine/Plugins/Runtime/Metasound/Source/MetasoundGraphCore/Public/MetasoundParamHelper.h
@@ -58,10 +58,12 @@
  * Use this macro to define your parameter if you used DECLARE_METASOUND_PARAM, otherwise just use METASOUND_PARAM in a source file
  */
 #if WITH_EDITOR
+#define LOC_DEFINE_REGION
 #define DEFINE_METASOUND_PARAM(NAME, NAME_TEXT, TOOLTIP_TEXT) \
 	const FLazyName NAME##Name = TEXT(NAME_TEXT); \
 	static const FText NAME##Tooltip = LOCTEXT(#NAME "Tooltip", TOOLTIP_TEXT); \
 	static const FText NAME##DisplayName = LOCTEXT(#NAME "DisplayName", NAME_TEXT);
+#undef LOC_DEFINE_REGION
 #else 
 #define DEFINE_METASOUND_PARAM(NAME, NAME_TEXT, TOOLTIP_TEXT) \
 	const FLazyName NAME##Name = TEXT(NAME_TEXT); \
