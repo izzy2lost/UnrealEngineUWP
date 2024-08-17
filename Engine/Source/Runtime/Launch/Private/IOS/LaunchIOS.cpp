45eff08780b058a346f5561c0862be6e24b12330
carl lloyd <carl.lloyd@epicgames.com>
2024-08-17 19:09:27 -0400

    Fix for rendering occuring when moving from the background to the foreground before the application is ready.
    #rb Zack.Neyland, Peter.Sauerbrei
    
    [CL 35616553 by carl lloyd in ue5-main branch]

--- Engine/Source/Runtime/Launch/Private/IOS/LaunchIOS.cpp
+++ Engine/Source/Runtime/Launch/Private/IOS/LaunchIOS.cpp
@@ -57,6 +57,8 @@ void FAppEntry::Suspend(bool bIsInterrupt)
 	{
 		GetMoviePlayer()->Suspend();
 	}
+	
+	FPreLoadScreenManager::EnableRendering(false);
 
 	// if background audio is active, then we don't want to do suspend any audio
 	if ([[IOSAppDelegate GetDelegate] IsFeatureActive:EAudioFeature::BackgroundAudio] == false)
@@ -150,6 +152,8 @@ void FAppEntry::Resume(bool bIsInterrupt)
 	{
 		GetMoviePlayer()->Resume();
 	}
+	
+	FPreLoadScreenManager::EnableRendering(true);
 
 	// if background audio is active, then we don't want to do suspend any audio
 	// @todo: should this check if we were suspended, in case this changes while in the background? (suspend with background off, but resume with background audio on? is that a thing?)
