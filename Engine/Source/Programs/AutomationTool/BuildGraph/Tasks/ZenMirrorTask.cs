cbdbfe69315367147674682ee40d82c773385187
matt peters <Matt.Peters@epicgames.com>
2024-08-20 13:38:29 -0400

    BuildGraph/ZenMirrorTask: Remove unused using statements; they cause warnings in static analysis. #rnx
    [CL 35660734 by matt peters in ue5-main branch]

--- Engine/Source/Programs/AutomationTool/BuildGraph/Tasks/ZenMirrorTask.cs
+++ Engine/Source/Programs/AutomationTool/BuildGraph/Tasks/ZenMirrorTask.cs
@@ -2,18 +2,11 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Xml;
 using EpicGames.Core;
-using EpicGames.Horde;
-using EpicGames.ProjectStore;
 using Microsoft.Extensions.Logging;
 
 namespace AutomationTool.Tasks
