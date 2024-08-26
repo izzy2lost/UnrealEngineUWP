48bce7489f01630147e2ab2acb01ec2dccd0523d
max striebel <max.striebel@epicgames.com>
2024-08-26 03:23:31 -0400

    [NNERuntimeRDG HLSL] Add Convolution variant based on Winograd
    Problem:  There is room for performance improvements for convolutions that have a 3x3 kernel. Since this is a common kernel size it's important to optimize for this.
    
    Solution: Implement a new convolution variant based on Winograd that gets used when we have a 3x3 kernel.
    
    Tested:
    * Win64 Standalone tests: all
    * Win64 Editor tests: all
    * Consoles tests: Convolution, OIDN and Smoke
    
    
    #jira UE-222141
    #rb florent.guinier
    #rnx
    
    
    #virtualized
    
    [CL 35794676 by max striebel in ue5-main branch]

--- Engine/Plugins/Experimental/NNERuntimeRDG/Source/NNERuntimeRDG/Private/Hlsl/NNERuntimeRDGConv.cpp
+++ Engine/Plugins/Experimental/NNERuntimeRDG/Source/NNERuntimeRDG/Private/Hlsl/NNERuntimeRDGConv.cpp
@@ -1,12 +1,18 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "NNERuntimeRDGConv.h"
+
 #include "Algo/AnyOf.h"
+#include "HAL/IConsoleManager.h"
 #include "NNEAttributeMap.h"
 #include "NNETensor.h"
 #include "NNETypes.h"
 #include "NNEHlslShadersConvCS.h"
 #include "NNEHlslShadersConvMatmulCS.h"
+#include "NNEHlslShadersConvWinogradInputCS.h"
+#include "NNEHlslShadersConvWinogradMMMCS.h"
+#include "NNEHlslShadersConvWinogradOutputCS.h"
+#include "NNEHlslShadersConvWinogradWeightsCS.h"
 #include "NNEHlslShadersTypeHelper.h"
 #include "NNERuntimeRDGHelperTranspose.h"
 #include "NNERuntimeRDGHlslHelper.h"
@@ -15,9 +21,23 @@ namespace UE::NNERuntimeRDG::Private::Hlsl
 {
 	DECLARE_GPU_STAT_NAMED(FNNEOperatorConvDefault, TEXT("NNE.Operator.Hlsl.Conv.Default"));
 	DECLARE_GPU_STAT_NAMED(FNNEOperatorConvMatmul, TEXT("NNE.Operator.Hlsl.Conv.Matmul"));
+	DECLARE_GPU_STAT_NAMED(FNNEOperatorConvWinogradInput, TEXT("NNE.Operator.Hlsl.Conv.Winograd.Input"));
+	DECLARE_GPU_STAT_NAMED(FNNEOperatorConvWinogradMMM, TEXT("NNE.Operator.Hlsl.Conv.Winograd.MMM"));
+	DECLARE_GPU_STAT_NAMED(FNNEOperatorConvWinogradOutput, TEXT("NNE.Operator.Hlsl.Conv.Winograd.Output"));
+	DECLARE_GPU_STAT_NAMED(FNNEOperatorConvWinogradWeights, TEXT("NNE.Operator.Hlsl.Conv.Winograd.Weights"));
 
 	using EConvAutoPad = UE::NNEHlslShaders::Internal::EConvAutoPad;
 	using EConvGroupSize = UE::NNEHlslShaders::Internal::EConvGroupSize;
+
+
+	TAutoConsoleVariable<int32> CVarWinogradPrecision(
+		TEXT("nne.hlsl.WinogradPrecision"),
+		0,
+		TEXT("Selects the Precision of the Winograd Convolution implementation.\n")
+		TEXT(" 0: Allow Float16 (fast, low precision) (default)\n")
+		TEXT(" 1: Disable Float16 (medium speed, medium precision)\n")
+		TEXT(" 2: Disable Winograd (slow)")
+	);
 
 	/**
 	 * Convolution operator implementation
@@ -25,6 +45,19 @@ namespace UE::NNERuntimeRDG::Private::Hlsl
 	class FConv : public FOperatorHlsl
 	{
 	public:
+
+		// The first values have to correspond to the values in CVarWinogradPrecision
+		// so that it is possible to convert from the CVar value to this enum.
+		enum class EWinogradPrecision
+		{
+			FP16 = 0,
+			FP32,
+			DISABLED,
+
+			UNDEFINED,
+
+			MAX
+		};
 
 		static FOperatorHlsl* Create()
 		{
@@ -48,6 +81,7 @@ namespace UE::NNERuntimeRDG::Private::Hlsl
 		bool bAreWeightsTransposed = false;
 		EPixelFormat BufferPixelFormat;
 		EConvGroupSize GroupSize = EConvGroupSize::MAX;
+		EWinogradPrecision WinogradTestPrecision;
 
 	public:
 
@@ -124,12 +158,21 @@ namespace UE::NNERuntimeRDG::Private::Hlsl
 				Pads = Attributes.GetValueOrDefault<TArray<int32>>(TEXT("pads"), PadsDefault);
 			}
 			Strides = Attributes.GetValueOrDefault<TArray<int32>>(TEXT("strides"), DilationsOrStridesDefault);
+			if (Strides.Num() != NumDimensions)
+			{
+				UE_LOG(LogNNE, Warning, TEXT("Conv strides count doesn't match the number of spatial dimensions"));
+				return false;
+			}
 			BufferPixelFormat = TensorDataTypeToPixelFormat(Input.GetDataType());
+
+			int32 WinogradTestPrecisionValue = Attributes.GetValueOrDefault<int32>(TEXT("__UE__WinogradPrecision"), static_cast<int32>(EWinogradPrecision::UNDEFINED));
+			WinogradTestPrecisionValue = FMath::Clamp(WinogradTestPrecisionValue, 0, static_cast<int32>(EWinogradPrecision::MAX) - 1);
+			WinogradTestPrecision = static_cast<EWinogradPrecision>(WinogradTestPrecisionValue);
 
 			return true;
 		}
 
-		void DispatchConvDefault (FRDGBuilder& GraphBuilder, const FTensorRDG& Input, const FTensorRDG& Weights, const FTensorRDG* Bias, const FTensorRDG& Output)
+		void DispatchConvDefault (FRDGBuilder& GraphBuilder, const FTensorRDG& Input, const FTensorRDG& Weights, const FTensorRDG* Bias, const FTensorRDG& Output) const
 		{
 			using namespace UE::NNEHlslShaders::Internal;
 
@@ -170,7 +213,7 @@ namespace UE::NNERuntimeRDG::Private::Hlsl
 				FConvCS::GetGroupCount(OutputShape, FConvCS::GetGroupShape(GroupSize, NumDimensions)));
 		}
 
-		bool DispatchConvMatmul(FRDGBuilder& GraphBuilder, const FTensorRDG& Input, const FTensorRDG& Weights, const FTensorRDG* Bias, const FTensorRDG& Output)
+		bool DispatchConvMatmul(FRDGBuilder& GraphBuilder, const FTensorRDG& Input, const FTensorRDG& Weights, const FTensorRDG* Bias, const FTensorRDG& Output) const
 		{
 			using namespace UE::NNEHlslShaders::Internal;
 
@@ -246,6 +289,227 @@ namespace UE::NNERuntimeRDG::Private::Hlsl
 			return true;
 		}
 
+		// This algorithm is inspired by this paper: https://github.com/xuqiantong/CUDA-Winograd/blob/master/report.pdf
+		bool DispatchConvWinograd(FRDGBuilder& GraphBuilder, const FTensorRDG& Input, const FTensorRDG& Weights, const FTensorRDG* Bias, const FTensorRDG& Output) const
+		{
+			using namespace UE::NNEHlslShaders::Internal;
+
+			if (Group != 1)
+				return false;
+			if (Input.GetShape().Rank() != 4)
+				return false;
+			if (Output.GetShape().Rank() != 4)
+				return false;
+			if (Weights.GetShape().Rank() != 4)
+				return false;
+
+			check(Strides.Num() == 2);
+			if (Strides[0] != 1 || Strides[1] != 1)
+				return false;
+			
+			if (bHasDilation)
+				return false;
+
+			TArray<int32> Padding = FConvCS::GetPadding(Input.GetShape().GetData(), Weights.GetShape().GetData(), AutoPad, Dilations, Strides, Pads);
+
+			check(Padding.Num() == 4);
+			if (Padding[0] != 1 || Padding[1] != 1 || Padding[2] != 1 || Padding[3] != 1)
+				return false;
+
+			const int Hw = Weights.GetShape().GetData()[2];
+			const int Ww = Weights.GetShape().GetData()[3];
+			if (Hw != 3 || Ww != 3)
+				return false;
+
+			check(!bAreWeightsTransposed);
+
+			const int Ni = Input.GetShape().GetData()[0];
+			const int Ci = Input.GetShape().GetData()[1];
+			const int Hi = Input.GetShape().GetData()[2];
+			const int Wi = Input.GetShape().GetData()[3];
+
+			check(Ni == Output.GetShape().GetData()[0]);
+			const int Cw = Output.GetShape().GetData()[1];
+			check(Hi == Output.GetShape().GetData()[2]);
+			check(Wi == Output.GetShape().GetData()[3]);
+
+			check(Cw == Weights.GetShape().GetData()[0]);
+			check(Ci == Weights.GetShape().GetData()[1]);
+
+			EWinogradPrecision Precision = WinogradTestPrecision;
+			if (Precision == EWinogradPrecision::UNDEFINED)
+			{
+				int32 CVarValue = CVarWinogradPrecision.GetValueOnRenderThread();
+				CVarValue = FMath::Clamp(CVarValue, 0, static_cast<int32>(EWinogradPrecision::UNDEFINED) - 1);
+				Precision = static_cast<EWinogradPrecision>(CVarValue);
+			}
+
+			if (Precision == EWinogradPrecision::DISABLED)
+				return false;
+			
+			check(Input.GetDataType() == ENNETensorDataType::Float || Input.GetDataType() == ENNETensorDataType::Half);
+			const ENNETensorDataType TensorDataType = Precision == EWinogradPrecision::FP16 ? Input.GetDataType() : ENNETensorDataType::Float;
+
+
+			const int BlockCountH = FMath::DivideAndRoundUp(Hi, 4);
+			const int BlockCountW = FMath::DivideAndRoundUp(Wi, 4);
+
+			//Ensures that the MMM shader gets called with an even number of elements in the M dimension
+			const int BlockCountWExtended = ((BlockCountH * BlockCountW) % 2) != 0 ? RoundUp<2>(BlockCountW) : BlockCountW;
+			const int CwExtended = RoundUp<2>(Cw);
+
+			const int TransformedWeightSize = 36 * CwExtended * Ci;
+			const int TransformedInputSize = Ni * 36 * (BlockCountH * BlockCountWExtended) * Ci;
+			const int TransformedOutputSize = Ni * 36 * (BlockCountH * BlockCountWExtended) * CwExtended;
+
+			const ENNEShaderDataType ShaderDataType = TensorToShaderDataType(TensorDataType);
+			const EPixelFormat IntermediateBufferPixelFormat = TensorDataTypeToPixelFormat(TensorDataType);
+			const EPixelFormat IntermediateBufferPixelFormat2 = TensorDataType == ENNETensorDataType::Float ? PF_G32R32F : PF_G16R16F;
+			const uint32 IntermediateElementByteSize = UE::NNE::GetTensorDataTypeSizeInBytes(TensorDataType);
+
+			const FRDGBufferDesc TransformedWeightsBufferDesc = FRDGBufferDesc::CreateBufferDesc(IntermediateElementByteSize, TransformedWeightSize);
+			const FRDGBufferDesc TransformedInputBufferDesc = FRDGBufferDesc::CreateBufferDesc(IntermediateElementByteSize, TransformedInputSize);
+			const FRDGBufferDesc TransformedOutputBufferDesc = FRDGBufferDesc::CreateBufferDesc(IntermediateElementByteSize, TransformedOutputSize);
+
+			const FRDGBufferRef TransformedWeights = GraphBuilder.CreateBuffer(TransformedWeightsBufferDesc, TEXT("NNE.Tensor.ConvWinograd.TransformedWeights"), ERDGBufferFlags::None);
+			const FRDGBufferRef TransformedInput = GraphBuilder.CreateBuffer(TransformedInputBufferDesc, TEXT("NNE.Tensor.ConvWinograd.TransformedInput"), ERDGBufferFlags::None);
+			const FRDGBufferRef TransformedOutput = GraphBuilder.CreateBuffer(TransformedOutputBufferDesc, TEXT("NNE.Tensor.ConvWinograd.TransformedOutput"), ERDGBufferFlags::None);
+
+
+			// Dispatch Weight transformation
+			{
+				FConvWinogradWeightsCS::FParameters* Params = GraphBuilder.AllocParameters<FConvWinogradWeightsCS::FParameters>();
+				Params->Input = GraphBuilder.CreateSRV(FRDGBufferSRVDesc(Weights.GetBuffer(), BufferPixelFormat));
+				Params->Output = GraphBuilder.CreateUAV(FRDGBufferUAVDesc(TransformedWeights, IntermediateBufferPixelFormat));
+				Params->Ci = Ci;
+				Params->Cw = Cw;
+				Params->CwInputStride = Ci;
+				Params->MatrixOutputStride = Ci * CwExtended;
+				Params->CiOutputStride = CwExtended;
+
+				TShaderMapRef<FConvWinogradWeightsCS> ComputeShader(GetGlobalShaderMap(GMaxRHIFeatureLevel));
+
+				RDG_EVENT_SCOPE(GraphBuilder, "NNE.Operator.Hlsl.Conv.Winograd.Weights");
+				RDG_GPU_STAT_SCOPE(GraphBuilder, FNNEOperatorConvWinogradWeights);
+
+				FComputeShaderUtils::AddPass(
+					GraphBuilder,
+					RDG_EVENT_NAME("NNE.Operator.Hlsl.Conv.Winograd.Weights.Dispatch"),
+					ERDGPassFlags::Compute | ERDGPassFlags::NeverCull,
+					ComputeShader,
+					Params,
+					FIntVector(FMath::DivideAndRoundUp(Ci, FConvWinogradWeightsConstants::THREADGROUP_SIZE_X), CwExtended, 1));
+			}
+			// Dispatch Input transformation
+			{
+				FConvWinogradInputCS::FParameters* Params = GraphBuilder.AllocParameters<FConvWinogradInputCS::FParameters>();
+				Params->Input = GraphBuilder.CreateSRV(FRDGBufferSRVDesc(Input.GetBuffer(), BufferPixelFormat));
+				Params->Output = GraphBuilder.CreateUAV(FRDGBufferUAVDesc(TransformedInput, IntermediateBufferPixelFormat));
+				Params->C = Ci;
+				Params->H = Hi;
+				Params->W = Wi;
+				Params->WBlockCount = BlockCountW;
+				Params->CInputStride = Hi * Wi;
+				Params->HInputStride = Wi;
+				Params->NiOutputStride = 36 * Ci * BlockCountH * BlockCountWExtended;
+				Params->MatrixOutputStride = Ci * BlockCountH * BlockCountWExtended;
+				Params->COutputStride = BlockCountH * BlockCountWExtended;
+				Params->HOutputStride = BlockCountWExtended;
+
+				TShaderMapRef<FConvWinogradInputCS> ComputeShader(GetGlobalShaderMap(GMaxRHIFeatureLevel));
+
+				RDG_EVENT_SCOPE(GraphBuilder, "NNE.Operator.Hlsl.Conv.Winograd.Input");
+				RDG_GPU_STAT_SCOPE(GraphBuilder, FNNEOperatorConvWinogradInput);
+
+				FComputeShaderUtils::AddPass(
+					GraphBuilder,
+					RDG_EVENT_NAME("NNE.Operator.Hlsl.Conv.Winograd.Input.Dispatch"),
+					ERDGPassFlags::Compute | ERDGPassFlags::NeverCull,
+					ComputeShader,
+					Params,
+					FIntVector(FMath::DivideAndRoundUp(BlockCountWExtended, FConvWinogradInputConstants::THREADGROUP_SIZE_X), BlockCountH, Ci * Ni));
+			}
+			// Dispatch MMM
+			{
+				const int VectorSize = 2;
+				const int M = (BlockCountH * BlockCountWExtended);
+				const int N = CwExtended;
+				const int K = Ci;
+				FConvWinogradMMMCS::FParameters* Params = GraphBuilder.AllocParameters<FConvWinogradMMMCS::FParameters>();
+				Params->Input = GraphBuilder.CreateSRV(FRDGBufferSRVDesc(TransformedInput, IntermediateBufferPixelFormat2));
+				Params->Weight = GraphBuilder.CreateSRV(FRDGBufferSRVDesc(TransformedWeights, IntermediateBufferPixelFormat2));
+				Params->Output = GraphBuilder.CreateUAV(FRDGBufferUAVDesc(TransformedOutput, IntermediateBufferPixelFormat2));
+				Params->M = M / VectorSize;
+				Params->N = N / VectorSize;
+				Params->K = K;
+				Params->MatrixInputStride = K * M / VectorSize;
+				Params->KInputStride = M / VectorSize;
+				Params->MatrixWeightStride = K * N / VectorSize;
+				Params->KWeightStride = N / VectorSize;
+				Params->MatrixOutputStride = N * M / VectorSize;
+				Params->NOutputStride = M / VectorSize;
+
+				const int BlockSizeN = FConvWinogradMMMCS::GetOptimalBlockSizeN(M, K, N);
+				FConvWinogradMMMCS::FPermutationDomain PermutationVector;
+				PermutationVector.Set<FConvWinogradMMMCS::FDataType>(ShaderDataType);
+				PermutationVector.Set<FConvWinogradMMMCS::FBlockSizeN>(BlockSizeN);
+				TShaderMapRef<FConvWinogradMMMCS> ComputeShader(GetGlobalShaderMap(GMaxRHIFeatureLevel), PermutationVector);
+
+				RDG_EVENT_SCOPE(GraphBuilder, "NNE.Operator.Hlsl.Conv.Winograd.MMM");
+				RDG_GPU_STAT_SCOPE(GraphBuilder, FNNEOperatorConvWinogradMMM);
+
+				FComputeShaderUtils::AddPass(
+					GraphBuilder,
+					RDG_EVENT_NAME("NNE.Operator.Hlsl.Conv.Winograd.MMM.Dispatch"),
+					ERDGPassFlags::Compute | ERDGPassFlags::NeverCull,
+					ComputeShader,
+					Params,
+					FIntVector(FMath::DivideAndRoundUp(M, 64), FMath::DivideAndRoundUp(N, BlockSizeN), 36 * Ni));
+			}
+			// Dispatch Output transformation
+			{
+				const int C = Cw;
+				const int H = Hi;
+				const int W = Wi;
+				const bool bHasBias = Bias != nullptr;
+				FConvWinogradOutputCS::FParameters* Params = GraphBuilder.AllocParameters<FConvWinogradOutputCS::FParameters>();
+				Params->Input = GraphBuilder.CreateSRV(FRDGBufferSRVDesc(TransformedOutput, IntermediateBufferPixelFormat));
+				if (bHasBias)
+				{
+					Params->Bias = GraphBuilder.CreateSRV(Bias->GetBuffer(), BufferPixelFormat);
+				}
+				Params->Output = GraphBuilder.CreateUAV(FRDGBufferUAVDesc(Output.GetBuffer(), BufferPixelFormat));
+				Params->C = C;
+				Params->H = H;
+				Params->W = W;
+				Params->WBlockCount = BlockCountW;
+				Params->NiInputStride = 36 * CwExtended * BlockCountH * BlockCountWExtended;
+				Params->MatrixInputStride = CwExtended * BlockCountH * BlockCountWExtended;
+				Params->CInputStride = BlockCountH * BlockCountWExtended;
+				Params->HInputStride = BlockCountWExtended;
+				Params->COutputStride = H * W;
+				Params->HOutputStride = W;
+
+				FConvWinogradOutputCS::FPermutationDomain PermutationVector;
+				PermutationVector.Set<FConvWinogradOutputCS::FHasBias>(bHasBias);
+				PermutationVector.Set<FConvWinogradOutputCS::FDataType>(ShaderDataType);
+				TShaderMapRef<FConvWinogradOutputCS> ComputeShader(GetGlobalShaderMap(GMaxRHIFeatureLevel), PermutationVector);
+
+				RDG_EVENT_SCOPE(GraphBuilder, "NNE.Operator.Hlsl.Conv.Winograd.Output");
+				RDG_GPU_STAT_SCOPE(GraphBuilder, FNNEOperatorConvWinogradOutput);
+
+				FComputeShaderUtils::AddPass(
+					GraphBuilder,
+					RDG_EVENT_NAME("NNE.Operator.Hlsl.Conv.Winograd.Output.Dispatch"),
+					ERDGPassFlags::Compute | ERDGPassFlags::NeverCull,
+					ComputeShader,
+					Params,
+					FIntVector(FMath::DivideAndRoundUp(BlockCountW, FConvWinogradOutputConstants::THREADGROUP_SIZE_X), BlockCountH, Cw * Ni));
+			}
+
+			return true;
+		}
+
 		virtual void Dispatch(FRDGBuilder& GraphBuilder, TConstArrayView<FTensorRDGRef> InputTensors, TConstArrayView<FTensorRDGRef> OutputTensors) override
 		{
 			check(InputTensors.Num() >= 2 && InputTensors.Num() <= 3);
@@ -269,6 +533,10 @@ namespace UE::NNERuntimeRDG::Private::Hlsl
 			check(Output.GetShape().Rank() == Input.GetShape().Rank());
 			check(NumDimensions == (Input.GetShape().Rank() - 2));
 
+			if (DispatchConvWinograd(GraphBuilder, Input, Weights, Bias, Output))
+			{
+				return;
+			}
 			if (DispatchConvMatmul(GraphBuilder, Input, Weights, Bias, Output))
 			{
 				return;
@@ -293,12 +561,27 @@ namespace UE::NNERuntimeRDG::Private::Hlsl
 			if (bHasDilation)
 				return;
 
+			// Don't transpose if Winograd might be possible
+			TArrayView<const uint32> WeightsShape = Weights->GetShape().GetData();
+			bool IsKernel3x3 = WeightsShape[2] == 3 && WeightsShape[3] == 3;
+			check(Strides.Num() == 2);
+			bool AreStrides1 = Strides[0] == 1 && Strides[1] == 1;
+			if (IsKernel3x3 && AreStrides1)
+				return;
+
 			//Transpose from CwCiHwWw to HwWwCiCw
 			if (Internal::CPUHelper::Transpose::TransposePreparedData(*Weights, {2,3,1,0} ))
 			{
 				bAreWeightsTransposed = true;
 			}
 		};
+
+	private:
+		template<int divider>
+		static int RoundUp(int value)
+		{
+			return FMath::DivideAndRoundUp(value, divider) * divider;
+		}
 	};
 
 	bool ValidateConvOperator(const NNE::FAttributeMap& AttributeMap, TConstArrayView<ENNETensorDataType> InputTypes, TConstArrayView<NNE::FSymbolicTensorShape> InputShapes)
@@ -312,6 +595,7 @@ namespace UE::NNERuntimeRDG::Private::Hlsl
 		AttributeValidator.AddOptional(TEXT("kernel_shape"), ENNEAttributeDataType::Int32Array); // idea: cross check input weight shape with this attribute if present
 		AttributeValidator.AddOptional(TEXT("pads"), ENNEAttributeDataType::Int32Array);
 		AttributeValidator.AddOptional(TEXT("strides"), ENNEAttributeDataType::Int32Array);
+		AttributeValidator.AddOptional(TEXT("__UE__WinogradPrecision"), ENNEAttributeDataType::Int32);
 
 		bIsValid &= AttributeValidator.Validate(AttributeMap);
 
