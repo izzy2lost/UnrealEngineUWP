d86eb1588d8482da3af81535e5071c8a61824b2e
calvin zheng <calvin.zheng@epicgames.com>
2024-08-28 14:07:23 -0400

    Fix NewSearchDictionary() crash
    #rb calvin.zheng
    #rnx
    
    [CL 35874951 by calvin zheng in ue5-main branch]

--- Engine/Plugins/Online/WebAuth/Source/Private/IOS/IOSPlatformWebAuth.cpp
+++ Engine/Plugins/Online/WebAuth/Source/Private/IOS/IOSPlatformWebAuth.cpp
@@ -29,7 +29,7 @@
 
 @end
 
-static NSMutableDictionary* MakeSearchDictionary(NSString *EnvironmentName);
+static NSMutableDictionary* NewSearchDictionary(NSString *EnvironmentName);
 static PresentationContext* PresentationContextProvider = nullptr;
 
 // Returns true if the request is made
@@ -92,13 +92,14 @@ bool FIOSWebAuth::AuthSessionWithURL(const FString &UrlStr, const FString &Schem
 	});
 
 	[SavedAuthSession start];
+    [SavedAuthSession release];
 
 	return bSessionInProgress;
 }
 
-NSMutableDictionary* MakeSearchDictionary(NSString *EnvironmentName)
+NSMutableDictionary* NewSearchDictionary(NSString *EnvironmentName)
 {
-    static NSString* ServiceName = [[UIDevice currentDevice] identifierForVendor].UUIDString;
+    NSString* ServiceName = [[UIDevice currentDevice] identifierForVendor].UUIDString;
 
 	NSString* keyName = [NSString stringWithFormat:@"DeviceCredentials_%@", EnvironmentName];
 	NSData* EncodedIdentifier = [keyName dataUsingEncoding:NSUTF8StringEncoding];
@@ -121,7 +122,7 @@ bool FIOSWebAuth::SaveCredentials(const FString& IdStr, const FString& TokenStr,
 	FTCHARToUTF8 TCEnvironmentNameStr(*EnvironmentNameStr);
 	NSString *EnvironmentName = [NSString stringWithUTF8String:TCEnvironmentNameStr.Get()];
 
-	NSMutableDictionary* SearchDictionary = MakeSearchDictionary(EnvironmentName);
+	NSMutableDictionary* SearchDictionary = NewSearchDictionary(EnvironmentName);
 
 	// erase any existing one
 	SecItemDelete((CFDictionaryRef)SearchDictionary);
@@ -140,7 +141,8 @@ bool FIOSWebAuth::SaveCredentials(const FString& IdStr, const FString& TokenStr,
 	// add it
 	OSStatus Status = SecItemAdd((CFDictionaryRef)SearchDictionary, NULL);
 	NSLog(@"Tried to add, status = %d", Status);
-
+    
+    [SearchDictionary release];
 	return Status == errSecSuccess;
 }
 
@@ -151,7 +153,7 @@ bool FIOSWebAuth::LoadCredentials(FString& OutIdStr, FString& OutTokenStr, const
 
 	NSString* Id = [NSString string];
 	NSString* Token = [NSString string];
-	NSMutableDictionary* SearchDictionary = MakeSearchDictionary(EnvironmentName);
+	NSMutableDictionary* SearchDictionary = NewSearchDictionary(EnvironmentName);
 
 	// a couple extra params for retrieval
 	[SearchDictionary setObject:(id)kSecMatchLimitOne forKey:(id)kSecMatchLimit];
@@ -202,6 +204,7 @@ bool FIOSWebAuth::LoadCredentials(FString& OutIdStr, FString& OutTokenStr, const
 		OutTokenStr = FString();
 	}
 
+    [SearchDictionary release];
 	return Status == errSecSuccess;
 }
 
