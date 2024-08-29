a59dac03d095c6870dc0e23d29ff913eb721562f
matt peters <Matt.Peters@epicgames.com>
2024-08-29 14:23:32 -0400

    ArchiveSavePackageDataBuffer: Suppress static analysis warning. #rnx
    [CL 35901603 by matt peters in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Public/Serialization/ArchiveSavePackageDataBuffer.h
+++ Engine/Source/Runtime/CoreUObject/Public/Serialization/ArchiveSavePackageDataBuffer.h
@@ -21,7 +21,9 @@ struct FArchiveSavePackageDataBuffer : public FArchiveSavePackageData
 {
 public:
 	FArchiveSavePackageDataBuffer(const ITargetPlatform* InTargetPlatform = nullptr)
-		: FArchiveSavePackageData(ObjectSavePackageSerializeContextBuffer, nullptr, nullptr)
+		// warning V1050 : The uninitialized class member 'ObjectSavePackageSerializeContextBuffer' is used when initializing the base class 'FArchiveSavePackageData'.
+		// This warning can be ignored because the base class only records the pointer and does not dereference it
+		: FArchiveSavePackageData(ObjectSavePackageSerializeContextBuffer, nullptr, nullptr) // -V1050
 		, ObjectSaveContextData(FObjectSaveContextData())
 		, ObjectSavePackageSerializeContextBuffer(*ObjectSaveContextData)
 		, CookContextBuffer(nullptr /*InPackage*/, UE::Cook::ECookType::Unknown, UE::Cook::ECookingDLC::Unknown,
