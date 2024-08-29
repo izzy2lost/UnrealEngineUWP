1f30848cb32ea2d65efaa3726b4c65df79f449a9
andy sonnenburg <andy.sonnenburg@epicgames.com>
2024-08-29 09:50:27 -0400

    Move walking of stack frames out of the socket debugger and to the interface between the debugger and rest of the VM.
    Add `Self` register to displayed variables.
    
    #rb russell.johnston
    
    [CL 35894912 by andy sonnenburg in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Public/VerseVM/VVMDebugger.h
+++ Engine/Source/Runtime/CoreUObject/Public/VerseVM/VVMDebugger.h
@@ -5,6 +5,9 @@
 #if WITH_VERSE_VM || defined(__INTELLISENSE__)
 
 #include "VerseVM/VVMContext.h"
+#include "VerseVM/VVMLocation.h"
+#include "VerseVM/VVMUniqueString.h"
+#include "VerseVM/VVMValue.h"
 
 namespace Verse
 {
@@ -23,6 +26,39 @@ struct FDebugger
 COREUOBJECT_API FDebugger* GetDebugger();
 
 COREUOBJECT_API void SetDebugger(FDebugger*);
+
+namespace Debugger
+{
+using FRegisters = TArray<TTuple<TWriteBarrier<VUniqueString>, VValue>>;
+
+struct FFrame
+{
+	explicit FFrame(FAccessContext Context, VUniqueString& Name, VUniqueString& FilePath, FRegisters Registers)
+		: Name{Context, &Name}
+		, FilePath{Context, &FilePath}
+		, Registers{::MoveTemp(Registers)}
+	{
+	}
+
+	TWriteBarrier<VUniqueString> Name;
+	TWriteBarrier<VUniqueString> FilePath;
+	FRegisters Registers;
+};
+
+template <typename TVisitor>
+void Visit(TVisitor& Visitor, FFrame& Frame, const TCHAR* ElementName)
+{
+	Visit(Visitor, Frame.Name, TEXT("Name"));
+	Visit(Visitor, Frame.FilePath, TEXT("FilePath"));
+	for (auto&& [Name, Value] : Frame.Registers)
+	{
+		Visit(Visitor, Name, TEXT("RegisterName"));
+		Visit(Visitor, Value, TEXT("RegisterValue"));
+	}
+}
+
+COREUOBJECT_API void ForEachStackFrame(FRunningContext, VFrame&, const FOp&, TFunctionRef<void(FFrame, const FLocation*)>);
+} // namespace Debugger
 } // namespace Verse
 
 #endif
