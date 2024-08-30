e8026863a8e71a2c5f83bd8908cf55f85b02a1d7
mark meijer <mark.meijer@xa.epicgames.com>
2024-08-30 05:47:10 -0400

    Gather text from source to include nested macros
    UE-176755
    
    #jira UE-176755
    #rb Jamie.Dale, eric.boucher, Leon.Huang
    
    [CL 35921389 by mark meijer in ue5-main branch]

--- Engine/Source/Editor/UnrealEd/Private/Commandlets/GatherTextCommandletBase.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Commandlets/GatherTextCommandletBase.cpp
@@ -28,6 +28,7 @@ const TCHAR* UGatherTextCommandletBase::EnableSourceControlSwitch = TEXT("Enable
 const TCHAR* UGatherTextCommandletBase::DisableSubmitSwitch = TEXT("DisableSCCSubmit");
 const TCHAR* UGatherTextCommandletBase::PreviewSwitch = TEXT("Preview");
 const TCHAR* UGatherTextCommandletBase::GatherTypeParam = TEXT("GatherType");
+const TCHAR* UGatherTextCommandletBase::RunNestedMacroPrepassSwitch = TEXT("RunNestedMacroPrepass");
 
 UGatherTextCommandletBase::UGatherTextCommandletBase(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
@@ -82,11 +83,16 @@ bool UGatherTextCommandletBase::ShouldSplitPlatformForPath(const FString& InPath
 
 FName UGatherTextCommandletBase::GetSplitPlatformNameFromPath(const FString& InPath) const
 {
-	for (const auto& SplitPlatformsPair : SplitPlatforms)
+	return GetSplitPlatformNameFromPath_Static(InPath, SplitPlatforms);
+}
+
+FName UGatherTextCommandletBase::GetSplitPlatformNameFromPath_Static(const FString& InPath, const TMap<FName, FString>& InSplitPlatforms)
+{
+	for (const auto& Pair : InSplitPlatforms)
 	{
-		if (InPath.Contains(SplitPlatformsPair.Value))
+		if (InPath.Contains(Pair.Value))
 		{
-			return SplitPlatformsPair.Key;
+			return Pair.Key;
 		}
 	}
 	return FName();
@@ -114,7 +120,7 @@ bool UGatherTextCommandletBase::GetStringFromConfig( const TCHAR* Section, const
 	return bSuccess;
 }
 
-void ResolveLocalizationPath(FString& InOutPath)
+void UGatherTextCommandletBase::ResolveLocalizationPath(FString& InOutPath)
 {
 	static const FString AbsoluteEnginePath = FPaths::ConvertRelativePathToFull(FPaths::EngineDir()) / FString();
 	static const FString AbsoluteProjectPath = FPaths::ConvertRelativePathToFull(FPaths::ProjectDir()) / FString();
