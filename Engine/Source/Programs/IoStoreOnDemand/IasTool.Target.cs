cd98a0b9624adc0a17beee221d92aaf2041d60b3
martin ridgers <Martin.Ridgers@epicgames.com>
2024-09-02 02:45:59 -0400

    IasTool builds for Mac
    #rb trivial
    #jira ue-222936
    #rnx
    
    [CL 35951803 by martin ridgers in ue5-main branch]

--- Engine/Source/Programs/IoStoreOnDemand/IasTool.Target.cs
+++ Engine/Source/Programs/IoStoreOnDemand/IasTool.Target.cs
@@ -2,7 +2,7 @@
 
 using UnrealBuildTool;
 
-[SupportedPlatforms("Win64", "Linux")]
+[SupportedPlatforms("Win64", "Linux", "Mac")]
 public class IasToolTarget : TargetRules
 {
 	public IasToolTarget(TargetInfo Target) : base(Target)
