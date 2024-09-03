5f560f423bb4b7cb328087032997274b87369a62
laura hermanns <laura.hermanns@epicgames.com>
2024-09-03 12:41:06 -0400

    [Shaders] Set USING_VERTEX_SHADER_LAYER depending on DDPI values, not hard coded in shader backend. Reintroduced after backout 35892687.
    - This also reverts the DDPI information from CL 35396864, since D3D does in fact support vertex shader layers (except for D3D11.2 or older).
    - Don't set USING_VERTEX_SHADER_LAYER when geometry shaders are available; This resulted in ShaderMinifier not able to find the entry point.
    
    #jira UE-221358
    #rnx
    #rb Arciel.Rekman, Sebastien.Hillaire
    [FYI] Christopher.Waters, Graham.Wihlidal, Erica.Stella
    
    [CL 35971246 by laura hermanns in ue5-main branch]

--- Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
+++ Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
@@ -3834,6 +3834,9 @@ void GlobalBeginCompileShader(
 
 	SET_SHADER_DEFINE(Input.Environment, COLORED_LIGHT_FUNCTION_ATLAS, GetLightFunctionAtlasFormat() > 0 ? 1 : 0);
 
+	// USING_VERTEX_SHADER_LAYER is only intended as alternative for geometry shaders, e.g. for Mac/IOS (-Preview) platform. Don't use it when geometry shaders are available.
+	SET_SHADER_DEFINE(Input.Environment, USING_VERTEX_SHADER_LAYER, !RHISupportsGeometryShaders(EShaderPlatform(Target.Platform)) && RHISupportsVertexShaderLayer(EShaderPlatform(Target.Platform)) ? 1 : 0);
+
 	SET_SHADER_DEFINE(Input.Environment, PLATFORM_SUPPORTS_SHADER_ROOT_CONSTANTS, RHISupportsShaderRootConstants(EShaderPlatform(Target.Platform)) ? 1 : 0);
 	SET_SHADER_DEFINE(Input.Environment, PLATFORM_SUPPORTS_SHADER_BUNDLE_DISPATCH, RHISupportsShaderBundleDispatch(EShaderPlatform(Target.Platform)) ? 1 : 0);
 	SET_SHADER_DEFINE(Input.Environment, PLATFORM_SUPPORTS_RENDERTARGET_WRITE_MASK, RHISupportsRenderTargetWriteMask(EShaderPlatform(Target.Platform)) ? 1 : 0);
