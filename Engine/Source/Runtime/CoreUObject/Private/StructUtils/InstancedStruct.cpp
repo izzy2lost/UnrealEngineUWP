579b795449fe29ca0474b5da3d59aa60c58eecce
yoan stamant <Yoan.Stamant@epicgames.com>
2024-09-04 10:36:35 -0400

    [InstancedStruct] fixed access to nullptr when reporting a serialization error
    #rb Mieszko.Zielinski, Patrick.Boutot
    
    [CL 36009094 by yoan stamant in ue5-main branch]

--- Engine/Source/Runtime/CoreUObject/Private/StructUtils/InstancedStruct.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/StructUtils/InstancedStruct.cpp
@@ -165,7 +165,10 @@ bool FInstancedStruct::Serialize(FArchive& Ar)
 			// In this case we manually seek in the archive to skip its serialized content. 
 			// We don't want to rely on TaggedSerialization that will mark an error in the archive that
 			// may cause other serialization to fail (e.g. FArchive& operator<<(FArchive& Ar, TArray& A))
-			UE_LOG(LogCore, Warning, TEXT("Unable to find serialized UScriptStruct -> Advance %u bytes in the archive and reset to empty FInstancedStruct. SerializedProperty:%s LinkerRoot:%s"), SerialSize, *Ar.GetSerializedProperty()->GetPathName(), Ar.GetLinker() ? *GetPathNameSafe(Ar.GetLinker()->LinkerRoot) : TEXT("NoLinker"));
+			UE_LOG(LogCore, Warning, TEXT("Unable to find serialized UScriptStruct -> Advance %u bytes in the archive and reset to empty FInstancedStruct. SerializedProperty:%s LinkerRoot:%s"),
+				SerialSize,
+				*GetPathNameSafe(Ar.GetSerializedProperty()),
+				Ar.GetLinker() ? *GetPathNameSafe(Ar.GetLinker()->LinkerRoot) : TEXT("NoLinker"));
 			Ar.Seek(Ar.Tell() + SerialSize);
 		}
 		else if (NonConstStruct != nullptr && ensureMsgf(GetMutableMemory() != nullptr, TEXT("A valid script struct should always have allocated memory")))
