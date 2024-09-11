f154d3664f7615ac079efb0e5fea05d1dbef9101
adrien logut <adrien.logut@epicgames.com>
2024-09-11 13:52:46 -0400

    [PCG] Niagara Interop
    * Add a new Interop plugin for PCG
    * Add a new node: Write to NDC that does a fire and forget write from PCG to Niagara
    * Add some traits for marshalling between PCG and Niagara types.
    
    #rnx
    #jira UE-220746
    #rb julien.lheureux, ryan.buehler, Simon.Tovey, wyatt.marvil
    
    [CL 36201922 by adrien logut in 5.5 branch]

---
+++ Engine/Plugins/Experimental/PCGInterops/PCGNiagaraInterop/Source/PCGNiagaraInterop/Public/Elements/PCGWriteToNiagaraDataChannel.h
@@ -0,0 +1,58 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "PCGContext.h"
+#include "PCGSettings.h"
+#include "Async/PCGAsyncLoadingContext.h"
+#include "Metadata/PCGAttributePropertySelector.h"
+
+#include "PCGWriteToNiagaraDataChannel.generated.h"
+
+class UNiagaraDataChannelAsset;
+
+/**
+* Allow writing attributes to a Niagara Data Channel.
+*/
+UCLASS(MinimalAPI, BlueprintType, ClassGroup = (Procedural))
+class UPCGWriteToNiagaraDataChannelSettings : public UPCGSettings
+{
+	GENERATED_BODY()
+
+public:
+	//~Begin UPCGSettings interface
+#if WITH_EDITOR
+	virtual FName GetDefaultNodeName() const override;
+	virtual FText GetDefaultNodeTitle() const override;
+	virtual EPCGSettingsType GetType() const override { return EPCGSettingsType::Generic; }
+#endif
+
+protected:
+	virtual TArray<FPCGPinProperties> InputPinProperties() const override;
+	virtual TArray<FPCGPinProperties> OutputPinProperties() const override;
+	virtual FPCGElementPtr CreateElement() const override;
+	//~End UPCGSettings interface
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings", meta = (PCG_Overridable))
+	TSoftObjectPtr<UNiagaraDataChannelAsset> DataChannel;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings")
+	TMap<FName, FPCGAttributePropertyInputSelector> NiagaraVariablesPCGAttributeMapping;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Debug")
+	bool bSynchronousLoad = false;
+};
+
+struct FPCGWriteToNiagaraDataChannelContext : public FPCGContext, public IPCGAsyncLoadingContext {};
+
+class FPCGWriteToNiagaraDataChannelElement : public IPCGElementWithCustomContext<FPCGWriteToNiagaraDataChannelContext>
+{
+public:
+	virtual bool IsCacheable(const UPCGSettings* InSettings) const override { return false; }
+	virtual bool CanExecuteOnlyOnMainThread(FPCGContext* Context) const override { return true; }
+protected:
+	virtual bool PrepareDataInternal(FPCGContext* InContext) const override;
+	virtual bool ExecuteInternal(FPCGContext* InContext) const override;
+};
+
