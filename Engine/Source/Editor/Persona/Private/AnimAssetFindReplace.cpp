ba82b2be9a1b7d5ada3c36f0610d6a833b7cd28d
thomas sarkanen <Thomas.Sarkanen@epicgames.com>
2024-09-13 06:54:54 -0400

    Updated icons for animation asset find/replace tab
    #jira UE-184430
    #rb Jurre.deBaare
    [RN] Updated icons for animation asset find/replace tab
    
    [CL 36260375 by thomas sarkanen in 5.5 branch]

--- Engine/Source/Editor/Persona/Private/AnimAssetFindReplace.cpp
+++ Engine/Source/Editor/Persona/Private/AnimAssetFindReplace.cpp
@@ -254,7 +254,7 @@ void UAnimAssetFindReplaceProcessor_StringBase::ExtendToolbar(FToolMenuSection& 
 			MatchCaseCheckbox,
 			LOCTEXT("MatchCaseCheckboxLabel", "Match Case"),
 			LOCTEXT("MatchCaseCheckboxTooltip", "Whether to match case when searching."),
-			FSlateIcon(FAppStyle::GetAppStyleSetName(), "Icons.Clipboard"),
+			FSlateIcon(FAppStyle::GetAppStyleSetName(), "Persona.FindReplace.MatchCase"),
 			EUserInterfaceActionType::ToggleButton));
 
 	FToolUIAction MatchWholeWordCheckbox;
@@ -274,7 +274,7 @@ void UAnimAssetFindReplaceProcessor_StringBase::ExtendToolbar(FToolMenuSection& 
 			MatchWholeWordCheckbox,
 			LOCTEXT("MatchWholeWordCheckboxLabel", "Match Whole Word"),
 			LOCTEXT("MatchWholeWordCheckboxTooltip", "Whether to match the whole word or just part of the word when searching."),
-			FSlateIcon(FAppStyle::GetAppStyleSetName(), "Icons.Use"),
+			FSlateIcon(FAppStyle::GetAppStyleSetName(), "Persona.FindReplace.MatchWholeWord"),
 			EUserInterfaceActionType::ToggleButton));
 
 	InSection.AddSeparator(NAME_None);
