fc3ba43b439d689667cc0150734d40806fe23516
ludovic chabant <Ludovic.Chabant@epicgames.com>
2024-09-16 02:20:22 -0400

    Cameras: extract position/offset math from collision node into a math utility class
    [CL 36295964 by ludovic chabant in 5.5 branch]

--- Engine/Plugins/Cameras/GameplayCameras/Source/GameplayCameras/Public/Nodes/CameraNodeTypes.h
+++ Engine/Plugins/Cameras/GameplayCameras/Source/GameplayCameras/Public/Nodes/CameraNodeTypes.h
@@ -7,16 +7,40 @@
 #include "CameraNodeTypes.generated.h"
 
 /**
- * Defines what space a camera node should operate in.
+ * Defines an origin position for a camera node to operate at.
  */
 UENUM()
-enum class ECameraNodeSpace
+enum class ECameraNodeOriginPosition : uint8
 {
-	/** Operates in the local camera pose space. */
+	/** The position of the current camera pose. */
 	CameraPose,
-	/** Operates in the space of the owning evaluation context's initial result. */
+	/** The origin of the active evaluation context on the main layer's blend stack. */
+	ActiveContext,
+	/** The origin of the evaluation context of the current camera node. */
 	OwningContext,
-	/** Operates in world space. */
+	/** The location of the current pivot. If no pivot is found, fallback to ActiveContext. */
+	Pivot,
+	/** The location of the player's controlled pawn. */
+	Pawn
+};
+
+/**
+ * Defines what space a camera node, or one of its features, should operate in.
+ */
+UENUM()
+enum class ECameraNodeSpace : uint8
+{
+	/** The local space of the current camera pose. */
+	CameraPose,
+	/** The space of the active evaluation context on the main layer's blend stack. */
+	ActiveContext,
+	/** The space of the evaluation context of the current camera node. */
+	OwningContext,
+	/** The space of the current pivot. If no pivot is found, fallback to ActiveContext. */
+	Pivot,
+	/** The space of the player's controlled pawn. */
+	Pawn,
+	/** The space of the world in which the camera rig evaluates. */
 	World
 };
 
