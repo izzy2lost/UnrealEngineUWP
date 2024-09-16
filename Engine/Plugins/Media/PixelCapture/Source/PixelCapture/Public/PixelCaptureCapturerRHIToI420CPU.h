251da6b24a240aa83c627a4dc2484322a73e5d81
william belcher <william.belcher@xa.epicgames.com>
2024-09-15 21:51:50 -0400

    [PS2] Fix: Poor VPX performance, freeze frame bugs, simulcast capture not working
    #rb eden.harris
    [FYI]
    
    [CL 36295206 by william belcher in 5.5 branch]

--- Engine/Plugins/Media/PixelCapture/Source/PixelCapture/Public/PixelCaptureCapturerRHIToI420CPU.h
+++ Engine/Plugins/Media/PixelCapture/Source/PixelCapture/Public/PixelCaptureCapturerRHIToI420CPU.h
@@ -7,10 +7,10 @@
 #include "RHIGPUReadback.h"
 
 /**
- * A basic capturer that will capture RHI texture frames to I420 buffers utilizing cpu functions.
- * Involves CPU readback of GPU textures and processing of that readback data.
- * Input: FPixelCaptureInputFrameRHI
- * Output: FPixelCaptureOutputFrameI420
+ * A basic capturer that will capture RHI texture frames to I420 buffers
+ * utilizing cpu functions. Involves CPU readback of GPU textures and processing
+ * of that readback data. Input: FPixelCaptureInputFrameRHI Output:
+ * FPixelCaptureOutputFrameI420
  */
 class PIXELCAPTURE_API FPixelCaptureCapturerRHIToI420CPU : public FPixelCaptureCapturer, public TSharedFromThis<FPixelCaptureCapturerRHIToI420CPU>
 {
@@ -23,18 +23,23 @@ public:
 	virtual ~FPixelCaptureCapturerRHIToI420CPU();
 
 protected:
-	virtual FString GetCapturerName() const override { return "RHIToI420CPU"; }
-	virtual void Initialize(int32 InputWidth, int32 InputHeight) override;
+	virtual FString					  GetCapturerName() const override { return "RHIToI420CPU"; }
+	virtual void					  Initialize(int32 InputWidth, int32 InputHeight) override;
 	virtual IPixelCaptureOutputFrame* CreateOutputBuffer(int32 InputWidth, int32 InputHeight) override;
-	virtual void BeginProcess(const IPixelCaptureInputFrame& InputFrame, IPixelCaptureOutputFrame* OutputBuffer) override;
+	virtual void					  BeginProcess(const IPixelCaptureInputFrame& InputFrame, IPixelCaptureOutputFrame* OutputBuffer) override;
 
 private:
 	float Scale = 1.0f;
 
-	TSharedPtr<FRHIGPUTextureReadback> TextureReader;
 	FTextureRHIRef StagingTexture;
 
 	FPixelCaptureCapturerRHIToI420CPU(float InScale);
+
+#if 0
+	TSharedPtr<FRHIGPUTextureReadback> TextureReader;
 	void OnRHIStageComplete(IPixelCaptureOutputFrame* OutputBuffer);
 	void CheckComplete(IPixelCaptureOutputFrame* OutputBuffer);
+#else
+	FTextureRHIRef ReadbackTexture;
+#endif
 };
