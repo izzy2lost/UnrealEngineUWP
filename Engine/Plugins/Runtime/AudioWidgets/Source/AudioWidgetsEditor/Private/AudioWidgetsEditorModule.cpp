9504fae274930d122e92d66ff0bfe40dfc7c47b2
igor dallavanzi <igor.dallavanzi@psyonix.com>
2024-09-17 19:43:26 -0400

    Create a new AudioDetailsCustomization module inside AudioWidgets to remove audio related details views from editor #rb Rob.Gay #rnx
    [CL 36364449 by igor dallavanzi in 5.5 branch]

---
+++ Engine/Plugins/Runtime/AudioWidgets/Source/AudioWidgetsEditor/Private/AudioWidgetsEditorModule.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AudioWidgetsEditorModule.h"
+#include "Modules/ModuleManager.h"
+
+
+void FAudioWidgetsEditorModule::StartupModule()
+{
+}
+
+void FAudioWidgetsEditorModule::ShutdownModule()
+{
+}
+
+	
+IMPLEMENT_MODULE(FAudioWidgetsEditorModule, AudioWidgetsEditor)
