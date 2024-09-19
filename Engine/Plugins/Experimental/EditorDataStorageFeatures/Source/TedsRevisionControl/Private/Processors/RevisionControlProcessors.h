780f5bda6902f8e7d66bd7b8e190276fde440a19
aditya ravichandran <aditya.ravichandran@epicgames.com>
2024-09-19 09:26:08 -0400

    Revision Control Overlays: Fix a bug with the revision control processors using the wrong context causing the viewport overlay color column to never be added Add observers to add/remove the viewport color column on selectoin change Add queries to update the relevant SCC rows when a viewport overlay setting is changed
    #rb ronald.koppers
    
    [CL 36428943 by aditya ravichandran in 5.5 branch]

--- Engine/Plugins/Experimental/EditorDataStorageFeatures/Source/TedsRevisionControl/Private/Processors/RevisionControlProcessors.h
+++ Engine/Plugins/Experimental/EditorDataStorageFeatures/Source/TedsRevisionControl/Private/Processors/RevisionControlProcessors.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "Elements/Columns/TypedElementPackageColumns.h"
 #include "Elements/Common/TypedElementHandles.h"
 #include "Elements/Interfaces/TypedElementDataStorageFactory.h"
 
@@ -20,14 +21,34 @@ public:
 	void RegisterTables(IEditorDataStorageProvider& DataStorage) override;
 	void RegisterQueries(IEditorDataStorageProvider& DataStorage) override;
 
+	// Update the overlays for all SCC rows that contain the Column specified
+	void UpdateOverlaysForSCCState(IEditorDataStorageProvider* DataStorage, const UScriptStruct* Column) const;
+
+	// Update the color for all actors that currently have an overlay
+	void UpdateOverlayColors(IEditorDataStorageProvider* DataStorage) const;
+
+
 private:
 	void RegisterFetchUpdates(IEditorDataStorageProvider& DataStorage);
 	void RegisterApplyOverlays(IEditorDataStorageProvider& DataStorage);
 	void RegisterRemoveOverlays(IEditorDataStorageProvider& DataStorage);
+	void RegisterGeneralQueries(IEditorDataStorageProvider& DataStorage);
+
+	void RequestPackageUpdates(IEditorDataStorageProvider* DataStorage, TArray<FTypedElementPackageUpdateColumn>& Packages) const;
+	
 	UE::Editor::DataStorage::QueryHandle FetchUpdates = UE::Editor::DataStorage::InvalidQueryHandle;
 	UE::Editor::DataStorage::QueryHandle ApplyNewOverlays = UE::Editor::DataStorage::InvalidQueryHandle;
 	UE::Editor::DataStorage::QueryHandle ChangeOverlay = UE::Editor::DataStorage::InvalidQueryHandle;
 	UE::Editor::DataStorage::QueryHandle ApplyOverlaysObjectToSCC = UE::Editor::DataStorage::InvalidQueryHandle;
 	UE::Editor::DataStorage::QueryHandle RemoveOverlays = UE::Editor::DataStorage::InvalidQueryHandle;
 	UE::Editor::DataStorage::QueryHandle FlushPackageUpdates = UE::Editor::DataStorage::InvalidQueryHandle;
+	UE::Editor::DataStorage::QueryHandle SelectionAdded = UE::Editor::DataStorage::InvalidQueryHandle;
+	UE::Editor::DataStorage::QueryHandle SelectionRemoved = UE::Editor::DataStorage::InvalidQueryHandle;
+
+	// General queries to gather SCC rows
+	TMap<const UScriptStruct*, UE::Editor::DataStorage::QueryHandle> GeneralQueriesMap;
+
+	// Query to fetch all rows with overlays
+	UE::Editor::DataStorage::QueryHandle FetchOverlayColors = UE::Editor::DataStorage::InvalidQueryHandle;
+
 };
