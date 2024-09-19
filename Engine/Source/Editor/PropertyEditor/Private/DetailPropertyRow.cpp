8e912454bd63c72a92f76e900ad5e34d80ee058a
logan buchy <logan.buchy@epicgames.com>
2024-09-19 18:18:01 -0400

    Make EditConditions work for customized properties
    * This change systemically allows customized properties to use the EditConditions UPROPERTY meta specifier.
    * This was specifically requested for FGuid as it's details panel representation is implemented as a customization at the engine level
    
    #rb aditya.ravichandran
    
    [CL 36455794 by logan buchy in 5.5 branch]

--- Engine/Source/Editor/PropertyEditor/Private/DetailPropertyRow.cpp
+++ Engine/Source/Editor/PropertyEditor/Private/DetailPropertyRow.cpp
@@ -21,6 +21,8 @@
 
 #define LOCTEXT_NAMESPACE	"DetailPropertyRow"
 
+static void TogglePropertyEditorEditCondition(bool bValue, TWeakPtr<FPropertyEditor> PropertyEditorWeak);
+
 FDetailPropertyRow::FDetailPropertyRow(TSharedPtr<FPropertyNode> InPropertyNode, TSharedRef<FDetailCategoryImpl> InParentCategory, TSharedPtr<FComplexPropertyNode> InExternalRootNode)
 	: PropertyNode( InPropertyNode )
 	, ParentCategory( InParentCategory )
@@ -380,6 +382,18 @@ void FDetailPropertyRow::OnItemNodeInitialized( TSharedRef<FDetailCategoryImpl> 
 		CustomPropertyWidget = MakeShared<FDetailWidgetRow>();
 
 		CustomTypeInterface->CustomizeHeader(PropertyHandle.ToSharedRef(), *CustomPropertyWidget, *this);
+
+		// set edit condition handlers - use customized if provided
+		TAttribute<bool> EditConditionValue = CustomEditConditionValue;
+		if (!EditConditionValue.IsSet() && PropertyEditor.IsValid())
+		{
+			EditConditionValue = TAttribute<bool>(PropertyEditor.ToSharedRef(), &FPropertyEditor::IsEditConditionMet);
+		}
+		
+		TWeakPtr<FPropertyEditor> PropertyEditorWeak = PropertyEditor;
+		FOnBooleanValueChanged OnEditConditionValueChanged = FOnBooleanValueChanged::CreateStatic(&TogglePropertyEditorEditCondition, PropertyEditorWeak);
+
+		CustomPropertyWidget->EditCondition(EditConditionValue, OnEditConditionValueChanged);
 
 		FixEmptyHeaderRowInContainers(PropertyHandle, CustomPropertyWidget);
 
