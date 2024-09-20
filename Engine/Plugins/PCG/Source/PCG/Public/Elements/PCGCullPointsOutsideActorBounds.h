cbf5f75cad9603b412bd16e17c81116a538022f3
julien lheureux <julien.lheureux@epicgames.com>
2024-09-20 15:24:46 -0400

    Tentative optimization for cull outside bounds when order is not important
    #rb adrien.logut, Patrick.Enfedaque, ryan.buehler, wyatt.marvil
    #jira UE-225071
    
    [CL 36481193 by julien lheureux in 5.5 branch]

--- Engine/Plugins/PCG/Source/PCG/Public/Elements/PCGCullPointsOutsideActorBounds.h
+++ Engine/Plugins/PCG/Source/PCG/Public/Elements/PCGCullPointsOutsideActorBounds.h
@@ -6,6 +6,13 @@
 
 #include "PCGCullPointsOutsideActorBounds.generated.h"
 
+UENUM(BlueprintType)
+enum class EPCGCullPointsMode : uint8
+{
+	Ordered = 0,
+	Unordered
+};
+
 /**
  * Removes points that lie outside the current actor bounds.
  */
@@ -33,6 +40,9 @@ protected:
 public:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta = (PCG_Overridable))
 	float BoundsExpansion = 0.0;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta = (PCG_Overridable))
+	EPCGCullPointsMode Mode = EPCGCullPointsMode::Ordered;
 };
 
 class FPCGCullPointsOutsideActorBoundsElement : public IPCGElement
