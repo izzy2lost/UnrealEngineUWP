fcdac550647c335bcc5bcd9d48988263793ca456
david bromberg <david.bromberg@epicgames.com>
2024-09-23 09:45:46 -0400

    Sequencer: Fix group conditions not respecting inner sub-conditions update frequency. This meant that if you used conditions requiring a tick in a sub-condition (or if you tried to combine conditions on track row/track/section), that they wouldn't work on a per-tick basis.
    Specifically this is for the new Sequencer Conditions feature that is being debuted with 5.5. There will be demos of this during UnrealFest and it would be good to fix any obvious issues with it as folks will play with it.
    
    [REVIEW] [at]ue-sequencer
    [FYI] [at]greg.richardson
    #jira UE-225043
    
    #rb Andrew.Rodham
    
    [CL 36504483 by david bromberg in 5.5 branch]

--- Engine/Source/Runtime/MovieScene/Public/Conditions/MovieSceneCondition.h
+++ Engine/Source/Runtime/MovieScene/Public/Conditions/MovieSceneCondition.h
@@ -99,7 +99,7 @@ public:
 	*/
 	virtual uint32 ComputeCacheKey(FGuid BindingGuid, FMovieSceneSequenceID SequenceID, TSharedRef<const UE::MovieScene::FSharedPlaybackState> SharedPlaybackState, UObject* EntityOwner) const;
 
-	bool CanCacheResult(TSharedRef<const UE::MovieScene::FSharedPlaybackState> SharedPlaybackState) const;
+	virtual bool CanCacheResult(TSharedRef<const UE::MovieScene::FSharedPlaybackState> SharedPlaybackState) const;
 
 	EMovieSceneConditionScope GetConditionScope() const;
 
