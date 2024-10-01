194dc82bf06d691d0893972b7d8f5d40b5dc9c69
josh adams <josh.adams@epicgames.com>
2024-10-01 19:04:45 -0400

    - Fixed an issue with Clearing Encryption Key wasn't saving, as it was using a hacky way to save to defaults (unlike Generate Encryption Key which was using the correct method)
    #jira UE-225207
    #rb pj.kack
    
    [CL 36755042 by josh adams in 5.5 branch]

--- Engine/Plugins/Editor/CryptoKeys/Source/CryptoKeys/Private/CryptoKeysSettingsDetails.cpp
+++ Engine/Plugins/Editor/CryptoKeys/Source/CryptoKeys/Private/CryptoKeysSettingsDetails.cpp
@@ -59,7 +59,7 @@ void FCryptoKeysSettingsDetails::CustomizeDetails(IDetailLayoutBuilder& DetailBu
 				.OnClicked_Lambda([this, Settings]()
 				{
 					Settings->EncryptionKey = TEXT("");
-					Settings->SaveConfig(CPF_Config, *Settings->GetDefaultConfigFilename());
+					Settings->TryUpdateDefaultConfigFile();
 					return(FReply::Handled());
 				})
 			]
