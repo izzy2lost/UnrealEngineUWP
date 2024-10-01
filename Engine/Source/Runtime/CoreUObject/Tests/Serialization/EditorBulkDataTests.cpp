049b7b406d93ba10cac01b321f2f169656019c94
matt peters <Matt.Peters@epicgames.com>
2024-10-01 18:46:09 -0400

    EditorBulkDataTests: Fix non-unity compile error #rnx
    [CL 36752922 by matt peters in 5.5 branch]

--- Engine/Source/Runtime/CoreUObject/Tests/Serialization/EditorBulkDataTests.cpp
+++ Engine/Source/Runtime/CoreUObject/Tests/Serialization/EditorBulkDataTests.cpp
@@ -1,5 +1,9 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+#if WITH_LOW_LEVEL_TESTS
+#include "Misc/CoreMiscDefines.h" // WITH_EDITORONLY_DATA
+#endif
+
 #if WITH_LOW_LEVEL_TESTS && WITH_EDITORONLY_DATA
 
 #include "Algo/AllOf.h"
