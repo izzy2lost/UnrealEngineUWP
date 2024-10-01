912a3d9b4db2e9a5375651a899565d9f36242541
samuele rigamonti <samuele.rigamonti@epicgames.com>
2024-10-01 19:24:32 -0400

    posesearch - DDC key generation speed up via partial caching of object hashes
    [REVIEW] [at]jose.villarroel, [at]daren.cheng, [at]keith.yerex, [at]roland.munguia
    #rb jose.villarroel
    
    [CL 36756843 by samuele rigamonti in 5.5 branch]

--- Engine/Plugins/Animation/PoseSearch/Source/Runtime/Public/PoseSearch/PoseSearchDerivedDataKey.h
+++ Engine/Plugins/Animation/PoseSearch/Source/Runtime/Public/PoseSearch/PoseSearchDerivedDataKey.h
@@ -12,15 +12,7 @@
 namespace UE::PoseSearch
 {
 
-// log properties and UObjects names
-#ifndef UE_POSE_SEARCH_DERIVED_DATA_LOGGING
-	#define UE_POSE_SEARCH_DERIVED_DATA_LOGGING 0
-#endif
-
-// log properties data
-#ifndef UE_POSE_SEARCH_DERIVED_DATA_LOGGING_VERBOSE
-	#define UE_POSE_SEARCH_DERIVED_DATA_LOGGING_VERBOSE 0
-#endif
+class FPartialKeyHashes;
 
 class POSESEARCH_API FKeyBuilder : public FArchiveUObject
 {
@@ -35,6 +27,20 @@ public:
 	
 	FKeyBuilder();
 	FKeyBuilder(const UObject* Object, bool bUseDataVer, bool bPerformConditionalPostLoadIfRequired);
+	
+	// Experimental, this feature might be removed without warning, not for production use
+	enum EDebugPartialKeyHashesMode
+	{
+		Use,
+		DoNotUse,
+		Validate
+	};
+
+	// Experimental, this feature might be removed without warning, not for production use
+	FKeyBuilder(const UObject* Object, bool bUseDataVer, bool bPerformConditionalPostLoadIfRequired, FPartialKeyHashes* InPartialKeyHashes, EDebugPartialKeyHashesMode InDebugPartialKeyHashesMode);
+
+	// Experimental, this feature might be removed without warning, not for production use
+	bool ValidateAgainst(const FKeyBuilder& Other) const;
 
 	using Super::IsSaving;
 	using Super::operator<<;
@@ -59,24 +65,156 @@ protected:
 	// to keep the key generation lightweight, we hash only the full names for these types. Object(s) will be added to Dependencies
 	static bool IsAddNameOnlyType(class UObject* Object);
 
-#if UE_POSE_SEARCH_DERIVED_DATA_LOGGING
-	FString GetIndentation() const;
-	int32 Indentation = 0;
-#endif
-
 	HashBuilderType Hasher;
 
 	// Set of objects that have already been serialized
 	TSet<const UObject*> Dependencies;
 
 	// Object currently being serialized
-	const UObject* ObjectBeingSerialized = nullptr;
+	UObject* ObjectBeingSerialized = nullptr;
 
 	// true if some dependent assets are not ready (fully loaded)
 	bool bAnyAssetNotReady = false;
 
 	// if true ConditionalPostLoad will be performed on the dependant assets requiring it
 	bool bPerformConditionalPostLoad = false;
+
+private:
+	// Experimental, this feature might be removed without warning, not for production use
+	void SerializeObjectInternal(UObject* Object);
+
+	// Experimental, this feature might be removed without warning, not for production use
+	FArchive& TryAddDependency(UObject* Object, bool bAddToPartialKeyHashes);
+
+	// Experimental, this feature might be removed without warning, not for production use
+	TArray<UObject*> ObjectsToSerialize;
+	
+	// Experimental, this feature might be removed without warning, not for production use
+	TArray<UObject*> ObjectBeingSerializedDependencies;
+
+	// Experimental, this feature might be removed without warning, not for production use
+	struct FLocalPartialKeyHash
+	{
+		UObject* Object = nullptr;
+		HashDigestType Hash;
+	};
+	
+	// Experimental, this feature might be removed without warning, not for production use
+	TArray<FLocalPartialKeyHash> LocalPartialKeyHashes;
+	
+	// Experimental, this feature might be removed without warning, not for production use
+	FPartialKeyHashes* PartialKeyHashes = nullptr;
+
+	// Experimental, this feature might be removed without warning, not for production use
+	EDebugPartialKeyHashesMode DebugPartialKeyHashesMode = EDebugPartialKeyHashesMode::Use;
+};
+
+// Experimental, this feature might be removed without warning, not for production use
+class FPartialKeyHashes
+{
+public:
+	struct FEntry
+	{
+		FKeyBuilder::HashDigestType Hash;
+		TArray<TWeakObjectPtr<>> Dependencies;
+
+		bool CheckDependencies(TArrayView<UObject*> OtherDependencies) const
+		{
+			#if DO_CHECK
+			if (Dependencies.Num() != OtherDependencies.Num())
+			{
+				return false;
+			}
+			
+			for (int32 DependencyIndex = 0; DependencyIndex < Dependencies.Num(); ++DependencyIndex)
+			{
+				if (!OtherDependencies[DependencyIndex])
+				{
+					return false;
+				}
+
+				// we could have lost a weak pointer here...
+				if (Dependencies[DependencyIndex].IsValid())
+				{
+					if (Dependencies[DependencyIndex].Get() != OtherDependencies[DependencyIndex])
+					{
+						return false;
+					}
+				}
+			}
+			#endif // DO_CHECK
+
+			return true;
+		}
+
+	};
+
+	void Reset()
+	{
+		Entries.Reset();
+	}
+
+	void Remove(UObject* Object)
+	{
+		Entries.Remove(Object);
+	}
+
+	void Add(UObject* Object, const FKeyBuilder::HashDigestType& Hash, TArrayView<UObject*> Dependencies)
+	{
+		check(Object);
+
+		check(!Hash.IsZero());
+
+		if (FEntry* OldEntry = Entries.Find(Object))
+		{
+			check(OldEntry->Hash == Hash);
+			check(OldEntry->CheckDependencies(Dependencies))
+		}
+		else
+		{
+			FEntry& NewEntry = Entries.Add(Object);
+			NewEntry.Hash = Hash;
+			NewEntry.Dependencies = Dependencies;
+		}
+	}
+
+	const FEntry* Find(UObject* Object)
+	{
+		check(Object);
+		if (const TPair<TWeakObjectPtr<>, FEntry>* Pair = Entries.FindPair(Object))
+		{
+			// making sure all the TWeakObjectPtr are still valid
+			if (!Pair->Key.IsValid())
+			{
+				Entries.Remove(Object);
+				return nullptr;
+			}
+			
+			for (const TWeakObjectPtr<> Dependency : Pair->Value.Dependencies)
+			{
+				if (!Dependency.IsValid())
+				{
+					Entries.Remove(Object);
+					return nullptr;
+				}
+			}
+
+			return &Pair->Value;
+		}
+
+		return nullptr;
+	}
+
+private:
+	struct FEntries : public TMap<TWeakObjectPtr<>, FEntry>
+	{
+		const TPair<TWeakObjectPtr<>, FEntry>* FindPair(TWeakObjectPtr<> Key) const
+		{
+			return Pairs.Find(Key);
+		}
+	};
+
+	FEntries Entries;
 };
 
 } // namespace UE::PoseSearch
