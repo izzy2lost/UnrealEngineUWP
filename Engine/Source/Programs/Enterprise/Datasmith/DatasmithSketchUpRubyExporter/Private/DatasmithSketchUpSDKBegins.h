da918b9a86182e4007e7b472b75f68af73097a55
jeanluc corenthin <JeanLuc.Corenthin@epicgames.com>
2024-10-03 16:40:59 -0400

    Fixed compilation error of SketchUp plugins on Mac
    #jira UE-224927
    #rnx
    #rb kerim.borchaev
    
    [CL 36841666 by jeanluc corenthin in 5.5 branch]

--- Engine/Source/Programs/Enterprise/Datasmith/DatasmithSketchUpRubyExporter/Private/DatasmithSketchUpSDKBegins.h
+++ Engine/Source/Programs/Enterprise/Datasmith/DatasmithSketchUpRubyExporter/Private/DatasmithSketchUpSDKBegins.h
@@ -3,11 +3,19 @@
 #include "HAL/Platform.h"
 
 #if PLATFORM_WINDOWS
-// Begin Datasmith platform include gard.
+
+// Temporary solution for static analysis of DatasmithSketchUpSDKCeases.h
+#define SKETCHUP_WINDOWS_PLATFORM_TYPES_GUARD
+
+// Begin Datasmith platform include guard.
 #include "Windows/AllowWindowsPlatformTypes.h"
 
 // Turn off some compiler warnings.
 #pragma warning(push)
 #pragma warning(disable: 4100) // C4100: unreferenced formal parameter
+
+#elif defined(PLATFORM_MAC) && PLATFORM_MAC
+
+#define volatile
 
 #endif
