90826f522f6708297e907c3cfb130f3cd3ea630a
patrick kelly <Patrick.Kelly@epicgames.com>
2024-10-08 19:20:35 -0400

    HeterogeneousVolumes: Adding DDX/DDY support
    When selected, the use of analytic derivatives will be supported:
    r.HeterogeneousVolumes.UseAnalyticDerivatives [default=1]
    
    #rb tim.doerries
    #jira UE-225064
    
    [CL 36952014 by patrick kelly in 5.5 branch]

--- Engine/Source/Runtime/Renderer/Private/HeterogeneousVolumes/HeterogeneousVolumes.cpp
+++ Engine/Source/Runtime/Renderer/Private/HeterogeneousVolumes/HeterogeneousVolumes.cpp
@@ -258,6 +258,13 @@ static TAutoConsoleVariable<int32> CVarHeterogeneousVolumesApplyFogInscatteringM
 	ECVF_RenderThreadSafe
 );
 
+static TAutoConsoleVariable<int32> CVarHeterogeneousVolumesUseAnalyticDerivatives(
+	TEXT("r.HeterogeneousVolumes.UseAnalyticDerivatives"),
+	1,
+	TEXT("Writes Heterogeneous Volumes velocity to the feature buffer (Default = 1)"),
+	ECVF_RenderThreadSafe
+);
+
 static TAutoConsoleVariable<int32> CVarHeterogeneousVolumesVelocity(
 	TEXT("r.HeterogeneousVolumes.Velocity"),
 	0,
@@ -561,6 +568,11 @@ namespace HeterogeneousVolumes
 	EFogMode GetApplyFogInscattering()
 	{
 		return static_cast<EFogMode>(FMath::Clamp(CVarHeterogeneousVolumesApplyFogInscatteringMode.GetValueOnRenderThread(), 0, 2));
+	}
+
+	bool UseAnalyticDerivatives()
+	{
+		return CVarHeterogeneousVolumesUseAnalyticDerivatives.GetValueOnRenderThread() != 0;
 	}
 
 	bool ShouldWriteVelocity()
