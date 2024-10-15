8eef8b677dbcdcb12b35b22ecb48c9c9db465f49
jeanfrancois dube <JeanFrancois.Dube@epicgames.com>
2024-10-15 13:54:54 -0400

    Fix FEditorModeTools::ActivateMode removing editor incompatible modes in a loop that affects the modes iterator.
    #p4v-cherrypick 36703815
    #rb Richard.Malo, Sebastien.Lussier
    
    [CL 37129852 by jeanfrancois dube in 5.5 branch]

--- Engine/Source/Editor/UnrealEd/Private/EditorModeManager.cpp
+++ Engine/Source/Editor/UnrealEd/Private/EditorModeManager.cpp
@@ -926,6 +926,7 @@ void FEditorModeTools::ActivateMode(FEditorModeID InID, bool bToggle)
 		FGCObjectScopeGuard ScriptModeGuard(ScriptableMode);
 
 		// Remove anything that isn't compatible with this mode
+		TArray<FEditorModeID> EditorModesToRemove;
 		const bool bIsVisibleMode = ScriptableMode->GetModeInfo().IsVisible();
 		for (int32 ModeIndex = ActiveScriptableModes.Num() - 1; ModeIndex >= 0; ModeIndex--)
 		{
@@ -933,8 +934,13 @@ void FEditorModeTools::ActivateMode(FEditorModeID InID, bool bToggle)
 			const bool bModesAreCompatible = ScriptableMode->IsCompatibleWith(Mode->GetID()) || Mode->IsCompatibleWith(ScriptableMode->GetID());
 			if (!bModesAreCompatible || (bIsVisibleMode && Mode->GetModeInfo().IsVisible()))
 			{
-				DeactivateMode(Mode->GetID());
+				EditorModesToRemove.Add(Mode->GetID());
 			}
+		}
+
+		for (FEditorModeID EditorModeToRemove : EditorModesToRemove)
+		{
+			DeactivateMode(EditorModeToRemove);
 		}
 	}
 
